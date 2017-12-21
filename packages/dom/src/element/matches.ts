ced7386d979f7ad68faefce5eafec501b1d0350b
Kasper Isager <kasperisager@gmail.com>
2017-12-21 16:51:58 +0100

    Element matcher improvements

--- packages/dom/src/element/matches.ts
+++ packages/dom/src/element/matches.ts
@@ -1,10 +1,13 @@
-import { Selector, IdSelector, ClassSelector, parse } from '@alfa/css'
+import { Selector, SelectorList, IdSelector, ClassSelector, parse } from '@alfa/css'
+import { memoize } from '@alfa/util'
 import { Element } from '../types'
 import { attribute } from './attribute'
 import { classlist } from './classlist'
 
-export function matches (element: Element, selector: string | Selector): boolean {
-  const parsed = typeof selector === 'string' ? parse(selector) : selector
+const parseMemoized = memoize(parse, { cache: { size: 50 } })
+
+export function matches (element: Element, selector: string | Selector | SelectorList): boolean {
+  const parsed = typeof selector === 'string' ? parseMemoized(selector) : selector
 
   if (parsed === null) {
     return false
@@ -16,7 +19,11 @@ export function matches (element: Element, selector: string | Selector): boolean
 
     case 'class-selector':
       return classlist(element).has(parsed.name)
-  }
 
-  return false
+    case 'compound-selector':
+      return parsed.selectors.every(selector => matches(element, selector))
+
+    case 'selector-list':
+      return parsed.selectors.some(selector => matches(element, selector))
+  }
 }
--- packages/dom/test/element/matches.spec.tsx
+++ packages/dom/test/element/matches.spec.tsx
@@ -8,3 +8,8 @@ test('Matches an element against a class', t => {
 test('Matches an element against an ID', t => {
   t.true(matches(<div id='foo'></div>, '#foo'))
 })
+
+test('Matches an element against a list of selectors', t => {
+  t.true(matches(<div class='foo'></div>, '.foo, #bar'))
+  t.true(matches(<div id='bar'></div>, '.foo, #bar'))
+})
