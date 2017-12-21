3c2b5b7ae2b207cea350ddd4f947e09b4eb59d00
Kasper Isager <kasperisager@gmail.com>
2017-12-21 13:57:37 +0100

    Element utility tweaks

--- packages/dom/src/element/classlist.ts
+++ packages/dom/src/element/classlist.ts
@@ -1,10 +1,12 @@
 import { Element } from '../types'
 import { attribute } from './attribute'
 
+const whitespace = /\s+/
+
 export function classlist (element: Element): Set<string> {
   const classes = attribute(element, 'class')
 
   return new Set(
-    classes === undefined ? [] : classes.split(/\s+/)
+    classes === undefined ? [] : classes.split(whitespace)
   )
 }
--- packages/dom/test/element/matches.spec.tsx
+++ packages/dom/test/element/matches.spec.tsx
@@ -1,6 +1,6 @@
 import test from 'ava'
-import { Element } from '../src/types'
-import { matches } from '../src/match'
+import { Element } from '../../src/types'
+import { matches } from '../../src/element/matches'
 
 test('Matches an element against a class', t => {
   const element: Element = <div class='foo'></div>
