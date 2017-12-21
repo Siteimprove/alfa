ecf8978c3d477ea0f021a4ddbe2ca524ceeab2a4
Kasper Isager <kasperisager@gmail.com>
2017-12-21 09:41:01 +0100

    Expose element functions

--- packages/dom/src/index.ts
+++ packages/dom/src/index.ts
@@ -3,3 +3,7 @@ export * from './compare'
 export * from './guards'
 export * from './traverse'
 export * from './types'
+
+export * from './element/attribute'
+export * from './element/classlist'
+export * from './element/matches'
