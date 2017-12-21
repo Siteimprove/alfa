e7ff8d20d64d570eaf0e51972cc53d2ebf88a5c7
Kasper Isager <kasperisager@gmail.com>
2017-12-21 09:43:06 +0100

    Remove excess checks in attribute

--- packages/dom/src/element/attribute.ts
+++ packages/dom/src/element/attribute.ts
@@ -8,16 +8,7 @@ export function attribute<K extends keyof Element['attributes']> (
   options: Options = { trim: false }
 ): Element['attributes'][K] {
   const { attributes } = element
-
-  if (!(name in attributes)) {
-    return undefined
-  }
-
   const value = attributes[name]
-
-  if (value === undefined) {
-    return undefined
-  }
 
   if (typeof value === 'string' && options.trim) {
     return value.trim()
