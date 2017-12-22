94d47ef38d45e4c11ae84e39b9bca54801cece4d
Kasper Isager <kasperisager@gmail.com>
2017-12-22 10:27:03 +0100

    Only allow find and findAll on parent nodes

--- packages/dom/src/element/find.ts
+++ packages/dom/src/element/find.ts
@@ -1,12 +1,12 @@
-import { Node, Element } from '../types'
+import { ParentNode, Element } from '../types'
 import { isElement } from '../guards'
 import { collect } from '../collect'
 import { matches } from './matches'
 
-export function find (context: Node, selector: string): Element | null {
+export function find (context: ParentNode, selector: string): Element | null {
   return collect(context).where(isElement).where(element => matches(element, selector)).first()
 }
 
-export function findAll (context: Node, selector: string): Iterable<Element> {
+export function findAll (context: ParentNode, selector: string): Iterable<Element> {
   return collect(context).where(isElement).where(element => matches(element, selector))
 }
