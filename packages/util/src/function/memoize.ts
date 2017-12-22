499849082e49c76d192394d1b55c21e8eb4f47f2
Kasper Isager <kasperisager@gmail.com>
2017-12-22 10:27:16 +0100

    Tweak memoization options

--- packages/util/src/function/memoize.ts
+++ packages/util/src/function/memoize.ts
@@ -3,14 +3,14 @@ import moize from 'moize'
 type Fn = (...args: any[]) => any
 
 type Options = {
-  cache?: { size: number } | { age: number }
+  cache?: { size?: number, age?: number }
 }
 
 export function memoize<T extends Fn> (fn: T, options: Options = {}): T {
-  const { cache } = options
+  const { cache = {} } = options
 
   return moize(fn, {
-    maxSize: cache ? cache.size : undefined,
-    maxAge: cache ? cache.age : undefined
+    maxSize: cache.size,
+    maxAge: cache.age
   })
 }
