5295033b921d0f6c7bdd1656316673b1dfb6ae19
Kasper Isager <kasperisager@gmail.com>
2017-12-22 10:27:34 +0100

    Use find and findAll in test cases

--- packages/wcag/test/unique-ids.spec.tsx
+++ packages/wcag/test/unique-ids.spec.tsx
@@ -1,33 +1,49 @@
 import test from 'ava'
 
 import { check } from '@alfa/rule'
-import { Element } from '@alfa/dom'
+import { Element, find, findAll } from '@alfa/dom'
 
 import { UNIQUE_IDS } from '../src/unique-ids/rule'
 import { outcome } from './helpers/outcome'
 
 test('Passes when no duplicate IDs exist within a document', async t => {
-  const bar: Element = <span id='bar'></span>
-  const foo: Element = <span id='foo'>{bar}</span>
+  const document: Element = (
+    <div id='foo'>
+      <div id='bar'></div>
+    </div>
+  )
 
-  const results = await check(UNIQUE_IDS, { document: foo })
+  const results = await check(UNIQUE_IDS, { document })
+
+  const foo = find(document, '#foo')
+  const bar = find(document, '#bar')
 
   outcome(t, results, { passed: [foo, bar] })
 })
 
 test('Fails when elements with duplicate IDs exist within a document', async t => {
-  const bar: Element = <span id='foo'></span>
-  const foo: Element = <span id='foo'>{bar}</span>
+  const document: Element = (
+    <div id='foo'>
+      <div id='foo'></div>
+    </div>
+  )
 
-  const results = await check(UNIQUE_IDS, { document: foo })
+  const results = await check(UNIQUE_IDS, { document })
 
-  outcome(t, results, { failed: [foo, bar] })
+  const foos = findAll(document, '#foo')
+
+  outcome(t, results, { failed: [...foos] })
 })
 
 test('Marks the document as inapplicable when no elements with IDs exist', async t => {
-  const foo: Element = <span>Foo <span>bar</span></span>
+  const document: Element = (
+    <div>
+      foo
+      <div>bar</div>
+    </div>
+  )
 
-  const results = await check(UNIQUE_IDS, { document: foo })
+  const results = await check(UNIQUE_IDS, { document })
 
-  outcome(t, results, { inapplicable: [foo] })
+  outcome(t, results, { inapplicable: [document] })
 })
