21f5ca3ae3fde00895e9b38e6ec659f98e50a02d
Kasper Isager <kasperisager@gmail.com>
2018-01-11 10:14:33 +0100

    Update test name

--- packages/act/test/markdown.spec.ts
+++ packages/act/test/markdown.spec.ts
@@ -4,7 +4,7 @@ import { UNIQUE_IDS } from '@alfa/wcag'
 
 import { markdown } from '../src/markdown'
 
-test('returns an act compliant markdown version of a rule', async t => {
+test('Creates an ACT compliant markdown version of a rule', async t => {
   const md = markdown(UNIQUE_IDS, 'en')
 
   t.true(md !== '')
