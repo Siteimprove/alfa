dd2087a829dcc47ba86ffbfdc8c08730e6f1899f
Kasper Isager <kasperisager@gmail.com>
2018-01-19 15:29:24 +0100

    Add test task

---
+++ build/test.js
@@ -0,0 +1,24 @@
+const { notify } = require("wsk");
+
+const { glob } = require("./utils/file");
+const tap = require("./tasks/tap/test");
+
+const config = {
+  silent: true
+};
+
+notify({
+  message: "Tests started...",
+  display: ["gray"]
+});
+
+glob("packages/**/*.spec.ts{,x}")
+  .then(async files => Promise.all(files.map(file =>
+    tap.onEvent(null, file, config)
+  )))
+  .then(() =>
+    notify({
+      message: "Tests succeeded!",
+      display: "success"
+    })
+  );
