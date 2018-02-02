1c037c79333a2999da2656851810617c5839ca05
Kasper Isager <kasperisager@gmail.com>
2018-02-02 15:02:43 +0100

    Refactor selector matcher

--- packages/dom/src/element/matches.ts
+++ packages/dom/src/element/matches.ts
@@ -1,6 +1,15 @@
-import { Selector, SelectorList, RelativeSelector, parse } from "@alfa/css";
 import { memoize } from "@alfa/util";
-import { Element } from "../types";
+import {
+  Selector,
+  SelectorList,
+  RelativeSelector,
+  TypeSelector,
+  ClassSelector,
+  IdSelector,
+  CompoundSelector,
+  parse
+} from "@alfa/css";
+import { Element, Parent } from "../types";
 import { isElement } from "../guards";
 import { attribute } from "./attribute";
 import { classlist } from "./classlist";
@@ -20,64 +29,127 @@ export function matches(
 
   switch (parsed.type) {
     case "type-selector":
-      return element.tag === parsed.name;
-
+      return matchesType(element, parsed);
     case "class-selector":
-      return classlist(element).has(parsed.name);
-
+      return matchesClass(element, parsed);
     case "id-selector":
-      return attribute(element, "id") === parsed.name;
-
+      return matchesId(element, parsed);
     case "compound-selector":
-      return parsed.selectors.every(selector => matches(element, selector));
-
+      return matchesCompound(element, parsed);
     case "selector-list":
-      return parsed.selectors.some(selector => matches(element, selector));
+      return matchesList(element, parsed);
+    case "relative-selector":
+      return matchesRelative(element, parsed);
+  }
 
-    case "relative-selector": {
-      let { parent } = element;
+  return false;
+}
 
-      if (!matches(element, parsed.selector) || !isElement(parent)) {
-        return false;
-      }
+function matchesType(element: Element, selector: TypeSelector): boolean {
+  return element.tag === selector.name;
+}
 
-      switch (parsed.combinator) {
-        case ">":
-          return matches(parent, parsed.relative);
+function matchesClass(element: Element, selector: ClassSelector): boolean {
+  return classlist(element).has(selector.name);
+}
 
-        case " ":
-          do {
-            if (matches(parent, parsed.relative)) {
-              return true;
-            }
+function matchesId(element: Element, selector: IdSelector): boolean {
+  return attribute(element, "id") === selector.name;
+}
 
-            parent = parent.parent;
-          } while (isElement(parent));
+function matchesCompound(
+  element: Element,
+  selector: CompoundSelector
+): boolean {
+  return selector.selectors.every(selector => matches(element, selector));
+}
 
-          return false;
+function matchesList(element: Element, selector: SelectorList): boolean {
+  return selector.selectors.some(selector => matches(element, selector));
+}
 
-        case "+":
-          const sibling = parent.children[parent.children.indexOf(element) - 1];
+function matchesRelative(
+  element: Element,
+  selector: RelativeSelector
+): boolean {
+  if (!matches(element, selector.selector)) {
+    return false;
+  }
 
-          if (sibling === undefined || !isElement(sibling)) {
-            return false;
-          }
+  switch (selector.combinator) {
+    case " ":
+      return matchesDescendant(element, selector);
+    case ">":
+      return matchesDirectDescendant(element, selector);
+    case "~":
+      return matchesSibling(element, selector);
+    case "+":
+      return matchesDirectSibling(element, selector);
+  }
+}
 
-          return matches(sibling, parsed.relative);
+function matchesDescendant(
+  element: Element,
+  selector: RelativeSelector
+): boolean {
+  let { parent } = element;
 
-        case "~":
-          for (let i = parent.children.indexOf(element) - 1; i >= 0; i--) {
-            const sibling = parent.children[i];
+  while (isElement(parent)) {
+    if (matches(parent, selector.relative)) {
+      return true;
+    }
 
-            if (isElement(sibling) && matches(sibling, parsed.relative)) {
-              return true;
-            }
-          }
+    if (parent.parent === null) {
+      break;
+    }
 
-          return false;
-      }
+    parent = parent.parent;
+  }
+
+  return false;
+}
+
+function matchesDirectDescendant(
+  element: Element,
+  selector: RelativeSelector
+): boolean {
+  const { parent } = element;
+  return isElement(parent) && matches(parent, selector.relative);
+}
+
+function matchesSibling(element: Element, selector: RelativeSelector): boolean {
+  const { parent } = element;
+
+  if (parent === null) {
+    return false;
+  }
+
+  for (let i = parent.children.indexOf(element) - 1; i >= 0; i--) {
+    const sibling = parent.children[i];
+
+    if (isElement(sibling) && matches(sibling, selector.relative)) {
+      return true;
     }
   }
 
   return false;
+}
+
+function matchesDirectSibling(
+  element: Element,
+  selector: RelativeSelector
+): boolean {
+  const { parent } = element;
+
+  if (parent === null) {
+    return false;
+  }
+
+  const sibling = parent.children[parent.children.indexOf(element) - 1];
+
+  if (sibling === undefined || !isElement(sibling)) {
+    return false;
+  }
+
+  return matches(sibling, selector.relative);
 }
