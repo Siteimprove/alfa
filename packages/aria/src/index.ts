24ed1b9ec0c2f7a869f67c7fff01dcfe193e8f6e
Kasper Isager <kasperisager@gmail.com>
2018-02-28 14:32:09 +0100

    Expose `getRole()` and `isLandmark()`

--- packages/aria/src/index.ts
+++ packages/aria/src/index.ts
@@ -2,5 +2,7 @@ import * as Attributes from "./attributes";
 import * as Roles from "./roles";
 import * as Features from "./features";
 
+export * from "./element/get-role";
+export * from "./element/is-landmark";
 export * from "./types";
 export { Attributes, Roles, Features };
