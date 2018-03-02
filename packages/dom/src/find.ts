4e7100151db1f21c58327bfb89ac89dee89cb5be
Kasper Isager <kasperisager@gmail.com>
2018-03-02 16:44:47 +0100

    Refactor `find()` and `findAll()`

--- packages/dom/src/find.ts
+++ packages/dom/src/find.ts
@@ -1,17 +1,18 @@
 import { Parent, Element } from "./types";
 import { isElement } from "./guards";
-import { collect } from "./collect";
+import { Collector, collect } from "./collect";
 import { matches } from "./matches";
 
-export function find(context: Parent, selector: string): Element | null {
-  for (const element of findAll(context, selector)) {
-    return element;
-  }
-  return null;
-}
-
-export function findAll(context: Parent, selector: string): Iterable<Element> {
+function query(context: Parent, selector: string): Collector<Element> {
   return collect(context)
     .where(isElement)
     .where(element => matches(element, selector));
+}
+
+export function find(context: Parent, selector: string): Element | null {
+  return query(context, selector).first();
+}
+
+export function findAll(context: Parent, selector: string): Iterable<Element> {
+  return query(context, selector);
 }
