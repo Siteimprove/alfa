a9a91615f861b0faebb32e721b8418e1c2af87a2
Kasper Isager <kasperisager@gmail.com>
2018-03-12 15:16:37 +0100

    Add references to all DOM interfaces

--- packages/dom/src/types.ts
+++ packages/dom/src/types.ts
@@ -1,19 +1,34 @@
+/**
+ * @see https://www.w3.org/TR/dom/#interface-node
+ */
 export interface Node {
   readonly type: string;
 }
 
+/**
+ * @see https://www.w3.org/TR/dom/#interface-parentnode
+ */
 export interface Parent extends Node {
   readonly children: Array<Child>;
 }
 
+/**
+ * @see https://www.w3.org/TR/dom/#interface-childnode
+ */
 export interface Child extends Node {
   readonly parent: Parent | null;
 }
 
+/**
+ * @see https://www.w3.org/TR/dom/#interface-document
+ */
 export interface Document extends Node, Parent {
   readonly type: "document";
 }
 
+/**
+ * @see https://www.w3.org/TR/dom/#interface-documenttype
+ */
 export interface DocumentType extends Node, Child {
   readonly type: "documentType";
   readonly name: string;
@@ -21,17 +36,29 @@ export interface DocumentType extends Node, Child {
   readonly systemId: string;
 }
 
+/**
+ * @see https://www.w3.org/TR/dom/#interface-documentfragment
+ */
 export interface DocumentFragment extends Node, Parent, Child {
   readonly type: "documentFragment";
 }
 
+/**
+ * @see https://www.w3.org/TR/dom/#interface-comment
+ */
 export interface Comment extends Node, Child {
   readonly type: "comment";
   readonly value: string;
 }
 
+/**
+ * @see https://www.w3.org/TR/dom/#interface-attr
+ */
 export type Attribute = string | number | boolean;
 
+/**
+ * @see https://www.w3.org/TR/dom/#interface-element
+ */
 export interface Element extends Node, Parent, Child {
   readonly type: "element";
   readonly tag: string;
@@ -44,6 +71,9 @@ export interface Element extends Node, Parent, Child {
   readonly shadow: DocumentFragment | null;
 }
 
+/**
+ * @see https://www.w3.org/TR/dom/#interface-text
+ */
 export interface Text extends Node, Child {
   readonly type: "text";
   readonly value: string;
