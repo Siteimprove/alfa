35890442c27e9bcd9e25ac9dc72b014c76eabf50
Kasper Isager <kasperisager@gmail.com>
2018-03-21 18:32:20 -0700

    Add note about deep traversal

--- packages/dom/src/traverse.ts
+++ packages/dom/src/traverse.ts
@@ -5,6 +5,9 @@ export type TraverseOptions = Readonly<{
   deep?: boolean;
 }>;
 
+/**
+ * @see https://www.w3.org/TR/dom/#concept-tree-order
+ */
 export function traverse(
   root: Node,
   visitor: (node: Node) => false | void,
@@ -23,6 +26,10 @@ export function traverse(
       queue.push(childNodes[i]);
     }
 
+    // Shadow roots should be traversed as soon as they're encountered per the
+    // definition of shadow-inclduing preorder depth-first traversal; the shadow
+    // root is therefore pushed in front of the queue.
+    // https://www.w3.org/TR/dom41/#shadow-including-preorder-depth-first-traversal
     if (options.deep && isElement(next) && next.shadowRoot !== null) {
       queue.push(next.shadowRoot);
     }
