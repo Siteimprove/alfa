bb2d3bcf11f5b9eb43aa2f181ec454dc2c89df3e
Kasper Isager <kasperisager@gmail.com>
2018-04-06 14:12:34 +0200

    Make `closest()` and `find()` more generic

--- packages/dom/src/closest.ts
+++ packages/dom/src/closest.ts
@@ -1,24 +1,30 @@
-import { Node } from "./types";
+import { Node, Element } from "./types";
 import { Predicate } from "./collect";
-import { isElement, isChild } from "./guards";
+import { isElement } from "./guards";
 import { matches } from "./matches";
+
+export function closest(context: Node, query: string): Element | null;
 
 export function closest<T extends Node>(
   context: Node,
-  predicate: Predicate<Node, T> | string
+  query: Predicate<Node, T>
+): T | null;
+
+export function closest<T extends Node>(
+  context: Node,
+  query: Predicate<Node, T> | string
 ): T | null {
-  if (typeof predicate === "string") {
-    const selector = predicate;
-    predicate = node => isElement(node) && matches(node, selector);
+  let predicate: Predicate<Node, T>;
+
+  if (typeof query === "string") {
+    predicate = node => isElement(node) && matches(node, query);
+  } else {
+    predicate = query;
   }
 
-  for (
-    let next: Node | null = context;
-    next;
-    next = isChild(next) ? next.parentNode || null : null
-  ) {
+  for (let next: Node | null = context; next; next = next.parentNode) {
     if (predicate(next)) {
-      return next as T;
+      return next;
     }
   }
 
--- packages/dom/src/find.ts
+++ packages/dom/src/find.ts
@@ -1,4 +1,5 @@
-import { ParentNode, Element } from "./types";
+import { Node, Element } from "./types";
+import { Predicate } from "./collect";
 import { isElement } from "./guards";
 import { matches } from "./matches";
 import { traverse, TraverseOptions } from "./traverse";
@@ -6,45 +7,85 @@ import { traverse, TraverseOptions } from "./traverse";
 export type FindOptions = TraverseOptions;
 
 export function find(
-  context: ParentNode,
-  selector: string,
+  context: Node,
+  query: string,
+  options?: FindOptions
+): Element | null;
+
+export function find<T extends Node>(
+  context: Node,
+  query: Predicate<Node, T>,
+  options?: FindOptions
+): T | null;
+
+export function find<T extends Node>(
+  context: Node,
+  query: Predicate<Node, T> | string,
   options: FindOptions = {}
-): Element | null {
-  let element: Element | null = null;
+): T | null {
+  let predicate: Predicate<Node, T>;
+
+  if (typeof query === "string") {
+    predicate = node => isElement(node) && matches(node, query);
+  } else {
+    predicate = query;
+  }
+
+  let found: T | null = null;
 
   traverse(
     context,
     node => {
-      if (element !== null) {
+      if (found !== null) {
         return false;
       }
 
-      if (isElement(node) && matches(node, selector)) {
-        element = node;
+      if (predicate(node)) {
+        found = node;
       }
     },
     options
   );
 
-  return element;
+  return found;
 }
 
 export function findAll(
-  context: ParentNode,
-  selector: string,
+  context: Node,
+  query: string,
+  options?: FindOptions
+): Array<Element>;
+
+export function findAll<T extends Node>(
+  context: Node,
+  query: Predicate<Node, T>,
+  options?: FindOptions
+): Array<T>;
+
+export function findAll<T extends Node>(
+  context: Node,
+  query: Predicate<Node, T> | string,
   options: FindOptions = {}
-): Array<Element> {
-  const elements: Array<Element> = [];
+): Array<T> {
+  let predicate: Predicate<Node, T>;
+
+  if (typeof query === "string") {
+    predicate = node => isElement(node) && matches(node, query);
+  } else {
+    predicate = query;
+  }
+
+  const found: Array<T> = [];
 
   traverse(
     context,
     node => {
-      if (isElement(node) && matches(node, selector)) {
-        elements.push(node);
+      if (predicate(node)) {
+        found.push(node);
       }
     },
     options
   );
 
-  return elements;
+  return found;
 }
