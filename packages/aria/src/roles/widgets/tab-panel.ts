8bc37864acf93102b5d41bd282dd7f583df0a340
Búgvi Benjamin Magnussen <bbm@siteimprove.com>
2018-04-12 13:00:59 +0200

    Added the roles tab-panel and spin-button

---
+++ packages/aria/src/roles/widgets/tab-panel.ts
@@ -0,0 +1,11 @@
+import { Role } from "../../types";
+import { Section } from "../abstract";
+
+/**
+ * @see https://www.w3.org/TR/wai-aria/#tabpanel
+ */
+export const TabPanel: Role = {
+  name: "tabpanel",
+  inherits: [Section],
+  label: { from: ["author"], required: true }
+};
