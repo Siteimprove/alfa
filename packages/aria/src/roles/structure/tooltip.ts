70a55ab356a81707e5327022f6f9c38f33d2ec7f
Bugvi-BM <38207818+Bugvi-BM@users.noreply.github.com>
2018-04-16 10:50:56 +0200

    Update tooltip.ts

--- packages/aria/src/roles/structure/tooltip.ts
+++ packages/aria/src/roles/structure/tooltip.ts
@@ -3,7 +3,7 @@ import { Section } from "../abstract";
 /**
  * @see https://www.w3.org/TR/wai-aria/#tooltip
  */
-export const ToolTip: Role = {
+export const Tooltip: Role = {
   name: "tooltip",
   inherits: [Section],
   label: { from: ["author", "contents"], required: true }
