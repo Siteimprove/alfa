8bc6f81cfcd831bd21d04a873c6e7c7e7c2410bd
Bugvi-BM <38207818+Bugvi-BM@users.noreply.github.com>
2018-04-16 10:51:53 +0200

    Update directory0.ts

--- packages/aria/src/roles/structure/directory0.ts
+++ packages/aria/src/roles/structure/directory0.ts
@@ -1,11 +1,1 @@
-import { Role } from "../../types";
-import { List } from "../structure";
 
-/**
- * @see https://www.w3.org/TR/wai-aria/#directory
- */
-export const Directory: Role = {
-  name: "directory",
-  inherits: [List],
-  label: { from: ["author"] }
-};
