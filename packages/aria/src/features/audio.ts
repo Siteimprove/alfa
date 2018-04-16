06016b70264707366901db7dd3177992e56b0cb4
Búgvi Benjamin Magnussen <bbm@siteimprove.com>
2018-04-16 12:25:38 +0200

    Added audio feature

---
+++ packages/aria/src/features/audio.ts
@@ -0,0 +1,11 @@
+import { Feature } from "../types";
+import * as Roles from "../roles";
+
+/**
+ * @see https://www.w3.org/TR/html-aria/#audio
+ */
+export const Audio: Feature = {
+  element: "audio",
+  role: Roles.Dialog,
+  allowedRoles: [Roles.AlertDialog]
+};
