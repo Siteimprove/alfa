2e121e58e519a091e2553ba2ba2bf4c184c58ede
Kasper Isager <kasperisager@gmail.com>
2018-04-20 12:27:20 +0200

    Improve performance of common DOM getters

--- packages/dom/src/get-class-list.ts
+++ packages/dom/src/get-class-list.ts
@@ -5,9 +5,15 @@ export interface ClassList extends Iterable<string> {
   has(className: string): boolean;
 }
 
-const whitespace = /\s+/;
+const classLists: WeakMap<Element, ClassList> = new WeakMap();
 
 export function getClassList(element: Element): ClassList {
-  const classes = getAttribute(element, "class");
-  return new Set(classes === null ? [] : classes.split(whitespace));
+  let classList = classLists.get(element);
+
+  if (classList === undefined) {
+    classList = new Set((getAttribute(element, "class") || "").split(/\s+/));
+    classLists.set(element, classList);
+  }
+
+  return classList;
 }
--- packages/dom/src/get-parent.ts
+++ packages/dom/src/get-parent.ts
@@ -1,23 +1,48 @@
 import { Node } from "./types";
-import { isParent } from "./guards";
 import { traverse } from "./traverse";
 
+const parentTrees: WeakMap<Node, ParentTree> = new WeakMap();
+
 export function getParent(node: Node, context: Node): Node | null {
-  let parent: Node | null = null;
+  let parentTree = parentTrees.get(context);
 
-  traverse(
-    context,
-    (candidate, found) => {
-      if (parent !== null) {
-        return false;
-      }
+  if (parentTree === undefined) {
+    parentTree = new ParentTree(context);
+    parentTrees.set(context, parentTree);
+  }
 
-      if (node === candidate) {
-        parent = found;
-      }
-    },
-    { composed: true }
-  );
+  return parentTree.get(node);
+}
 
-  return parent;
+/**
+ * @see https://en.wikipedia.org/wiki/Parent_pointer_tree
+ */
+class ParentTree {
+  /**
+   * Instead of storing the parent tree as a literal tree of parent pointers,
+   * we instead use a map in order to provide indexed access to nodes.
+   */
+  private _pointers: Map<Node, Node> = new Map();
+
+  public constructor(context: Node) {
+    traverse(
+      context,
+      (node, parent) => {
+        if (parent !== null) {
+          this._pointers.set(node, parent);
+        }
+      },
+      { composed: true }
+    );
+  }
+
+  public get(node: Node): Node | null {
+    const parent = this._pointers.get(node);
+
+    if (parent === undefined) {
+      return null;
+    }
+
+    return parent;
+  }
 }
