824ace6044a9b2a4f373b0327f2f2dc1b1d43478
Kasper Isager <kasperisager@gmail.com>
2018-04-24 11:49:25 +0200

    Add missing length constant to `map()`

--- packages/util/src/array/map.ts
+++ packages/util/src/array/map.ts
@@ -6,9 +6,10 @@ export function map<T, U>(
   array: ArrayLike<T>,
   iteratee: (element: T, index: number) => U
 ): Array<U> {
+  const { length } = array;
   const mapped: Array<U> = new Array(length);
 
-  for (let i = 0, n = array.length; i < n; i++) {
+  for (let i = 0, n = length; i < n; i++) {
     mapped[i] = iteratee(array[i], i);
   }
 
