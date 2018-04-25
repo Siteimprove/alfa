639c458cc378809c9cf8e7c66244c64491202bbe
Kasper Isager <kasperisager@gmail.com>
2018-04-25 11:09:58 +0200

    Add style rule guards

--- packages/dom/src/guards.ts
+++ packages/dom/src/guards.ts
@@ -8,7 +8,18 @@ import {
   Text,
   Comment,
   ParentNode,
-  ChildNode
+  ChildNode,
+  Rule,
+  GroupingRule,
+  StyleRule,
+  ImportRule,
+  MediaRule,
+  FontFaceRule,
+  PageRule,
+  KeyframesRule,
+  KeyframeRule,
+  NamespaceRule,
+  SupportsRule
 } from "./types";
 
 export function isElement(node: Node): node is Element {
@@ -47,4 +58,44 @@ export function isChild(node: Node): node is ChildNode {
   return (
     isElement(node) || isText(node) || isComment(node) || isDocumentType(node)
   );
+}
+
+export function isGroupingRule(rule: Rule): rule is GroupingRule {
+  return isMediaRule(rule) || isSupportsRule(rule);
+}
+
+export function isStyleRule(rule: Rule): rule is StyleRule {
+  return rule.type === 1;
+}
+
+export function isImportRule(rule: Rule): rule is ImportRule {
+  return rule.type === 3;
+}
+
+export function isMediaRule(rule: Rule): rule is MediaRule {
+  return rule.type === 4;
+}
+
+export function isFontFaceRule(rule: Rule): rule is FontFaceRule {
+  return rule.type === 5;
+}
+
+export function isPageRule(rule: Rule): rule is PageRule {
+  return rule.type === 6;
+}
+
+export function isKeyframesRule(rule: Rule): rule is KeyframesRule {
+  return rule.type === 7;
+}
+
+export function isKeyframeRule(rule: Rule): rule is KeyframeRule {
+  return rule.type === 8;
+}
+
+export function isNamespaceRule(rule: Rule): rule is NamespaceRule {
+  return rule.type === 10;
+}
+
+export function isSupportsRule(rule: Rule): rule is SupportsRule {
+  return rule.type === 12;
 }
