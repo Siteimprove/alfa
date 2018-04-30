bfce2242c7d85c72ee49f72b06e6554488e0872e
Kasper Isager <kasperisager@gmail.com>
2018-04-30 08:52:40 +0200

    Kill layout package

--- packages/layout/src/width.ts
+++
@@ -1,8 +0,0 @@
-import { Layout } from "./types";
-
-/**
- * @see https://www.w3.org/TR/geometry/#dom-domrectreadonly-domrect-width
- */
-export function width(layout: Layout): number {
-  return layout.width;
-}
