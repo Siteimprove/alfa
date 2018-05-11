918d4778e9afd8714329db14d3fa3055d64e72d2
Kasper Isager <kasperisager@gmail.com>
2018-05-11 11:30:16 +0200

    Implement `filter()` function

--- packages/util/src/array.ts
+++ packages/util/src/array.ts
@@ -1,5 +1,6 @@
 export * from "./array/concat";
 export * from "./array/each";
+export * from "./array/filter";
 export * from "./array/first";
 export * from "./array/includes";
 export * from "./array/index-of";
