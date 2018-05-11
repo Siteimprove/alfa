693ab774c16c6917e176c3ff718d039b3659358a
Kasper Isager <kasperisager@gmail.com>
2018-05-11 16:45:21 +0200

    Support exit visitors when traversing nodes

--- packages/dom/src/traverse-node.ts
+++ packages/dom/src/traverse-node.ts
@@ -1,23 +1,43 @@
+import { last, noop } from "@alfa/util";
 import { Node } from "./types";
 import { isElement } from "./guards";
 
+export type NodeVisitor = (target: Node, parent: Node | null) => false | void;
+
 export function traverseNode(
   context: Node,
-  visitor: (target: Node, parent: Node | null) => false | void,
+  visitor: NodeVisitor | Readonly<{ enter: NodeVisitor; exit: NodeVisitor }>,
   options: { composed?: boolean } = {}
 ): void {
+  const visitors =
+    typeof visitor === "function" ? { enter: visitor, exit: noop } : visitor;
+
   const queue: Array<Node> = [];
+
+  const exits: Map<Node, () => false | void> = new Map();
 
   for (
     let child: Node | undefined = context, parent: Node | undefined;
     child;
     child = queue.pop(), parent = queue.pop()
   ) {
-    if (visitor(child, parent || null) === false) {
+    if (visitors.enter(child, parent || null) === false) {
       break;
     }
 
     const { childNodes } = child;
+
+    const exit = last(childNodes);
+
+    const args = { child, parent };
+
+    if (exit === null) {
+      if (visitors.exit(args.child, args.parent || null) === false) {
+        break;
+      }
+    } else {
+      exits.set(exit, () => visitors.exit(args.child, args.parent || null));
+    }
 
     for (let i = childNodes.length - 1; i >= 0; i--) {
       queue.push(child, childNodes[i]);
@@ -29,6 +49,14 @@ export function traverseNode(
     // https://www.w3.org/TR/dom41/#shadow-including-preorder-depth-first-traversal
     if (options.composed && isElement(child) && child.shadowRoot !== null) {
       queue.push(child, child.shadowRoot);
+    }
+
+    if (exits.has(child)) {
+      const exit = exits.get(child)!;
+
+      if (exit() === false) {
+        break;
+      }
     }
   }
 }
