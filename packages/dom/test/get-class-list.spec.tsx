1a321e2194f9c0add73add12af55c550e2c7cbe5
Kasper Isager <kasperisager@gmail.com>
2018-05-11 17:19:22 +0200

    Adjust import order

--- packages/dom/test/get-class-list.spec.tsx
+++ packages/dom/test/get-class-list.spec.tsx
@@ -1,5 +1,5 @@
-import { jsx } from "@alfa/jsx";
 import { test } from "@alfa/test";
+import { jsx } from "@alfa/jsx";
 import { getClassList } from "../src/get-class-list";
 
 test("Constructs a set of classes from an element", async t => {
