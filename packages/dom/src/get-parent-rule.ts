3a87b5368f6ba6ebc7c000921f8007fae90a3a32
Kasper Isager <kasperisager@gmail.com>
2018-05-11 11:04:46 +0200

    Allow getting parent rule from list of style sheets

--- packages/dom/src/get-parent-rule.ts
+++ packages/dom/src/get-parent-rule.ts
@@ -2,6 +2,8 @@ import { Rule, StyleSheet } from "./types";
 import { traverseRule } from "./traverse-rule";
 import { traverseStyleSheet } from "./traverse-style-sheet";
 
+const { isArray } = Array;
+
 const parentMaps: WeakMap<
   Rule | StyleSheet,
   WeakMap<Rule, Rule>
@@ -14,29 +16,37 @@ const parentMaps: WeakMap<
  */
 export function getParentRule(
   rule: Rule,
-  context: Rule | StyleSheet
+  context: Rule | StyleSheet | ArrayLike<StyleSheet>
 ): Rule | null {
-  let parentMap = parentMaps.get(context);
+  const contexts = isArray(context) ? context : [context];
 
-  if (parentMap === undefined) {
-    parentMap = new WeakMap();
+  for (const context of contexts) {
+    let parentMap = parentMaps.get(context);
 
-    if ("disabled" in context) {
-      traverseStyleSheet(context, (rule, parent) => {
-        if (parent !== null && parentMap !== undefined) {
-          parentMap.set(rule, parent);
-        }
-      });
-    } else {
-      traverseRule(context, (rule, parent) => {
-        if (parent !== null && parentMap !== undefined) {
-          parentMap.set(rule, parent);
-        }
-      });
+    if (parentMap === undefined) {
+      parentMap = new WeakMap();
+
+      if ("disabled" in context) {
+        traverseStyleSheet(context, (rule, parent) => {
+          if (parent !== null && parentMap !== undefined) {
+            parentMap.set(rule, parent);
+          }
+        });
+      } else {
+        traverseRule(context, (rule, parent) => {
+          if (parent !== null && parentMap !== undefined) {
+            parentMap.set(rule, parent);
+          }
+        });
+      }
+
+      parentMaps.set(context, parentMap);
     }
 
-    parentMaps.set(context, parentMap);
+    if (parentMap.has(rule)) {
+      return parentMap.get(rule)!;
+    }
   }
 
-  return parentMap.get(rule) || null;
+  return null;
 }
