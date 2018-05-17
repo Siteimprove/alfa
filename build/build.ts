b62a085813d0a533c1d28e1b3164a7ea5f293301
Kasper Isager <kasperisager@gmail.com>
2018-05-17 20:05:08 +0200

    Reorder build steps a tad

--- build/build.ts
+++ build/build.ts
@@ -6,24 +6,35 @@ import * as typescript from "./tasks/typescript";
 import * as locale from "./tasks/locale";
 
 async function build(): Promise<void> {
-  const packages = new Packages({ include: "packages/*" });
-
-  for (const pkg of await expand("packages/*")) {
-    packages.add(pkg);
-    await remove(`${pkg}/dist`);
-  }
-
-  for (const path of await expand("packages/**/*.hjson")) {
+  for (const path of await expand(["build/**/*.ts"])) {
     notify({ message: "Building", value: path, desktop: false });
 
     try {
-      await execute([locale.transform], path);
+      await execute([typescript.diagnose], path);
     } catch (error) {
       process.exit(1);
     }
   }
 
+  const packages = new Packages({ include: "packages/*" });
+
+  for (const pkg of await expand("packages/*")) {
+    packages.add(pkg);
+  }
+
   await packages.traverse(async pkg => {
+    await remove(`${pkg}/dist`);
+
+    for (const path of await expand(`${pkg}/**/*.hjson`)) {
+      notify({ message: "Building", value: path, desktop: false });
+
+      try {
+        await execute([locale.transform], path);
+      } catch (error) {
+        process.exit(1);
+      }
+    }
+
     for (const path of await expand(`${pkg}/src/**/*.ts`)) {
       notify({ message: "Building", value: path, desktop: false });
 
@@ -44,16 +55,6 @@ async function build(): Promise<void> {
       }
     }
   });
-
-  for (const path of await expand(["build/**/*.ts"])) {
-    notify({ message: "Building", value: path, desktop: false });
-
-    try {
-      await execute([typescript.diagnose], path);
-    } catch (error) {
-      process.exit(1);
-    }
-  }
 }
 
 build();
