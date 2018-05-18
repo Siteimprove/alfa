cd43131944209fc57840d3e19128340fb8d2a040
Kasper Isager <kasperisager@gmail.com>
2018-05-18 17:27:28 +0200

    Clean up unique id rule spec

--- packages/alfa-wcag/test/unique-id.spec.tsx
+++ packages/alfa-wcag/test/unique-id.spec.tsx
@@ -1,45 +1,42 @@
 import { jsx } from "@siteimprove/alfa-jsx";
 import { test } from "@siteimprove/alfa-test";
 import { audit } from "@siteimprove/alfa-act";
-import { find, findAll } from "@siteimprove/alfa-dom";
 
 import { UniqueId } from "../src/unique-id/rule";
 import { outcome } from "./helpers/outcome";
 
 test("Passes when no duplicate IDs exist within a document", async t => {
+  const bar = <div id="bar" />;
+  const foo = <div id="foo">{bar}</div>;
   const document = (
-    <div id="foo">
-      <div id="bar" />
-    </div>
+    <html>
+      <body>{foo}</body>
+    </html>
   );
 
   const results = await audit(UniqueId, { document });
-
-  const foo = find(document, document, "#foo");
-  const bar = find(document, document, "#bar");
 
   outcome(t, results, { passed: [foo, bar] });
 });
 
 test("Fails when elements with duplicate IDs exist within a document", async t => {
+  const bar = <div id="foo" />;
+  const foo = <div id="foo">{bar}</div>;
   const document = (
-    <div id="foo">
-      <div id="foo" />
-    </div>
+    <html>
+      <body>{foo}</body>
+    </html>
   );
 
   const results = await audit(UniqueId, { document });
 
-  const foos = findAll(document, document, "#foo");
-
-  outcome(t, results, { failed: [...foos] });
+  outcome(t, results, { failed: [foo, bar] });
 });
 
 test("Marks the document as inapplicable when no elements with IDs exist", async t => {
   const document = (
     <div>
-      foo
-      <div>bar</div>
+      <span />
     </div>
   );
 
