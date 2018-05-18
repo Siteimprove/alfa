70d306b11ed0af351f9dcf135db4e3f22e48e828
Kasper Isager <kasperisager@gmail.com>
2018-05-18 14:04:53 +0200

    Progress on meta refresh rule

---
+++ packages/alfa-wcag/src/meta-refresh/rule.ts
@@ -0,0 +1,66 @@
+import { isNumeric, isWhitespace } from "@siteimprove/alfa-util";
+import { Stream } from "@siteimprove/alfa-lang";
+import { Rule } from "@siteimprove/alfa-act";
+import { Element, isElement, find, getAttribute } from "@siteimprove/alfa-dom";
+
+export const MetaRefresh: Rule<Element, "document"> = {
+  id: "alfa:wcag:meta-refresh",
+  criteria: ["wcag:2.2.1", "wcag:3.2.5"],
+  locales: [],
+  context: () => null,
+  applicability: ({ document }) =>
+    find<Element>(document, document, node => {
+      if (
+        !isElement(node) ||
+        node.localName !== "meta" ||
+        getAttribute(node, "http-equiv", { lowerCase: true }) !== "refresh"
+      ) {
+        return false;
+      }
+
+      const content = getAttribute(node, "content");
+
+      if (content === null) {
+        return false;
+      }
+
+      return getRefreshTime(content) !== null;
+    }),
+  expectations: {
+    1: (element, aspects, question) => {
+      return getRefreshTime(getAttribute(element, "content")!) === 0;
+    }
+  }
+};
+
+/**
+ * @see https://www.w3.org/TR/html/document-metadata.html#statedef-http-equiv-refresh
+ */
+function getRefreshTime(content: string): number | null {
+  const stream = new Stream(content.split(""));
+
+  stream.accept(isWhitespace);
+
+  const sign = stream.accept(char => char === "+" || char === "-", 1);
+
+  if (sign === "-") {
+    return null;
+  }
+
+  const time = stream.accept(isNumeric);
+
+  if (time === false) {
+    return null;
+  }
+
+  const next = stream.peek();
+
+  // As long as the time of the refresh is ended correctly, the URL won't matter
+  // in terms of the validity of the refresh. If the URL is therefore invalid,
+  // the refresh will simply redirect to the current page.
+  if (next !== null && next !== ";" && next !== "," && isWhitespace(next)) {
+    return null;
+  }
+
+  return parseInt(time.join(""), 10);
+}
---
+++ packages/alfa-wcag/test/meta-refresh.spec.tsx
@@ -0,0 +1,58 @@
+import { jsx } from "@siteimprove/alfa-jsx";
+import { test } from "@siteimprove/alfa-test";
+import { audit } from "@siteimprove/alfa-act";
+
+import { MetaRefresh } from "../src/meta-refresh/rule";
+import { outcome } from "./helpers/outcome";
+
+test("Passes when a refresh has a timeout of 0 and no URL", async t => {
+  const meta = <meta http-equiv="refresh" content="0" />;
+  const document = (
+    <html>
+      <head>{meta}</head>
+    </html>
+  );
+
+  const results = await audit(MetaRefresh, { document });
+
+  outcome(t, results, { passed: [meta] });
+});
+
+test("Passes when a refresh has a timeout of 0 and a URL", async t => {
+  const meta = <meta http-equiv="refresh" content="0; http://example.com" />;
+  const document = (
+    <html>
+      <head>{meta}</head>
+    </html>
+  );
+
+  const results = await audit(MetaRefresh, { document });
+
+  outcome(t, results, { passed: [meta] });
+});
+
+test("Fails when a refresh has a timeout greater than 0 and no URL", async t => {
+  const meta = <meta http-equiv="refresh" content="5" />;
+  const document = (
+    <html>
+      <head>{meta}</head>
+    </html>
+  );
+
+  const results = await audit(MetaRefresh, { document });
+
+  outcome(t, results, { failed: [meta] });
+});
+
+test("Fails when a refresh has a timeout greater than 0 and a URL", async t => {
+  const meta = <meta http-equiv="refresh" content="5; http://example.com" />;
+  const document = (
+    <html>
+      <head>{meta}</head>
+    </html>
+  );
+
+  const results = await audit(MetaRefresh, { document });
+
+  outcome(t, results, { failed: [meta] });
+});
