bf016a53eff278530439259c62df3311dd3794c6
Kasper Isager <kasperisager@gmail.com>
2018-05-27 22:02:23 +0200

    Simplify `union()`

--- packages/alfa-util/src/array/union.ts
+++ packages/alfa-util/src/array/union.ts
@@ -1,23 +1,14 @@
 export function union<T>(...arrays: Array<ArrayLike<T>>): Array<T> {
   const result: Array<T> = new Array();
 
-  // Use a set for keeping track of the values that have already been seen. Even
-  // though it's possible to iterate through sets in insertion order and as such
-  // avoid also allocating the above array and instead using the expanded set as
-  // the result, doing so would require us to actually iterate through the set.
-  // We really don't want to do that as array iteration is way faster in most
-  // JavaScript engines.
-  const seen: Set<T> = new Set();
-
   for (let i = 0, n = arrays.length; i < n; i++) {
     const array = arrays[i];
 
     for (let j = 0, m = array.length; j < m; j++) {
       const value = array[j];
 
-      if (!seen.has(value)) {
+      if (result.indexOf(value) === -1) {
         result.push(value);
-        seen.add(value);
       }
     }
   }
