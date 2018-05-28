0ed9061fc692678d319ddd9e561f26ad744e0b46
Kasper Isager <kasperisager@gmail.com>
2018-05-28 12:30:08 +0200

    Add missing file

---
+++ packages/alfa-wcag/src/rules.ts
@@ -0,0 +1,5 @@
+export { HtmlXmlLangMatch } from "./html-xml-lang-match/rule";
+export { Lang } from "./lang/rule";
+export { MetaRefresh } from "./meta-refresh/rule";
+export { Title } from "./title/rule";
+export { UniqueId } from "./unique-id/rule";
