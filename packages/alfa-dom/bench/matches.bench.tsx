83b56c2977766d97edb129ad373a2541e6b38040
Kasper Isager <kasperisager@gmail.com>
2018-05-30 21:28:00 +0200

    Add relative selectors to benchmark

--- packages/alfa-dom/bench/matches.bench.tsx
+++ packages/alfa-dom/bench/matches.bench.tsx
@@ -3,24 +3,51 @@ import { jsx } from "@siteimprove/alfa-jsx";
 import { parseSelector } from "@siteimprove/alfa-css";
 import { matches } from "../src/matches";
 
-const element = <foo id="foo" class="foo" href="foo" />;
+const element = <div id="foo" class="foo" href="foo" />;
+
+const context = (
+  <main>
+    <header />
+    <section>{element}</section>
+  </main>
+);
 
 const id = parseSelector("#foo")!;
 const className = parseSelector(".foo")!;
-const type = parseSelector("foo")!;
+const type = parseSelector("div")!;
 const attribute = parseSelector("[foo]")!;
 
 benchmark()
   .add("Match an ID", () => {
-    matches(element, element, id);
+    matches(element, context, id);
   })
   .add("Match a class", () => {
-    matches(element, element, className);
+    matches(element, context, className);
   })
   .add("Match a type", () => {
-    matches(element, element, type);
+    matches(element, context, type);
   })
   .add("Match an attribute", () => {
-    matches(element, element, attribute);
+    matches(element, context, attribute);
+  })
+  .run();
+
+const descendant = parseSelector("section div")!;
+const directDescendant = parseSelector("section > div")!;
+const sibling = parseSelector("section ~ div")!;
+const directSibling = parseSelector("section + div")!;
+
+benchmark()
+  .add("Match a descendant", () => {
+    matches(element, context, descendant);
+  })
+  .add("Match a direct descendant", () => {
+    matches(element, context, directDescendant);
+  })
+  .add("Match a sibling", () => {
+    matches(element, context, sibling);
+  })
+  .add("Match a direct sibling", () => {
+    matches(element, context, directSibling);
   })
   .run();
