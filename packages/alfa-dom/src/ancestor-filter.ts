4d303ec0c8a90aec143935e7d07b7ec4a0fdbde7
Kasper Isager <kasperisager@gmail.com>
2018-06-03 19:58:39 +0200

    Improve ancestor filtering

--- packages/alfa-dom/src/ancestor-filter.ts
+++ packages/alfa-dom/src/ancestor-filter.ts
@@ -13,23 +13,6 @@ export class AncestorFilter {
 
   private types: AncestorBucket = new Map();
 
-  private process(
-    element: Element,
-    fn: (bucket: AncestorBucket, entry: string) => void
-  ) {
-    const id = getAttribute(element, "id");
-
-    if (id !== null) {
-      fn(this.ids, id);
-    }
-
-    fn(this.types, element.localName);
-
-    for (const className of getClassList(element)) {
-      fn(this.classes, className);
-    }
-  }
-
   public add(element: Element): void {
     this.process(element, addEntry);
   }
@@ -54,6 +37,28 @@ export class AncestorFilter {
     }
 
     return bucket.has(selector.name);
+  }
+
+  private process(
+    element: Element,
+    fn: (bucket: AncestorBucket, entry: string) => void
+  ) {
+    // Elements with no child nodes are not relevant for ancestor filtering.
+    if (element.childNodes.length === 0) {
+      return;
+    }
+
+    const id = getAttribute(element, "id");
+
+    if (id !== null) {
+      fn(this.ids, id);
+    }
+
+    fn(this.types, element.localName);
+
+    for (const className of getClassList(element)) {
+      fn(this.classes, className);
+    }
   }
 }
 
