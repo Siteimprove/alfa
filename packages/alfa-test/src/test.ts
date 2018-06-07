473ca29c91600d1333ce6c819f40b821f9fd80a3
Kasper Isager <kasperisager@gmail.com>
2018-06-07 20:49:15 +0200

    Initial draft of new test functionality

--- packages/alfa-test/src/test.ts
+++ packages/alfa-test/src/test.ts
@@ -1,40 +1,92 @@
-import * as tape from "tape";
+const path = require("path");
+const assert = require("assert").strict;
+const Stack = require("error-stack-parser");
+const chalk = require("chalk");
 
-const { assign } = Object;
+const { diff } = require("concordance");
+const { theme } = require("concordance-theme-ava");
 
-export type Test = tape.Test & { title: string };
-export type TestOptions = tape.TestOptions;
-export type TestCase = (test: Test) => void | Promise<void>;
+export interface Assertions {
+  /**
+   * @see https://nodejs.org/api/assert.html#assert_assert_value_message
+   */
+  (value: any, message?: string | Error): void;
 
-export function test(title: string, callback: TestCase): void;
+  /**
+   * @see https://nodejs.org/api/assert.html#assert_assert_equal_actual_expected_message
+   */
+  equal<T>(actual: T, expected: T, message?: string | Error): void;
 
-export function test(
-  title: string,
-  options: TestOptions,
-  callback: TestCase
-): void;
+  /**
+   * @see https://nodejs.org/api/assert.html#assert_assert_notequal_actual_expected_message
+   */
+  notEqual<T>(actual: T, expected: T, message?: string | Error): void;
 
-export function test(
-  title: string,
-  options: TestOptions | TestCase,
-  callback: TestCase = () => {}
-): void {
-  if (typeof options === "function") {
-    callback = options;
-    options = {};
-  }
+  /**
+   * @see https://nodejs.org/api/assert.html#assert_assert_deepequal_actual_expected_message
+   */
+  deepEqual<T>(actual: T, expected: T, message?: string | Error): void;
 
-  tape(title, options, t => {
-    try {
-      const result = callback(assign(t, { title }));
+  /**
+   * @see https://nodejs.org/api/assert.html#assert_assert_notdeepequal_actual_expected_message
+   */
+  notDeepEqual<T>(actual: T, expected: T, message?: string | Error): void;
 
-      if (result && result.constructor === Promise) {
-        result.then(() => t.end()).catch(err => t.error(err));
-      } else {
-        t.end();
-      }
-    } catch (err) {
-      t.error(err);
+  /**
+   * @see https://nodejs.org/api/assert.html#assert_assert_fail_message
+   */
+  fail(message?: string | Error): void;
+
+  /**
+   * @see https://nodejs.org/api/assert.html#assert_assert_ok_value_message
+   */
+  ok(value: any, message?: string | Error): void;
+
+  /**
+   * @see https://nodejs.org/api/assert.html#assert_assert_rejects_block_error_message
+   */
+  rejects(
+    block: Function | Promise<any>,
+    error?: RegExp | Function | Object | Error,
+    message?: string | Error
+  ): void;
+
+  /**
+   * @see https://nodejs.org/api/assert.html#assert_assert_throws_block_error_message
+   */
+  throws(
+    block: Function,
+    error?: RegExp | Function | Object | Error,
+    message?: string | Error
+  ): void;
+}
+
+export async function test(
+  name: string,
+  assertion: (assert: Assertions) => void | Promise<void>
+): Promise<void> {
+  try {
+    await assertion(assert);
+  } catch (err) {
+    let message = "\n" + chalk.bold(name);
+
+    const [location] = Stack.parse(err);
+
+    message +=
+      "\n" +
+      chalk.gray(
+        path.relative(process.cwd(), location.fileName) +
+          ":" +
+          location.lineNumber
+      );
+
+    if ("actual" in err && "expected" in err) {
+      message += "\n\n" + "Difference:";
+      message += "\n\n" + diff(err.actual, err.expected, { theme });
     }
-  });
+
+    console.error(message);
+
+    process.exit(1);
+  }
 }
