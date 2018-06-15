29ad4133ddb580b29ab093cab01971f030648b72
Bugvi-BM <38207818+Bugvi-BM@users.noreply.github.com>
2018-06-15 13:44:43 +0200

    Update and rename tablefoot.ts to tfoot.ts

--- packages/alfa-aria/src/features/tfoot.ts
+++ packages/alfa-aria/src/features/tfoot.ts
@@ -4,7 +4,7 @@ import * as Roles from "../roles";
 /**
  * @see https://www.w3.org/TR/html-aria/#tfoot
  */
-export const TableFoot: Feature = {
+export const Tfoot: Feature = {
   element: "tfoot",
   role: Roles.RowGroup,
   allowedRoles: Any(Roles)
