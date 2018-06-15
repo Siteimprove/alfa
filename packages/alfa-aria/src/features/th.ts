c97e1a97af858a169923ade6b3f46733f62d29cc
Kasper Isager <kasperisager@gmail.com>
2018-06-15 14:22:57 +0200

    Rename table header

--- packages/alfa-aria/src/features/th.ts
+++ packages/alfa-aria/src/features/th.ts
@@ -5,7 +5,7 @@ import * as Roles from "../roles";
 /**
  * @see https://www.w3.org/TR/html-aria/#th
  */
-export const Theader: Feature = {
+export const Th: Feature = {
   element: "th",
   role,
   allowedRoles: Any(Roles)
