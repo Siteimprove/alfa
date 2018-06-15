96a80434b23b61493cbd0146931fb807574aa5e7
Bugvi-BM <38207818+Bugvi-BM@users.noreply.github.com>
2018-06-15 13:45:38 +0200

    Update and rename tableheader.ts to theader.ts

--- packages/alfa-aria/src/features/theader.ts
+++ packages/alfa-aria/src/features/theader.ts
@@ -5,7 +5,7 @@ import * as Roles from "../roles";
 /**
  * @see https://www.w3.org/TR/html-aria/#th
  */
-export const TableHeader: Feature = {
+export const Theader: Feature = {
   element: "th",
   role,
   allowedRoles: Any(Roles)
