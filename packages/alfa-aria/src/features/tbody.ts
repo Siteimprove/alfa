fbf8cab1f8397174abedd6c9ebe7b53621ea5165
Bugvi-BM <38207818+Bugvi-BM@users.noreply.github.com>
2018-06-15 13:43:27 +0200

    Update and rename tablebody.ts to tbody.ts

--- packages/alfa-aria/src/features/tbody.ts
+++ packages/alfa-aria/src/features/tbody.ts
@@ -4,7 +4,7 @@ import * as Roles from "../roles";
 /**
  * @see https://www.w3.org/TR/html-aria/#tbody-tfoot-thead
  */
-export const TableBody: Feature = {
+export const Tbody: Feature = {
   element: "tbody",
   role: Roles.RowGroup,
   allowedRoles: Any(Roles)
