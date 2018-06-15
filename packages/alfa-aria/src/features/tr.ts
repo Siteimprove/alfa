118a053acfc331678ae9c9e6da8931f856dfa936
Bugvi-BM <38207818+Bugvi-BM@users.noreply.github.com>
2018-06-15 13:45:59 +0200

    Update and rename tablerow.ts to tr.ts

--- packages/alfa-aria/src/features/tr.ts
+++ packages/alfa-aria/src/features/tr.ts
@@ -4,7 +4,7 @@ import * as Roles from "../roles";
 /**
  * @see https://www.w3.org/TR/html-aria/#tr
  */
-export const TableRow: Feature = {
+export const Tr: Feature = {
   element: "tr",
   role: Roles.Row,
   allowedRoles: Any(Roles)
