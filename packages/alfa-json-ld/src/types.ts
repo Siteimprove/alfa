26adbbe0494cce90227f63bf43fe93789ac4a9a2
Kasper Isager <kasperisager@gmail.com>
2018-06-29 14:59:34 +0200

    Simplify JSON-LD types

--- packages/alfa-json-ld/src/types.ts
+++ packages/alfa-json-ld/src/types.ts
@@ -24,25 +24,29 @@ export type Identifier = {
   readonly "@id": string;
 };
 
+export type Type = {
+  readonly "@type": string;
+};
+
+export type Vocabulary<C> = {
+  readonly "@vocab": Context<C>;
+};
+
 export type Definitions<C> = {
-  readonly [P in Exclude<keyof C, Keyword>]:
-    | string
-    | Identifier & { readonly "@type": string }
+  readonly [P in Exclude<keyof C, Keyword>]: string | Identifier & Type
+};
+
+export type Terms<C> = C extends Vocabulary<any>
+  ? Exclude<keyof C | keyof C["@vocab"], Keyword>
+  : Exclude<keyof C, Keyword>;
+
+export type Properties<C> = {
+  readonly [P in Terms<C>]?: string | number | Identifier
 };
 
 export type Context<C> = Definitions<C> & Partial<Identifier>;
 
-export type ContextWithVocabulary<C, V extends Context<V> = V> = Context<C> & {
-  readonly "@vocab": V;
-};
-
-export type Terms<C extends Context<C>> = {
-  readonly [P in Exclude<
-    C extends ContextWithVocabulary<C> ? keyof C | keyof C["@vocab"] : keyof C,
-    Keyword
-  >]?: string | Identifier
-};
-
-export type Node<C extends Context<C>> = Terms<C> & {
-  readonly "@context": C;
-};
+export type Node<C extends Context<C>> = Properties<C> &
+  Partial<Identifier> & {
+    readonly "@context": C;
+  };
