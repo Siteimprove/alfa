eed67963aeebe1d5bf4689ceb4b3e64de056d541
Kasper Isager <kasperisager@gmail.com>
2018-07-24 11:11:55 +0200

    Add missing exports

--- packages/alfa-css/src/index.ts
+++ packages/alfa-css/src/index.ts
@@ -5,3 +5,18 @@ export * from "./alphabet";
 export * from "./grammar";
 export * from "./parse";
 export * from "./types";
+
+export * from "./grammars/color";
+export * from "./grammars/content";
+export * from "./grammars/declaration";
+export * from "./grammars/font-family";
+export * from "./grammars/font-size";
+export * from "./grammars/rule";
+export * from "./grammars/selector";
+
+export * from "./properties/color";
+export * from "./properties/content";
+export * from "./properties/display";
+export * from "./properties/font-family";
+export * from "./properties/font-size";
+export * from "./properties/visibility";
