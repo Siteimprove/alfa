171140cb4a2e61156195662e2bd17f1cc7ef66ca
Niclas Hedam <niclas@hed.am>
2018-08-06 10:39:35 +0200

    Removing foo-tests

--- packages/alfa-test/src/foo.ts
+++
@@ -1,8 +0,0 @@
-export function foo(foo: string) {
-  let p = 0 + 2;
-  if (foo === "foo") {
-    p = p + 1 + 2 + 3;
-  }
-
-  return p;
-}
--- packages/alfa-test/test/foo.spec.ts
+++
@@ -1,6 +0,0 @@
-import { foo } from "../src/foo";
-import { test } from "../src/test";
-
-test("foo", t => {
-  foo("bar");
-});
