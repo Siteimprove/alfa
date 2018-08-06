7bc28f374edeb119196e97124384c3528375c6aa
Kasper Isager <kasperisager@gmail.com>
2018-08-06 13:25:08 +0200

    Implement `getChildNodes()`

---
+++ packages/alfa-dom/test/get-child-nodes.spec.tsx
@@ -0,0 +1,33 @@
+import { jsx } from "@siteimprove/alfa-jsx";
+import { test } from "@siteimprove/alfa-test";
+import { getChildNodes } from "../src/get-child-nodes";
+
+const foo = <div>Foo</div>;
+const bar = <div>Bar</div>;
+const baz = (
+  <div>
+    {foo}
+    <shadow>
+      <slot />
+      {bar}
+    </shadow>
+  </div>
+);
+
+test("Gets the direct child nodes of an element", t => {
+  t.deepEqual(getChildNodes(baz, <div>{baz}</div>), [foo]);
+});
+
+test("Gets the composed child nodes of an element", t => {
+  t.deepEqual(getChildNodes(baz, <div>{baz}</div>, { composed: true }), [
+    baz.shadowRoot!,
+    foo
+  ]);
+});
+
+test("Gets the flattened child nodes of an element", t => {
+  t.deepEqual(getChildNodes(baz, <div>{baz}</div>, { flattened: true }), [
+    foo,
+    bar
+  ]);
+});
