db13f71c5074e6b669b0c99a3f176023cd2358ad
Niclas Hedam <niclas@hed.am>
2018-08-07 16:32:04 +0200

    Simplifying the test suite of css-escape even further

--- packages/alfa-css/test/escape.spec.ts
+++ packages/alfa-css/test/escape.spec.ts
@@ -1,27 +1,24 @@
-import { Char } from "@siteimprove/alfa-lang";
 import { test } from "@siteimprove/alfa-test";
 import { escape } from "../src/escape";
 
-const { fromCharCode } = String;
-
 test("Can escape null character correctly", t => {
-  t.equal(escape(fromCharCode(Char.Null)), "\ufffd");
+  t.equal(escape("\u0000"), "\ufffd");
 });
 
 test("Can escape bell character correctly", t => {
-  t.equal(escape(fromCharCode(Char.Bell)), `\\${Char.Bell.toString(16)} `);
+  t.equal(escape("\u0007"), `\\7 `);
 });
 
 test("Can escape delete character correctly", t => {
-  t.equal(escape(fromCharCode(Char.Delete)), `\\${Char.Delete.toString(16)} `);
+  t.equal(escape("\u007f"), `\\7f `);
 });
 
 test("Can escape 7 correctly", t => {
-  t.equal(escape("7"), `\\${Char.DigitSeven.toString(16)} `);
+  t.equal(escape("7"), `\\37 `);
 });
 
 test("Can escape -7 correctly", t => {
-  t.equal(escape("-7"), `-\\${Char.DigitSeven.toString(16)} `);
+  t.equal(escape("-7"), `-\\37 `);
 });
 
 test("Can escape - correctly", t => {
