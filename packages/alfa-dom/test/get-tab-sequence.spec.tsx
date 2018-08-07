9c39cbe2a0a05c8a0a1344c059b77ac451868148
Niclas Hedam <niclas@hed.am>
2018-08-07 13:39:00 +0200

    More concrete test titles in getTabSequence as well as one very vague

--- packages/alfa-dom/test/get-tab-sequence.spec.tsx
+++ packages/alfa-dom/test/get-tab-sequence.spec.tsx
@@ -2,7 +2,7 @@ import { jsx } from "@siteimprove/alfa-jsx";
 import { test } from "@siteimprove/alfa-test";
 import { getTabSequence } from "../src/get-tab-sequence";
 
-test("Computes the tab sequence of three non-prioritized elements", t => {
+test("Gets the tab sequence of a node and its children", t => {
   const div = <div />;
   const a = <a href="#" />;
   const input = <input type="text" />;
@@ -18,7 +18,7 @@ test("Computes the tab sequence of three non-prioritized elements", t => {
   t.deepEqual(getTabSequence(context, context), [a, input]);
 });
 
-test("Computes the tab sequence of three elements with priotization", t => {
+test("Sorts element based on their tab index", t => {
   const div = <div />;
   const a = <a href="#" />;
   const input = <input tabindex="1" type="text" />;
@@ -34,7 +34,7 @@ test("Computes the tab sequence of three elements with priotization", t => {
   t.deepEqual(getTabSequence(context, context), [input, a]);
 });
 
-test("Computes the tab sequence of three elements with ignoring priotization", t => {
+test("Leaves out elements with negative tab indices", t => {
   const div = <div />;
   const a = <a href="#" />;
   const input = <input tabindex="-1" type="text" />;
@@ -50,7 +50,7 @@ test("Computes the tab sequence of three elements with ignoring priotization", t
   t.deepEqual(getTabSequence(context, context), [a]);
 });
 
-test("Computes the tab sequence of colliding elements", t => {
+test("Positions elements with the same tab indices in tree order", t => {
   const div = <div />;
   const button = <button />;
   const a = <a tabindex="1" href="#" />;
@@ -68,10 +68,28 @@ test("Computes the tab sequence of colliding elements", t => {
   t.deepEqual(getTabSequence(context, context), [a, input, button]);
 });
 
-test("Computes the tab sequence of three elements with out of bounds priotization", t => {
+test("Positions elements with a tab index of zero after elements with a non-zero tab index", t => {
+  const div = <div />;
+  const button = <button tabindex="0" />;
+  const a = <a tabindex="1" href="#" />;
+  const input = <input tabindex="1" type="text" />;
+
+  const context = (
+    <div>
+      {div}
+      {button}
+      {a}
+      {input}
+    </div>
+  );
+
+  t.deepEqual(getTabSequence(context, context), [a, input, button]);
+});
+
+test("Positions elements with spread tab index in correct order", t => {
   const div = <div />;
   const a = <a tabindex="6" href="#" />;
-  const input = <input tabindex="2" type="text" />;
+  const input = <input tabindex="1" type="text" />;
 
   const context = (
     <div>
