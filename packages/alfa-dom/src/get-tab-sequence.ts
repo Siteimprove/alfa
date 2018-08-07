a474b421b3911ddde4e8260367d184383efea443
Niclas Hedam <niclas@hed.am>
2018-08-07 10:38:12 +0200

    One more refactoring to avoid TSX clash

--- packages/alfa-dom/src/get-tab-sequence.ts
+++ packages/alfa-dom/src/get-tab-sequence.ts
@@ -6,7 +6,7 @@ import { Element } from "./types";
 function indexWithin(array: Array<Element>, element: Element) {
   let lower = 0;
   let upper = array.length;
-  const index = <number>getTabIndex(element);
+  const index = getTabIndex(element) as number;
 
   if (index === 0) {
     return upper;
@@ -14,7 +14,7 @@ function indexWithin(array: Array<Element>, element: Element) {
 
   while (lower < upper) {
     const middle = (lower + (upper - lower) / 2) | 0;
-    const other = <number>getTabIndex(array[middle]);
+    const other = getTabIndex(array[middle]) as number;
     if (other <= index && other !== 0) {
       lower = middle + 1;
     } else {
