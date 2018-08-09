e46c7f5ad7061dd323b7a20c701f9bafa9437511
Niclas Hedam <niclas@frax.dk>
2018-08-09 14:01:52 +0200

    Adding some missing test-cases for the meta-refresh wcag rule (#30)

--- packages/alfa-wcag/test/meta-refresh/rule.spec.tsx
+++ packages/alfa-wcag/test/meta-refresh/rule.spec.tsx
@@ -56,3 +56,29 @@ test("Fails when a refresh has a timeout greater than 0 and a URL", t => {
 
   outcome(t, results, { failed: [meta] });
 });
+
+test("Fails when a refresh has a timeout and not being ended correctly", t => {
+  const meta = <meta http-equiv="refresh" content="5." />;
+  const document = (
+    <html>
+      <head>{meta}</head>
+    </html>
+  );
+
+  const results = audit({ document }, MetaRefresh);
+
+  outcome(t, results, { failed: [meta] });
+});
+
+test("Inapplicable when a refresh has no content attribute", t => {
+  const meta = <meta http-equiv="refresh" />;
+  const document = (
+    <html>
+      <head>{meta}</head>
+    </html>
+  );
+
+  const results = audit({ document }, MetaRefresh);
+
+  outcome(t, results, { inapplicable: [meta] });
+});
