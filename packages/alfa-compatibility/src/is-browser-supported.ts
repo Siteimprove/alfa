0fe36b95cefa27ab948ba1bf1dfcab14f9140b3c
Kasper Isager <kasperisager@gmail.com>
2018-08-13 13:38:22 +0200

    Allow version-less browser queries in `isBrowserSupported()`

--- packages/alfa-compatibility/src/is-browser-supported.ts
+++ packages/alfa-compatibility/src/is-browser-supported.ts
@@ -4,9 +4,12 @@ import browserslist = require("browserslist");
 
 const supportedBrowsers = new Set(browserslist());
 
+const whitespace = /\s+/;
+
 /**
- * Given a browser query, check if the browsers that the query resolves to are
- * supported by the current browser scope.
+ * Given the name of a browser, or a browserslist query, check if the browser,
+ * or the browsers that the browserslist query resolves to, are supported by the
+ * current browser scope.
  *
  * @see http://browserl.ist/
  */
@@ -14,10 +17,30 @@ export function isBrowserSupported(
   browser: string,
   options: Readonly<{ browsers?: string | Array<string> }> = {}
 ): boolean {
+  browser = browser.toLowerCase();
+
   const browsers =
     options.browsers === undefined
       ? supportedBrowsers
       : new Set(browserslist(options.browsers));
+
+  switch (browser) {
+    case "chrome":
+    case "edge":
+    case "firefox":
+    case "ie":
+    case "opera":
+    case "safari":
+      for (const found of browsers) {
+        const [name] = found.split(whitespace);
+
+        if (name === browser) {
+          return true;
+        }
+      }
+
+      return false;
+  }
 
   return browserslist(browser).every(browser => browsers.has(browser));
 }
--- packages/alfa-compatibility/test/is-browser-supported.spec.ts
+++ packages/alfa-compatibility/test/is-browser-supported.spec.ts
@@ -2,9 +2,17 @@ import { test } from "@siteimprove/alfa-test";
 import { isBrowserSupported } from "../src/is-browser-supported";
 
 test("Returns true if a given browser is supported", t => {
-  t(isBrowserSupported("chrome > 55", { browsers: "chrome > 55" }));
+  t(isBrowserSupported("chrome", { browsers: "chrome > 55, firefox > 50" }));
 });
 
 test("Returns false if a given browser is not supported", t => {
+  t(!isBrowserSupported("opera", { browsers: "chrome > 55, firefox > 50" }));
+});
+
+test("Returns true if a given browserslist query is supported", t => {
+  t(isBrowserSupported("chrome > 55", { browsers: "chrome > 55" }));
+});
+
+test("Returns false if a given browserslist query is not supported", t => {
   t(!isBrowserSupported("chrome 55", { browsers: "chrome > 55" }));
 });
