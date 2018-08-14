e5984085aded2be5f39f9c6fcb5a636fae684944
Niclas Hedam <niclas@hed.am>
2018-08-14 15:42:12 +0200

    Actually, lets copypasta the W3C HslToRgb function and insert their license

--- packages/alfa-css/src/grammars/color.ts
+++ packages/alfa-css/src/grammars/color.ts
@@ -165,38 +165,57 @@ function hslaColor(stream: Stream<Token>): Color {
     }
   }
 
-  const { r, g, b } = hslToRgb(h / 60, s / 100, l / 100);
-
-  return { red: r, green: g, blue: b, alpha: a };
-}
-
-function hslToRgb(h: number, s: number, l: number) {
-  const snd = l <= 0.5 ? l * (s + 1) : l + s - l * s;
-  const fst = l * 2 - snd;
+  const rgb = hslToRgb(h / 60, s / 100, l / 100);
   const { round } = Math;
 
   return {
-    r: round(hueToRgb(fst, snd, h + 2) * 255),
-    g: round(hueToRgb(fst, snd, h) * 255),
-    b: round(hueToRgb(fst, snd, h - 2) * 255)
+    red: round(rgb[0] * 255),
+    green: round(rgb[1] * 255),
+    blue: round(rgb[2] * 255),
+    alpha: a
   };
 }
 
-function hueToRgb(fst: number, snd: number, hue: number) {
+/**
+ * @copyright  Copyright © 2016 W3C® (MIT, ERCIM, Keio, Beihang). W3C liability,
+ *             trademark and document use rules apply.
+ * @license    https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+ */
+function hslToRgb(hue: number, sat: number, light: number) {
+  let t2;
+  if (light <= 0.5) {
+    t2 = light * (sat + 1);
+  } else {
+    t2 = light + sat - light * sat;
+  }
+  const t1 = light * 2 - t2;
+  const r = hueToRgb(t1, t2, hue + 2);
+  const g = hueToRgb(t1, t2, hue);
+  const b = hueToRgb(t1, t2, hue - 2);
+  return [r, g, b];
+}
+
+/**
+ * @copyright  Copyright © 2016 W3C® (MIT, ERCIM, Keio, Beihang). W3C liability,
+ *             trademark and document use rules apply.
+ * @license    https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+ */
+function hueToRgb(t1: number, t2: number, hue: number) {
+  if (hue < 0) {
+    hue += 6;
+  }
   if (hue >= 6) {
     hue -= 6;
-  } else if (hue < 0) {
-    hue += 6;
   }
 
   if (hue < 1) {
-    return (snd - fst) * hue + fst;
+    return (t2 - t1) * hue + t1;
   } else if (hue < 3) {
-    return snd;
+    return t2;
   } else if (hue < 4) {
-    return (snd - fst) * (4 - hue) + fst;
+    return (t2 - t1) * (4 - hue) + t1;
   } else {
-    return fst;
+    return t1;
   }
 }
 
