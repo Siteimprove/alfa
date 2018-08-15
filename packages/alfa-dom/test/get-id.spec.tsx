9633b3e687d5196f4ad0f2c362b583e6926fa53a
Búgvi Benjamin Magnussen <bbm@siteimprove.com>
2018-08-15 09:39:36 +0200

    Added two tests for the get-id function

---
+++ packages/alfa-dom/test/get-id.spec.tsx
@@ -0,0 +1,13 @@
+import { jsx } from "@siteimprove/alfa-jsx";
+import { test } from "@siteimprove/alfa-test";
+import { getId } from "../src/get-id";
+
+test("Retrieves the ID of an Element", t => {
+  const foo = <div id="bar" />;
+  t.equal(getId(foo), "bar");
+});
+
+test("Retrieves the ID of an Element that has no ID", t => {
+  const foo = <p>Foo</p>;
+  t.equal(getId(foo), null);
+});
