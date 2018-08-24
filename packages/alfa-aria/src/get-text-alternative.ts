da9f5cdc85c013ae9e12a0356e511d7e2374833a
Kasper Isager <kasperisager@gmail.com>
2018-08-24 13:03:51 +0200

    Make `getRole()` return a browser specific role

--- packages/alfa-aria/src/get-text-alternative.ts
+++ packages/alfa-aria/src/get-text-alternative.ts
@@ -1,3 +1,4 @@
+import { BrowserSpecific, map, reduce } from "@siteimprove/alfa-compatibility";
 import {
   Element,
   getAttribute,
@@ -16,6 +17,7 @@ import {
   querySelector,
   Text
 } from "@siteimprove/alfa-dom";
+import { none, option, Option, some } from "@siteimprove/alfa-util";
 import { getRole } from "./get-role";
 import { hasNameFrom } from "./has-name-from";
 import { isVisible } from "./is-visible";
@@ -53,7 +55,7 @@ export type TextAlternativeOptions = Readonly<{
 export function getTextAlternative(
   node: Element | Text,
   context: Node
-): string | null;
+): Option<string> | BrowserSpecific<Option<string>>;
 
 /**
  * @internal
@@ -63,14 +65,14 @@ export function getTextAlternative(
   context: Node,
   visited: Set<Element | Text>,
   options?: TextAlternativeOptions
-): string | null;
+): Option<string> | BrowserSpecific<Option<string>>;
 
 export function getTextAlternative(
   node: Element | Text,
   context: Node,
   visited: Set<Element | Text> = new Set(),
   options: TextAlternativeOptions = {}
-): string | null {
+): Option<string> | BrowserSpecific<Option<string>> {
   if (visited.has(node) && options.revisiting !== true) {
     return null;
   }
@@ -96,123 +98,86 @@ export function getTextAlternative(
     return label;
   }
 
-  // https://www.w3.org/TR/accname/#step2B
-  const labelledBy = getAttribute(node, "aria-labelledby");
-  if (
-    labelledBy !== null &&
-    labelledBy !== "" &&
-    options.referencing !== true
-  ) {
-    const rootNode = getRootNode(node, context);
+  return (
+    BrowserSpecific.of(null, [
+      "chrome",
+      "edge",
+      "firefox",
+      "ie",
+      "opera",
+      "safari"
+    ])
+      // https://www.w3.org/TR/accname/#step2B
+      .map(alt =>
+        none(alt, () =>
+          getAriaLabelledbyTextAlternative(node, context, visited, options)
+        )
+      )
 
-    const references = resolveReferences(rootNode, context, labelledBy).map(
-      element =>
-        getTextAlternative(element, context, visited, {
-          recursing: true,
-          referencing: true
-        })
-    );
+      // https://www.w3.org/TR/accname/#step2C
+      .map(alt =>
+        none(alt, () =>
+          getAriaLabelTextAlternative(node, context, visited, options)
+        )
+      )
 
-    if (references.length > 0) {
-      return flatten(references.join(" "), options);
-    }
-  }
-
-  // https://www.w3.org/TR/accname/#step2C
-  const label = getAttribute(node, "aria-label", { trim: true });
-  if (label !== null && label !== "") {
-    return flatten(label, options);
-  }
-
-  const role = getRole(node, context);
-
-  // https://www.w3.org/TR/accname/#step2D
-  if (role !== Roles.Presentation && role !== Roles.None) {
-    const native = getNativeTextAlternative(node, context, visited);
-    if (native !== null) {
-      return flatten(native, options);
-    }
-  }
-
-  // https://www.w3.org/TR/accname/#step2E
-  if (options.labelling === true || options.referencing === true) {
-    switch (role) {
-      case Roles.TextBox:
-        switch (node.localName) {
-          case "input":
-            const value = getAttribute(node, "value");
-            if (value !== null && value !== "") {
-              return flatten(value, options);
+      // https://www.w3.org/TR/accname/#step2D
+      .map(alt =>
+        none(alt, () =>
+          map(getRole(node, context), role => {
+            if (role !== Roles.Presentation && role !== Roles.None) {
+              return getNativeTextAlternative(node, context, visited, options);
             }
-            break;
-          default:
-            return flatten(
-              getTextContent(node, context, { flattened: true }),
+
+            return null;
+          })
+        )
+      )
+
+      // https://www.w3.org/TR/accname/#step2E
+      .map(alt =>
+        none(alt, () => {
+          if (options.labelling === true || options.referencing === true) {
+            return getEmbeddedControlTextAlternative(
+              node,
+              context,
+              visited,
               options
             );
-        }
-        break;
-      case Roles.Button:
-        const label = getTextAlternative(node, context, visited, {
-          recursing: true,
-          revisiting: true
-        });
+          }
 
-        if (label !== null) {
-          return flatten(label, options);
-        }
-    }
-  }
-
-  // https://www.w3.org/TR/accname/#step2F
-  // https://www.w3.org/TR/accname/#step2G
-  if (
-    (role !== null && hasNameFrom(role, "contents")) ||
-    options.referencing === true ||
-    options.descending === true ||
-    isNativeTextAlternativeElement(node)
-  ) {
-    const children = getChildNodes(node, context, { flattened: true })
-      .map(
-        child =>
-          isElement(child) || isText(child)
-            ? getTextAlternative(child, context, visited, {
-                recursing: true,
-                descending: true,
-                // Pass down the labelling flag as the current call may have
-                // been initiated from a labelling element; the subtree will
-                // therefore also have to be considered part of the labelling
-                // element.
-                labelling: options.labelling
-              })
-            : null
+          return null;
+        })
       )
-      .filter(child => child !== null);
 
-    const before = getComputedStyle(node, context, { pseudo: "before" });
+      // https://www.w3.org/TR/accname/#step2F
+      // https://www.w3.org/TR/accname/#step2G
+      .map(alt =>
+        none(alt, () =>
+          map(getRole(node, context), role => {
+            if (
+              (role !== null && hasNameFrom(role, "contents")) ||
+              options.referencing === true ||
+              options.descending === true ||
+              isNativeTextAlternativeElement(node)
+            ) {
+              return getSubtreeTextAlternative(node, context, visited, options);
+            }
 
-    if (isArray(before.content)) {
-      children.unshift(before.content.join(""));
-    }
+            return null;
+          })
+        )
+      )
 
-    const after = getComputedStyle(node, context, { pseudo: "after" });
+      // https://www.w3.org/TR/accname/#step2I
+      .map(alt =>
+        none(alt, () =>
+          getTooltipTextAlternative(node, context, visited, options)
+        )
+      )
 
-    if (isArray(after.content)) {
-      children.push(after.content.join(""));
-    }
-
-    if (children.length > 0) {
-      return flatten(children.join(""), options);
-    }
-  }
-
-  // https://www.w3.org/TR/accname/#step2I
-  const title = getAttribute(node, "title");
-  if (title !== null && title !== "") {
-    return flatten(title, options);
-  }
-
-  return null;
+      .get()
+  );
 }
 
 const whitespace = /\s+/g;
@@ -229,21 +194,81 @@ function flatten(string: string, options: TextAlternativeOptions): string {
 }
 
 /**
+ * Get the text alternative of an element provided by the `aria-labelledby`
+ * attribute if present.
+ */
+function getAriaLabelledbyTextAlternative(
+  element: Element,
+  context: Node,
+  visited: Set<Element | Text>,
+  options: TextAlternativeOptions
+): Option<string> | BrowserSpecific<Option<string>> {
+  const labelledBy = getAttribute(element, "aria-labelledby");
+
+  if (
+    labelledBy !== null &&
+    labelledBy !== "" &&
+    options.referencing !== true
+  ) {
+    const rootNode = getRootNode(element, context);
+
+    const references = resolveReferences(rootNode, context, labelledBy).map(
+      element =>
+        getTextAlternative(element, context, visited, {
+          recursing: true,
+          referencing: true
+        })
+    );
+
+    const label = reduce(
+      references,
+      (alt, text) =>
+        some(text, text => option(alt, alt => `${alt} ${text}`, () => text)),
+      null as Option<string>
+    );
+
+    return map(label, label => some(label, label => flatten(label, options)));
+  }
+
+  return null;
+}
+
+/**
+ * Get the text alternative of an element provided by the `aria-label` attribute
+ * if present.
+ */
+function getAriaLabelTextAlternative(
+  element: Element,
+  context: Node,
+  visited: Set<Element | Text>,
+  options: TextAlternativeOptions
+): Option<string> | BrowserSpecific<Option<string>> {
+  const label = getAttribute(element, "aria-label", { trim: true });
+
+  if (label !== null && label !== "") {
+    return flatten(label, options);
+  }
+
+  return null;
+}
+
+/**
  * Get the text alternative of an element provided by native markup.
  */
 function getNativeTextAlternative(
   element: Element,
   context: Node,
-  visited: Set<Element | Text>
-): string | null {
+  visited: Set<Element | Text>,
+  options: TextAlternativeOptions
+): Option<string> | BrowserSpecific<Option<string>> {
   const namespace = getElementNamespace(element, context);
 
   if (namespace !== null) {
     switch (namespace) {
       case Namespace.HTML:
-        return getHtmlTextAlternative(element, context, visited);
+        return getHtmlTextAlternative(element, context, visited, options);
       case Namespace.SVG:
-        return getSvgTextAlternative(element, context, visited);
+        return getSvgTextAlternative(element, context, visited, options);
     }
   }
 
@@ -256,9 +281,11 @@ function getNativeTextAlternative(
 function getHtmlTextAlternative(
   element: Element,
   context: Node,
-  visited: Set<Element | Text>
-): string | null {
+  visited: Set<Element | Text>,
+  options: TextAlternativeOptions
+): Option<string> | BrowserSpecific<Option<string>> {
   const label = getLabel(element, context);
+
   if (label !== null) {
     return getTextAlternative(label, context, visited, {
       recursing: true,
@@ -269,12 +296,13 @@ function getHtmlTextAlternative(
   switch (element.localName) {
     case "input":
       const type = getInputType(element);
-      switch (getInputType(element)) {
+      switch (type) {
         // https://www.w3.org/TR/html-aam/#input-type-button-input-type-submit-and-input-type-reset
         case InputType.Button:
         case InputType.Submit:
         case InputType.Reset: {
           const value = getAttribute(element, "value");
+
           if (value !== null && value !== "") {
             return value;
           }
@@ -293,13 +321,15 @@ function getHtmlTextAlternative(
         // https://www.w3.org/TR/html-aam/#input-type-image
         case InputType.Image: {
           const alt = getAttribute(element, "alt");
+
           if (alt !== null && alt !== "") {
-            return alt;
+            return flatten(alt, options);
           }
 
           const value = getAttribute(element, "value");
+
           if (value !== null && value !== "") {
-            return value;
+            return flatten(value, options);
           }
         }
       }
@@ -308,41 +338,51 @@ function getHtmlTextAlternative(
     // https://www.w3.org/TR/html-aam/#fieldset-and-legend-elements
     case "fieldset": {
       const legend = querySelector(element, context, "legend");
+
       if (legend !== null) {
         return getTextAlternative(legend, context, visited, {
           recursing: true,
           descending: true
         });
       }
+
       break;
     }
 
     // https://www.w3.org/TR/html-aam/#figure-and-figcaption-elements
     case "figure": {
       const caption = querySelector(element, context, "figcaption");
+
       if (caption !== null) {
         return getTextAlternative(caption, context, visited, {
           recursing: true,
           descending: true
         });
       }
+
       break;
     }
 
     // https://www.w3.org/TR/html-aam/#img-element
     case "img": {
       const alt = getAttribute(element, "alt");
+
       if (alt !== null && alt !== "") {
-        return alt;
+        return flatten(alt, options);
       }
+
       break;
     }
 
     // https://www.w3.org/TR/html-aam/#table-element
     case "table": {
       const caption = querySelector(element, context, "caption");
+
       if (caption !== null) {
-        return getTextContent(caption, context, { flattened: true });
+        return flatten(
+          getTextContent(caption, context, { flattened: true }),
+          options
+        );
       }
     }
   }
@@ -356,10 +396,11 @@ function getHtmlTextAlternative(
 function getSvgTextAlternative(
   element: Element,
   context: Node,
-  visited: Set<Element | Text>
-): string | null {
+  visited: Set<Element | Text>,
+  options: TextAlternativeOptions
+): Option<string> | BrowserSpecific<Option<string>> {
   if (element.localName === "title") {
-    return getTextContent(element, context);
+    return flatten(getTextContent(element, context), options);
   }
 
   const title = querySelector(element, context, ":scope > title");
@@ -373,9 +414,129 @@ function getSvgTextAlternative(
   switch (element.localName) {
     case "a":
       const title = getAttribute(element, "xlink:title");
+
       if (title !== null && title !== "") {
-        return title;
+        return flatten(title, options);
       }
+  }
+
+  return null;
+}
+
+/**
+ * Get the text alternative of an element that is deemed an embedded control.
+ */
+function getEmbeddedControlTextAlternative(
+  element: Element,
+  context: Node,
+  visited: Set<Element | Text>,
+  options: TextAlternativeOptions
+): Option<string> | BrowserSpecific<Option<string>> {
+  const role = getRole(element, context);
+
+  switch (role) {
+    case Roles.TextBox:
+      switch (element.localName) {
+        case "input":
+          const value = getAttribute(element, "value");
+
+          if (value !== null && value !== "") {
+            return flatten(value, options);
+          }
+
+          break;
+
+        default:
+          return flatten(
+            getTextContent(element, context, { flattened: true }),
+            options
+          );
+      }
+      break;
+
+    case Roles.Button:
+      const label = getTextAlternative(element, context, visited, {
+        recursing: true,
+        revisiting: true
+      });
+
+      return map(label, label => some(label, label => flatten(label, options)));
+  }
+
+  return null;
+}
+
+/**
+ * Get the text alternative of an element provided by its subtree contents.
+ */
+function getSubtreeTextAlternative(
+  element: Element,
+  context: Node,
+  visited: Set<Element | Text>,
+  options: TextAlternativeOptions
+): Option<string> | BrowserSpecific<Option<string>> {
+  const children: Array<Option<string> | BrowserSpecific<Option<string>>> = [];
+
+  for (const child of getChildNodes(element, context, { flattened: true })) {
+    if (isElement(child) || isText(child)) {
+      children.push(
+        getTextAlternative(child, context, visited, {
+          recursing: true,
+          descending: true,
+          // Pass down the labelling flag as the current call may have
+          // been initiated from a labelling element; the subtree will
+          // therefore also have to be considered part of the labelling
+          // element.
+          labelling: options.labelling
+        })
+      );
+    }
+  }
+
+  const alt = reduce<Option<string>, Option<string>>(
+    children,
+    (alt, child) =>
+      option(
+        child,
+        child => option(alt, alt => alt + child, () => child),
+        () => alt
+      ),
+    null
+  );
+
+  const after = getComputedStyle(element, context, { pseudo: "after" });
+
+  const before = getComputedStyle(element, context, { pseudo: "before" });
+
+  return map(alt, alt =>
+    some(alt, alt => {
+      if (isArray(before.content)) {
+        alt = before.content.join("") + alt;
+      }
+
+      if (isArray(after.content)) {
+        alt = alt + after.content.join("");
+      }
+
+      return flatten(alt, options);
+    })
+  );
+}
+
+/**
+ * Get the text alternative of an element provided by a native tooltip
+ * attribute.
+ */
+function getTooltipTextAlternative(
+  element: Element,
+  context: Node,
+  visited: Set<Element | Text>,
+  options: TextAlternativeOptions
+): Option<string> {
+  const title = getAttribute(element, "title");
+
+  if (title !== null && title !== "") {
+    return flatten(title, options);
   }
 
   return null;
