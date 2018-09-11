3f96db6852084bbe1f81bb3927757ddee7d4e626
Niclas Hedam <niclas@hed.am>
2018-09-11 09:15:42 +0200

    Update map.spec.ts

--- packages/alfa-util/test/array/map.spec.ts
+++ packages/alfa-util/test/array/map.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "@siteimprove/alfa-test";
 import { map } from "../../src/array/map";
 
-test("Can group by element in array", t => {
+test("Can map elements in array", t => {
   const input = [1, 3, 4];
   const expected = [2, 6, 8];
   const predicate = (k: number) => k * 2;
