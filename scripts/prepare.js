cd8f19821592ec8c5ecea8f90a761cd5585d7924
Niclas Hedam <niclas@hed.am>
2018-09-11 10:16:16 +0200

    Refactoring to something that does not look horrifying

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -2,7 +2,10 @@ const { findFiles } = require("./helpers/file-system");
 const { endsWith, not } = require("./helpers/predicates");
 const { packages } = require("./helpers/meta");
 const { format, now } = require("./helpers/time");
+const { default: chalk } = require("chalk");
 const notify = require("./helpers/notify");
+const fs = require("fs");
+const path = require("path");
 
 const { build } = require("./tasks/build");
 const { clean } = require("./tasks/clean");
@@ -35,11 +38,24 @@ for (const pkg of packages) {
 
   handle(findFiles(`${root}/scripts`, endsWith(".js")));
 
-  handle(
-    findFiles(root, endsWith(".ts", ".tsx")).filter(
-      not(endsWith(".spec.ts", ".spec.tsx"))
-    )
+  const files = findFiles(root, endsWith(".ts", ".tsx")).filter(
+    not(endsWith(".spec.ts", ".spec.tsx"))
   );
+
+  for (const file of files) {
+    const dir = path
+      .dirname(file)
+      .split(path.sep)
+      .map(part => (part === "src" ? "test" : part))
+      .join(path.sep);
+    const testFile = path.join(dir, `${path.basename(file, ".ts")}.spec.ts`);
+
+    if (!fs.existsSync(testFile)) {
+      notify.warn(`${chalk.dim(file)}: Missing spec file`); // This could be an error in the future and actually fail the build.
+    }
+  }
+
+  handle(files);
 }
 
 handle(findFiles("docs", endsWith(".ts", ".tsx")));
--- scripts/test.js
+++ scripts/test.js
@@ -1,38 +1,19 @@
-const { default: chalk } = require("chalk");
 const { findFiles } = require("./helpers/file-system");
-const fs = require("fs");
 const { endsWith } = require("./helpers/predicates");
 const { packages } = require("./helpers/meta");
-const path = require("path");
 const notify = require("./helpers/notify");
 
 const { test } = require("./tasks/test");
 
 for (const pkg of packages) {
-  const root = `packages/${pkg}/`;
-  const files = findFiles(root, endsWith(".ts", ".tsx"));
+  const root = `packages/${pkg}/test`;
+  const files = findFiles(root, endsWith(".spec.ts", ".spec.tsx"));
 
   for (const file of files) {
-    if (file.endsWith(".spec.ts") || file.endsWith(".spec.tsx")) {
-      if (test(file)) {
-        notify.success(file);
-        continue;
-      }
-
+    if (test(file)) {
+      notify.success(file);
+    } else {
       process.exit(1);
-    }
-
-    if (file.endsWith(".ts") && !file.endsWith(".d.ts")) {
-      const dir = path
-        .dirname(file)
-        .split(path.sep)
-        .map(part => (part === "src" ? "test" : part))
-        .join(path.sep);
-      const testFile = path.join(dir, `${path.basename(file, ".ts")}.spec.js`);
-
-      if (!fs.existsSync(testFile)) {
-        notify.warn(`${chalk.dim(file)}: Missing spec file`); // This could be an error in the future and actually fail the build.
-      }
     }
   }
 }
