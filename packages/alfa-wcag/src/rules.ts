31d68140e0e5931ce8fefeec336af9871bcbe491
Kasper Isager <kasperisager@gmail.com>
2018-10-03 11:46:49 +0200

    Implement SIA-R7

--- packages/alfa-wcag/src/rules.ts
+++ packages/alfa-wcag/src/rules.ts
@@ -4,3 +4,4 @@ export { SIA_R3 } from "./sia-r3/rule";
 export { SIA_R4 } from "./sia-r4/rule";
 export { SIA_R5 } from "./sia-r5/rule";
 export { SIA_R6 } from "./sia-r6/rule";
+export { SIA_R7 } from "./sia-r7/rule";
