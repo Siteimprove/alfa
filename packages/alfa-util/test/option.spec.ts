f1a29f7dea84bc4db69e35d2115dd8e2c319c35f
Niclas Hedam <niclas@hed.am>
2018-10-09 10:37:47 +0200

    Much better annotation in the util tests

--- packages/alfa-util/test/option.spec.ts
+++ packages/alfa-util/test/option.spec.ts
@@ -1,23 +1,20 @@
 import { test } from "@siteimprove/alfa-test";
-import { none, option, some } from "../src/option";
+import { none, Option, option, some } from "../src/option";
+
+const something: Option<number> = 4;
+const nothing: Option<number> = null;
 
 test("Some only runs callback if parameter is something", t => {
-  t.equal(some(4, (num: number) => "callback"), "callback");
-  t.equal(some(null, (num: number) => "callback"), null);
+  t.equal(some(4, num => num), something);
+  t.equal(some(null, num => num), nothing);
 });
 
 test("None only runs callback if parameter is not something", t => {
-  t.equal(none(4, () => "callback"), 4);
-  t.equal(none(null, () => "callback"), "callback");
+  t.equal(none(4, () => null), something);
+  t.equal(none(null, () => null), nothing);
 });
 
 test("Options runs some if parameter is something, else none", t => {
-  t.equal(
-    option(4, (num: number) => "something", () => "nothing"),
-    "something"
-  );
-  t.equal(
-    option(null, (num: number) => "something", () => "nothing"),
-    "nothing"
-  );
+  t.equal(option(4, num => num, () => null), something);
+  t.equal(option(null, num => num, () => null), nothing);
 });
