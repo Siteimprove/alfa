144c72a2b3f341dba08435cf19c7de89b7efb1a4
Búgvi Benjamin Magnussen <bbm@siteimprove.com>
2018-10-09 14:29:04 +0200

    more tests for alfa-dom

---
+++ packages/alfa-dom/test/get-parent-element.spec.tsx
@@ -0,0 +1,25 @@
+import { jsx } from "@siteimprove/alfa-jsx";
+import { test } from "@siteimprove/alfa-test";
+import { getParentElement } from "../src/get-parent-element";
+import { Document } from "../src/types";
+
+test("Returns parent element", t => {
+  const body = <body />;
+  const html = <html>{body}</html>;
+  t.equal(getParentElement(body, html), html);
+});
+
+test("Returns null when parent is undefined", t => {
+  const div = <div />;
+  t.equal(getParentElement(div, div), null);
+});
+
+test("Returns null when parent is not an element", t => {
+  const document: Document = {
+    nodeType: 9,
+    childNodes: [<html />],
+    compatMode: "CSS1Compat",
+    styleSheets: []
+  };
+  t.equal(getParentElement(document.childNodes[0], document), null);
+});
---
+++ packages/alfa-dom/test/get-parent-rule.spec.tsx
@@ -0,0 +1,61 @@
+import { jsx } from "@siteimprove/alfa-jsx";
+import { test } from "@siteimprove/alfa-test";
+import { getParentRule } from "../src/get-parent-rule";
+import {
+  Document,
+  MediaRule,
+  Rule,
+  StyleDeclaration,
+  StyleRule,
+  StyleSheet
+} from "../src/types";
+
+test("Returns parent rule", t => {
+  const childStyleDec: StyleDeclaration = {
+    cssText: "background-color:black;"
+  };
+  const childRule: StyleRule = {
+    type: 1,
+    selectorText: "button",
+    style: childStyleDec
+  };
+  const parentRule: MediaRule = {
+    type: 4,
+    cssRules: [childRule],
+    media: ["fooscreen", "barscreen"]
+  };
+
+  const styleSheet: StyleSheet = { cssRules: [parentRule] };
+  const document: Document = {
+    nodeType: 9,
+    childNodes: [<div />],
+    compatMode: "CSS1Compat",
+    styleSheets: [styleSheet]
+  };
+  t.equal(getParentRule(childRule, document), parentRule);
+});
+
+test("Returns null when context is not document type", t => {
+  const div = <div />;
+  const rule: Rule = { type: 1 };
+  t.equal(getParentRule(rule, div), null);
+});
+
+test("Returns null when parent rule is undefined", t => {
+  const childStyleDec: StyleDeclaration = {
+    cssText: "background-color:black;"
+  };
+  const childRule: StyleRule = {
+    type: 1,
+    selectorText: "button",
+    style: childStyleDec
+  };
+  const styleSheet: StyleSheet = { cssRules: [] };
+  const document: Document = {
+    nodeType: 9,
+    childNodes: [<div />],
+    compatMode: "CSS1Compat",
+    styleSheets: [styleSheet]
+  };
+  t.equal(getParentRule(childRule, document), null);
+});
