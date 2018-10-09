67e97fe8ef9492cf505c27aad80aa8f58b06c7da
Búgvi Benjamin Magnussen <bbm@siteimprove.com>
2018-10-09 17:01:21 +0200

    Add some more DOM tests

---
+++ packages/alfa-dom/test/get-previous-sibling.spec.tsx
@@ -0,0 +1,27 @@
+import { jsx } from "@siteimprove/alfa-jsx";
+import { test } from "@siteimprove/alfa-test";
+import { getPreviousSibling } from "../src/get-previous-sibling";
+
+test("Returns null when no previous sibling exists", t => {
+  const button = <button />;
+  const div = <div>{button}</div>;
+  t.equal(getPreviousSibling(button, div), null);
+});
+
+test("Returns null when no parent exists", t => {
+  const button = <button />;
+  const div = <div />;
+  t.equal(getPreviousSibling(button, div), null);
+});
+
+test("Returns previous defined sibling", t => {
+  const button = <button />;
+  const p = <p>Foo</p>;
+  const div = (
+    <div>
+      {p}
+      {button}
+    </div>
+  );
+  t.equal(getPreviousSibling(button, div), p);
+});
