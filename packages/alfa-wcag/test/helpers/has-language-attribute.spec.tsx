24a03e08abf3e7faca34413f673bfc833c299fb4
Niclas Hedam <niclas@hed.am>
2018-10-23 16:11:44 +0200

    Added last case using non-jsx

--- packages/alfa-wcag/test/helpers/has-language-attribute.spec.tsx
+++ packages/alfa-wcag/test/helpers/has-language-attribute.spec.tsx
@@ -1,3 +1,4 @@
+import { Element, NodeType } from "@siteimprove/alfa-dom";
 import { jsx } from "@siteimprove/alfa-jsx";
 import { test } from "@siteimprove/alfa-test";
 import { hasLanguageAttribute } from "../../src/helpers/has-language-attribute";
@@ -14,5 +15,23 @@ test("Returns false, if tag has an empty language attribute", t => {
 
 test("Returns true, if tag has a language attribute", t => {
   const tag = <html lang="en" />;
+  t(hasLanguageAttribute(tag));
+});
+
+test("Returns true, if tag has a xml language attribute", t => {
+  const tag: Element = {
+    nodeType: NodeType.Element,
+    prefix: null,
+    localName: "html",
+    attributes: [
+      {
+        prefix: "xml",
+        localName: "lang",
+        value: "en"
+      }
+    ],
+    shadowRoot: null,
+    childNodes: []
+  };
   t(hasLanguageAttribute(tag));
 });
