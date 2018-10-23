a10ad5bb325cee74f1c6de1828ddd7c3b7490bec
Niclas Hedam <niclas@hed.am>
2018-10-23 09:40:09 +0200

    swapped expected and actual

--- packages/alfa-aria/test/resolve-references.spec.tsx
+++ packages/alfa-aria/test/resolve-references.spec.tsx
@@ -5,5 +5,5 @@ import { resolveReferences } from "../src/resolve-references";
 test("Can resolve references", t => {
   const button = <button id="bar">Foo</button>;
   const html = <html>{button}</html>;
-  t.deepEqual([button], resolveReferences(html, html, "bar"));
+  t.deepEqual(resolveReferences(html, html, "bar"), [button]);
 });
