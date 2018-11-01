9d5b7ea41a1ae363a690cffba52653e9dcd93de0
Niclas Hedam <niclas@hed.am>
2018-11-01 16:49:33 +0100

    Working on testing parser

---
+++ packages/alfa-css/test/parse.spec.ts
@@ -0,0 +1,67 @@
+import { test } from "@siteimprove/alfa-test";
+import { parseDeclaration, parseRule } from "../src/parse";
+
+test("Can parse a declaration", t => {
+  t.deepEqual(parseDeclaration("text-color: #000 !important;"), [
+    {
+      name: "text-color",
+      value: [
+        {
+          type: 10
+        },
+        {
+          type: 3,
+          unrestricted: true,
+          value: "000"
+        },
+        {
+          type: 10
+        }
+      ],
+      important: true
+    }
+  ]);
+});
+
+test("Can parse a rule", t => {
+  t.deepEqual(parseRule("b { text-decoration: none; }"), {
+    prelude: [
+      {
+        type: 0,
+        value: "b"
+      },
+      {
+        type: 10
+      }
+    ],
+    value: [
+      {
+        type: 10
+      },
+      {
+        type: 0,
+        value: "text-decoration"
+      },
+      {
+        type: 11
+      },
+      {
+        type: 10
+      },
+      {
+        type: 0,
+        value: "none"
+      },
+      {
+        type: 12
+      },
+      {
+        type: 10
+      }
+    ]
+  });
+});
+
+test("Cannot parse a gibberish rule", t => {
+  t.deepEqual(parseRule("{ text-decoration: none; }"), null);
+});
