e7625052d93731d13d93465ab41bcf3f41ee73cf
Niclas Hedam <niclas@hed.am>
2018-11-08 14:32:18 +0100

    helper to minimize the document construction

--- packages/alfa-wcag/test/sia-r1/rule.spec.tsx
+++ packages/alfa-wcag/test/sia-r1/rule.spec.tsx
@@ -1,73 +1,54 @@
 import { audit } from "@siteimprove/alfa-act";
-import { Document, NodeType } from "@siteimprove/alfa-dom";
 import { jsx } from "@siteimprove/alfa-jsx";
 import { test } from "@siteimprove/alfa-test";
 
 import { SIA_R1 } from "../../src/sia-r1/rule";
+
 import { outcome } from "../helpers/outcome";
+import { documentFromNodes } from "../helpers/document-from-nodes";
 
 test("SIA-R1 passes when document has a title", t => {
-  const document: Document = {
-    nodeType: NodeType.Document,
-    compatMode: "CSS1Compat",
-    styleSheets: [],
-    childNodes: [
-      <html>
-        <head>
-          <title>Hello world</title>
-        </head>
-      </html>
-    ]
-  };
+  const document = documentFromNodes([
+    <html>
+      <head>
+        <title>Hello world</title>
+      </head>
+    </html>
+  ]);
 
   outcome(t, audit({ document }, [SIA_R1]), { passed: [document] });
 });
 
 test("SIA-R1 fails when document has no title", t => {
-  const document: Document = {
-    nodeType: NodeType.Document,
-    compatMode: "CSS1Compat",
-    styleSheets: [],
-    childNodes: [
-      <html>
-        <head />
-      </html>
-    ]
-  };
+  const document = documentFromNodes([
+    <html>
+      <head />
+    </html>
+  ]);
 
   outcome(t, audit({ document }, [SIA_R1]), { failed: [document] });
 });
 
 test("SIA-R1 fails when only other namespace has a title", t => {
-  const document: Document = {
-    nodeType: NodeType.Document,
-    compatMode: "CSS1Compat",
-    styleSheets: [],
-    childNodes: [
-      <html>
-        <head>
-          <svg>
-            <title />
-          </svg>
-        </head>
-      </html>
-    ]
-  };
+  const document = documentFromNodes([
+    <html>
+      <head>
+        <svg>
+          <title />
+        </svg>
+      </head>
+    </html>
+  ]);
 
   outcome(t, audit({ document }, [SIA_R1]), { failed: [document] });
 });
 
 test("SIA-R1 only works in the HTML namespace", t => {
-  const document: Document = {
-    nodeType: NodeType.Document,
-    compatMode: "CSS1Compat",
-    styleSheets: [],
-    childNodes: [
-      <svg>
-        <title>Hello World</title>
-      </svg>
-    ]
-  };
+  const document = documentFromNodes([
+    <svg>
+      <title>Hello World</title>
+    </svg>
+  ]);
 
   outcome(t, audit({ document }, [SIA_R1]), { inapplicable: [document] });
 });
