1b33014d8f163bd6f8454ff7eb2d55c3d8865a6b
Kasper Isager <kasperisager@gmail.com>
2018-11-08 14:06:09 +0100

    Lintingz

--- packages/alfa-compatibility/test/browser-specific.spec.ts
+++ packages/alfa-compatibility/test/browser-specific.spec.ts
@@ -9,11 +9,17 @@ const f: (n: number) => number = n => n * 2;
 const g: (n: number) => number = n => n + 4;
 
 test("Satifies the law of left identity", t => {
-  t.deepEqual(map(n, f), BrowserSpecific.of(f(1), ["chrome"]).branch(f(2), ["firefox"]));
+  t.deepEqual(
+    map(n, f),
+    BrowserSpecific.of(f(1), ["chrome"]).branch(f(2), ["firefox"])
+  );
 });
 
 test("Satifies the law of right identity", t => {
-  t.deepEqual(map(n, n => BrowserSpecific.of(n, ["chrome"]).branch(n, ["firefox"])), n);
+  t.deepEqual(
+    map(n, n => BrowserSpecific.of(n, ["chrome"]).branch(n, ["firefox"])),
+    n
+  );
 });
 
 test("Satifies the law of associativity", t => {
