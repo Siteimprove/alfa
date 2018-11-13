7aa7406a7a6f34fccf1bc57f1c23064abd270d99
Niclas Hedam <niclas@hed.am>
2018-11-13 13:27:12 +0100

    SIA-R11

---
+++ packages/alfa-wcag/test/sia-r11/rule.spec.tsx
@@ -0,0 +1,29 @@
+import { Outcome } from "@siteimprove/alfa-act";
+import { jsx } from "@siteimprove/alfa-jsx";
+import { test } from "@siteimprove/alfa-test";
+
+import { SIA_R11 } from "../../src/sia-r11/rule";
+
+import { documentFromNodes } from "../helpers/document-from-nodes";
+import { outcome } from "../helpers/outcome";
+
+test("SIA-R11 passes when element has a textual accessible name", t => {
+  const link = <a href="foo" aria-label="bar" />;
+  const document = documentFromNodes([link]);
+
+  outcome(t, SIA_R11, { document }, { passed: [link] });
+});
+
+test("SIA-R11 fails when link has no accessible name", t => {
+  const link = <a href="foo" />;
+  const document = documentFromNodes([link]);
+
+  outcome(t, SIA_R11, { document }, { failed: [link] });
+});
+
+test("SIA-R11 is inapplicable when element is not in the accessibility tree", t => {
+  const link = <a href="foo" aria-hidden="true" />;
+  const document = documentFromNodes([link]);
+
+  outcome(t, SIA_R11, { document }, Outcome.Inapplicable);
+});
