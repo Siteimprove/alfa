ae81d4612ee81d636044a8d69b6374cbb3647f74
Niclas Hedam <niclas@hed.am>
2018-11-13 13:35:00 +0100

    SIA-R13

--- packages/alfa-wcag/test/sia-r12/rule.spec.tsx
+++ packages/alfa-wcag/test/sia-r12/rule.spec.tsx
@@ -8,22 +8,22 @@ import { documentFromNodes } from "../helpers/document-from-nodes";
 import { outcome } from "../helpers/outcome";
 
 test("SIA-R12 passes when a button has an accessible name", t => {
-  const image = <input type="button" aria-label="foo" />;
-  const document = documentFromNodes([image]);
+  const input = <input type="button" aria-label="foo" />;
+  const document = documentFromNodes([input]);
 
-  outcome(t, SIA_R12, { document }, { passed: [image] });
+  outcome(t, SIA_R12, { document }, { passed: [input] });
 });
 
 test("SIA-R12 fails when a button has no accessible name", t => {
-  const image = <input type="button" />;
-  const document = documentFromNodes([image]);
+  const input = <input type="button" />;
+  const document = documentFromNodes([input]);
 
-  outcome(t, SIA_R12, { document }, { failed: [image] });
+  outcome(t, SIA_R12, { document }, { failed: [input] });
 });
 
 test("SIA-R12 is inapplicable when element is not in the accessibility tree", t => {
-  const image = <input type="button" aria-hidden="true" />;
-  const document = documentFromNodes([image]);
+  const input = <input type="button" aria-hidden="true" />;
+  const document = documentFromNodes([input]);
 
   outcome(t, SIA_R12, { document }, Outcome.Inapplicable);
 });
