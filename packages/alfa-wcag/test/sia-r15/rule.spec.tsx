8b0dcedf9cc809fa6e90de44625dd98dbb88e2c4
Niclas Hedam <niclas@hed.am>
2018-11-13 15:11:30 +0100

    SIA-R15

---
+++ packages/alfa-wcag/test/sia-r15/rule.spec.tsx
@@ -0,0 +1,53 @@
+import { Outcome } from "@siteimprove/alfa-act";
+import { jsx } from "@siteimprove/alfa-jsx";
+import { test } from "@siteimprove/alfa-test";
+
+import { SIA_R15 } from "../../src/sia-r15/rule";
+
+import { documentFromNodes } from "../helpers/document-from-nodes";
+import { outcome } from "../helpers/outcome";
+
+test("SIA-R15 passes when an iframe has a unique accessible name", t => {
+  const foo = <iframe src="https://foo.bar/baz.html" aria-label="foo" />;
+  const bar = <iframe src="https://foo.bar/" aria-label="bar" />;
+  const html = (
+    <html>
+      {foo}
+      {bar}
+    </html>
+  );
+
+  const document = documentFromNodes([html]);
+
+  outcome(t, SIA_R15, { document }, { passed: [foo, bar] });
+});
+
+test("SIA-R15 fails when an iframe has no unique accessible name", t => {
+  const foo = <iframe src="https://foo.bar/baz.html" aria-label="foo" />;
+  const bar = <iframe src="https://foo.bar/" aria-label="foo" />;
+  const html = (
+    <html>
+      {foo}
+      {bar}
+    </html>
+  );
+
+  const document = documentFromNodes([html]);
+
+  outcome(t, SIA_R15, { document }, { failed: [foo, bar] });
+});
+
+test("SIA-R15 is inapplicable when element is not in the accessibility tree", t => {
+  const foo = <iframe src="https://foo.bar/baz.html" />;
+  const bar = <iframe src="https://foo.bar/" />;
+  const html = (
+    <html>
+      {foo}
+      {bar}
+    </html>
+  );
+
+  const document = documentFromNodes([html]);
+
+  outcome(t, SIA_R15, { document }, Outcome.Inapplicable);
+});
