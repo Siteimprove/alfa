b8357c3ea6f3051beb4e54f02b2156269d39ba53
Niclas Hedam <niclas@hed.am>
2018-11-13 13:38:42 +0100

    SIA-R14

---
+++ packages/alfa-wcag/test/sia-r14/rule.spec.tsx
@@ -0,0 +1,37 @@
+import { Outcome } from "@siteimprove/alfa-act";
+import { jsx } from "@siteimprove/alfa-jsx";
+import { test } from "@siteimprove/alfa-test";
+
+import { SIA_R14 } from "../../src/sia-r14/rule";
+
+import { documentFromNodes } from "../helpers/document-from-nodes";
+import { outcome } from "../helpers/outcome";
+
+test("SIA-R14 passes when aria-label matches textual content", t => {
+  const div = (
+    <div role="link" aria-label="foo ">
+      foo
+    </div>
+  );
+  const document = documentFromNodes([div]);
+
+  outcome(t, SIA_R14, { document }, { passed: [div] });
+});
+
+test("SIA-R14 failes when aria-label does not match textual content", t => {
+  const div = (
+    <div role="link" aria-label="foo">
+      bar
+    </div>
+  );
+  const document = documentFromNodes([div]);
+
+  outcome(t, SIA_R14, { document }, { failed: [div] });
+});
+
+test("SIA-R14 is inapplicable when element is not a widget", t => {
+  const a = <a aria-label="foo">bar</a>;
+  const document = documentFromNodes([a]);
+
+  outcome(t, SIA_R14, { document }, Outcome.Inapplicable);
+});
