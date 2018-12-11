ecdf53727b33c7fac110d878fe50114f21cdff31
Búgvi Magnussen <bugvimagnussen@gmail.com>
2018-12-11 15:28:12 +0100

    Better variable names in get-assigned-nodes.spec

--- packages/alfa-dom/test/get-assigned-nodes.spec.tsx
+++ packages/alfa-dom/test/get-assigned-nodes.spec.tsx
@@ -2,11 +2,11 @@ import { jsx } from "@siteimprove/alfa-jsx";
 import { test } from "@siteimprove/alfa-test";
 import { getAssignedNodes } from "../src/get-assigned-nodes";
 
-const slot = <slot name="foo" />;
-const slot2 = <slot name="bar" />;
+const slotInShadow = <slot name="foo" />;
+const slotInDiv = <slot name="bar" />;
 const element = <span slot="foo">Hello world</span>;
-const shadow = <shadow>{slot}</shadow>;
-const div = <div>{slot2}</div>;
+const shadow = <shadow>{slotInShadow}</shadow>;
+const div = <div>{slotInDiv}</div>;
 const context = (
   <div>
     {element}
@@ -22,7 +22,7 @@ const contextNoSpan = (
 );
 
 test("Gets the assigned nodes of a slot", t => {
-  t.deepEqual(getAssignedNodes(slot, context), [element]);
+  t.deepEqual(getAssignedNodes(slotInShadow, context), [element]);
 });
 
 test("Gets an empty array when element is not slot", t => {
@@ -30,34 +30,40 @@ test("Gets an empty array when element is not slot", t => {
 });
 
 test("Gets an empty array when root node is not a shadow node", t => {
-  t.deepEqual(getAssignedNodes(slot2, context), []);
+  t.deepEqual(getAssignedNodes(slotInDiv, context), []);
 });
 
 test("Gets the assigned nodes of a slot with flattened traversal", t => {
-  t.deepEqual(getAssignedNodes(slot, context, { flattened: true }), [element]);
+  t.deepEqual(getAssignedNodes(slotInShadow, context, { flattened: true }), [
+    element
+  ]);
 });
 
 test("Gets an empty array when root node is not a shadow node with flattened traversal", t => {
-  t.deepEqual(getAssignedNodes(slot2, context, { flattened: true }), []);
+  t.deepEqual(getAssignedNodes(slotInDiv, context, { flattened: true }), []);
 });
 
 test("Gets an empty array when there are no slotables with flattened traversal", t => {
-  t.deepEqual(getAssignedNodes(slot, contextNoSpan, { flattened: true }), []);
+  t.deepEqual(
+    getAssignedNodes(slotInShadow, contextNoSpan, { flattened: true }),
+    []
+  );
 });
 
 test("Gets the assigned nodes of a slot with child when there are no slotables with flattened traversal", t => {
   const button = <button />;
-  const slot = <slot name="foo">{button}</slot>;
-  const shadow = <shadow>{slot}</shadow>;
+  const slotInShadow = <slot name="foo">{button}</slot>;
+  const shadow = <shadow>{slotInShadow}</shadow>;
   const contextNoSpan = (
     <div>
       {shadow}
       {div}
     </div>
   );
-  t.deepEqual(getAssignedNodes(slot, contextNoSpan, { flattened: true }), [
-    button
-  ]);
+  t.deepEqual(
+    getAssignedNodes(slotInShadow, contextNoSpan, { flattened: true }),
+    [button]
+  );
 });
 
 /**
@@ -74,5 +80,7 @@ test("Gets assigned node when ", t => {
       {div}
     </div>
   );
-  t.deepEqual(getAssignedNodes(slot, context, { flattened: true }), [element]);
+  t.deepEqual(getAssignedNodes(slotInShadow, context, { flattened: true }), [
+    element
+  ]);
 });
