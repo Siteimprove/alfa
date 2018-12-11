6e04b3e25b89486a71b089802688f16a4e17727f
Búgvi Benjamin Magnussen <bbm@siteimprove.com>
2018-12-11 14:47:07 +0100

    using bit flags instead of decimal values in compare-document-position spec

--- packages/alfa-dom/test/compare-document-position.spec.tsx
+++ packages/alfa-dom/test/compare-document-position.spec.tsx
@@ -1,6 +1,9 @@
 import { jsx } from "@siteimprove/alfa-jsx";
 import { test } from "@siteimprove/alfa-test";
-import { compareDocumentPosition } from "../src/compare-document-position";
+import {
+  compareDocumentPosition,
+  DocumentPosition
+} from "../src/compare-document-position";
 
 const em = <em />;
 const strong = <strong />;
@@ -25,16 +28,61 @@ test("Returns > 0 when the first node comes after the second", t => {
   t(compareDocumentPosition(strong, em, context) > 0);
 });
 
-test("Returns 35 or 37 if the nodes are not in the same tree", t => {
+test("Returns Disconnected if the nodes are not in the same tree", t => {
   const div = <div />;
   const cmp = compareDocumentPosition(strong, div, context);
-  t(cmp === 35 || cmp === 37);
+  const revcmp = compareDocumentPosition(div, strong, context);
+
+  // Check if nodes are disconnected
+  const isDisc = cmp & DocumentPosition.Disconnected;
+  t.equal(isDisc, DocumentPosition.Disconnected);
+
+  /*
+   * The following checks are needed according to the spec
+   * @see https://www.w3.org/TR/dom/#dom-node-comparedocumentposition
+   */
+
+  const isPrec = cmp & DocumentPosition.Preceding;
+  const isFoll = cmp & DocumentPosition.Following;
+
+  // check that only the preceding flag xor the following flag is set
+  t.equal((isPrec >> 1) ^ (isFoll >> 2), 1);
+  // chech that if strong precedes div then the div should follow strong
+  if (isPrec === DocumentPosition.Preceding) {
+    t(
+      cmp ===
+        (DocumentPosition.Disconnected |
+          DocumentPosition.ImplementationSpecific |
+          DocumentPosition.Preceding) &&
+        revcmp ===
+          (DocumentPosition.Disconnected |
+            DocumentPosition.ImplementationSpecific |
+            DocumentPosition.Following)
+    );
+  } else {
+    t(
+      cmp ===
+        (DocumentPosition.Disconnected |
+          DocumentPosition.ImplementationSpecific |
+          DocumentPosition.Following) &&
+        revcmp ===
+          (DocumentPosition.Disconnected |
+            DocumentPosition.ImplementationSpecific |
+            DocumentPosition.Preceding)
+    );
+  }
 });
 
-test("Returns -20 when the other node is contained by the reference node", t => {
-  t.equal(compareDocumentPosition(span, em, context), -20);
+test("Returns ContainedBy and Following as negative value when the other node is contained by the reference node", t => {
+  t.equal(
+    compareDocumentPosition(span, em, context),
+    -1 * (DocumentPosition.ContainedBy | DocumentPosition.Following)
+  );
 });
 
-test("Returns 10 when the reference node is contained by the other node", t => {
-  t.equal(compareDocumentPosition(em, span, context), 10);
+test("Returns Contains and Preceding when the reference node is contained by the other node", t => {
+  t.equal(
+    compareDocumentPosition(em, span, context),
+    DocumentPosition.Contains | DocumentPosition.Preceding
+  );
 });
