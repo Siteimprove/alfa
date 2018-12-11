cb89cb76006a97a9e81f1585231b3fea227196f0
Búgvi Benjamin Magnussen <bbm@siteimprove.com>
2018-12-11 12:55:00 +0100

    changed variable names in compare-document-position.spec

--- packages/alfa-dom/test/compare-document-position.spec.tsx
+++ packages/alfa-dom/test/compare-document-position.spec.tsx
@@ -2,39 +2,39 @@ import { jsx } from "@siteimprove/alfa-jsx";
 import { test } from "@siteimprove/alfa-test";
 import { compareDocumentPosition } from "../src/compare-document-position";
 
-const foo = <em />;
-const bar = <strong />;
-const foobar = <span>{foo}</span>;
+const em = <em />;
+const strong = <strong />;
+const span = <span>{em}</span>;
 
 const context = (
   <div>
-    {foobar}
-    {bar}
+    {span}
+    {strong}
   </div>
 );
 
 test("Returns 0 when a node is compared to itself", t => {
-  t.equal(compareDocumentPosition(foo, foo, context), 0);
+  t.equal(compareDocumentPosition(em, em, context), 0);
 });
 
 test("Returns < 0 when the first node comes before the second", t => {
-  t(compareDocumentPosition(foo, bar, context) < 0);
+  t(compareDocumentPosition(em, strong, context) < 0);
 });
 
 test("Returns > 0 when the first node comes after the second", t => {
-  t(compareDocumentPosition(bar, foo, context) > 0);
+  t(compareDocumentPosition(strong, em, context) > 0);
 });
 
 test("Returns 35 or 37 if the nodes are not in the same tree", t => {
   const div = <div />;
-  const cmp = compareDocumentPosition(bar, div, context);
+  const cmp = compareDocumentPosition(strong, div, context);
   t(cmp === 35 || cmp === 37);
 });
 
 test("Returns -20 when the other node is contained by the reference node", t => {
-  t.equal(compareDocumentPosition(foobar, foo, context), -20);
+  t.equal(compareDocumentPosition(span, em, context), -20);
 });
 
 test("Returns 10 when the reference node is contained by the other node", t => {
-  t.equal(compareDocumentPosition(foo, foobar, context), 10);
+  t.equal(compareDocumentPosition(em, span, context), 10);
 });
