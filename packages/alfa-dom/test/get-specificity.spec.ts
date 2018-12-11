9676665604f9eed15ea5e8f46e5ee4697474b889
Búgvi Magnussen <bugvimagnussen@gmail.com>
2018-12-11 15:41:32 +0100

    format get-specificity.spec

--- packages/alfa-dom/test/get-specificity.spec.ts
+++ packages/alfa-dom/test/get-specificity.spec.ts
@@ -10,7 +10,7 @@ function selector(input: string): Selector {
   if (parsed === null || isArray(parsed)) {
     throw new Error(`Invalid selector: ${input}`);
   }
-  
+
   return parsed;
 }
 
@@ -50,7 +50,7 @@ test("A class is more specific than a pseudo-element-selector", t => {
     getSpecificity(selector(".foo")) >
       getSpecificity(selector("::first-letter"))
   );
-});
+}); 
 
 test("A compound selector is more specific than a class", t => {
   t(
