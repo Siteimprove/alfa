ac36543980cbb3d3213fa5cd926c3c3acce945d4
Niclas Hedam <niclas@hed.am>
2019-01-08 14:52:54 +0100

    Revert "Testing Semaphore cache"
    This reverts commit 9b0440f27ec23757184c95beaebac21af616203e.

--- packages/alfa-lang/src/is-alpha.ts
+++ packages/alfa-lang/src/is-alpha.ts
@@ -3,7 +3,7 @@ import { isBetween } from "./is-between";
 
 export function isAlpha(char: number): boolean {
   return (
-    isBetween(char, Char.SmallLetterB, Char.SmallLetterZ) ||
+    isBetween(char, Char.SmallLetterA, Char.SmallLetterZ) ||
     isBetween(char, Char.CapitalLetterA, Char.CapitalLetterZ)
   );
 }
