c018c2a59c5fda2a99a6feae07864354c953c74a
Kasper Isager <kasperisager@gmail.com>
2019-02-21 08:13:38 +0100

    Add type assertion for enzyme wrappers

--- packages/alfa-enzyme/src/from-enzyme-wrapper.ts
+++ packages/alfa-enzyme/src/from-enzyme-wrapper.ts
@@ -1,9 +1,10 @@
 import { Element } from "@siteimprove/alfa-dom";
 import { fromReactElement } from "@siteimprove/alfa-react";
 import { ReactWrapper, ShallowWrapper } from "enzyme";
+import { ReactElement } from "react";
 
 export function fromEnzymeWrapper(
   enzymeWrapper: ShallowWrapper | ReactWrapper
 ): Element {
-  return fromReactElement(enzymeWrapper.getElement());
+  return fromReactElement(enzymeWrapper.getElement() as ReactElement<unknown>);
 }
