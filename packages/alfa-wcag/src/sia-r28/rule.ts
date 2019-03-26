1827b0918db56c6969c37ec4075627814f59af40
Kasper Isager <kasperisager@gmail.com>
2019-03-26 12:18:44 +0100

    Implement SIA-R28

---
+++ packages/alfa-wcag/src/sia-r28/rule.ts
@@ -0,0 +1,78 @@
+import { Atomic } from "@siteimprove/alfa-act";
+import {
+  getRole,
+  getTextAlternative,
+  isExposed,
+  Roles
+} from "@siteimprove/alfa-aria";
+import { BrowserSpecific } from "@siteimprove/alfa-compatibility";
+import { Device } from "@siteimprove/alfa-device";
+import {
+  Document,
+  Element,
+  getElementNamespace,
+  getInputType,
+  InputType,
+  isElement,
+  Namespace,
+  Node,
+  querySelectorAll
+} from "@siteimprove/alfa-dom";
+
+const { map } = BrowserSpecific;
+
+export const SIA_R28: Atomic.Rule<Device | Document, Element> = {
+  id: "sanshikan:rules/sia-r28.html",
+  requirements: [
+    { id: "wcag:non-text-content", partial: true },
+    { id: "wcag:name-role-value", partial: true }
+  ],
+  evaluate: ({ device, document }) => {
+    return {
+      applicability: () => {
+        return querySelectorAll<Element>(
+          document,
+          document,
+          node => {
+            return isElement(node) && isImageButton(node, document);
+          },
+          {
+            composed: true
+          }
+        ).map(element => {
+          return map(isExposed(element, document, device), isExposed => {
+            return {
+              applicable: isExposed,
+              aspect: document,
+              target: element
+            };
+          });
+        });
+      },
+
+      expectations: (aspect, target) => {
+        return map(
+          getTextAlternative(target, document, device),
+          textAlternative => {
+            return {
+              1: {
+                holds: textAlternative !== null && textAlternative !== "",
+                data: {
+                  alt: textAlternative
+                }
+              }
+            };
+          }
+        );
+      }
+    };
+  }
+};
+
+function isImageButton(element: Element, context: Node): boolean {
+  if (getElementNamespace(element, context) !== Namespace.HTML) {
+    return false;
+  }
+
+  return getInputType(element) === InputType.Button;
+}
