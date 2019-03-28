989d7dbd923406ca30f001ab4a866ad5a8ac01b8
Kasper Isager <kasperisager@gmail.com>
2019-03-28 08:59:24 +0100

    Update SIA-R16

--- packages/alfa-wcag/src/sia-r16/rule.ts
+++ packages/alfa-wcag/src/sia-r16/rule.ts
@@ -1,11 +1,6 @@
 import { Atomic } from "@siteimprove/alfa-act";
 import { getRole, Role } from "@siteimprove/alfa-aria";
-import {
-  BrowserSpecific,
-  every,
-  map,
-  some
-} from "@siteimprove/alfa-compatibility";
+import { BrowserSpecific, map } from "@siteimprove/alfa-compatibility";
 import { Device } from "@siteimprove/alfa-device";
 import {
   Document,
@@ -23,51 +18,31 @@ export const SIA_R16: Atomic.Rule<Device | Document, Element> = {
   evaluate: ({ device, document }) => {
     return {
       applicability: () => {
-        return querySelectorAll<Element>(document, document, node => {
-          return isElement(node) && hasExplicitRole(node, document, device);
+        return querySelectorAll(document, document, isElement, {
+          composed: true
         }).map(element => {
-          return {
-            applicable: true,
-            aspect: document,
-            target: element
-          };
+          return map(
+            hasExplicitRole(element, document, device),
+            hasExplicitRole => {
+              return {
+                applicable: hasExplicitRole,
+                aspect: document,
+                target: element
+              };
+            }
+          );
         });
       },
 
       expectations: (aspect, target) => {
-        const role = getExplicitRole(target, document, device);
-
-        return {
-          1: {
-            holds: every(role, role => {
-              if (role === null || role.required === undefined) {
-                return true;
-              }
-
-              const implicits =
-                role.implicits === undefined
-                  ? []
-                  : role.implicits(target, document, device);
-
-              for (const attribute of role.required(target, document, device)) {
-                const value = getAttribute(target, attribute.name, {
-                  trim: true
-                });
-
-                if (value === null || value === "") {
-                  if (
-                    implicits.find(implicit => implicit[0] === attribute) ===
-                    undefined
-                  ) {
-                    return false;
-                  }
-                }
-              }
-
-              return true;
-            })
+        return map(
+          hasRequiredValues(target, document, device),
+          hasRequiredValues => {
+            return {
+              1: { holds: hasRequiredValues }
+            };
           }
-        };
+        );
       }
     };
   }
@@ -78,28 +53,66 @@ function getExplicitRole(
   context: Node,
   device: Device
 ): Option<Role> | BrowserSpecific<Option<Role>> {
-  const implicitRole = getRole(element, context, device, { explicit: false });
-  const explicitRole = getRole(element, context, device, { implicit: false });
+  return map(
+    getRole(element, context, device, { implicit: false }),
+    explicitRole => {
+      if (explicitRole === null) {
+        return null;
+      }
 
-  return map(explicitRole, explicitRole => {
-    if (
-      explicitRole !== null &&
-      some(implicitRole, implicitRole => explicitRole !== implicitRole)
-    ) {
-      return explicitRole;
+      return map(
+        getRole(element, context, device, { explicit: false }),
+        implicitRole => {
+          if (explicitRole !== implicitRole) {
+            return explicitRole;
+          }
+
+          return null;
+        }
+      );
     }
-
-    return null;
-  });
+  );
 }
 
 function hasExplicitRole(
   element: Element,
   context: Node,
   device: Device
-): boolean {
-  return some(
-    getExplicitRole(element, context, device),
-    explicitRole => explicitRole !== null
-  );
+): boolean | BrowserSpecific<boolean> {
+  return map(getExplicitRole(element, context, device), explicitRole => {
+    return explicitRole !== null;
+  });
+}
+
+function hasRequiredValues(
+  element: Element,
+  context: Node,
+  device: Device
+): boolean | BrowserSpecific<boolean> {
+  return map(getExplicitRole(element, document, device), role => {
+    if (role === null || role.required === undefined) {
+      return true;
+    }
+
+    const implicits =
+      role.implicits === undefined
+        ? []
+        : role.implicits(element, document, device);
+
+    for (const attribute of role.required(element, document, device)) {
+      const value = getAttribute(element, attribute.name, {
+        trim: true
+      });
+
+      if (value === null || value === "") {
+        if (
+          implicits.find(implicit => implicit[0] === attribute) === undefined
+        ) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  });
 }
