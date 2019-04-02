f540a19493dfe437e377624271e3f213c6f0267b
Kasper Isager <kasperisager@gmail.com>
2019-04-02 10:50:59 +0200

    Fix rule guards

--- packages/alfa-act/src/guards.ts
+++ packages/alfa-act/src/guards.ts
@@ -3,11 +3,11 @@ import { Aspect, Atomic, Composite, Rule, Target } from "./types";
 export function isAtomic<A extends Aspect, T extends Target>(
   rule: Rule<A, T>
 ): rule is Atomic.Rule<A, T> {
-  return "composes" in rule === false;
+  return "compose" in rule === false;
 }
 
 export function isComposite<A extends Aspect, T extends Target>(
   rule: Rule<A, T>
 ): rule is Composite.Rule<A, T> {
-  return "composes" in rule;
+  return "compose" in rule;
 }
