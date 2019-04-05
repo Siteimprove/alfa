c2f04cbdf163950aba912f2232ccc40d98140bf2
Kasper Isager <kasperisager@gmail.com>
2019-04-05 11:13:40 +0200

    Implement `isWhitespace()`

--- packages/alfa-unicode/src/types.ts
+++ packages/alfa-unicode/src/types.ts
@@ -2,49 +2,55 @@
  * @see https://www.unicode.org/versions/Unicode11.0.0/ch04.pdf#G134153
  */
 export const enum Category {
-  // Letter
-  Lu,
-  Ll,
-  Lt,
-  Lm,
-  Lo,
+  Lu = 1 << 0, // Uppercase
+  Ll = 1 << 1, // Lowercase
+  Lt = 1 << 2, // Titlecase
+  Lm = 1 << 3, // Modifier
+  Lo = 1 << 4, // Other
+  Letter = Category.Lu | Category.Ll | Category.Lt | Category.Lm | Category.Lo,
 
-  // Mark
-  Mn,
-  Mc,
-  Me,
+  Mn = 1 << 5, // Non-spacing
+  Mc = 1 << 6, // Combining
+  Me = 1 << 7, // Enclosing
+  Mark = Category.Mn | Category.Mc | Category.Me,
 
-  // Number
-  Nd,
-  Nl,
-  No,
+  Nd = 1 << 8, // Decimal
+  Nl = 1 << 9, // Letter
+  No = 1 << 10, // Other
+  Number = Category.Nd | Category.Nl | Category.No,
 
-  // Punctuation
-  Pc,
-  Pd,
-  Ps,
-  Pe,
-  Pi,
-  Pf,
-  Po,
+  Pc = 1 << 11, // Connector
+  Pd = 1 << 12, // Dash
+  Ps = 1 << 13, // Open
+  Pe = 1 << 14, // Close
+  Pi = 1 << 15, // Initial quote
+  Pf = 1 << 16, // Final quote
+  Po = 1 << 17, // Other
+  Punctuation = Category.Pc |
+    Category.Pd |
+    Category.Ps |
+    Category.Pe |
+    Category.Pi |
+    Category.Pf |
+    Category.Po,
 
-  // Symbol
-  Sm,
-  Sc,
-  Sk,
-  So,
+  Sm = 1 << 18, // Math
+  Sc = 1 << 19, // Currency
+  Sk = 1 << 20, // Modifier
+  So = 1 << 21, // Other
+  Symbol = Category.Sm | Category.Sc | Category.Sk | Category.So,
 
-  // Separator
-  Zs,
-  Zl,
-  Zp,
+  Zs = 1 << 22, // Sapce
+  Zl = 1 << 23, // Line
+  Zp = 1 << 24, // Paragraph
+  Separator = Category.Zs | Category.Zl | Category.Zp,
 
-  // Other
-  Cc,
-  Cf,
-  Cs,
-  Co,
-  Cn
+  Cc = 1 << 25, // Control
+  Cf = 1 << 26, // Format
+  Cs = 1 << 27, // Surrogate
+  Co = 1 << 28, // Private use
+  Cn = 1 << 29, // Not assigned
+  Other = Category.Cc | Category.Cf | Category.Cs | Category.Co | Category.Cn
 }
 
 export interface Character {
