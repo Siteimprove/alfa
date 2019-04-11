aecb950877de53fbc1d694247e0b53068d50c8b6
Niclas Hedam <niclas@hed.am>
2019-04-11 10:33:01 +0200

    SIA-R28 done

--- packages/alfa-wcag/test/sia-r28/rule.spec.tsx
+++ packages/alfa-wcag/test/sia-r28/rule.spec.tsx
@@ -1,3 +1,4 @@
+import { Outcome } from "@siteimprove/alfa-act";
 import { getDefaultDevice } from "@siteimprove/alfa-device";
 import { jsx } from "@siteimprove/alfa-jsx";
 import { test } from "@siteimprove/alfa-test";
@@ -18,5 +19,29 @@ test("Passes when image button element has an accessible name", t => {
     SIA_R28,
     { document, device: getDefaultDevice() },
     { passed: [button] }
+  );
+});
+
+test("Fails when image button element has no accessible name", t => {
+  const button = <input type="image" name="submit" src="foo.gif" />;
+  const document = documentFromNodes([button]);
+
+  outcome(
+    t,
+    SIA_R28,
+    { document, device: getDefaultDevice() },
+    { failed: [button] }
+  );
+});
+
+test("Inapplicable when image button element is not of type input", t => {
+  const img = <img src="foo.gif" />;
+  const document = documentFromNodes([img]);
+
+  outcome(
+    t,
+    SIA_R28,
+    { document, device: getDefaultDevice() },
+    Outcome.Inapplicable
   );
 });
