dd80a65aadfa7be4ffd82e8301bf693eaa1fb663
Kasper Isager <kasperisager@gmail.com>
2019-04-17 11:22:23 +0200

    Use single workspace now that `characters.ts` isn't hogging memory

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,6 +1,5 @@
 const { findFiles } = require("./helpers/file-system");
 const { endsWith } = require("./helpers/predicates");
-const { Project } = require("./helpers/project");
 const { workspace } = require("./helpers/workspace");
 const { packages } = require("./helpers/meta");
 const { forEach } = require("./helpers/iterable");
@@ -12,51 +11,40 @@ const { diagnose } = require("./tasks/diagnose");
 const { clean } = require("./tasks/clean");
 
 /**
- * @param {Project} [project]
- * @return {(file: string) => void}
+ * @param {string} file
  */
-function handle(project) {
-  return file => {
-    const start = time.now();
+function handle(file) {
+  const start = time.now();
 
-    project = project || workspace.projectFor(file);
+  const project = workspace.projectFor(file);
 
-    project.addFile(file);
+  project.addFile(file);
 
-    [...project.buildProgram()];
+  [...project.buildProgram()];
 
-    const success = diagnose(file, project) && build(file, project);
+  const success = diagnose(file, project) && build(file, project);
 
-    if (success) {
-      const duration = time.now(start);
+  if (success) {
+    const duration = time.now(start);
 
-      notify.success(
-        `${file} ${time.format(duration, { color: "yellow", threshold: 400 })}`
-      );
-    } else {
-      process.exit(1);
-    }
-  };
+    notify.success(
+      `${file} ${time.format(duration, { color: "yellow", threshold: 400 })}`
+    );
+  } else {
+    process.exit(1);
+  }
 }
 
-forEach(findFiles("scripts", endsWith(".js")), handle());
+forEach(findFiles("scripts", endsWith(".js")), handle);
 
 for (const pkg of packages) {
   const root = `packages/${pkg}`;
 
   clean(root);
 
-  forEach(findFiles(`${root}/scripts`, endsWith(".js")), handle());
+  forEach(findFiles(`${root}/scripts`, endsWith(".js")), handle);
 
-  forEach(
-    findFiles(root, endsWith(".ts", ".tsx")),
-    handle(
-      // Construct a project for use solely within the current scope, ensuring
-      // that allocated resources can be freed as soon as the package has been
-      // handled.
-      new Project(`${root}/tsconfig.json`)
-    )
-  );
+  forEach(findFiles(root, endsWith(".ts", ".tsx")), handle);
 }
 
-forEach(findFiles("docs", endsWith(".ts", ".tsx")), handle());
+forEach(findFiles("docs", endsWith(".ts", ".tsx")), handle);
