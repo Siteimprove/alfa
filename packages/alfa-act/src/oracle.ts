f8bcc0b3b57c41f5a162517a156db9f964b02b3b
Kasper Isager <kasperisager@gmail.com>
2019-05-16 12:24:15 +0200

    Rework default oracle

--- packages/alfa-act/src/oracle.ts
+++ packages/alfa-act/src/oracle.ts
@@ -43,37 +43,60 @@ function findAnswer<A extends Aspect, T extends Target, Q extends QuestionType>(
   question: Question<A, T, Q>
 ): Answer<A, T, Q> | null {
   for (const answer of answers) {
-    if (
-      answer.type !== question.type ||
-      answer.id !== question.id ||
-      answer.aspect !== question.aspect ||
-      answer.target !== question.target
-    ) {
-      continue;
-    }
-
-    if (answer.rule === undefined) {
-      if (question.scope === QuestionScope.Global) {
-        return answer as Answer<A, T, Q>;
-      }
-    } else {
-      const haystack = isIterable(answer.rule)
-        ? [...answer.rule]
-        : [answer.rule];
-
-      const needle = isIterable(question.rule)
-        ? [...question.rule]
-        : [question.rule];
-
-      if (
-        haystack.some(haystack =>
-          needle.some(needle => haystack.id === needle.id)
-        )
-      ) {
-        return answer as Answer<A, T, Q>;
-      }
+    if (isAnswerTo(question, answer)) {
+      return answer;
     }
   }
 
   return null;
+}
+
+function isAnswerTo<A extends Aspect, T extends Target, Q extends QuestionType>(
+  question: Question<A, T, Q>,
+  answer: Answer<A, T>
+): answer is Answer<A, T, Q> {
+  if (
+    answer.type !== question.type ||
+    answer.id !== question.id ||
+    answer.aspect !== question.aspect
+  ) {
+    return false;
+  }
+
+  if (isIterable(answer.target) && isIterable(question.target)) {
+    const haystack = new Set(answer.target);
+    const needle = new Set(question.target);
+
+    if (haystack.size !== needle.size) {
+      return false;
+    }
+
+    for (const target of haystack) {
+      if (!needle.has(target)) {
+        return false;
+      }
+    }
+  } else if (answer.target !== question.target) {
+    return false;
+  }
+
+  if (answer.rule === undefined) {
+    return question.scope === QuestionScope.Global;
+  }
+
+  const haystack = new Set(
+    isIterable(answer.rule) ? answer.rule : [answer.rule]
+  );
+
+  const needle = new Set(
+    isIterable(question.rule) ? question.rule : [question.rule]
+  );
+
+  for (const rule of haystack) {
+    if (!needle.has(rule)) {
+      return false;
+    }
+  }
+
+  return true;
 }
