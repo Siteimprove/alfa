7223b15dd9e4be1fa5e3627084543c65b0b1f85b
Kasper Isager <kasperisager@gmail.com>
2019-05-23 09:25:13 +0200

    Progress on React converter

--- packages/alfa-react/src/from-react-element.ts
+++ packages/alfa-react/src/from-react-element.ts
@@ -1,7 +1,15 @@
-import { Attribute, Element, NodeType, Text } from "@siteimprove/alfa-dom";
-import { keys } from "@siteimprove/alfa-util";
+import {
+  Attribute,
+  Element,
+  Node,
+  NodeType,
+  Text
+} from "@siteimprove/alfa-dom";
+import { hasKey, isObject } from "@siteimprove/alfa-util";
 import { ReactElement } from "react";
 import * as TestRenderer from "react-test-renderer";
+
+const { keys } = Object;
 
 export function fromReactElement<T>(reactElement: ReactElement<T>): Element {
   const tree = TestRenderer.create(reactElement).toJSON();
@@ -10,36 +18,34 @@ export function fromReactElement<T>(reactElement: ReactElement<T>): Element {
     throw new Error("Could not render React element");
   }
 
-  return convertTree(tree);
+  return asElement(tree);
 }
 
-function convertTree(tree: TestRenderer.ReactTestRendererJSON): Element {
-  const { type: localName, props, children } = tree;
+type TestNode = TestElement | string;
 
-  const attributes: Array<Attribute> = keys(props).map(localName => {
-    return {
-      nodeType: NodeType.Attribute,
-      prefix: null,
-      localName: String(localName),
-      value: props[localName],
-      childNodes: []
-    };
-  });
+type TestElement = TestRenderer.ReactTestRendererJSON;
 
-  const childNodes: Array<Element | Text> =
-    children === null
-      ? []
-      : children.map(child => {
-          if (typeof child === "string") {
-            return {
-              nodeType: NodeType.Text,
-              data: child,
-              childNodes: []
-            };
-          }
+function asNode(node: TestNode): Element | Text {
+  return typeof node === "string" ? asText(node) : asElement(node);
+}
 
-          return convertTree(child);
-        });
+function asElement(element: TestElement): Element {
+  const { type: localName, props, children } = element;
+
+  const attributes = keys(props).reduce<Array<Attribute>>(
+    (attributes, prop) => {
+      const attribute = asAttribute(prop, props[prop]);
+
+      if (attribute !== null) {
+        attributes.push(attribute);
+      }
+
+      return attributes;
+    },
+    []
+  );
+
+  const childNodes: Array<Node> = children === null ? [] : children.map(asNode);
 
   return {
     nodeType: NodeType.Element,
@@ -49,4 +55,89 @@ function convertTree(tree: TestRenderer.ReactTestRendererJSON): Element {
     shadowRoot: null,
     childNodes
   };
+}
+
+function asAttribute(localName: string, value: unknown): Attribute | null {
+  switch (value) {
+    // Attributes that are either `null` or `undefined` are always ignored.
+    case null:
+    case undefined:
+      return null;
+  }
+
+  localName = asAttributeName(localName);
+  value = asAttributeValue(localName, value);
+
+  if (typeof value !== "string") {
+    return null;
+  }
+
+  return {
+    nodeType: NodeType.Attribute,
+    prefix: null,
+    localName,
+    value,
+    childNodes: []
+  };
+}
+
+function asAttributeName(localName: string): string {
+  switch (localName) {
+    case "className":
+      return "class";
+
+    case "htmlFor":
+      return "for";
+  }
+
+  return localName;
+}
+
+function asAttributeValue(localName: string, value: unknown): string | null {
+  switch (localName) {
+    case "style":
+      if (isObject(value) && value !== null) {
+        return asInlineStyle(value);
+      }
+  }
+
+  if (localName.startsWith("aria-") && typeof value === "boolean") {
+    return String(value);
+  }
+
+  switch (value) {
+    case false:
+      return null;
+
+    case true:
+      return localName;
+  }
+
+  return String(value);
+}
+
+function asText(data: string): Text {
+  return {
+    nodeType: NodeType.Text,
+    data,
+    childNodes: []
+  };
+}
+
+function asInlineStyle(props: object): string {
+  let style = "";
+  let delimiter = "";
+
+  for (const prop of keys(props)) {
+    if (hasKey(props, prop)) {
+      style += prop.replace(/([A-Z])/g, "-$1").toLowerCase();
+      style += ":";
+      style += String(props[prop]);
+      style += delimiter;
+
+      delimiter = ";";
+    }
+  }
+
+  return style;
 }
