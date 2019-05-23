68eb38377ae7fda7ec12845fcbc85f1f2f08ef86
Kasper Isager <kasperisager@gmail.com>
2019-05-23 09:35:37 +0200

    Remove `describe()`

--- docs/examples/component-testing/angular/components/button.spec.ts
+++ docs/examples/component-testing/angular/components/button.spec.ts
@@ -3,21 +3,19 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ButtonComponent } from "./button";
 
-describe("ButtonComponent", () => {
-  let fixture: ComponentFixture<ButtonComponent>;
+let fixture: ComponentFixture<ButtonComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ButtonComponent]
-    }).compileComponents();
-  });
+beforeEach(async () => {
+  await TestBed.configureTestingModule({
+    declarations: [ButtonComponent]
+  }).compileComponents();
+});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ButtonComponent);
-    fixture.detectChanges();
-  });
+beforeEach(() => {
+  fixture = TestBed.createComponent(ButtonComponent);
+  fixture.detectChanges();
+});
 
-  it("should be accessible", () => {
-    expect(fixture).toBeAccessible();
-  });
+it("should be accessible", () => {
+  expect(fixture).toBeAccessible();
 });
