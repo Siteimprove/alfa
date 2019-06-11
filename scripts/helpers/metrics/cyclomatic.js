99b2a6c99375f6a4a846d4471c122f87444c3cc0
Búgvi Benjamin Magnussen <bbm@siteimprove.com>
2019-06-11 12:41:29 +0200

    Small refator in the metrics

--- scripts/helpers/metrics/cyclomatic.js
+++ scripts/helpers/metrics/cyclomatic.js
@@ -1,4 +1,5 @@
 const TypeScript = require("typescript");
+const { parseSource } = require("../typescript");
 
 /**
  * @typedef {import("../coverage").Script} Script
@@ -13,16 +14,6 @@ const TypeScript = require("typescript");
  */
 
 /**
- * @param {String} source
- */
-const createSource = source =>
-  TypeScript.createSourceFile(
-    "anon.ts",
-    source,
-    TypeScript.ScriptTarget.ES2015
-  );
-
-/**
  * @param {Script} script
  */
 function totalOperations(script) {
@@ -30,11 +21,11 @@ function totalOperations(script) {
   if (script.sources.length > 1) {
     // typescript
     for (let i = 1, n = script.sources.length; i < n; i++) {
-      total += visit(createSource(script.sources[i].content));
+      total += visit(parseSource(script.sources[i].content));
     }
   } else {
     // javascript
-    total += visit(createSource(script.sources[0].content));
+    total += visit(parseSource(script.sources[0].content));
   }
 
   return total;
@@ -88,7 +79,7 @@ const Cyclomatic = {
         }
 
         uncovered += visit(
-          createSource(
+          parseSource(
             file.content.substring(
               block.range.start.offset,
               block.range.end.offset
@@ -121,7 +112,7 @@ const Cyclomatic = {
     }
 
     const uncovered = visit(
-      createSource(
+      parseSource(
         file.content.substring(block.range.start.offset, block.range.end.offset)
       )
     );
--- scripts/helpers/metrics/logical.js
+++ scripts/helpers/metrics/logical.js
@@ -1,4 +1,5 @@
 const TypeScript = require("typescript");
+const { parseSource } = require("../typescript");
 
 /**
  * @typedef {import("../coverage").Script} Script
@@ -13,16 +14,6 @@ const TypeScript = require("typescript");
  */
 
 /**
- * @param {String} source
- */
-const createSource = source =>
-  TypeScript.createSourceFile(
-    "anon.ts",
-    source,
-    TypeScript.ScriptTarget.ES2015
-  );
-
-/**
  * @param {Script} script
  */
 function totalOperations(script) {
@@ -30,11 +21,11 @@ function totalOperations(script) {
   if (script.sources.length > 1) {
     // typescript
     for (let i = 1, n = script.sources.length; i < n; i++) {
-      total += visit(createSource(script.sources[i].content));
+      total += visit(parseSource(script.sources[i].content));
     }
   } else {
     // javascript
-    total += visit(createSource(script.sources[0].content));
+    total += visit(parseSource(script.sources[0].content));
   }
 
   return total;
@@ -91,7 +82,7 @@ const Logical = {
         }
 
         uncovered += visit(
-          createSource(
+          parseSource(
             file.content.substring(
               block.range.start.offset,
               block.range.end.offset
@@ -124,7 +115,7 @@ const Logical = {
     }
 
     const uncovered = visit(
-      createSource(
+      parseSource(
         file.content.substring(block.range.start.offset, block.range.end.offset)
       )
     );
