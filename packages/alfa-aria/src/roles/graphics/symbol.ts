3e7af981c3d9b60e5f095acd82380a2046ece199
Búgvi Benjamin Magnussen <bugvibenjamin@magnussen.email>
2019-06-13 18:34:11 +0200

    Update symbol.ts

--- packages/alfa-aria/src/roles/graphics/symbol.ts
+++ packages/alfa-aria/src/roles/graphics/symbol.ts
@@ -5,7 +5,7 @@ import { Img } from "../structure/img";
  * @see https://www.w3.org/TR/graphics-aria/#graphics-symbol
  */
 export const GraphicsSymbol: Role = {
-  name: "graphicssymbol",
+  name: "graphics-symbol",
   category: Category.Graphics,
   inherits: () => [Img],
   label: { from: ["author"], required: true }
