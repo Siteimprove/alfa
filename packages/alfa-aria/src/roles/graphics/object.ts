e04fd9f3c1be37dd50c2bdd2546499d99f96fbfe
Búgvi Benjamin Magnussen <bugvibenjamin@magnussen.email>
2019-06-13 18:34:33 +0200

    Update object.ts

--- packages/alfa-aria/src/roles/graphics/object.ts
+++ packages/alfa-aria/src/roles/graphics/object.ts
@@ -5,7 +5,7 @@ import { Group } from "../structure/group";
  * @see https://www.w3.org/TR/graphics-aria/#graphics-object
  */
 export const GraphicsObject: Role = {
-  name: "graphicsobject",
+  name: "graphics-object",
   category: Category.Graphics,
   inherits: () => [Group],
   label: { from: ["author", "contents"] }
