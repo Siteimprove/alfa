2d1ad9f580ae860c401d573c3d76d46ce8db4c02
Búgvi Benjamin Magnussen <bugvibenjamin@magnussen.email>
2019-06-13 18:34:52 +0200

    Update document.ts

--- packages/alfa-aria/src/roles/graphics/document.ts
+++ packages/alfa-aria/src/roles/graphics/document.ts
@@ -5,7 +5,7 @@ import { Document } from "../structure/document";
  * @see https://www.w3.org/TR/graphics-aria/#graphics-document
  */
 export const GraphicsDocument: Role = {
-  name: "graphicsdocument",
+  name: "graphics-document",
   category: Category.Graphics,
   inherits: () => [Document],
   label: { from: ["author"], required: true }
