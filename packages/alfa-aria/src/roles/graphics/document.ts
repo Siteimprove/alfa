9a28757f0145d150354076f9d2bb6e71d5254eb6
Búgvi Benjamin Magnussen <bugvibenjamin@magnussen.email>
2019-06-17 09:45:03 +0200

    Add ARIA graphics roles (#126)
    Relates to #112

---
+++ packages/alfa-aria/src/roles/graphics/document.ts
@@ -0,0 +1,12 @@
+import { Category, Role } from "../../types";
+import { Document } from "../structure/document";
+
+/**
+ * @see https://www.w3.org/TR/graphics-aria/#graphics-document
+ */
+export const GraphicsDocument: Role = {
+  name: "graphics-document",
+  category: Category.Graphics,
+  inherits: () => [Document],
+  label: { from: ["author"], required: true }
+};
