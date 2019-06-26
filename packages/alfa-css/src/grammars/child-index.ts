0bec4081899bb001ff32db510b8c26c5b33f870c
Niclas Hedam <niclas@hed.am>
2019-06-26 15:49:46 +0200

    WIP on child index grammar

--- packages/alfa-css/src/grammars/child-index.ts
+++ packages/alfa-css/src/grammars/child-index.ts
@@ -1,32 +1,69 @@
 import * as Lang from "@siteimprove/alfa-lang";
-import { Grammar, Stream } from "@siteimprove/alfa-lang";
+// import { Grammar, Stream } from "@siteimprove/alfa-lang";
 import { Token, Tokens, TokenType } from "../alphabet";
 import { ChildIndex } from "../types";
 
 namespace ChildIndex {
   export const enum TokenType {
-    NDimension
+    NDimension,
+    NIdent,
+    NNumber
   }
 
   export namespace Tokens {
     interface Token<T extends TokenType> extends Lang.Token<T> {}
 
     export interface NDimension extends Token<TokenType.NDimension> {
-      // What do we need?
+      readonly unit: string;
+      readonly value: number;
+    }
+
+    export interface NIdent extends Token<TokenType.NIdent> {
+      readonly value: string;
+    }
+
+    export interface NNumber extends Token<TokenType.NNumber> {
+      readonly value: number;
     }
   }
 
-  export type Token = Tokens.NDimension;
+  export type Token = Tokens.NDimension | Tokens.NIdent | Tokens.NNumber;
 }
 
 function fromToken(token: Token): ChildIndex.Token | null {
+  console.log(token);
   switch (token.type) {
     case TokenType.Dimension:
       if (token.integer === true && token.unit.toLowerCase() === "n") {
         return {
+          type: ChildIndex.TokenType.NDimension,
+          unit: "n",
+          value: token.value
+        };
+      } else if (token.unit.toLowerCase().startsWith("n")) {
+        return {
+          value: token.value,
+          unit: token.unit.toLowerCase(),
+          type: ChildIndex.TokenType.NDimension
+        };
+      } else if (token.unit.toLowerCase().startsWith("-n")) {
+        return {
+          value: token.value,
+          unit: token.unit.toLowerCase(),
           type: ChildIndex.TokenType.NDimension
         };
       }
+      break;
+    case TokenType.Ident:
+      return {
+        value: token.value,
+        type: ChildIndex.TokenType.NIdent
+      };
+    case TokenType.Number:
+      return {
+        value: token.value,
+        type: ChildIndex.TokenType.NNumber
+      };
   }
 
   return null;
@@ -35,15 +72,108 @@ function fromToken(token: Token): ChildIndex.Token | null {
 type Production<T extends Token> = Lang.Production<Token, ChildIndex, T>;
 
 const dimension: Production<Tokens.Dimension> = {
-  token: TokenType.Dimension
+  token: TokenType.Dimension,
+  prefix(token, stream) {
+    let a = 0;
+    let b = 0;
+
+    const childToken = fromToken(token);
+
+    if (
+      childToken === null ||
+      childToken.type !== ChildIndex.TokenType.NDimension
+    ) {
+      return null;
+    }
+
+    a = childToken.value;
+
+    if (childToken.unit !== "n") {
+      const offset = childToken.unit.startsWith("-") ? 2 : 1;
+      b = Number.parseInt(childToken.unit.substring(offset));
+    }
+
+    return { a, b };
+  },
+  infix(token, stream, expression, left) {
+    return null;
+  }
 };
 
 const ident: Production<Tokens.Ident> = {
-  token: TokenType.Ident
+  token: TokenType.Ident,
+  prefix(token, stream) {
+    const childToken = fromToken(token);
+
+    if (
+      childToken === null ||
+      childToken.type !== ChildIndex.TokenType.NIdent
+    ) {
+      return null;
+    }
+
+    switch (childToken.value) {
+      case "n":
+        return {
+          a: 1,
+          b: 0
+        };
+      case "-n":
+        return {
+          a: -1,
+          b: 0
+        };
+      case "even":
+        return {
+          a: 2,
+          b: 0
+        };
+      case "odd":
+        return {
+          a: 2,
+          b: 1
+        };
+    }
+
+    return null;
+  },
+  infix(token, stream, expression, left) {
+    return null;
+  }
 };
 
 const number: Production<Tokens.Number> = {
-  token: TokenType.Number
+  token: TokenType.Number,
+  prefix(token, stream) {
+    const childToken = fromToken(token);
+
+    if (
+      childToken === null ||
+      childToken.type !== ChildIndex.TokenType.NDimension
+    ) {
+      return null;
+    }
+
+    return {
+      a: childToken.value,
+      b: 0
+    };
+  },
+  infix(token, stream, expression, left) {
+    const childToken = fromToken(token);
+
+    if (
+      childToken === null ||
+      childToken.type !== ChildIndex.TokenType.NNumber
+    ) {
+      return null;
+    }
+
+    return {
+      a: left.a,
+      b: childToken.value
+    };
+  }
 };
 
 export const ChildIndexGrammar: Lang.Grammar<
---
+++ packages/alfa-css/test/grammars/child-index.spec.ts
@@ -0,0 +1,57 @@
+import { lex, parse } from "@siteimprove/alfa-lang";
+import { Assertions, test } from "@siteimprove/alfa-test";
+import { Alphabet } from "../../src/alphabet";
+import { ChildIndexGrammar } from "../../src/grammars/child-index";
+import { ChildIndex } from "../../src/types";
+
+function childIndex(t: Assertions, input: string, expected: ChildIndex | null) {
+  const lexer = lex(input, Alphabet);
+  const parser = parse(lexer.result, ChildIndexGrammar);
+
+  t.deepEqual(parser.result, expected, input);
+}
+
+test("Can parse a n-dimension signless-integer child index", t => {
+  childIndex(t, "2n+3", {
+    a: 2,
+    b: 3
+  });
+});
+
+test("Can parse a n-dimension child index", t => {
+  childIndex(t, "n", {
+    a: 1,
+    b: 0
+  });
+
+  childIndex(t, "-n", {
+    a: -1,
+    b: 0
+  });
+});
+
+test("Can parse a even odd child index", t => {
+  childIndex(t, "even", {
+    a: 2,
+    b: 0
+  });
+
+  childIndex(t, "odd", {
+    a: 2,
+    b: 1
+  });
+});
+
+test("Can parse a ndashdigit-dimension signed-integer child index", t => {
+  childIndex(t, "-2n-3", {
+    a: -2,
+    b: -3
+  });
+});
+
+test("Can parse a ndashdigit-dimension singless-integer child index", t => {
+  childIndex(t, "-2n3", {
+    a: -2,
+    b: 3
+  });
+});
