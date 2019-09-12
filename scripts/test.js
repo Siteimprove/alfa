d00a8213832030554ca9bc0e20cdf5fb8a62d7fe
Jean-Yves Moyen <jym@siteimprove.com>
2019-09-12 14:19:01 +0200

    Accept paths as input on top of packages names

--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,5 @@
 const { findFiles } = require("./helpers/file-system");
+const fs = require("fs");
 const { endsWith } = require("./helpers/predicates");
 let { packages } = require("./helpers/meta");
 const notify = require("./helpers/notify");
@@ -11,7 +12,7 @@ if (process.argv.length > 2) {
 }
 
 for (const pkg of packages) {
-  const root = `packages/${pkg.replace("packages/", "")}/test`;
+  const root = fs.existsSync(pkg) ? pkg : `packages/${pkg}/test`;
   const files = findFiles(root, endsWith(".spec.ts", ".spec.tsx"));
 
   for (const file of files) {
