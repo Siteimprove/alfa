cef8543397c0f96aab6ba3ac11b3c8737c0b4bf9
Jean-Yves Moyen <jym@siteimprove.com>
2019-09-12 13:53:04 +0200

    Allow CLI selection off packages to test

--- scripts/test.js
+++ scripts/test.js
@@ -1,10 +1,14 @@
 const { findFiles } = require("./helpers/file-system");
 const { endsWith } = require("./helpers/predicates");
-const { packages } = require("./helpers/meta");
+let { packages } = require("./helpers/meta");
 const notify = require("./helpers/notify");
 
 const { build } = require("./tasks/build");
 const { test } = require("./tasks/test");
+
+if (process.argv.length > 2) {
+  packages = process.argv.slice(2);
+}
 
 for (const pkg of packages) {
   const root = `packages/${pkg}/test`;
