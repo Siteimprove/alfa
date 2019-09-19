7f4e11530552e23812a1f53bd5c64783e086836b
Kasper Isager <kasperisager@gmail.com>
2019-09-19 13:44:45 +0200

    Add support for nested document positions

--- packages/alfa-dom/test/get-document-position.spec.tsx
+++ packages/alfa-dom/test/get-document-position.spec.tsx
@@ -2,61 +2,49 @@ import { test } from "@siteimprove/alfa-test";
 import { jsx } from "../jsx";
 import { getDocumentPosition } from "../src/get-document-position";
 
-test("Returns 3 when element has position 3 with naive traversal", t => {
-  const strong = <strong />;
-  const span = <span>{strong}</span>;
-  const div = (
-    <div>
-      <p />
-      {span}
-    </div>
-  );
-  t.equal(getDocumentPosition(strong, div), 3);
+const p1 = <p>foo</p>;
+const p2 = <p>bar</p>;
+const p3 = <p>baz</p>;
+const p4 = <p>qux</p>;
+
+const shadow = (
+  <shadow>
+    <slot />
+    {p2}
+  </shadow>
+);
+
+const iframe = (
+  <iframe>
+    <content>{p3}</content>
+  </iframe>
+);
+
+const div = (
+  <div id="host">
+    {shadow}
+    {p1}
+    {iframe}
+    {p4}
+  </div>
+);
+
+test("Returns the document position of an element", t => {
+  t.equal(getDocumentPosition(p1, div), 1);
 });
 
-test("Returns null when element is not in context", t => {
-  const strong = <strong />;
-  const span = <span />;
-  const div = <div>{span}</div>;
-  t.equal(getDocumentPosition(strong, div), null);
-});
-
-test("Returns 3 when element has position 3 with composed traversal", t => {
-  const p1 = <p>foo</p>;
-  const p2 = <p>bar</p>;
-  const slot = <slot />;
-  const shadow = (
-    <shadow>
-      {slot}
-      {p2}
-    </shadow>
-  );
-  const div = (
-    <div id="host">
-      {shadow}
-      {p1}
-    </div>
-  );
-
+test("Returns the composed document position of an element", t => {
   t.equal(getDocumentPosition(p2, div, { composed: true }), 3);
 });
 
-test("Returns 1 when element has position 1 with flattened traversal", t => {
-  const p1 = <p>foo</p>;
-  const p2 = <p>bar</p>;
-  const slot = <slot />;
-  const shadow = (
-    <shadow>
-      {slot}
-      {p2}
-    </shadow>
-  );
-  const div = (
-    <div id="host">
-      {shadow}
-      {p1}
-    </div>
-  );
-
+test("Returns the flattened document position of an element", t => {
   t.equal(getDocumentPosition(p1, div, { flattened: true }), 1);
+});
+
+test("Returns the nested document position of an element", t => {
+  t.equal(getDocumentPosition(p4, div, { nested: true }), 7);
+});
+
+test("Returns null when an element is not in a context", t => {
+  t.equal(getDocumentPosition(p2, div), null);
 });
