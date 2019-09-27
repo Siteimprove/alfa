d92cb0018f7fa2c29ee90b3621367a9bf6c48cd4
Kasper Isager <kasperisager@gmail.com>
2019-09-27 13:22:31 +0200

    Use single, frozen default device

--- packages/alfa-device/src/get-default-device.ts
+++ packages/alfa-device/src/get-default-device.ts
@@ -1,15 +1,19 @@
 import { Device, DeviceType, Orientation } from "./types";
 
+const { freeze } = Object;
+
+const defaultDevice: Device = freeze({
+  type: DeviceType.Screen,
+  viewport: {
+    width: 1280,
+    height: 720,
+    orientation: Orientation.Landscape
+  },
+  display: {
+    resolution: 1
+  }
+});
+
 export function getDefaultDevice(): Device {
-  return {
-    type: DeviceType.Screen,
-    viewport: {
-      width: 1280,
-      height: 720,
-      orientation: Orientation.Landscape
-    },
-    display: {
-      resolution: 1
-    }
-  };
+  return defaultDevice;
 }
