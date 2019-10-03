96352d2ab07c50988cd1f197a8842fc6264a10bd
Jean-Yves Moyen <jym@siteimprove.com>
2019-10-03 10:52:16 +0200

    Move Browser Specific binOp up one level

--- packages/alfa-compatibility/src/binary-operation.ts
+++ packages/alfa-compatibility/src/binary-operation.ts
@@ -1,1 +1,16 @@
-export * from "./binary-operation/binary-operation";
+import { BrowserSpecific } from "./browser-specific";
+
+// generic builder
+export function binOp<T, U, V>(
+  op: (t: T, u: U) => V
+): (
+  t: T | BrowserSpecific<T>,
+  u: U | BrowserSpecific<U>
+) => V | BrowserSpecific<V> {
+  return (t, u) =>
+    BrowserSpecific.map(t, x => BrowserSpecific.map(u, y => op(x, y)));
+}
+
+// Frequently used operations
+export const and = binOp((x: boolean, y: boolean) => x && y);
+export const or = binOp((x: boolean, y: boolean) => x || y);
