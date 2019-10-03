362252ecf49b368c20d1156489bfaf720bdfac19
Jean-Yves Moyen <jym@siteimprove.com>
2019-10-03 11:42:38 +0200

    Use predicate builder

--- packages/alfa-rules/src/sia-r8/rule.ts
+++ packages/alfa-rules/src/sia-r8/rule.ts
@@ -6,11 +6,10 @@ import { Device } from "@siteimprove/alfa-device";
 import {
   Document,
   Element,
-  isElement,
   Namespace,
-  Node,
   querySelectorAll
 } from "@siteimprove/alfa-dom";
+import { isElement } from "../helpers/predicate-builder";
 
 const {
   map,
@@ -27,16 +26,30 @@ export const SIA_R8: Atomic.Rule<Device | Document, Element> = {
       applicability: () => {
         return map(
           filter(
-            querySelectorAll<Element>(document, document, isElement, {
+            querySelectorAll<Element>(document, document, isElement(), {
               flattened: true
             }),
-            element => {
-              return map(
-                isFormField(element, document, device),
-                isFormField =>
-                  isFormField && isExposed(element, document, device)
-              );
-            }
+            element =>
+              isElement(builder =>
+                builder
+                  .withNamespace(document, Namespace.HTML)
+                  .withRole(
+                    device,
+                    document,
+                    Roles.Checkbox,
+                    Roles.Combobox,
+                    Roles.ListBox,
+                    Roles.MenuItemCheckbox,
+                    Roles.MenuItemRadio,
+                    Roles.Radio,
+                    Roles.SearchBox,
+                    Roles.Slider,
+                    Roles.SpinButton,
+                    Roles.Switch,
+                    Roles.TextBox
+                  )
+                  .and(element => isExposed(element, document, device))
+              )(element)
           ),
           elements => {
             return Seq(elements).map(element => {
@@ -63,32 +76,3 @@ export const SIA_R8: Atomic.Rule<Device | Document, Element> = {
     };
   }
 };
-
-function isFormField(
-  element: Element,
-  context: Node,
-  device: Device
-): boolean | BrowserSpecific<boolean> {
-  if (getElementNamespace(element, context) !== Namespace.HTML) {
-    return false;
-  }
-
-  return map(getRole(element, context, device), role => {
-    switch (role) {
-      case Roles.Checkbox:
-      case Roles.Combobox:
-      case Roles.ListBox:
-      case Roles.MenuItemCheckbox:
-      case Roles.MenuItemRadio:
-      case Roles.Radio:
-      case Roles.SearchBox:
-      case Roles.Slider:
-      case Roles.SpinButton:
-      case Roles.Switch:
-      case Roles.TextBox:
-        return true;
-    }
-
-    return false;
-  });
-}
