f6bdbc7359c05086f34a25f16f9a124a6a300366
Kasper Isager <kasperisager@gmail.com>
2019-10-08 10:40:28 +0200

    Move some functions around

--- packages/alfa-compatibility/src/predicate.ts
+++ packages/alfa-compatibility/src/predicate.ts
@@ -9,6 +9,20 @@ export type Predicate<T, P extends Predicate.Value = boolean> = (
 export namespace Predicate {
   export type Value = BrowserSpecific.Maybe<boolean>;
 
+  export type Chain<
+    T,
+    U extends T = T,
+    P extends Predicate.Value = boolean,
+    O extends "and" | "or" = never
+  > = Omit<Builder<T, U, P>, Exclude<"and" | "or", O>>;
+
+  export type Chainable<
+    T,
+    U extends T = T,
+    P extends Predicate.Value = boolean,
+    O extends "and" | "or" = never
+  > = Predicate<T, P> | Chain<T, U, P, O>;
+
   export class Builder<
     T,
     U extends T = T,
@@ -77,60 +91,46 @@ export namespace Predicate {
     }
   }
 
-  export type Chain<
-    T,
-    U extends T = T,
-    P extends Predicate.Value = boolean,
-    O extends "and" | "or" = never
-  > = Omit<Builder<T, U, P>, Exclude<"and" | "or", O>>;
+  export function not<T, U extends T>(
+    predicate: Predicate.Chainable<T, U>
+  ): Predicate.Builder<T, U>;
 
-  export type Chainable<
-    T,
-    U extends T = T,
-    P extends Predicate.Value = boolean,
-    O extends "and" | "or" = never
-  > = Predicate<T, P> | Chain<T, U, P, O>;
+  export function not<T, U extends T, P extends Predicate.Value>(
+    predicate: Predicate.Chainable<T, U, P>
+  ): Predicate.Builder<T, U, P>;
+
+  export function not<T, U extends T, P extends Predicate.Value>(
+    predicate: Predicate.Chainable<T, U, P>
+  ): Predicate.Builder<T, U, P> {
+    return Predicate.Builder.of(item => {
+      if (typeof predicate === "function") {
+        predicate = Predicate.Builder.of(predicate);
+      }
+
+      return predicate.test<P>(item, () => false, () => true);
+    });
+  }
+
+  export function test<T, U extends T>(
+    item: T,
+    predicate: Predicate.Chainable<T, U>
+  ): item is U;
+
+  export function test<T, U extends T, P extends Predicate.Value>(
+    item: T,
+    predicate: Predicate.Chainable<T, U, P>
+  ): boolean | P;
+
+  export function test<T, U extends T, P extends Predicate.Value>(
+    item: T,
+    predicate: Predicate.Chainable<T, U, P>
+  ): boolean | P {
+    return typeof predicate === "function"
+      ? predicate(item)
+      : predicate.test(item);
+  }
 }
 
 function is<T, U extends T>(item: T, ok: boolean): item is U {
   return ok;
-}
-
-export function not<T, U extends T>(
-  predicate: Predicate.Chainable<T, U>
-): Predicate.Builder<T, U>;
-
-export function not<T, U extends T, P extends Predicate.Value>(
-  predicate: Predicate.Chainable<T, U, P>
-): Predicate.Builder<T, U, P>;
-
-export function not<T, U extends T, P extends Predicate.Value>(
-  predicate: Predicate.Chainable<T, U, P>
-): Predicate.Builder<T, U, P> {
-  return Predicate.Builder.of(item => {
-    if (typeof predicate === "function") {
-      predicate = Predicate.Builder.of(predicate);
-    }
-
-    return predicate.test<P>(item, () => false, () => true);
-  });
-}
-
-export function test<T, U extends T>(
-  item: T,
-  predicate: Predicate.Chainable<T, U>
-): item is U;
-
-export function test<T, U extends T, P extends Predicate.Value>(
-  item: T,
-  predicate: Predicate.Chainable<T, U, P>
-): boolean | P;
-
-export function test<T, U extends T, P extends Predicate.Value>(
-  item: T,
-  predicate: Predicate.Chainable<T, U, P>
-): boolean | P {
-  return typeof predicate === "function"
-    ? predicate(item)
-    : predicate.test(item);
 }
