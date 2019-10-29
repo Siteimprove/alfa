6b4e7a86a43e12ab9574c14dc13f26f867b1a75b
Kasper Isager <kasperisager@gmail.com>
2019-10-29 10:04:39 +0100

    Add additional `Generator` and `Iterable` functions

--- packages/alfa-iterable/src/iterable.ts
+++ packages/alfa-iterable/src/iterable.ts
@@ -1,7 +1,13 @@
+import { Equality } from "@siteimprove/alfa-compare";
+import { Mapper } from "@siteimprove/alfa-mapper";
+import { None, Option, Some } from "@siteimprove/alfa-option";
+import { Predicate } from "@siteimprove/alfa-predicate";
+import { Reducer } from "@siteimprove/alfa-reducer";
+
 export namespace Iterable {
   export function* map<T, U>(
     iterable: Iterable<T>,
-    mapper: (value: T) => U
+    mapper: Mapper<T, U>
   ): Iterable<U> {
     for (const value of iterable) {
       yield mapper(value);
@@ -10,11 +16,71 @@ export namespace Iterable {
 
   export function* flatMap<T, U>(
     iterable: Iterable<T>,
-    mapper: (value: T) => Iterable<U>
+    mapper: Mapper<T, Iterable<U>>
   ): Iterable<U> {
     for (const value of iterable) {
       yield* mapper(value);
     }
+  }
+
+  export function reduce<T, U>(
+    iterable: Iterable<T>,
+    reducer: Reducer<T, U>,
+    accumulator: U
+  ): U {
+    for (const value of iterable) {
+      accumulator = reducer(accumulator, value);
+    }
+
+    return accumulator;
+  }
+
+  export function includes<T>(iterable: Iterable<T>, value: T): boolean {
+    for (const found of iterable) {
+      if (Equality.equals(value, found)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  export function find<T, U extends T>(
+    iterable: Iterable<T>,
+    predicate: Predicate<T, U>
+  ): Option<U> {
+    for (const value of iterable) {
+      if (Predicate.test(predicate, value)) {
+        return Some.of(value);
+      }
+    }
+
+    return None;
+  }
+
+  export function* filter<T, U extends T>(
+    iterable: Iterable<T>,
+    predicate: Predicate<T, U>
+  ): Iterable<U> {
+    for (const value of iterable) {
+      if (Predicate.test(predicate, value)) {
+        yield value;
+      }
+    }
+  }
+
+  export function subtract<T>(
+    left: Iterable<T>,
+    right: Iterable<T>
+  ): Iterable<T> {
+    return filter(left, left => !includes(right, left));
+  }
+
+  export function intersect<T>(
+    left: Iterable<T>,
+    right: Iterable<T>
+  ): Iterable<T> {
+    return filter(left, left => includes(right, left));
   }
 
   export function isIterable<T>(value: unknown): value is Iterable<T> {
