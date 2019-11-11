6dcbd87b4382045eb9cb29541d432d2c9e40dfda
Kasper Isager <kasperisager@gmail.com>
2019-11-11 21:58:04 +0100

    Add type guards for HTTP types

--- packages/alfa-http/src/types.ts
+++ packages/alfa-http/src/types.ts
@@ -1,3 +1,5 @@
+import { isNumber, isObject, isString } from "@siteimprove/alfa-guards";
+
 /**
  * @see https://fetch.spec.whatwg.org/#headers-class
  */
@@ -6,6 +8,12 @@ export interface Headers {
    * @see https://fetch.spec.whatwg.org/#dom-headers-get
    */
   [name: string]: string | undefined;
+}
+
+export namespace Headers {
+  export function isHeaders(value: unknown): value is Headers {
+    return isObject(value);
+  }
 }
 
 /**
@@ -28,6 +36,17 @@ export interface Request {
   readonly headers: Headers;
 }
 
+export namespace Request {
+  export function isRequest(value: unknown): value is Request {
+    return (
+      isObject(value) &&
+      isString(value.method) &&
+      isString(value.url) &&
+      Headers.isHeaders(value.headers)
+    );
+  }
+}
+
 /**
  * @see https://fetch.spec.whatwg.org/#body-mixin
  */
@@ -40,6 +59,12 @@ export interface Body {
    * @see https://fetch.spec.whatwg.org/#dom-body-body
    */
   readonly body: ArrayBuffer;
+}
+
+export namespace Body {
+  export function isBody(value: unknown): value is Body {
+    return isObject(value) && value.body instanceof ArrayBuffer;
+  }
 }
 
 /**
@@ -55,4 +80,14 @@ export interface Response extends Body {
    * @see https://fetch.spec.whatwg.org/#dom-response-headers
    */
   readonly headers: Headers;
+}
+
+export namespace Response {
+  export function isResponse(value: unknown): value is Response {
+    return (
+      isObject(value) &&
+      isNumber(value.status) &&
+      Headers.isHeaders(value.headers)
+    );
+  }
 }
