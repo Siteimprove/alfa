a010e940cbf3ba2e421fe7793bd5cd705dcbe360
Kasper Isager <kasperisager@gmail.com>
2019-11-11 10:19:14 +0100

    Implement `FNV` type

---
+++ packages/alfa-fnv/src/fnv.ts
@@ -0,0 +1,29 @@
+import { Hash } from "@siteimprove/alfa-hash";
+
+export class FNV implements Hash {
+  public static empty(): FNV {
+    return new FNV();
+  }
+
+  private hash = 2166136261;
+
+  private constructor() {}
+
+  public write(data: Uint8Array): this {
+    let hash = this.hash;
+
+    for (const octet of data) {
+      hash ^= octet;
+      hash +=
+        (hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24);
+    }
+
+    this.hash = hash;
+
+    return this;
+  }
+
+  public finish(): number {
+    return this.hash >>> 0;
+  }
+}
