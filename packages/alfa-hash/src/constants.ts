254853f6139c57fd60bcfe7b9c03aa1704090437
Kasper Isager <kasperisager@gmail.com>
2019-11-11 10:18:31 +0100

    Implement `Hash` and `Hashable` types

---
+++ packages/alfa-hash/src/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * @internal
+ */
+export const IntegerOverflow = 0b10000000_00000000_00000000_00000000;
+
+/**
+ * @internal
+ */
+export const BuiltinOffset = 0b10000_10000_10000_10000_10000_10000_00;
