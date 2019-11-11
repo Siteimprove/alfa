1ca0a4e744118c31d695ce45898a86590d220801
Kasper Isager <kasperisager@gmail.com>
2019-11-11 10:15:57 +0100

    Implement `Bits` functions

---
+++ packages/alfa-bits/src/bits.ts
@@ -0,0 +1,37 @@
+export namespace Bits {
+  export function bit(i: number): number {
+    return 1 << i;
+  }
+
+  export function set(bits: number, i: number): number {
+    return bits | bit(i);
+  }
+
+  export function clear(bits: number, i: number): number {
+    return bits & ~bit(i);
+  }
+
+  export function test(bits: number, i: number): boolean {
+    return (bits & bit(i)) !== 0;
+  }
+
+  export function take(bits: number, n: number): number {
+    return bits & ((1 << n) - 1);
+  }
+
+  export function skip(bits: number, n: number): number {
+    return bits >>> n;
+  }
+
+  /**
+   * @see https://en.wikipedia.org/wiki/Hamming_weight
+   */
+  export function popCount(bits: number): number {
+    bits -= (bits >> 1) & 0x55555555;
+    bits = (bits & 0x33333333) + ((bits >> 2) & 0x33333333);
+    bits = (bits + (bits >> 4)) & 0x0f0f0f0f;
+    bits += bits >> 8;
+    bits += bits >> 16;
+    return bits & 0x7f;
+  }
+}
