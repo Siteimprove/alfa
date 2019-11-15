07612ebab5bfdee2dc29dde997a130342cdeef19
Kasper Isager <kasperisager@gmail.com>
2019-11-15 14:39:44 +0100

    Don't use hacky workaround for `Generator` redeclaration

--- packages/alfa-generator/src/index.ts
+++ packages/alfa-generator/src/index.ts
@@ -1,6 +1,1 @@
-import { Generator } from "./generator";
-
-type generator<T, R = unknown, N = unknown> = Generator<T, R, N>;
-const generator = Generator;
-
-export { generator as Generator };
+export * from "./generator";
