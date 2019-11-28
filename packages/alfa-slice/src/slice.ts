bd5f83f12cb762e768217ecca76f92779b9ab79c
Kasper Isager <kasperisager@gmail.com>
2019-11-28 10:32:13 +0100

    Implement `Slice` type

---
+++ packages/alfa-slice/src/slice.ts
@@ -0,0 +1,65 @@
+import { None, Option } from "@siteimprove/alfa-option";
+
+export class Slice<T> implements Iterable<T> {
+  public static of<T>(
+    array: Readonly<Array<T>>,
+    start: number = 0,
+    end: number = array.length
+  ): Slice<T> {
+    start = clamp(start, array.length);
+
+    return new Slice(array, start, clamp(end, array.length) - start);
+  }
+
+  public readonly array: Readonly<Array<T>>;
+  public readonly offset: number;
+  public readonly length: number;
+
+  private constructor(
+    array: Readonly<Array<T>>,
+    offset: number,
+    length: number
+  ) {
+    this.array = array;
+    this.offset = offset;
+    this.length = length;
+  }
+
+  public get(index: number): Option<T> {
+    if (index < 0 || index >= this.length) {
+      return None;
+    }
+
+    return Option.of(this.array[this.offset + index]);
+  }
+
+  public slice(start: number, end: number = this.length): Slice<T> {
+    start = clamp(start, this.length);
+
+    return new Slice(
+      this.array,
+      this.offset + start,
+      clamp(end, this.length) - start
+    );
+  }
+
+  public *[Symbol.iterator](): Iterator<T> {
+    for (let i = this.offset, n = i + this.length; i < n; i++) {
+      yield this.array[i];
+    }
+  }
+
+  public toJSON(): unknown {
+    return [...this];
+  }
+
+  public toString(): string {
+    const values = [...this].join(", ");
+
+    return `Slice [${values === "" ? "" : ` ${values} `}]`;
+  }
+}
+
+function clamp(value: number, length: number): number {
+  return Math.max(0, Math.min(value, length));
+}
