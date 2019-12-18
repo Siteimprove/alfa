946150383d91adb57973c0ae5bcc435fdfd1acd2
Jean-Yves Moyen <jym@siteimprove.com>
2019-12-18 09:34:35 +0100

    SIA-R59: Document has headings (#162)
    * Write rule SIA-R59: Document has headings

---
+++ packages/alfa-rules/src/sia-r59/rule.ts
@@ -0,0 +1,50 @@
+import { Rule } from "@siteimprove/alfa-act";
+import { Document, Element, Namespace } from "@siteimprove/alfa-dom";
+import { Iterable } from "@siteimprove/alfa-iterable";
+import { Predicate } from "@siteimprove/alfa-predicate";
+import { Err, Ok } from "@siteimprove/alfa-result";
+import { Page } from "@siteimprove/alfa-web";
+
+import { hasChild } from "../common/predicate/has-child";
+import { hasName } from "../common/predicate/has-name";
+import { hasNamespace } from "../common/predicate/has-namespace";
+import { hasRole } from "../common/predicate/has-role";
+import { isDocumentElement } from "../common/predicate/is-document-element";
+
+const { some } = Iterable;
+const { and, equals, test } = Predicate;
+
+export default Rule.Atomic.of<Page, Document>({
+  uri: "https://siteimprove.github.io/sanshikan/rules/sia-r59.html",
+  evaluate({ document }) {
+    return {
+      applicability() {
+        return test(
+          hasChild(and(Element.isElement, isDocumentElement())),
+          document
+        )
+          ? [document]
+          : [];
+      },
+
+      expectations(target) {
+        const hasHeadings = some(
+          target.descendants({ flattened: true }),
+          and(
+            Element.isElement,
+            and(
+              hasNamespace(equals(Namespace.HTML)),
+              hasRole(hasName(equals("heading")))
+            )
+          )
+        );
+
+        return {
+          1: hasHeadings
+            ? Ok.of("The document has at least one heading element")
+            : Err.of("The document does not have a heading element")
+        };
+      }
+    };
+  }
+});
