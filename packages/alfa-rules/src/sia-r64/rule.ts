2760b51b85e1ebb9052b0336815503bea558f1cf
Kasper Isager <kasperisager@gmail.com>
2020-01-20 15:30:36 +0100

    Initial implementation of SIA-R64

---
+++ packages/alfa-rules/src/sia-r64/rule.ts
@@ -0,0 +1,56 @@
+import { Rule } from "@siteimprove/alfa-act";
+import { Element, Namespace } from "@siteimprove/alfa-dom";
+import { Iterable } from "@siteimprove/alfa-iterable";
+import { Predicate } from "@siteimprove/alfa-predicate";
+import { Err, Ok } from "@siteimprove/alfa-result";
+import { Page } from "@siteimprove/alfa-web";
+
+import { hasAccessibleName } from "../common/predicate/has-accessible-name";
+import { hasName } from "../common/predicate/has-name";
+import { hasNamespace } from "../common/predicate/has-namespace";
+import { hasRole } from "../common/predicate/has-role";
+import { isIgnored } from "../common/predicate/is-ignored";
+
+const { filter } = Iterable;
+const { and, not, equals, fold } = Predicate;
+
+export default Rule.Atomic.of<Page, Element>({
+  uri: "https://siteimprove.github.io/sanshikan/rules/sia-r64.html",
+  evaluate({ device, document }) {
+    return {
+      applicability() {
+        return filter(
+          document.descendants({ flattened: true, nested: true }),
+          and(
+            Element.isElement,
+            and(
+              hasNamespace(equals(Namespace.HTML)),
+              and(hasRole(hasName(equals("heading"))), not(isIgnored(device)))
+            )
+          )
+        );
+      },
+
+      expectations(target) {
+        return {
+          1: fold(
+            hasAccessibleName(device),
+            target,
+            () => Outcomes.HasAccessibleName,
+            () => Outcomes.HasNoAccessibleName
+          )
+        };
+      }
+    };
+  }
+});
+
+export namespace Outcomes {
+  export const HasAccessibleName = Ok.of(
+    "The heading has a non-empty accessible name"
+  );
+
+  export const HasNoAccessibleName = Err.of(
+    "The heading has an empty accessible name"
+  );
+}
