65caa3f11342b7dec85562d9d8b7abe4c19acf22
Jean-Yves Moyen <jym@siteimprove.com>
2020-01-23 14:45:55 +0100

    Improve helper function

--- packages/alfa-rules/test/common/make-oracle.ts
+++ packages/alfa-rules/test/common/make-oracle.ts
@@ -1,31 +1,28 @@
 import { Oracle } from "@siteimprove/alfa-act";
 import { Node } from "@siteimprove/alfa-dom";
 import { Future } from "@siteimprove/alfa-future";
+import { Iterable } from "@siteimprove/alfa-iterable";
 import { None, Option } from "@siteimprove/alfa-option";
+import { Predicate } from "@siteimprove/alfa-predicate";
 import { Question } from "../../src/";
+import isBoolean = Predicate.isBoolean;
+import isOption = Option.isOption;
+import isIterable = Iterable.isIterable;
 
-export function makeOracle(
-  boolAnswers: { [_: string]: boolean },
-  nodeAnswers: { [_: string]: Option<Node> },
-  nodeListAnswers: { [_: string]: Iterable<Node> }
-): Oracle<Question> {
+// simple helper to build oracles
+// only work for the specialised Question type in @siteimprove/alfa-rules
+export function makeOracle(answers: {
+  [_: string]: boolean | Option<Node> | Iterable<Node>;
+}): Oracle<Question> {
   return (rule, question) => {
-    switch (question.type) {
-      case "boolean":
-        return question.uri in boolAnswers
-          ? Future.now(Option.of(question.answer(boolAnswers[question.uri])))
-          : Future.now(None);
-      case "node":
-        return question.uri in nodeAnswers
-          ? Future.now(Option.of(question.answer(nodeAnswers[question.uri])))
-          : Future.now(None);
-      case "node[]":
-        return question.uri in nodeListAnswers
-          ? Future.now(
-              Option.of(question.answer(nodeListAnswers[question.uri]))
-            )
-          : Future.now(None);
-    }
+    const answer = answers[question.uri];
+    if (question.type === "boolean" && isBoolean(answer))
+      return Future.now(Option.of(question.answer(answer)));
+    if (question.type === "node" && isOption(answer))
+      return Future.now(Option.of(question.answer(answer)));
+    if (question.type === "node[]" && isIterable(answer))
+      return Future.now(Option.of(question.answer(answer)));
+
     return Future.now(None);
   };
 }
