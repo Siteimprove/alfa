5d98297b810b50a65f562249ebb878a8366ee33b
Kasper Isager <kasperisager@gmail.com>
2020-01-29 13:44:57 +0100

    Add `--pretty` flags to reporters

--- scripts/common/flags.js
+++ scripts/common/flags.js
@@ -2,11 +2,12 @@ const process = require("process");
 const minimist = require("minimist");
 
 const argv = minimist(process.argv.slice(2), {
-  boolean: ["force", "verbose"]
+  boolean: ["force", "verbose", "pretty"]
 });
 
 exports.flags = {
   project: argv._[0],
   force: argv.force,
-  verbose: argv.verbose
+  verbose: argv.verbose,
+  pretty: argv.pretty
 };
