795820cc215a94f529cfff5c53c1c0993268f238
Jean-Yves Moyen <jym@siteimprove.com>
2020-01-30 13:43:38 +0100

    Refactor description track code

--- packages/alfa-rules/src/sia-r34/rule.ts
+++ packages/alfa-rules/src/sia-r34/rule.ts
@@ -1,9 +1,9 @@
 import { Rule } from "@siteimprove/alfa-act";
 import { Element } from "@siteimprove/alfa-dom";
-import { Err, Ok } from "@siteimprove/alfa-result";
 import { Page } from "@siteimprove/alfa-web";
 
 import { video } from "../common/applicability/video";
+import { videoDescriptionTrackAccurate } from "../common/expectations/video-description-track-accurate";
 
 import { Question } from "../common/question";
 
@@ -19,27 +19,7 @@ export default Rule.Atomic.of<Page, Element, Question>({
       },
 
       expectations(target) {
-        return {
-          1: Question.of(
-            "track-describes-video",
-            "boolean",
-            target,
-            `Does at least 1 track describe the visual information of the
-            <video> element, either in the language of the <video> element or
-            the language of the page?`
-          ).map(trackDescribesVideo =>
-            trackDescribesVideo
-              ? Ok.of(
-                  `The <video> element has a track that describes its visual
-                  information in the language of the <video> element or the page`
-                )
-              : Err.of(
-                  `The <video> element does not have a track that describes its
-                  visual information in the language of the <video> element or
-                  the page`
-                )
-          )
-        };
+        return videoDescriptionTrackAccurate(target);
       }
     };
   }
