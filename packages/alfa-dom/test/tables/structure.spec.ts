a596d546087a55a6fe13e967b57f86678f117ae9
Jean-Yves Moyen <jym@siteimprove.com>
2020-02-20 13:56:42 +0100

    Tes col/row span attributes parsing

--- packages/alfa-dom/test/tables/structure.spec.ts
+++ packages/alfa-dom/test/tables/structure.spec.ts
@@ -1,5 +1,6 @@
 import {test} from "@siteimprove/alfa-test";
 import {None} from "@siteimprove/alfa-option";
+import {Attribute, Element, parseSpan} from "../../src";
 
 import {Cell, ColGroup, isCoveredBy, RowGroup, Slot} from "../../src/tables/structure";
 
@@ -55,3 +56,26 @@ test("isCoveredBy ̤correctly computes group coverage", t => {
     t.equal(isCoveredBy(slot(5, 8), rowGroup(6, 2)), false);
   }
 );
+
+test("parse span attribute according to specs", t => {
+  function span(name: string, value:string): Element {
+    return Element.of(None, None, "foo", (elt)=> [Attribute.of(None, None, `${name}span`, value)])
+  }
+  const nospan = Element.of(None, None, "foo");
+
+  t.equal(parseSpan(span("col", "2"), "colspan", 1, 1000, 1), 2);
+  t.equal(parseSpan(span("col", "0"), "colspan", 1, 1000, 1), 1);
+  t.equal(parseSpan(span("col", "2000"), "colspan", 1, 1000, 1), 1000);
+  t.equal(parseSpan(span("col", ""), "colspan", 1, 1000, 1), 1);
+  t.equal(parseSpan(span("col", "-2"), "colspan", 1, 1000, 1), 1);
+  t.equal(parseSpan(span("col", "abc"), "colspan", 1, 1000, 1), 1);
+  t.equal(parseSpan(nospan, "colspan", 1, 1000, 1), 1);
+
+  t.equal(parseSpan(span("row", "2"), "rowspan", 0, 65534, 1), 2);
+  t.equal(parseSpan(span("row", "0"), "rowspan", 0, 65534, 1), 0);
+  t.equal(parseSpan(span("row", "70000"), "rowspan", 0, 65534, 1), 65534);
+  t.equal(parseSpan(span("row", ""), "rowspan", 0, 65534, 1), 1);
+  t.equal(parseSpan(span("row", "-2"), "rowspan", 0, 65534, 1), 1);
+  t.equal(parseSpan(span("row", "abc"), "rowspan", 0, 65534, 1), 1);
+  t.equal(parseSpan(nospan, "rowspan", 0, 65534, 1), 1);
+});
