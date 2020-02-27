d0ecbd03a639348c94e684433c8edba5d9fe47a8
Kasper Isager <kasperisager@gmail.com>
2020-02-27 14:05:45 +0100

    Disregard decorative images in SIA-R2 (#192)

--- packages/alfa-rules/test/sia-r2/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r2/rule.spec.tsx
@@ -25,22 +25,6 @@ test("evaluate() passes an image with an accessible name", async t => {
   ]);
 });
 
-test("evaluate() passes an image that is marked as decorative", async t => {
-  const document = Document.of(self => [
-    Element.fromElement(<img role="none"></img>, Option.of(self))
-  ]);
-
-  const img = document
-    .children()
-    .filter(Element.isElement)
-    .first()
-    .get();
-
-  t.deepEqual(await evaluate(R2, { document }), [
-    passed(R2, img, [["1", Outcomes.IsDecorative]])
-  ]);
-});
-
 test("evaluate() fails an image that has no accessible name", async t => {
   const document = Document.of(self => [
     Element.fromElement(<img></img>, Option.of(self))
@@ -53,12 +37,40 @@ test("evaluate() fails an image that has no accessible name", async t => {
     .get();
 
   t.deepEqual(await evaluate(R2, { document }), [
-    failed(R2, img, [["1", Outcomes.HasNoAccessibleNameNorIsDecorative]])
+    failed(R2, img, [["1", Outcomes.HasNoAccessibleName]])
   ]);
 });
 
 test("evaluate() is inapplicable when a document has no images", async t => {
   const document = Document.empty();
+
+  t.deepEqual(await evaluate(R2, { document }), [inapplicable(R2)]);
+});
+
+test("evaluate() is inapplicable to an image that is marked as decorative", async t => {
+  const document = Document.of(self => [
+    Element.fromElement(<img role="none"></img>, Option.of(self))
+  ]);
+
+  const img = document
+    .children()
+    .filter(Element.isElement)
+    .first()
+    .get();
+
+  t.deepEqual(await evaluate(R2, { document }), [inapplicable(R2)]);
+});
+
+test("evaluate() is inapplicable to an image that is hidden", async t => {
+  const document = Document.of(self => [
+    Element.fromElement(<img hidden alt="Hello world"></img>, Option.of(self))
+  ]);
+
+  const img = document
+    .children()
+    .filter(Element.isElement)
+    .first()
+    .get();
 
   t.deepEqual(await evaluate(R2, { document }), [inapplicable(R2)]);
 });
