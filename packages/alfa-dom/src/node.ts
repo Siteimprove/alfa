d420529c5c3c407fb8fe8c459c4567ac7accc5e8
Kasper Isager <kasperisager@gmail.com>
2020-03-03 10:42:08 +0100

    Implement `Equatable` for `Node`

--- packages/alfa-dom/src/node.ts
+++ packages/alfa-dom/src/node.ts
@@ -1,3 +1,4 @@
+import { Equatable } from "@siteimprove/alfa-equatable";
 import { Lazy } from "@siteimprove/alfa-lazy";
 import { Mapper } from "@siteimprove/alfa-mapper";
 import { None, Option } from "@siteimprove/alfa-option";
@@ -10,7 +11,7 @@ import * as json from "@siteimprove/alfa-json";
 const { equals } = Predicate;
 
 export abstract class Node
-  implements Iterable<Node>, json.Serializable, earl.Serializable {
+  implements Iterable<Node>, Equatable, json.Serializable, earl.Serializable {
   protected readonly _children: Array<Node>;
   protected readonly _parent: Option<Node>;
 
@@ -163,6 +164,10 @@ export abstract class Node
 
   public *[Symbol.iterator](): Iterator<Node> {
     yield* this.descendants();
+  }
+
+  public equals(value: unknown): value is this {
+    return value === this;
   }
 
   public abstract toJSON(): Node.JSON;
