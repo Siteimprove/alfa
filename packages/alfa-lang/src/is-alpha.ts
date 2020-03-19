a7f933cf76e6091c490ea4dfeeeaeaee9c042d18
Kasper Isager <kasperisager@gmail.com>
2020-03-19 12:45:25 +0100

    Remove the @siteimprove/alfa-lang package
    Closes #200

--- packages/alfa-lang/src/is-alpha.ts
+++
@@ -1,9 +0,0 @@
-import { Char } from "./char";
-import { isBetween } from "./is-between";
-
-export function isAlpha(char: number): boolean {
-  return (
-    isBetween(char, Char.SmallLetterA, Char.SmallLetterZ) ||
-    isBetween(char, Char.CapitalLetterA, Char.CapitalLetterZ)
-  );
-}
