339d48fdf1676f10e73a7d7280692520f98ef46e
Jean-Yves Moyen <jym@siteimprove.com>
2020-03-24 09:34:21 +0100

    Make getter publics

--- packages/alfa-dom/src/tables/groups.ts
+++ packages/alfa-dom/src/tables/groups.ts
@@ -19,13 +19,13 @@ export class RowGroup {
     this._element = element;
   }
 
-  get anchor() {
+  public get anchor() {
     return this._anchor;
   }
-  get height() {
+  public get height() {
     return this._height;
   }
-  get element() {
+  public get element() {
     return this._element;
   }
 
@@ -50,13 +50,13 @@ export class ColGroup {
     this._element = element;
   }
 
-  get anchor() {
+  public get anchor() {
     return this._anchor;
   }
-  get width() {
+  public get width() {
     return this._width;
   }
-  get element() {
+  public get element() {
     return this._element;
   }
 
@@ -114,14 +114,20 @@ export class Cell {
     this._element = element;
   }
 
-  get anchor() {
+  public get anchor() {
     return this._anchor;
   }
-  get width() {
+  public get width() {
     return this._width;
   }
-  get height() {
+  public get height() {
     return this._height;
+  }
+  public get kind() {
+    return this._kind;
+  }
+  public get element() {
+    return this._element;
   }
 
   public isRowGroup(): this is RowGroup {
@@ -137,6 +143,7 @@ export class Cell {
 
   // https://html.spec.whatwg.org/multipage/tables.html#algorithm-for-growing-downward-growing-cells
   public growDownward(yCurrent: number): Cell {
+    // console.log(`   growing ${this._element.attribute("id").get()} from ${this._height} to ${Math.max(this._height, yCurrent - this._anchor.y + 1)}`);
     // we need yCurrent to be covered, hence y+h-1>=yCurrent, hence h>=yCurrent-y+1
     return new Cell(this._kind, this._anchor.x, this._anchor.y, this._width, Math.max(this._height, yCurrent - this._anchor.y + 1), this._element);
   }
@@ -148,6 +155,7 @@ export class Cell {
     let rowspan = parseSpan(cell, "rowspan", 0, 65534, 1);
     // 10 assuming we are not in quirks mode because I don't know if we test that yet…
     const grow = (rowspan === 0);
+    // console.log(`   Processing ${cell.attribute("id").get()}, attribute: ${cell.attribute("rowspan").getOr("empty")}, rowspan: ${rowspan}, grow: ${grow}`);
     if (rowspan === 0) {
       rowspan = 1
     }
@@ -185,25 +193,94 @@ export function isCovering(x: number, y: number): Predicate<Cell | RowGroup | Co
 }
 
 // Build artefact, corresponds to a single <tr> element
-export class Row {
-  private readonly _width: number;
-  private readonly _height: number;
-  private readonly _element: Element;
-  private readonly _cells: Set<Cell>;
-  private readonly _downwardGrowingCells: Set<Cell>;
-
-  constructor(w: number, h: number, element: Element, cells: Set<Cell> = Set.empty(), growing: Set<Cell> = Set.empty()) {
-    this._width = w;
-    this._height = h;
-    this._element = element;
-    this._cells = cells;
-    this._downwardGrowingCells = growing;
-  }
-
-  public addCell(cell: Cell): Row {
-    return new Row(this._width, this._height, this._element, this._cells.add(cell), this._downwardGrowingCells);
-  }
-  public addGrowingCell(cell: Cell): Row {
-    return new Row(this._width, this._height, this._element, this._cells.add(cell), this._downwardGrowingCells.add(cell));
-  }
-}
+// export class Row {
+//   private readonly _width: number;
+//   private readonly _height: number;
+//   private readonly _element: Element;
+//   private readonly _cells: Set<Cell>;
+//   private readonly _downwardGrowingCells: Set<Cell>;
+//
+//   constructor(w: number, h: number, element: Element, cells: Set<Cell> = Set.empty(), growing: Set<Cell> = Set.empty()) {
+//     this._width = w;
+//     this._height = h;
+//     this._element = element;
+//     this._cells = cells;
+//     this._downwardGrowingCells = growing;
+//   }
+//
+//   public get width() {
+//     return this._width;
+//   }
+//   public get height() {
+//     return this._height;
+//   }
+//   public get element() {
+//     return this._element;
+//   }
+//   public get cells() {
+//     return this._cells;
+//   }
+//   public get downwardGrowingCells() {
+//     return this._downwardGrowingCells;
+//   }
+//
+//   public addCell(cell: Cell): Row {
+//     return new Row(this._width, this._height, this._element, this._cells.add(cell), this._downwardGrowingCells);
+//   }
+//   public addGrowingCell(cell: Cell): Row {
+//     return new Row(this._width, this._height, this._element, this._cells.add(cell), this._downwardGrowingCells.add(cell));
+//   }
+//
+//   public adjustWidth(w: number): Row {
+//     return new Row(Math.max(this._width, w), this._height, this._element, this._cells);
+//   }
+//   public adjustHeight(h: number): Row {
+//     return new Row(this._width, Math.max(this._height, h), this._element, this._cells);
+//   }
+//
+//   // https://html.spec.whatwg.org/multipage/tables.html#algorithm-for-processing-rows
+//   // Need to pass a set of cells from previous rows which may "overflow" into this one.
+//   // public static of(tr: Element, previousCells: Set<Cell>): Row {
+//   //   assert(tr.name === "tr");
+//   //   let width = 0;
+//   //   let height = 1;
+//   //   // 1
+//   //   // assert(yCurrent <= table.height);
+//   //   // if (table.height === yCurrent) {
+//   //   //   table.height++
+//   //   // }
+//   //   // 2
+//   //   let xCurrent = 0;
+//   //   // 3 no need to grow the cells by one line. They will anyway all be adjusted to correct height later.
+//   //
+//   //   let children = tr.children().filter(isElementByName("th", "td"));
+//   //   for (const currentCell of children) { // loop control between 4-5, and 16-17-18
+//   //     // 6 (Cells)
+//   //     while (xCurrent < width &&
+//   //       previousCells.some(isCovering(xCurrent, yCurrent))
+//   //       ) {
+//   //       xCurrent++
+//   //     }
+//   //     // 7
+//   //     if (xCurrent === table.width) {
+//   //       table.width++
+//   //     }
+//   //     // 8, 9, 10, 13
+//   //     const { cell: floatingCell, downwardGrowing } = Cell.of(currentCell);
+//   //     const cell = floatingCell.anchorAt(xCurrent, yCurrent);
+//   //     // 11
+//   //     table.width = Math.max(table.width, xCurrent + cell.width);
+//   //     // 12
+//   //     table.height = Math.max(table.height, yCurrent + cell.height);
+//   //     // 13
+//   //     // Double coverage check made at the end of table building to de-entangle code
+//   //     table.cells = table.cells.add(cell);
+//   //     // 14
+//   //     if (downwardGrowing) growingCellsList.push(cell);
+//   //     // 15
+//   //     xCurrent = xCurrent + cell.width;
+//   //   }
+//   //   return growingCellsList;
+//   //   // 4 and 16 done after the calls to avoid side effects.
+//   // }
+// }
