39719b11985e46a33866f819a5ca6181ab233767
Jean-Yves Moyen <jym@siteimprove.com>
2020-04-14 14:45:25 +0200

    Introduce hasImplicit/ExplicitRole

--- packages/alfa-rules/src/common/predicate/has-role.ts
+++ packages/alfa-rules/src/common/predicate/has-role.ts
@@ -1,49 +1,98 @@
 import { Role } from "@siteimprove/alfa-aria";
 import { Element } from "@siteimprove/alfa-dom";
 import { Predicate } from "@siteimprove/alfa-predicate";
-import {hasName} from "./has-name";
+import { hasName } from "./has-name";
 
 const { equals } = Predicate;
 
-export function hasRole(
-  predicate: Predicate<Role>,
-  options?: Role.from.Options
-): Predicate<Element>;
+export function hasRole(predicate?: Predicate<Role>): Predicate<Element>;
+
 export function hasRole(
   name: string,
   ...rest: Array<string>
 ): Predicate<Element>;
+
 export function hasRole(
+  nameOrPredicate: undefined | string | Predicate<Role>,
+  ...names: Array<string>
+): Predicate<Element> {
+  const options = {};
+
+  return typeof nameOrPredicate === "function" ||
+    typeof nameOrPredicate === "undefined"
+    ? hasRoleWithOptions(options, nameOrPredicate)
+    : hasRoleWithOptions(options, nameOrPredicate, ...names);
+}
+
+export function hasExplicitRole(
+  predicate?: Predicate<Role>
+): Predicate<Element>;
+
+export function hasExplicitRole(
+  name: string,
+  ...rest: Array<string>
+): Predicate<Element>;
+
+export function hasExplicitRole(
+  nameOrPredicate: undefined | string | Predicate<Role>,
+  ...names: Array<string>
+): Predicate<Element> {
+  const options = { implicit: false };
+
+  return typeof nameOrPredicate === "function" ||
+    typeof nameOrPredicate === "undefined"
+    ? hasRoleWithOptions(options, nameOrPredicate)
+    : hasRoleWithOptions(options, nameOrPredicate, ...names);
+}
+
+export function hasImplicitRole(
+  predicate?: Predicate<Role>
+): Predicate<Element>;
+
+export function hasImplicitRole(
+  name: string,
+  ...rest: Array<string>
+): Predicate<Element>;
+
+export function hasImplicitRole(
+  nameOrPredicate: undefined | string | Predicate<Role>,
+  ...names: Array<string>
+): Predicate<Element> {
+  const options = { explicit: false };
+
+  return typeof nameOrPredicate === "function" ||
+    typeof nameOrPredicate === "undefined"
+    ? hasRoleWithOptions(options, nameOrPredicate)
+    : hasRoleWithOptions(options, nameOrPredicate, ...names);
+}
+
+function hasRoleWithOptions(
+  options: Role.from.Options,
+  predicate?: Predicate<Role>
+): Predicate<Element>;
+
+function hasRoleWithOptions(
   options: Role.from.Options,
   name: string,
   ...rest: Array<string>
 ): Predicate<Element>;
-export function hasRole(
-  nameOrPredicateOrOptions: string | Predicate<Role> | Role.from.Options,
-  nameOrOptions?: string | Role.from.Options,
+
+function hasRoleWithOptions(
+  options: Role.from.Options,
+  nameOrPredicate: undefined | string | Predicate<Role>,
   ...names: Array<string>
 ): Predicate<Element> {
   let predicate: Predicate<Role>;
-  let options: Role.from.Options;
 
-  if (typeof nameOrPredicateOrOptions === "function") {
-    predicate = nameOrPredicateOrOptions;
-    if (
-      typeof nameOrOptions === "undefined" ||
-      typeof nameOrOptions === "string" // should never happen
-    ) {
-      options = {};
-    } else {
-      options = nameOrOptions;
-    }
-  } else {
-    if (typeof nameOrPredicateOrOptions === "string") {
-      options = {};
-      predicate = hasName(equals(nameOrPredicateOrOptions, nameOrOptions, ...names));
-    } else {
-      options = nameOrPredicateOrOptions;
-      predicate = hasName(equals(nameOrOptions, ...names));
-    }
+  switch (typeof nameOrPredicate) {
+    case "function":
+      predicate = nameOrPredicate;
+      break;
+    case "undefined":
+      predicate = () => true;
+      break;
+    default:
+      predicate = hasName(equals(nameOrPredicate, ...names));
   }
 
   return (element) =>
