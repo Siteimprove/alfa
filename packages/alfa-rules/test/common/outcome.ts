f6a0f960c5018f765ab9ee8145cf6aa2a891f810
Jean-Yves Moyen <jym@siteimprove.com>
2020-04-14 12:30:29 +0200

    Make outcome test helpers accept expectation object (#206)

--- packages/alfa-rules/test/common/outcome.ts
+++ packages/alfa-rules/test/common/outcome.ts
@@ -1,5 +1,4 @@
 import { Rule, Outcome } from "@siteimprove/alfa-act";
-import { Iterable } from "@siteimprove/alfa-iterable";
 import { Some } from "@siteimprove/alfa-option";
 import { Record } from "@siteimprove/alfa-record";
 import { Result } from "@siteimprove/alfa-result";
@@ -8,24 +7,28 @@ import { Page } from "@siteimprove/alfa-web";
 export function passed<T, Q>(
   rule: Rule<Page, T, Q>,
   target: T,
-  expectations: Iterable<[string, Result<string, string>]>
+  expectations: { [id: string]: Result<string, string> }
 ): Outcome.Passed<Page, T, Q> {
   return Outcome.Passed.of(
     rule,
     target,
-    Record.from(Iterable.map(expectations, ([id, exp]) => [id, Some.of(exp)]))
+    Record.from(
+      Object.entries(expectations).map(([id, exp]) => [id, Some.of(exp)])
+    )
   );
 }
 
 export function failed<T, Q>(
   rule: Rule<Page, T, Q>,
   target: T,
-  expectations: Iterable<[string, Result<string, string>]>
+  expectations: { [id: string]: Result<string, string> }
 ): Outcome.Failed<Page, T, Q> {
   return Outcome.Failed.of(
     rule,
     target,
-    Record.from(Iterable.map(expectations, ([id, exp]) => [id, Some.of(exp)]))
+    Record.from(
+      Object.entries(expectations).map(([id, exp]) => [id, Some.of(exp)])
+    )
   );
 }
 
