b6efd614269e0e02d939c15f3c5c80522b768c40
Jean-Yves Moyen <jym@siteimprove.com>
2020-04-14 14:20:07 +0200

    Update packages/alfa-rules/src/common/predicate/has-name.ts
    Co-Authored-By: Kasper Isager <kasperisager@gmail.com>

--- packages/alfa-rules/src/common/predicate/has-name.ts
+++ packages/alfa-rules/src/common/predicate/has-name.ts
@@ -5,10 +5,12 @@ const { equals, property } = Predicate;
 export function hasName<T extends { readonly name: string }>(
   predicate: Predicate<string>
 ): Predicate<T>;
+
 export function hasName<T extends { readonly name: string }>(
   name: string,
   ...rest: Array<string>
 ): Predicate<T>;
+
 export function hasName<T extends { readonly name: string }>(
   nameOrPredicate: string | Predicate<string>,
   ...names: Array<string>
