10f9676c920e8f982f1089cc50e9c4918a9076a0
Jean-Yves Moyen <jym@siteimprove.com>
2020-04-14 13:13:33 +0200

    Force at least one argument

--- packages/alfa-rules/src/common/predicate/has-namespace.ts
+++ packages/alfa-rules/src/common/predicate/has-namespace.ts
@@ -7,19 +7,19 @@ export function hasNamespace(
   predicate: Predicate<Namespace>
 ): Predicate<Element>;
 export function hasNamespace(
-  ...namespaces: Array<Namespace>
+  namespace: Namespace,
+  ...rest: Array<Namespace>
 ): Predicate<Element>;
-export function hasNamespace(): Predicate<Element> {
+export function hasNamespace(
+  namespaceOrPredicate: Namespace | Predicate<Namespace>,
+  ...namespaces: Array<Namespace>
+): Predicate<Element> {
   let predicate: Predicate<Namespace>;
 
-  if (arguments.length === 0) {
-    predicate = () => true;
-  }
-  if (arguments.length === 1 && typeof arguments[0] === "function") {
-    predicate = arguments[0];
-  }
-  if (arguments.length !== 0 && typeof arguments[0] !== "function") {
-    predicate = equals(...arguments);
+  if (typeof namespaceOrPredicate === "function") {
+    predicate = namespaceOrPredicate;
+  } else {
+    predicate = equals(namespaceOrPredicate, ...namespaces);
   }
 
   return (element) => element.namespace.some(predicate);
