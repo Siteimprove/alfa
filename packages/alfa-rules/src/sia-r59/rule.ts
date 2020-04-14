588c304b67d79f031de6a1902570953d4f02ec77
Jean-Yves Moyen <jym@siteimprove.com>
2020-04-14 12:55:02 +0200

    Factor isElement in isDocumentElement (#207)
    * Factor isElement in isDocumentElement

--- packages/alfa-rules/src/sia-r59/rule.ts
+++ packages/alfa-rules/src/sia-r59/rule.ts
@@ -21,12 +21,7 @@ export default Rule.Atomic.of<Page, Document>({
   evaluate({ document }) {
     return {
       applicability() {
-        return test(
-          hasChild(and(Element.isElement, isDocumentElement())),
-          document
-        )
-          ? [document]
-          : [];
+        return test(hasChild(isDocumentElement), document) ? [document] : [];
       },
 
       expectations(target) {
