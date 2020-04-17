375c045eedde6a6cd6b6fc46da5e6a6d9f877680
Jean-Yves Moyen <jym@siteimprove.com>
2020-04-17 09:56:34 +0200

    Improve parsing of integers

--- packages/alfa-table/test/microsyntaxes.spec.ts
+++ packages/alfa-table/test/microsyntaxes.spec.ts
@@ -11,17 +11,17 @@ const result = <T>(x: T) => Ok.of(x);
 
 test("parseInteger() parses integers according to specs", (t) => {
   t.deepEqual(parseInteger("2"), result(2));
-  t.deepEqual(parseInteger("1234"), result(1234));
+  t.deepEqual(parseInteger("+1234"), result(1234));
   t.deepEqual(parseInteger("-5678"), result(-5678));
   t.deepEqual(parseInteger("-0"), result(0));
   t.deepEqual(parseInteger("    14"), result(14));
 
   t.deepEqual(parseInteger("touan"), Outcomes.notNumber);
-  t.deepEqual(parseInteger("12snr"), Outcomes.notNumber);
-  t.deepEqual(parseInteger("1.2"), Outcomes.notInteger);
+  t.deepEqual(parseInteger("12snr"), result(12));
+  t.deepEqual(parseInteger("1.2"), result(1));
 
-  t.deepEqual(parseInteger(""), Outcomes.empty);
-  t.deepEqual(parseInteger("    "), Outcomes.empty);
+  t.deepEqual(parseInteger(""), Outcomes.notNumber);
+  t.deepEqual(parseInteger("    "), Outcomes.notNumber);
 });
 
 test("parseNonNegativeInteger() parses non-negative integers according to specs", (t) => {
