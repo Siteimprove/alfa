760cbff7e5576dde5eaff01146074fdd158da797
Jean-Yves Moyen <jym@siteimprove.com>
2020-04-21 15:51:35 +0200

    Use Attribuet#enumerate (#216)

--- packages/alfa-rules/src/common/predicate/has-input-type.ts
+++ packages/alfa-rules/src/common/predicate/has-input-type.ts
@@ -9,43 +9,38 @@ export function hasInputType(
       return false;
     }
 
-    const type = element
-      .attribute("type")
-      .map((attr) =>
-        // The `type` attribute of is an enumerated attribute and is therefore
-        // case-insensitive.
-        // https://html.spec.whatwg.org/#enumerated-attribute
-        attr.value.toLowerCase()
-      )
-      .getOr("text")
-      .toLowerCase();
-
-    switch (type) {
-      case "hidden":
-      case "search":
-      case "tel":
-      case "url":
-      case "email":
-      case "password":
-      case "date":
-      case "month":
-      case "week":
-      case "time":
-      case "datetime-local":
-      case "number":
-      case "range":
-      case "color":
-      case "checkbox":
-      case "radio":
-      case "file":
-      case "submit":
-      case "image":
-      case "reset":
-      case "button":
-      case "text":
-        return predicate(type);
-      default:
-        return predicate("text");
-    }
+    return predicate(
+      element
+        .attribute("type")
+        .map((attribute) =>
+          attribute
+            .enumerate(
+              "hidden",
+              "search",
+              "tel",
+              "url",
+              "email",
+              "password",
+              "date",
+              "month",
+              "week",
+              "time",
+              "datetime-local",
+              "number",
+              "range",
+              "color",
+              "checkbox",
+              "radio",
+              "file",
+              "submit",
+              "image",
+              "reset",
+              "button",
+              "text"
+            )
+            .getOr("text")
+        )
+        .getOr("text")
+    );
   };
 }
