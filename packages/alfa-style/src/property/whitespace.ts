c0f29257cdd6d80aaf586a586e4fab7dcf6d1606
Kasper Isager <kasperisager@gmail.com>
2020-05-27 14:12:51 +0200

    Implement SIA-R83

---
+++ packages/alfa-style/src/property/whitespace.ts
@@ -0,0 +1,26 @@
+import { Keyword } from "@siteimprove/alfa-css";
+
+import { Property } from "../property";
+
+export type Whitespace = Keyword<
+  "normal" | "pre" | "nowrap" | "pre-wrap" | "break-spaces" | "pre-line"
+>;
+
+/**
+ * @see https://drafts.csswg.org/css-text/#propdef-white-space
+ */
+export const Whitespace: Property<Whitespace> = Property.of(
+  Keyword.of("normal"),
+  Keyword.parse(
+    "normal",
+    "pre",
+    "nowrap",
+    "pre-wrap",
+    "break-spaces",
+    "pre-line"
+  ),
+  (style) => style.specified("white-space"),
+  {
+    inherits: true,
+  }
+);
