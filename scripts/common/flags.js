e7d11d889cb0bbc2a39c77c686cc6f54da3f16b2
Kasper Isager <kasperisager@gmail.com>
2020-06-10 09:56:48 +0200

    Give build scripts a once-over

--- scripts/common/flags.js
+++ scripts/common/flags.js
@@ -2,12 +2,12 @@ const process = require("process");
 const minimist = require("minimist");
 
 const argv = minimist(process.argv.slice(2), {
-  boolean: ["force", "verbose", "pretty"]
+  boolean: ["force", "verbose", "pretty"],
 });
 
 exports.flags = {
   project: argv._[0],
   force: argv.force,
   verbose: argv.verbose,
-  pretty: argv.pretty
+  pretty: argv.pretty,
 };
