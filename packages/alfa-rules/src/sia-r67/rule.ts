ed73fc5ce458bb9b81b806ae8d5a5e1c169a1724
Jean-Yves Moyen <jym@siteimprove.com>
2020-06-12 12:16:30 +0200

    Add predicated for embedded content and marked as decorative

--- packages/alfa-rules/src/sia-r67/rule.ts
+++ packages/alfa-rules/src/sia-r67/rule.ts
@@ -1,5 +1,5 @@
 import { Rule, Diagnostic } from "@siteimprove/alfa-act";
-import { Element, Namespace } from "@siteimprove/alfa-dom";
+import { Attribute, Element, Namespace } from "@siteimprove/alfa-dom";
 import { Predicate } from "@siteimprove/alfa-predicate";
 import { Err, Ok } from "@siteimprove/alfa-result";
 import { Page } from "@siteimprove/alfa-web";
@@ -7,10 +7,14 @@ import { Page } from "@siteimprove/alfa-web";
 import { expectation } from "../common/expectation";
 
 import { hasAccessibleName } from "../common/predicate/has-accessible-name";
+import { hasAttribute } from "../common/predicate/has-attribute";
+import { hasExplicitRole } from "../common/predicate/has-role";
+import { hasValue } from "../common/predicate/has-value";
 import { isDecorative } from "../common/predicate/is-decorative";
+import equals = Predicate.equals;
 
 const { isElement, hasName, hasNamespace } = Element;
-const { and } = Predicate;
+const { and, or } = Predicate;
 
 export default Rule.Atomic.of<Page, Element>({
   uri: "https://siteimprove.github.io/sanshikan/rules/sia-r67.html",
@@ -53,3 +57,36 @@ export namespace Outcomes {
     )
   );
 }
+
+/**
+ * @see https://html.spec.whatwg.org/#embedded-content-category
+ */
+const isEmbeddedContent: Predicate<Element> = or(
+  and(hasNamespace(Namespace.SVG), Element.hasName("svg")),
+  and(hasNamespace(Namespace.MathML), Element.hasName("math")),
+  and(
+    hasNamespace(Namespace.HTML),
+    Element.hasName(
+      "audio",
+      "canvas",
+      "embed",
+      "iframe",
+      "img",
+      "object",
+      "picture",
+      "video"
+    )
+  )
+);
+
+/**
+ * Not fully matching the Sanshikan def as <a>Foo</a> (no href) is not flagged here.
+ * Hence, this is OK for SIA R67 but will need more work if moved to a common helper.
+ */
+export const isMarkedAsDecorative: Predicate<Element> = or(
+  hasExplicitRole("none", "presentation"),
+  and(
+    Element.hasName("img"),
+    hasAttribute(and(Attribute.hasName("alt"), hasValue(equals(""))))
+  )
+);
