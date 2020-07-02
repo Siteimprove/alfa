13eeb6a52fa3ac3243255ee3c3066525ae524bf6
Kasper Isager <kasperisager@gmail.com>
2020-07-02 12:59:26 +0200

    Add more test cases

--- packages/alfa-scraper/test/scraper.spec.ts
+++ packages/alfa-scraper/test/scraper.spec.ts
@@ -37,3 +37,27 @@ test("#scrape() scrapes a page with a delayed meta refresh", async (t) =>
 
     t.equal(response.url, url);
   }));
+
+test("#scrape() scrapes a page with an immediate location change", async (t) =>
+  await Scraper.with(async (scraper) => {
+    const url = `file://${__dirname}/fixture/location-change-immediate.html`;
+    const result = await scraper.scrape(url);
+
+    t.equal(result.isOk(), true);
+
+    const { response } = result.get();
+
+    t.equal(response.url, "https://example.com/");
+  }));
+
+test("#scrape() scrapes a page with a delayed location change", async (t) =>
+  await Scraper.with(async (scraper) => {
+    const url = `file://${__dirname}/fixture/location-change-delayed.html`;
+    const result = await scraper.scrape(url);
+
+    t.equal(result.isOk(), true);
+
+    const { response } = result.get();
+
+    t.equal(response.url, url);
+  }));
