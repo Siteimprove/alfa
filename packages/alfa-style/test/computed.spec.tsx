8cb89a27ff2d59af98448f06234c23afb29aa9e6
Jean-Yves Moyen <jym@siteimprove.com>
2020-07-10 13:30:40 +0200

    Add test cases

--- packages/alfa-style/test/computed.spec.tsx
+++ packages/alfa-style/test/computed.spec.tsx
@@ -24,10 +24,43 @@ const lime = {
   },
 };
 
+const transparent = {
+  type: "color",
+  format: "rgb",
+  red: {
+    type: "percentage",
+    value: 0,
+  },
+  green: {
+    type: "percentage",
+    value: 0,
+  },
+  blue: {
+    type: "percentage",
+    value: 0,
+  },
+  alpha: {
+    type: "percentage",
+    value: 0,
+  },
+};
+
 test("#computed() substitute a custom property defined at the same time", (t) => {
   const element = (
     <div style={{ "--foo": "lime", backgroundColor: "var(--foo, cyan)" }}></div>
   );
+
+  const style = Style.from(element, Device.standard());
+
+  t.deepEqual(style.computed("background-color").toJSON(), {
+    value: lime,
+    source: h.declaration("background-color", "var(--foo, cyan)").toJSON(),
+  });
+});
+
+test("#computed() substitute an inherited custom property", (t) => {
+  const element = <div style={{ backgroundColor: "var(--foo, cyan)" }}></div>;
+  const parent = <div style={{ "--foo": "lime" }}>{element}</div>;
 
   const style = Style.from(element, Device.standard());
 
@@ -46,4 +79,23 @@ test("#computed() uses fallback if a custom property does not exist", (t) => {
     value: lime,
     source: h.declaration("background-color", "var(--foo, lime)").toJSON(),
   });
+});
+
+test("#computed() use default value if property is invalid at computed-value time", (t) => {
+  const elements = [
+    <div style={{ backgroundColor: "var(--foo)" }}></div>, // no custom, no fallback
+    <div
+      style={{ "--foo": "invalid", backgroundColor: "var(--foo, cyan)" }}
+    ></div>, // invalid custom (fallback is ignored)
+    <div style={{ backgroundColor: "var(--foo, invalid)" }}></div>, // no custom, invalid fallback
+  ];
+
+  for (const element of elements) {
+    const style = Style.from(element, Device.standard());
+
+    t.deepEqual(style.computed("background-color").toJSON(), {
+      value: transparent,
+      source: null,
+    });
+  }
 });
