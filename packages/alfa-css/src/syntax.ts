a9f41323402351e9670315c4297abc1db949ca96
Kasper Isager <kasperisager@gmail.com>
2020-08-05 10:04:06 +0200

    Progress on refactoring custom property implementation (#302)

--- packages/alfa-css/src/syntax.ts
+++ packages/alfa-css/src/syntax.ts
@@ -1,5 +1,6 @@
 export * from "./syntax/block";
 export * from "./syntax/component";
 export * from "./syntax/declaration";
+export * from "./syntax/function";
 export * from "./syntax/lexer";
 export * from "./syntax/token";
