1d3e12bd8e09c058370ef746da2abaa64c7473c8
Kasper Isager <kasperisager@gmail.com>
2020-08-27 15:57:28 +0200

    Implement minimal crawler based on the existing scraper (#309)

---
+++ packages/alfa-crawler/src/crawler.ts
@@ -0,0 +1,100 @@
+import { Element } from "@siteimprove/alfa-dom";
+import { Frontier } from "@siteimprove/alfa-frontier";
+import { Mapper } from "@siteimprove/alfa-mapper";
+import { Result } from "@siteimprove/alfa-result";
+import { Scraper, Screenshot } from "@siteimprove/alfa-scraper";
+import { Page } from "@siteimprove/alfa-web";
+
+const { isElement } = Element;
+
+export class Crawler {
+  public static async of(
+    scraper: Promise<Scraper> = Scraper.of()
+  ): Promise<Crawler> {
+    return new Crawler(await scraper);
+  }
+
+  public static async with<T>(
+    mapper: Mapper<Crawler, Promise<T>>,
+    scraper?: Promise<Scraper>
+  ): Promise<T> {
+    const crawler = await this.of(scraper);
+    const result = await mapper(crawler);
+
+    await crawler.close();
+    return result;
+  }
+
+  private readonly _scraper: Scraper;
+
+  private constructor(scraper: Scraper) {
+    this._scraper = scraper;
+  }
+
+  /**
+   * Start crawling the specified frontier.
+   *
+   * @remarks
+   * Crawling stops once the frontier has no additional URLs in queue.
+   */
+  public async *crawl(
+    frontier: Frontier,
+    options: Crawler.crawl.Options = {}
+  ): AsyncIterable<Result<Page, [URL, string]>> {
+    while (frontier.hasWaiting()) {
+      const url = frontier.dequeue().get();
+
+      const result = await this._scraper.scrape(url, {
+        ...options,
+        screenshot: options.screenshot?.(url),
+      });
+
+      if (result.isOk()) {
+        frontier.complete(url);
+
+        for (const page of result) {
+          if (page.response.url !== url.href) {
+            frontier.redirect(url.href, page.response.url);
+          }
+
+          for (const url of urls(page)) {
+            frontier.enqueue(url);
+          }
+        }
+      } else {
+        frontier.error(url);
+      }
+
+      yield result.mapErr((error) => [url, error]);
+    }
+  }
+
+  /**
+   * Close the crawler and its associated scraper.
+   */
+  public async close(): Promise<void> {
+    await this._scraper.close();
+  }
+}
+
+export namespace Crawler {
+  export namespace crawl {
+    export interface Options
+      extends Omit<Scraper.scrape.Options, "screenshot"> {
+      screenshot?: Mapper<URL, Screenshot>;
+    }
+  }
+}
+
+function* urls(page: Page): Iterable<URL> {
+  for (const node of page.document.descendants({
+    composed: true,
+    nested: true,
+  })) {
+    if (isElement(node) && node.name === "a") {
+      yield* node
+        .attribute("href")
+        .map((href) => new URL(href.value, page.response.url));
+    }
+  }
+}
