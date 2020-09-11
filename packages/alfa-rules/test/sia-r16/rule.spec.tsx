0be9d0ec8385371b25ceafa3b02321e7968029d5
Kasper Isager <kasperisager@gmail.com>
2020-09-11 09:21:54 +0200

    Add another test case

--- packages/alfa-rules/test/sia-r16/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r16/rule.spec.tsx
@@ -45,6 +45,18 @@ test(`evaluate() passes an <hr> element`, async (t) => {
   ]);
 });
 
+test(`evaluate() passes a non-focusable <div> element with a role of separator`, async (t) => {
+  const separator = <div role="separator" />;
+
+  const document = Document.of([separator]);
+
+  t.deepEqual(await evaluate(R16, { document }), [
+    passed(R16, separator, {
+      1: Outcomes.HasAllStates,
+    }),
+  ]);
+});
+
 test(`evaluate() passes a focusable <div> element with a role of separator and
       an aria-valuenow attribute`, async (t) => {
   const separator = <div role="separator" tabindex="0" aria-valuenow="50" />;
@@ -71,8 +83,8 @@ test(`evaluate() fails a <div> element with a role of checkbox and no
   ]);
 });
 
-test(`evaluate() failed a focusable <div> element with a role of separator and
-      no aria-valuenow attribute`, async (t) => {
+test(`evaluate() fails a focusable <div> element with a role of separator and no
+      aria-valuenow attribute`, async (t) => {
   const separator = <div role="separator" tabindex="0" />;
 
   const document = Document.of([separator]);
