3e220adc65cfc0da99201cd72b61d841a04933dd
Jean-Yves Moyen <jym@siteimprove.com>
2020-09-21 11:57:56 +0200

    Correctly handle children of `<iframe>` elements in accessibility tree and `isRendered()` (#383)
    Co-authored-by: Kasper Isager <kasperisager@gmail.com>

--- packages/alfa-rules/src/common/predicate/is-rendered.ts
+++ packages/alfa-rules/src/common/predicate/is-rendered.ts
@@ -3,8 +3,17 @@ import { Element, Comment, Node } from "@siteimprove/alfa-dom";
 import { Predicate } from "@siteimprove/alfa-predicate";
 import { Style } from "@siteimprove/alfa-style";
 
+const { isElement, hasName } = Element;
+const { and } = Predicate;
+
 export function isRendered(device: Device): Predicate<Node> {
   return (node) => {
+    // Children of <iframe> elements act as fallback content in legacy user
+    // agents and should therefore never be considered rendered.
+    if (node.parent().some(and(isElement, hasName("iframe")))) {
+      return false;
+    }
+
     if (Element.isElement(node)) {
       const display = Style.from(node, device).computed("display").value;
 
