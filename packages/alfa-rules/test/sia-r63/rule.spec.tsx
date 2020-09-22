8128432ac1eaddd4ecb9c8f7bf22d85afc386831
Jean-Yves Moyen <jym@siteimprove.com>
2020-09-22 09:33:10 +0200

    Clean up syntax

--- packages/alfa-rules/test/sia-r63/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r63/rule.spec.tsx
@@ -13,8 +13,8 @@ test("evaluate() passes an object with a non-empty name", async (t) => {
   const document = Document.of([target]);
 
   t.deepEqual(await evaluate(R63, { document }), [
-    passed(R63, target, { 1: Outcomes.HasName})
-  ])
+    passed(R63, target, { 1: Outcomes.HasName }),
+  ]);
 });
 
 test("evaluate() fails an object with an empty name", async (t) => {
@@ -22,36 +22,30 @@ test("evaluate() fails an object with an empty name", async (t) => {
   const document = Document.of([target]);
 
   t.deepEqual(await evaluate(R63, { document }), [
-    failed(R63, target, { 1: Outcomes.HasNoName})
-  ])
+    failed(R63, target, { 1: Outcomes.HasNoName }),
+  ]);
 });
 
 test("evaluate() fails an object with no name", async (t) => {
   // Content document is needed to expose the object.
-  // const target = <object>{h.document(["Some nested document"])}</object>;
-  const target = <object></object>;
-  target._attachContent(h.document(["Some nested document"]));
+  const target = <object>{h.document(["Some nested document"])}</object>;
   const document = Document.of([target]);
 
   t.deepEqual(await evaluate(R63, { document }), [
-    failed(R63, target, { 1: Outcomes.HasNoName})
-  ])
+    failed(R63, target, { 1: Outcomes.HasNoName }),
+  ]);
 });
 
 test("evaluate() is inapplicable if there is no object", async (t) => {
   const target = <img src="foo.jpg"></img>;
   const document = Document.of([target]);
 
-  t.deepEqual(await evaluate(R63, { document }), [
-    inapplicable(R63)
-  ])
+  t.deepEqual(await evaluate(R63, { document }), [inapplicable(R63)]);
 });
 
 test("evaluate() is inapplicable on empty object", async (t) => {
   const target = <object title="Some object"></object>;
   const document = Document.of([target]);
 
-  t.deepEqual(await evaluate(R63, { document }), [
-    inapplicable(R63)
-  ])
+  t.deepEqual(await evaluate(R63, { document }), [inapplicable(R63)]);
 });
