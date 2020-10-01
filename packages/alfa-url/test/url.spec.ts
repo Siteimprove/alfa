741b82fac22d85feeb01fd7d07c010f7f4c65522
Kasper Isager <kasperisager@gmail.com>
2020-10-01 12:06:51 +0200

    Add very minimal spec

--- packages/alfa-url/test/url.spec.ts
+++ packages/alfa-url/test/url.spec.ts
@@ -1,3 +1,37 @@
 import { test } from "@siteimprove/alfa-test";
 
+import { None, Option } from "@siteimprove/alfa-option";
+
 import { URL } from "../src/url";
+
+test(".parse() parses an absolute URL", (t) => {
+  t.deepEqual(
+    URL.parse("https://example.com/page.html").get().toJSON(),
+    URL.of(
+      "https",
+      None,
+      None,
+      Option.of("example.com"),
+      None,
+      ["page.html"],
+      None,
+      None
+    ).toJSON()
+  );
+});
+
+test(".parse() parses a relative URL against a base URL", (t) => {
+  t.deepEqual(
+    URL.parse("/page.html", "https://example.com/").get().toJSON(),
+    URL.of(
+      "https",
+      None,
+      None,
+      Option.of("example.com"),
+      None,
+      ["page.html"],
+      None,
+      None
+    ).toJSON()
+  );
+});
