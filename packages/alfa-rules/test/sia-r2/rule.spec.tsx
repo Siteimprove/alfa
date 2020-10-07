5b9c34c1080bf432252e8eee6743cebdf052c67d
Kasper Isager <kasperisager@gmail.com>
2020-10-07 15:08:09 +0100

    Fix several `Element#tabIndex()` cases

--- packages/alfa-rules/test/sia-r2/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r2/rule.spec.tsx
@@ -32,20 +32,27 @@ test("evaluate() fails an <img> element without an accessible name", async (t) =
   ]);
 });
 
-test("evaluate() is inapplicable to a document without images", async (t) => {
-  const document = Document.empty();
-
-  t.deepEqual(await evaluate(R2, { document }), [inapplicable(R2)]);
-});
-
-test("evaluate() is inapplicable to an image that is marked as decorative", async (t) => {
+test("evaluate() is inapplicable to an <img> element with a presentational role", async (t) => {
   const document = Document.of([<img role="none"></img>]);
 
   t.deepEqual(await evaluate(R2, { document }), [inapplicable(R2)]);
 });
 
-test("evaluate() is inapplicable to an image that is hidden", async (t) => {
+test(`evaluate() is inapplicable to a non-draggable <img> element with a
+      presentational role`, async (t) => {
+  const document = Document.of([<img role="none" draggable="false"></img>]);
+
+  t.deepEqual(await evaluate(R2, { document }), [inapplicable(R2)]);
+});
+
+test("evaluate() is inapplicable to an <img> element that is hidden", async (t) => {
   const document = Document.of([<img hidden alt="Hello world"></img>]);
+
+  t.deepEqual(await evaluate(R2, { document }), [inapplicable(R2)]);
+});
+
+test("evaluate() is inapplicable to a document without images", async (t) => {
+  const document = Document.empty();
 
   t.deepEqual(await evaluate(R2, { document }), [inapplicable(R2)]);
 });
