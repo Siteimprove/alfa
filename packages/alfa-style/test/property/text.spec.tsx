dee04691ac68faee9f9cf9b3eb1a288d0e234865
Jean-Yves Moyen <jym@siteimprove.com>
2020-11-10 14:27:11 +0100

    Add tests

--- packages/alfa-style/test/property/text.spec.tsx
+++ packages/alfa-style/test/property/text.spec.tsx
@@ -6,10 +6,12 @@ import { Device } from "@siteimprove/alfa-device";
 
 import { Style } from "../../src/style";
 
+const device = Device.standard();
+
 test("#cascaded() parses `text-decoration: underline`", (t) => {
   const element = <div style={{ textDecoration: "underline" }}></div>;
 
-  const style = Style.from(element, Device.standard());
+  const style = Style.from(element, device);
 
   t.deepEqual(style.cascaded("text-decoration-line").get().toJSON(), {
     value: {
@@ -45,7 +47,7 @@ test("#cascaded() parses `text-decoration: underline`", (t) => {
 test("#cascaded() parses `text-decoration: underline overline`", (t) => {
   const element = <div style={{ textDecoration: "underline overline" }}></div>;
 
-  const style = Style.from(element, Device.standard());
+  const style = Style.from(element, device);
 
   t.deepEqual(style.cascaded("text-decoration-line").get().toJSON(), {
     value: {
@@ -69,7 +71,7 @@ test("#cascaded() parses `text-decoration: underline overline`", (t) => {
 test("#cascaded() parses `text-decoration: underline solid`", (t) => {
   const element = <div style={{ textDecoration: "underline solid" }}></div>;
 
-  const style = Style.from(element, Device.standard());
+  const style = Style.from(element, device);
 
   t.deepEqual(style.cascaded("text-decoration-line").get().toJSON(), {
     value: {
@@ -97,7 +99,7 @@ test("#cascaded() parses `text-decoration: underline solid`", (t) => {
 test("#cascaded() parses `text-decoration: underline red`", (t) => {
   const element = <div style={{ textDecoration: "underline red" }}></div>;
 
-  const style = Style.from(element, Device.standard());
+  const style = Style.from(element, device);
 
   t.deepEqual(style.cascaded("text-decoration-line").get().toJSON(), {
     value: {
@@ -126,7 +128,7 @@ test("#cascaded() parses `text-decoration: underline red`", (t) => {
 test("#cascaded() parses `text-decoration: underline solid red`", (t) => {
   const element = <div style={{ textDecoration: "underline solid red" }}></div>;
 
-  const style = Style.from(element, Device.standard());
+  const style = Style.from(element, device);
 
   t.deepEqual(style.cascaded("text-decoration-line").get().toJSON(), {
     value: {
@@ -157,5 +159,114 @@ test("#cascaded() parses `text-decoration: underline solid red`", (t) => {
       color: "red",
     },
     source: h.declaration("text-decoration", "underline solid red").toJSON(),
+  });
+});
+
+test("#cascaded() parses `text-indent: <length>`", (t) => {
+  const element = <div style={{ textIndent: "5px" }}></div>;
+
+  const style = Style.from(element, device);
+
+  t.deepEqual(style.cascaded("text-indent").get().toJSON(), {
+    value: [
+      {
+        type: "length",
+        unit: "px",
+        value: 5,
+      },
+      {
+        type: "none",
+      },
+      {
+        type: "none",
+      },
+    ],
+    source: h.declaration("text-indent", "5px").toJSON(),
+  });
+});
+
+test("#cascaded() parses `text-indent: <length> hanging`", (t) => {
+  const element = <div style={{ textIndent: "5px hanging" }}></div>;
+
+  const style = Style.from(element, device);
+
+  t.deepEqual(style.cascaded("text-indent").get().toJSON(), {
+    value: [
+      {
+        type: "length",
+        unit: "px",
+        value: 5,
+      },
+      {
+        type: "some",
+        value: {
+          type: "keyword",
+          value: "hanging",
+        },
+      },
+      {
+        type: "none",
+      },
+    ],
+    source: h.declaration("text-indent", "5px hanging").toJSON(),
+  });
+});
+
+test("#cascaded() parses `text-indent: <length> hanging each-line`", (t) => {
+  const element = <div style={{ textIndent: "5px hanging each-line" }}></div>;
+
+  const style = Style.from(element, device);
+
+  t.deepEqual(style.cascaded("text-indent").get().toJSON(), {
+    value: [
+      {
+        type: "length",
+        unit: "px",
+        value: 5,
+      },
+      {
+        type: "some",
+        value: {
+          type: "keyword",
+          value: "hanging",
+        },
+      },
+      {
+        type: "some",
+        value: {
+          type: "keyword",
+          value: "each-line",
+        },
+      },
+    ],
+    source: h.declaration("text-indent", "5px hanging each-line").toJSON(),
+  });
+});
+
+test("#cascaded() parses `text-indent: <length> each-line`", (t) => {
+  const element = <div style={{ textIndent: "5px each-line" }}></div>;
+
+  const style = Style.from(element, device);
+
+  t.deepEqual(style.cascaded("text-indent").get().toJSON(), {
+    value: [
+      {
+        type: "length",
+        unit: "px",
+        value: 5,
+      },
+
+      {
+        type: "none",
+      },
+      {
+        type: "some",
+        value: {
+          type: "keyword",
+          value: "each-line",
+        },
+      },
+    ],
+    source: h.declaration("text-indent", "5px each-line").toJSON(),
   });
 });
