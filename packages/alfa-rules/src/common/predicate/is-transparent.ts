8a6d771e68d992b5d9775a69c5bacb22254f2418
Jean-Yves Moyen <jym@siteimprove.com>
2020-11-19 16:07:13 +0100

    Cache results

--- packages/alfa-rules/src/common/predicate/is-transparent.ts
+++ packages/alfa-rules/src/common/predicate/is-transparent.ts
@@ -1,27 +1,32 @@
+import { Cache } from "@siteimprove/alfa-cache";
 import { Device } from "@siteimprove/alfa-device";
 import { Element, Node } from "@siteimprove/alfa-dom";
 import { Predicate } from "@siteimprove/alfa-predicate";
 import { Context } from "@siteimprove/alfa-selector";
 import { Style } from "@siteimprove/alfa-style";
 
+const cache = Cache.empty<[Device, Context, Node], boolean>();
+
 export function isTransparent(
   device: Device,
-  context?: Context
+  context: Context = Context.empty()
 ): Predicate<Node> {
   return function isTransparent(node) {
-    if (
-      Element.isElement(node) &&
-      Style.from(node, device, context)
-        .computed("opacity")
-        .some((opacity) => opacity.value === 0)
-    ) {
-      return true;
-    }
+    return cache.get([device, context, node], () => {
+      if (
+        Element.isElement(node) &&
+        Style.from(node, device, context)
+          .computed("opacity")
+          .some((opacity) => opacity.value === 0)
+      ) {
+        return true;
+      }
 
-    return node
-      .parent({
-        flattened: true,
-      })
-      .some(isTransparent);
+      return node
+        .parent({
+          flattened: true,
+        })
+        .some(isTransparent);
+    });
   };
 }
