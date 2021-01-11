f03b589fec81e81b465e185e943a2424be2a6c42
Jean-Yves Moyen <jym@siteimprove.com>
2021-01-11 15:48:39 +0100

    Write basic test

---
+++ packages/alfa-css/test/value/position.spec.ts
@@ -0,0 +1,43 @@
+import { Assertions, test } from "@siteimprove/alfa-test";
+
+import { Slice } from "@siteimprove/alfa-slice";
+
+import { Lexer } from "../../src/syntax/lexer";
+import { Position } from "../../src";
+
+const parser = Position.parseOld;
+
+function parse(t: Assertions, input: string, expected: Position.JSON) {
+  t.deepEqual(
+    parser(Slice.of(Lexer.lex(input)))
+      .map(([_, [horizontal, vertical]]) =>
+        Position.of(vertical.get(), horizontal.get())
+      )
+      .get()
+      .toJSON(),
+    expected,
+    input
+  );
+}
+
+test("parse() parses left top", (t) => {
+  parse(t, "left top", {
+    type: "position",
+    horizontal: {
+      type: "side",
+      offset: null,
+      side: {
+        type: "keyword",
+        value: "left",
+      },
+    },
+    vertical: {
+      type: "side",
+      offset: null,
+      side: {
+        type: "keyword",
+        value: "top",
+      },
+    },
+  });
+});
