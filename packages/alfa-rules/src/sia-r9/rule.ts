9a9e5ebfc07a8c41fde7af36003d8f73366b8a81
Jean-Yves Moyen <jym@siteimprove.com>
2021-01-22 11:36:20 +0100

    Refactor getRefreshTime

--- packages/alfa-rules/src/sia-r9/rule.ts
+++ packages/alfa-rules/src/sia-r9/rule.ts
@@ -1,12 +1,12 @@
 import { Rule, Diagnostic } from "@siteimprove/alfa-act";
 import { Element, Namespace } from "@siteimprove/alfa-dom";
-import { None, Option } from "@siteimprove/alfa-option";
 import { Predicate } from "@siteimprove/alfa-predicate";
 import { Err, Ok } from "@siteimprove/alfa-result";
 import { Criterion, Technique } from "@siteimprove/alfa-wcag";
 import { Page } from "@siteimprove/alfa-web";
 
 import { expectation } from "../common/expectation";
+import { getRefreshTime } from "../common/expectation/get-refresh-time";
 
 import { hasAttribute } from "../common/predicate/has-attribute";
 
@@ -67,45 +67,6 @@ export default Rule.Atomic.of<Page, Element>({
     };
   },
 });
-
-const whitespace = /\s/;
-const digit = /\d/;
-
-/**
- * @see https://html.spec.whatwg.org/#attr-meta-http-equiv-refresh
- */
-function getRefreshTime(content: string): Option<number> {
-  if (content.length === 0) {
-    return None;
-  }
-
-  let i = 0;
-
-  while (whitespace.test(content[i])) {
-    i++;
-  }
-
-  const start = i;
-
-  while (digit.test(content[i])) {
-    i++;
-  }
-
-  if (start === i) {
-    return None;
-  }
-
-  const next = content[i];
-
-  // As long as the time of the refresh is ended correctly, the URL won't matter
-  // in terms of the validity of the refresh. If the URL is therefore invalid,
-  // the refresh will simply redirect to the current page.
-  if (next !== undefined && next !== ";" && next !== ",") {
-    return None;
-  }
-
-  return Option.of(parseInt(content.substring(start, i), 10));
-}
 
 export namespace Outcomes {
   export const HasImmediateRefresh = Ok.of(
