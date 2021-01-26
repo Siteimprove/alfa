3bb0d9becf523bb1864710b96cb69309ba36fd69
Jean-Yves Moyen <jym@siteimprove.com>
2021-01-26 11:31:39 +0100

    Add comment about not implemeting path now

--- packages/alfa-css/src/value/shape/path.ts
+++ packages/alfa-css/src/value/shape/path.ts
@@ -1,3 +1,9 @@
 /**
  * @see https://drafts.csswg.org/css-shapes/#funcdef-path
+ *
+ * Path require parsing a SVG path data string, which we do not have currently.
+ * Given our current use case of the CSS shapes (clipping elements to make them invisible),
+ * it seems unlikely that a `path` will be used.
+ *
+ * So, we do not implement path until the need arise…
  */
