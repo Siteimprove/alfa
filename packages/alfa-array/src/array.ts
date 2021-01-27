65968c774927d921c93a4042c21c67753100c80c
Jean-Yves Moyen <jym@siteimprove.com>
2021-01-27 15:52:26 +0100

    Make utilities work on readonly arrays

--- packages/alfa-array/src/array.ts
+++ packages/alfa-array/src/array.ts
@@ -42,34 +42,34 @@ export namespace Array {
     return [...iterable];
   }
 
-  export function size<T>(array: Array<T>): number {
+  export function size<T>(array: ReadonlyArray<T>): number {
     return array.length;
   }
 
-  export function isEmpty<T>(array: Array<T>): array is Array<never> {
+  export function isEmpty<T>(array: ReadonlyArray<T>): array is Array<never> {
     return array.length === 0;
   }
 
-  export function copy<T>(array: Array<T>): Array<T> {
+  export function copy<T>(array: ReadonlyArray<T>): Array<T> {
     return array.slice(0);
   }
 
-  export function clone<T extends Clone<T>>(array: Array<T>): Array<T> {
+  export function clone<T extends Clone<T>>(array: ReadonlyArray<T>): Array<T> {
     return array.map(Clone.clone);
   }
 
   export function find<T, U extends T>(
-    array: Array<T>,
+    array: ReadonlyArray<T>,
     refinement: Refinement<T, U, [number]>
   ): Option<U>;
 
   export function find<T>(
-    array: Array<T>,
+    array: ReadonlyArray<T>,
     predicate: Predicate<T, [number]>
   ): Option<T>;
 
   export function find<T>(
-    array: Array<T>,
+    array: ReadonlyArray<T>,
     predicate: Predicate<T, [number]>
   ): Option<T> {
     for (let i = 0, n = array.length; i < n; i++) {
@@ -84,17 +84,17 @@ export namespace Array {
   }
 
   export function findLast<T, U extends T>(
-    array: Array<T>,
+    array: ReadonlyArray<T>,
     refinement: Refinement<T, U, [number]>
   ): Option<U>;
 
   export function findLast<T>(
-    array: Array<T>,
+    array: ReadonlyArray<T>,
     predicate: Predicate<T, [number]>
   ): Option<T>;
 
   export function findLast<T>(
-    array: Array<T>,
+    array: ReadonlyArray<T>,
     predicate: Predicate<T, [number]>
   ): Option<T> {
     for (let i = array.length - 1; i >= 0; i--) {
@@ -139,14 +139,14 @@ export namespace Array {
   }
 
   export function compare<T extends Comparable<T>>(
-    a: Array<T>,
+    a: ReadonlyArray<T>,
     b: Iterable<T>
   ): Comparison {
     return compareWith(a, b, compareComparable);
   }
 
   export function compareWith<T>(
-    a: Array<T>,
+    a: ReadonlyArray<T>,
     b: Iterable<T>,
     comparer: Comparer<T>
   ): Comparison {
@@ -154,7 +154,7 @@ export namespace Array {
   }
 
   export function search<T>(
-    array: Array<T>,
+    array: ReadonlyArray<T>,
     value: T,
     comparer: Comparer<T>
   ): number {
@@ -179,7 +179,7 @@ export namespace Array {
     return lower;
   }
 
-  export function equals<T>(a: Array<T>, b: Array<T>): boolean {
+  export function equals<T>(a: ReadonlyArray<T>, b: ReadonlyArray<T>): boolean {
     if (a.length !== b.length) {
       return false;
     }
@@ -193,7 +193,7 @@ export namespace Array {
     return true;
   }
 
-  export function hash<T>(array: Array<T>, hash: Hash): void {
+  export function hash<T>(array: ReadonlyArray<T>, hash: Hash): void {
     for (let i = 0, n = array.length; i < n; i++) {
       Hashable.hash(hash, array[i]);
     }
@@ -201,7 +201,9 @@ export namespace Array {
     Hash.writeUint32(hash, array.length);
   }
 
-  export function toJSON<T>(array: Array<T>): Array<Serializable.ToJSON<T>> {
+  export function toJSON<T>(
+    array: ReadonlyArray<T>
+  ): Array<Serializable.ToJSON<T>> {
     return array.map((value) => Serializable.toJSON(value));
   }
 }
