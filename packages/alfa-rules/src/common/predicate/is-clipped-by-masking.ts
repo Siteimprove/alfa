138d25736c3752c60cb1d97e33af98f812f7179a
Jean-Yves Moyen <jym@siteimprove.com>
2021-02-04 14:50:25 +0100

    Only consider clip on absolutely positioned elements

--- packages/alfa-rules/src/common/predicate/is-clipped-by-masking.ts
+++ packages/alfa-rules/src/common/predicate/is-clipped-by-masking.ts
@@ -21,15 +21,16 @@ export function isClippedByMasking(
       .get(context, Cache.empty)
       .get(node, () => {
         if (isElement(node)) {
-          const { value: clip } = Style.from(node, device, context).computed(
-            "clip"
-          );
+          const style = Style.from(node, device, context);
+          const { value: clip } = style.computed("clip");
+          const { value: position } = style.computed("position");
 
-          // If the clip rectangle is not "auto", and one of its dimension (horizontal/vertical)
+          // If element is absolutely positioned, the clip rectangle is not "auto", and one of its dimension (horizontal/vertical)
           // has equals non-"auto" values, then the element is totally clipped (0 pixels height or width clipping).
           // This does not handle "auto" values in rect() since these depend on the box size which we don't currently have.
           // Additionally, it is assumed that rect(auto, 0px, auto, 0px) is unlikely to be used as clipping method…
           if (
+            (position.value === "absolute" || position.value === "fixed") &&
             clip.type === "shape" &&
             ((clip.shape.top.type === "length" &&
               clip.shape.top.equals(clip.shape.bottom)) ||
