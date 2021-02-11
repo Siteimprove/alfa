e1d2ae1fed20a465e9477a6d8cb42456588a3447
Kasper Isager <kasperisager@gmail.com>
2021-02-11 15:55:13 +0100

    Use new ESLint API

--- scripts/common/linter.js
+++ scripts/common/linter.js
@@ -1,26 +1,26 @@
-const Linter = require("eslint").CLIEngine;
+const Linter = require("eslint").ESLint;
 
 const { system } = require("./system");
 const { flags } = require("./flags");
 
 const linter = new Linter({ useEslintrc: true });
 
-const formatter = Linter.getFormatter(
+const formatter = linter.loadFormatter(
   flags.pretty ? "stylish" : "visualstudio"
 );
 
 exports.linter = {
-  lint(root = "packages") {
-    const { results, errorCount } = linter.executeOnFiles(
+  async lint(root = "packages") {
+    const { results, errorCount } = await linter.lintFiles(
       system
         .readDirectory(root, [".ts", ".tsx"])
-        .filter(fileName => !fileName.endsWith(".d.ts"))
+        .filter((fileName) => !fileName.endsWith(".d.ts"))
     );
 
     if (results.length !== 0) {
-      system.write(formatter(results) + "\n");
+      system.write(formatter(results));
     }
 
     return errorCount === 0 ? 0 : 1;
-  }
+  },
 };
