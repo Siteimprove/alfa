69d450e15a40499ca6811bf8678ccc0d70e32fee
Jean-Yves Moyen <jym@siteimprove.com>
2021-03-03 11:35:22 +0100

    Implement isAtTheStart

--- packages/alfa-rules/src/common/predicate/is-at-the-start.ts
+++ packages/alfa-rules/src/common/predicate/is-at-the-start.ts
@@ -1,6 +1,12 @@
 import { Node } from "@siteimprove/alfa-dom";
 import { Predicate } from "@siteimprove/alfa-predicate";
 import { Option, None } from "@siteimprove/alfa-option";
+import { isPerceivable } from "./is-perceivable";
+import { Device } from "@siteimprove/alfa-device";
+import test = Predicate.test;
+import and = Predicate.and;
+
+const { equals, or } = Predicate;
 
 /**
  * @see https://act-rules.github.io/glossary/#just-before
@@ -16,13 +22,56 @@ import { Option, None } from "@siteimprove/alfa-option";
  * * [N1, …, N2] => N1 is at the start of N2 if there is no perceivable in [N1, …, N2[
  * * [N2, …, N1] => N1 is at the start of N2 if there is no perceivable in [N2, …, N1[
  * In both cases, jumping to N1 give access to all the perceivable nodes after N2, and no more.
+ *
+ * Note that the def is actually symmetrical, but N2 is conceptually a container (`<main>`)
+ * while N1 is a single point (an anchor), hence the asymmetry in the code.
+ *
+ * Complexity: the size of the subtree anchored at the lowest common ancestor.
  **/
-export function isAtTheStart(node1: Node): Predicate<Node> {
-  return function atTheStart(node2: Node): boolean {
+export function isAtTheStart(node2: Node, device: Device): Predicate<Node> {
+  return function isAtTheStart(node1: Node): boolean {
     if (node2.equals(node1)) {
       return true;
     }
-    return false;
+
+    const context = lowestCommonAncestor(node1, node2, {
+      flattened: true,
+      nested: true,
+    });
+    if (context.isNone()) {
+      // the nodes are not even in the same tree…
+      return false;
+    }
+
+    // Get descendants of the LCA, and skip everything before both nodes.
+    let descendants = context
+      .get()
+      .inclusiveDescendants({ flattened: true, nested: true });
+    descendants = descendants.skipUntil(or(equals(node1), equals(node2)));
+
+    // node1 and node2 cannot be equal due to first test, so if the first one is perceivable, this is bad.
+    if (test(isPerceivable(device), descendants.first().get())) {
+      return false;
+    }
+
+    // Go through descendants until we reach perceivable content, or the second of the nodes
+    descendants = descendants.rest();
+    descendants = descendants.skipUntil(
+      or(isPerceivable(device), equals(node1), equals(node2))
+    );
+
+    // if we've found perceivable content which is not the second node, this is bad.
+    // descendant cannot be empty because it contained at least node1 and node2 which are different.
+    if (
+      test(
+        and(isPerceivable(device), or(equals(node1), equals(node2))),
+        descendants.first().get()
+      )
+    ) {
+      return false;
+    }
+
+    return true;
   };
 }
 
