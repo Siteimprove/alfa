0d08c5abebf1eeabc1bacc8e0d6b9e907f04d6e2
Jean-Yves Moyen <jym@siteimprove.com>
2021-03-09 13:48:22 +0100

    Create placeholder radius class

--- packages/alfa-style/src/property/border-bottom-left-radius.ts
+++ packages/alfa-style/src/property/border-bottom-left-radius.ts
@@ -1,53 +1,68 @@
-// import { Length, Percentage } from "@siteimprove/alfa-css";
-// import { Parser } from "@siteimprove/alfa-parser";
-// import { Resolver } from "../resolver";
+import { Length, Percentage, Value } from "@siteimprove/alfa-css";
+import { Parser } from "@siteimprove/alfa-parser";
+import { Resolver } from "../resolver";
 
-// import { Property } from "../property";
+import { Property } from "../property";
 
-// const { takeBetween, either, map } = Parser;
+const { takeBetween, either, map } = Parser;
 
-// /**
-//  * @internal
-//  */
+export class Radius<
+  T extends Length | Percentage = Length | Percentage
+> extends Value<"radius"> {
+  public static of<T extends Length | Percentage = Length | Percentage>(
+    horizontal: T,
+    vertical: T
+  ): Radius<T> {
+    return new Radius<T>(horizontal, vertical);
+  }
+  private readonly _horizontal: T;
+  private readonly _vertical: T;
 
-// export type Specified = [
-//   horizontal: Length | Percentage,
-//   vertical: Length | Percentage
-// ];
+  private constructor(horizontal: T, vertical: T) {
+    super();
+    this._horizontal = horizontal;
+    this._vertical = vertical;
+  }
+}
 
-// /**
-//  * @internal
-//  */
-// export type Computed = [
-//   horizontal: Length<"px"> | Percentage,
-//   vertical: Length<"px"> | Percentage
-// ];
+export namespace Radius {
+  /**
+   * @internal
+   */
+  export const parse = map(
+    takeBetween(either(Length.parse, Percentage.parse), 1, 2),
+    // TODO
+    ([horizontal, vertical = horizontal]) => Radius.of(horizontal, vertical)
+  );
+}
 
-// /**
-//  * @internal
-//  */
-// export const parse = map(
-//   takeBetween(either(Length.parse, Percentage.parse), 1, 2),
-//   // TODO
-//   ([horizontal, vertical] = horizontal) => [horizontal, vertical] as const
-// );
+/**
+ * @internal
+ */
 
-// /**
-//  * @see https://developer.mozilla.org/en-US/docs/Web/CSS/border-bottom-left-radius
-//  * @internal
-//  */
-// export default Property.of<Specified, Computed>(
-//   [Length.of(0, "px"), Length.of(0, "px")],
-//   // TODO
-//   parse,
-//   (leftRadius, style) => {
-//     leftRadius.value.map((value) => {
-//       switch (value.type) {
-//         case "length":
-//           return Resolver.length(value, style);
-//         default:
-//           return value;
-//       }
-//     });
-//   }
-// );
+export type Specified = Radius<Length | Percentage>;
+
+/**
+ * @internal
+ */
+export type Computed = Radius<Length<"px"> | Percentage>;
+
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/CSS/border-bottom-left-radius
+ * @internal
+ */
+export default Property.of<Specified, Computed>(
+  Radius.of(Length.of(0, "px"), Length.of(0, "px")),
+  // TODO
+  Radius.parse,
+  (leftRadius, style) => {
+    leftRadius.value.map((value) => {
+      switch (value.type) {
+        case "length":
+          return Resolver.length(value, style);
+        default:
+          return value;
+      }
+    });
+  }
+);
