0cb2a1693f6a13d6b6c80c86cd959917b72c5b81
Jean-Yves Moyen <jym@siteimprove.com>
2021-03-09 15:55:31 +0100

    Correct handling of `opacity` when computing foreground color of elements (#739)
    * Add failing example
    * Default to standard device if none is provided
    * Fix handling of foreground opacity
    * Handle opacity at layers reduction time
    * Add examples fix opacity and alpha channel
    
    Co-authored-by: Kasper Isager <kasperisager@gmail.com>

---
+++ packages/alfa-rules/test/common/expectation/get-colors.spec.tsx
@@ -0,0 +1,118 @@
+import { h } from "@siteimprove/alfa-dom/h";
+import { jsx } from "@siteimprove/alfa-dom/jsx";
+import { test } from "@siteimprove/alfa-test";
+
+import { Iterable } from "@siteimprove/alfa-iterable";
+
+import {
+  getBackground,
+  getForeground,
+} from "../../../src/common/expectation/get-colors";
+
+test("getBackground() handles opacity correctly", (t) => {
+  const target = (
+    <span
+      style={{ backgroundColor: "#005BBB", color: "white", opacity: "0.4" }}
+    >
+      Hello
+    </span>
+  );
+
+  h.document([
+    <html>
+      <div style={{ backgroundColor: "white" }}>{target}</div>
+    </html>,
+  ]);
+
+  t.deepEqual(Iterable.first(getBackground(target).get()).get().toJSON(), {
+    type: "color",
+    format: "rgb",
+    red: { type: "percentage", value: 0.6 },
+    green: { type: "percentage", value: 0.7427451 },
+    blue: { type: "percentage", value: 0.8933333 },
+    alpha: { type: "percentage", value: 1 },
+  });
+});
+
+test("getBackground() handles mix of opacity and transparency", (t) => {
+  const target = (
+    <span
+      style={{
+        backgroundColor: "rgba(0, 0, 255, .5)",
+        color: "white",
+        opacity: "0.5",
+      }}
+    >
+      Hello
+    </span>
+  );
+
+  h.document([
+    <html>
+      <div style={{ backgroundColor: "red" }}>{target}</div>
+    </html>,
+  ]);
+
+  t.deepEqual(Iterable.first(getBackground(target).get()).get().toJSON(), {
+    type: "color",
+    format: "rgb",
+    red: { type: "percentage", value: 0.75 },
+    green: { type: "percentage", value: 0 },
+    blue: { type: "percentage", value: 0.25 },
+    alpha: { type: "percentage", value: 1 },
+  });
+});
+
+test("getForeground() handles opacity correctly", (t) => {
+  const target = (
+    <span
+      style={{ backgroundColor: "#005BBB", color: "white", opacity: "0.4" }}
+    >
+      Hello
+    </span>
+  );
+
+  h.document([
+    <html>
+      <div style={{ backgroundColor: "white" }}>{target}</div>
+    </html>,
+  ]);
+
+  t.deepEqual(Iterable.first(getForeground(target).get()).get().toJSON(), {
+    type: "color",
+    format: "rgb",
+    red: { type: "percentage", value: 1 },
+    green: { type: "percentage", value: 1 },
+    blue: { type: "percentage", value: 1 },
+    alpha: { type: "percentage", value: 1 },
+  });
+});
+
+test("getForeground() handles mix of opacity and transparency", (t) => {
+  const target = (
+    <span
+      style={{
+        backgroundColor: "blue",
+        color: "rgba(255, 255, 255, .5)",
+        opacity: "0.5",
+      }}
+    >
+      Hello
+    </span>
+  );
+
+  h.document([
+    <html>
+      <div style={{ backgroundColor: "red" }}>{target}</div>
+    </html>,
+  ]);
+
+  t.deepEqual(Iterable.first(getForeground(target).get()).get().toJSON(), {
+    type: "color",
+    format: "rgb",
+    red: { type: "percentage", value: 0.75 },
+    green: { type: "percentage", value: 0.25 },
+    blue: { type: "percentage", value: 0.5 },
+    alpha: { type: "percentage", value: 1 },
+  });
+});
