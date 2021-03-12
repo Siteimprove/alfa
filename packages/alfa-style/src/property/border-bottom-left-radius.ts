55ed74b2dcc9efe45bd5f60abde6bc0a7fb98a23
Tomas Vemola <tvm@siteimprove.com>
2021-03-12 13:03:38 +0100

    border-bottom-left-radius wip

--- packages/alfa-style/src/property/border-bottom-left-radius.ts
+++ packages/alfa-style/src/property/border-bottom-left-radius.ts
@@ -1,19 +1,21 @@
 import { Length, Percentage, Value } from "@siteimprove/alfa-css";
 import { Parser } from "@siteimprove/alfa-parser";
-import { Resolver } from "../resolver";
+import { Hash } from "@siteimprove/alfa-hash";
 
 import { Property } from "../property";
+import { Resolver } from "../resolver";
+import { List } from "@siteimprove/alfa-list";
 
 const { takeBetween, either, map } = Parser;
 
-export class Radius<
+export class BorderRadius<
   T extends Length | Percentage = Length | Percentage
 > extends Value<"radius"> {
   public static of<T extends Length | Percentage = Length | Percentage>(
     horizontal: T,
     vertical: T
-  ): Radius<T> {
-    return new Radius<T>(horizontal, vertical);
+  ): BorderRadius<T> {
+    return new BorderRadius<T>(horizontal, vertical);
   }
   private readonly _horizontal: T;
   private readonly _vertical: T;
@@ -23,16 +25,62 @@ export class Radius<
     this._horizontal = horizontal;
     this._vertical = vertical;
   }
+
+  public get type(): "radius" {
+    return "radius";
+  }
+
+  public get horizontal(): T {
+    return this._horizontal;
+  }
+
+  public get vertical(): T {
+    return this._vertical;
+  }
+
+  public equals(value: BorderRadius): boolean;
+
+  public equals(value: unknown): value is this;
+
+  public equals(value: unknown): boolean {
+    return (
+      value instanceof BorderRadius &&
+      value._horizontal.equals(this._horizontal) &&
+      value._vertical.equals(this._vertical)
+    );
+  }
+
+  public hash(hash: Hash) {
+    this._horizontal.hash(hash);
+    this._vertical.hash(hash);
+  }
+
+  public toJSON(): BorderRadius.JSON {
+    return {
+      type: "radius",
+      horizontal: this._horizontal.toJSON(),
+      vertical: this._vertical.toJSON(),
+    };
+  }
+
+  public toString(): string {
+    return `radius(${this._horizontal.toString()}, ${this._vertical.toString()})`;
+  }
 }
 
-export namespace Radius {
-  /**
-   * @internal
-   */
+/**
+ * @public
+ */
+export namespace BorderRadius {
+  export interface JSON extends Value.JSON<"radius"> {
+    horizontal: Length.JSON | Percentage.JSON;
+    vertical: Length.JSON | Percentage.JSON;
+  }
+
   export const parse = map(
     takeBetween(either(Length.parse, Percentage.parse), 1, 2),
-    // TODO
-    ([horizontal, vertical = horizontal]) => Radius.of(horizontal, vertical)
+    ([horizontal, vertical = horizontal]) =>
+      BorderRadius.of(horizontal, vertical)
   );
 }
 
@@ -40,29 +88,39 @@ export namespace Radius {
  * @internal
  */
 
-export type Specified = Radius<Length | Percentage>;
+export type Specified = BorderRadius<Length | Percentage>;
 
 /**
  * @internal
  */
-export type Computed = Radius<Length<"px"> | Percentage>;
+export type Computed = BorderRadius<Length<"px"> | Percentage>;
 
 /**
  * @see https://developer.mozilla.org/en-US/docs/Web/CSS/border-bottom-left-radius
  * @internal
  */
 export default Property.of<Specified, Computed>(
-  Radius.of(Length.of(0, "px"), Length.of(0, "px")),
+  BorderRadius.of(Length.of(0, "px"), Length.of(0, "px")),
   // TODO
-  Radius.parse,
-  (leftRadius, style) => {
-    leftRadius.value.map((value) => {
-      switch (value.type) {
-        case "length":
-          return Resolver.length(value, style);
-        default:
-          return value;
-      }
-    });
+  BorderRadius.parse,
+  (value, style) => {
+    // return List.of(
+    //   [...value].map((borderRadius) => {
+    //     BorderRadius.of(
+    //       Length.isLength(borderRadius.horizontal)
+    //         ? Resolver.length(borderRadius.horizontal, style)
+    //         : borderRadius.horizontal,
+    //       Length.isLength(borderRadius.vertical)
+    //         ? Resolver.length(borderRadius.vertical, style)
+    //         : borderRadius.vertical
+    //     );
+    //   })
+    // );
+    // return value.map((value) => {
+    //   if (Length.isLength(value)) {
+    //     return Resolver.length(value, style);
+    //   }
+    //   return value;
+    // });
   }
 );
