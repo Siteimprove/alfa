2bcd6eee1241e687220146eee2dde021e01ac3ed
Jean-Yves Moyen <jym@siteimprove.com>
2021-03-16 15:30:20 +0100

    Restrict R74 to elements with a paragraph role

--- packages/alfa-rules/test/sia-r74/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r74/rule.spec.tsx
@@ -7,9 +7,9 @@ import R74, { Outcomes } from "../../src/sia-r74/rule";
 import { evaluate } from "../common/evaluate";
 import { passed, failed, inapplicable } from "../common/outcome";
 
-test(`evaluate() passes an element with a font size specified using a relative
+test(`evaluate() passes a paragraph with a font size specified using a relative
       length`, async (t) => {
-  const target = <div style={{ fontSize: "1em" }}>Hello world</div>;
+  const target = <p style={{ fontSize: "1em" }}>Hello world</p>;
 
   const document = Document.of([target]);
 
@@ -20,9 +20,9 @@ test(`evaluate() passes an element with a font size specified using a relative
   ]);
 });
 
-test(`evaluate() fails an element with a font size specified using an absolute
+test(`evaluate() fails a paragraph with a font size specified using an absolute
       length`, async (t) => {
-  const target = <div style={{ fontSize: "16px" }}>Hello world</div>;
+  const target = <p style={{ fontSize: "16px" }}>Hello world</p>;
 
   const document = Document.of([target]);
 
@@ -33,18 +33,47 @@ test(`evaluate() fails an element with a font size specified using an absolute
   ]);
 });
 
-test("evaluate() is inapplicable to an element that has no text", async (t) => {
-  const document = Document.of([<div style={{ fontSize: "16px" }} />]);
+test("evaluate() is inapplicable to a paragraph that has no text", async (t) => {
+  const document = Document.of([<p style={{ fontSize: "16px" }} />]);
 
   t.deepEqual(await evaluate(R74, { document }), [inapplicable(R74)]);
 });
 
-test("evaluate() is inapplicable to an element that isn't visible", async (t) => {
+test("evaluate() is inapplicable to a paragraph that isn't visible", async (t) => {
   const document = Document.of([
-    <div style={{ fontSize: "16px" }} hidden>
+    <p style={{ fontSize: "16px" }} hidden>
       Hello world
-    </div>,
+    </p>,
   ]);
+
+  t.deepEqual(await evaluate(R74, { document }), [inapplicable(R74)]);
+});
+
+test(`evaluate() fails an ARIA paragraph with a font size specified using an absolute
+      length`, async (t) => {
+  const target = (
+    <div role="paragraph" style={{ fontSize: "16px" }}>
+      Hello world
+    </div>
+  );
+
+  const document = Document.of([target]);
+
+  t.deepEqual(await evaluate(R74, { document }), [
+    failed(R74, target, {
+      1: Outcomes.HasAbsoluteUnit,
+    }),
+  ]);
+});
+
+test(`evaluate() ignores <p> element whose role is changed`, async (t) => {
+  const target = (
+    <p role="generic" style={{ fontSize: "16px" }}>
+      Hello world
+    </p>
+  );
+
+  const document = Document.of([target]);
 
   t.deepEqual(await evaluate(R74, { document }), [inapplicable(R74)]);
 });
