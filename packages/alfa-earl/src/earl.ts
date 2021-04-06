6ed1c2ce12b141b0d788889b30e99893e664ecdd
Kasper Isager <kasperisager@gmail.com>
2021-04-06 13:12:18 +0200

    Add additional vendor data to EARL and SARIF output (#753)

--- packages/alfa-earl/src/earl.ts
+++ packages/alfa-earl/src/earl.ts
@@ -8,6 +8,8 @@ export interface EARL extends Document {
     earl?: "http://www.w3.org/ns/earl#";
     cnt?: "http://www.w3.org/2011/content#";
     dct?: "http://purl.org/dc/terms/";
+    doap?: "http://usefulinc.com/ns/doap#";
+    foaf?: "http://xmlns.com/foaf/spec/#";
     http?: "http://www.w3.org/2011/http#";
     ptr?: "http://www.w3.org/2009/pointers#";
   };
--- packages/alfa-formatter-sarif/src/sarif.ts
+++ packages/alfa-formatter-sarif/src/sarif.ts
@@ -1,6 +1,8 @@
 import { Serializable, Log } from "@siteimprove/alfa-sarif";
 import { Formatter } from "@siteimprove/alfa-formatter";
 
+import { version } from "../package.json";
+
 const { stringify } = JSON;
 
 /**
@@ -9,13 +11,21 @@ const { stringify } = JSON;
 export default function <I, T, Q>(): Formatter<I, T, Q> {
   return function SARIF(input, rules, outcomes) {
     const log: Log = {
-      $schema: "https://json.schemastore.org/sarif-2.1.0-rtm.5.json",
+      $schema: "https://json.schemastore.org/sarif-2.1.0.json",
       version: "2.1.0",
       runs: [
         {
           tool: {
             driver: {
               name: "Alfa",
+              informationUri: "https://alfa.siteimprove.com/",
+              version,
+              shortDescription: {
+                text:
+                  "Suite of open and standards-based tools for performing reliable accessibility conformance testing at scale",
+              },
+              downloadUri: "https://github.com/siteimprove/alfa",
+              organization: "Siteimprove A/S",
               rules: [...rules].map((rule) => rule.toSARIF()),
             },
           },
