a23c445bda309573b52b43869efead1d1ffa721c
Jean-Yves Moyen <jym@siteimprove.com>
2021-04-06 15:53:43 +0200

    Add tests and refactor

--- packages/alfa-style/test/property/border-[top,right,bottom,left].spec.tsx
+++ packages/alfa-style/test/property/border-[top,right,bottom,left].spec.tsx
@@ -1,10 +1,64 @@
 import { Device } from "@siteimprove/alfa-device";
-import { h } from "@siteimprove/alfa-dom";
-import { test } from "@siteimprove/alfa-test";
+import { Declaration, h } from "@siteimprove/alfa-dom";
+import { Assertions, test } from "@siteimprove/alfa-test";
 
 import { Style } from "../../src";
 
 const device = Device.standard();
+
+function testStyle(
+  t: Assertions,
+  style: Style,
+  declaration: Declaration,
+  side: "top" | "right" | "bottom" | "left",
+  color: string,
+  styleValue: "none" | "dotted", // the only one used in the tests
+  width: number
+): void {
+  t.deepEqual(
+    style
+      .cascaded(`border-${side}-color` as const)
+      .get()
+      .toJSON(),
+    {
+      value: {
+        format: "named",
+        type: "color",
+        color: color,
+      },
+      source: declaration.toJSON(),
+    }
+  );
+
+  t.deepEqual(
+    style
+      .cascaded(`border-${side}-style` as const)
+      .get()
+      .toJSON(),
+    {
+      value: {
+        type: "keyword",
+        value: styleValue,
+      },
+      source: declaration.toJSON(),
+    }
+  );
+
+  t.deepEqual(
+    style
+      .cascaded(`border-${side}-width` as const)
+      .get()
+      .toJSON(),
+    {
+      value: {
+        type: "length",
+        value: width,
+        unit: "px",
+      },
+      source: declaration.toJSON(),
+    }
+  );
+}
 
 for (const side of ["top"] as const) {
   const shorthand = `border-${side}` as const;
@@ -17,48 +71,28 @@ for (const side of ["top"] as const) {
 
     const style = Style.from(element, device);
 
-    t.deepEqual(
-      style
-        .cascaded(`${shorthand}-color` as const)
-        .get()
-        .toJSON(),
-      {
-        value: {
-          format: "named",
-          type: "color",
-          color: "red",
-        },
-        source: declaration.toJSON(),
-      }
-    );
+    testStyle(t, style, declaration, side, "red", "none", 3);
+  });
 
-    t.deepEqual(
-      style
-        .cascaded(`${shorthand}-style` as const)
-        .get()
-        .toJSON(),
-      {
-        value: {
-          type: "keyword",
-          value: "none",
-        },
-        source: declaration.toJSON(),
-      }
-    );
+  test(`#cascaded() parses \`${shorthand}: red dotted\``, (t) => {
+    const element = <div />;
+    const declaration = h.declaration(shorthand, "red dotted");
 
-    t.deepEqual(
-      style
-        .cascaded(`${shorthand}-width` as const)
-        .get()
-        .toJSON(),
-      {
-        value: {
-          type: "length",
-          value: 3,
-          unit: "px",
-        },
-        source: declaration.toJSON(),
-      }
-    );
+    h.document([element], [h.sheet([h.rule.style("div", [declaration])])]);
+
+    const style = Style.from(element, device);
+
+    testStyle(t, style, declaration, side, "red", "dotted", 3);
+  });
+
+  test(`#cascaded() parses \`${shorthand}: 2px dotted red\``, (t) => {
+    const element = <div />;
+    const declaration = h.declaration(shorthand, "2px dotted red");
+
+    h.document([element], [h.sheet([h.rule.style("div", [declaration])])]);
+
+    const style = Style.from(element, device);
+
+    testStyle(t, style, declaration, side, "red", "dotted", 2);
   });
 }
