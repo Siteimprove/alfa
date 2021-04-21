47fc38439d77f81da0c19d0c1c69ed52a3ecaca9
Jean-Yves Moyen <jym@siteimprove.com>
2021-04-21 13:18:52 +0200

    Add role and required parents in Diagnostic for R42

--- packages/alfa-rules/src/sia-r42/rule.ts
+++ packages/alfa-rules/src/sia-r42/rule.ts
@@ -1,7 +1,9 @@
 import { Rule, Diagnostic } from "@siteimprove/alfa-act";
 import { Role } from "@siteimprove/alfa-aria";
+import { Array } from "@siteimprove/alfa-array";
 import { Device } from "@siteimprove/alfa-device";
 import { Element, Namespace } from "@siteimprove/alfa-dom";
+import { None, Option } from "@siteimprove/alfa-option";
 import { Predicate } from "@siteimprove/alfa-predicate";
 import { Ok, Err } from "@siteimprove/alfa-result";
 import { Criterion } from "@siteimprove/alfa-wcag";
@@ -36,11 +38,17 @@ export default Rule.Atomic.of<Page, Element>({
       },
 
       expectations(target) {
+        const node = aria.Node.from(target, device);
+        const role = node.role.map((role) => role.name).getOr("");
+        const requiredParents = node.role
+          .map((role) => role.requiredParent)
+          .getOr([]);
+
         return {
           1: expectation(
-            hasRequiredParent(device)(target),
-            () => Outcomes.IsOwnedByContextRole,
-            () => Outcomes.IsNotOwnedByContextRole
+            hasRequiredParent(device)(node),
+            () => Outcomes.IsOwnedByContextRole(role, requiredParents),
+            () => Outcomes.IsNotOwnedByContextRole(role, requiredParents)
           ),
         };
       },
@@ -49,23 +57,33 @@ export default Rule.Atomic.of<Page, Element>({
 });
 
 export namespace Outcomes {
-  export const IsOwnedByContextRole = Ok.of(
-    Diagnostic.of(
-      `The element is owned by an element of its required context role`
-    )
-  );
+  export const IsOwnedByContextRole = (
+    role: Role.Name | "",
+    requiredParents: ReadonlyArray<ReadonlyArray<Role.Name>>
+  ) =>
+    Ok.of(
+      RequiredParent.of(
+        `The element is owned by an element of its required context role`,
+        role,
+        requiredParents
+      )
+    );
 
-  export const IsNotOwnedByContextRole = Err.of(
-    Diagnostic.of(
-      `The element is not owned by an element of its required context role`
-    )
-  );
+  export const IsNotOwnedByContextRole = (
+    role: Role.Name | "",
+    requiredParents: ReadonlyArray<ReadonlyArray<Role.Name>>
+  ) =>
+    Err.of(
+      RequiredParent.of(
+        `The element is not owned by an element of its required context role`,
+        role,
+        requiredParents
+      )
+    );
 }
 
-function hasRequiredParent(device: Device): Predicate<Element> {
-  return (element) => {
-    const node = aria.Node.from(element, device);
-
+function hasRequiredParent(device: Device): Predicate<aria.Node> {
+  return (node) => {
     return node.role
       .filter((role) => role.hasRequiredParent())
       .every((role) =>
@@ -98,5 +116,67 @@ function isRequiredParent(
 
       return false;
     };
+  }
+}
+
+class RequiredParent extends Diagnostic {
+  public static of(
+    message: string,
+    role: Role.Name | "" = "",
+    requiredParents: ReadonlyArray<ReadonlyArray<Role.Name>> = []
+  ): RequiredParent {
+    return new RequiredParent(message, role, requiredParents);
+  }
+
+  private readonly _role: Role.Name | "";
+  private readonly _requiredParents: ReadonlyArray<ReadonlyArray<Role.Name>>;
+
+  private constructor(
+    message: string,
+    role: Role.Name | "",
+    requiredParents: ReadonlyArray<ReadonlyArray<Role.Name>>
+  ) {
+    super(message);
+    this._role = role;
+    this._requiredParents = requiredParents;
+  }
+
+  public get role(): Role.Name | "" {
+    return this._role;
+  }
+
+  public get requiredParents(): ReadonlyArray<ReadonlyArray<Role.Name>> {
+    return this._requiredParents;
+  }
+
+  public equals(value: RequiredParent): boolean;
+
+  public equals(value: unknown): value is this;
+
+  public equals(value: unknown): boolean {
+    return (
+      value instanceof RequiredParent &&
+      value._message === this._message &&
+      value._role === this._role &&
+      value._requiredParents.length === this._requiredParents.length &&
+      Array.equals(value._requiredParents, this._requiredParents)
+    );
+  }
+
+  public toJSON(): RequiredParent.JSON {
+    return {
+      ...super.toJSON(),
+      role: this._role,
+      requiredParents: Array.toJSON(
+        this._requiredParents.map((ancestors) => Array.toJSON(ancestors))
+      ),
+    };
+  }
+}
+
+namespace RequiredParent {
+  export interface JSON extends Diagnostic.JSON {
+    role: Role.Name | "";
+    requiredParents: Array<Array<Role.Name>>;
   }
 }
