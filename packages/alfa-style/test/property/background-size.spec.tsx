b6e4038f2db91df8128ae1ac5d9a9e278565e531
Jean-Yves Moyen <jym@siteimprove.com>
2021-05-04 13:52:57 +0200

    Add failing examples

--- packages/alfa-style/test/property/background-size.spec.tsx
+++ packages/alfa-style/test/property/background-size.spec.tsx
@@ -1,3 +1,4 @@
+import { Device } from "@siteimprove/alfa-device";
 import { h } from "@siteimprove/alfa-dom";
 import { test } from "@siteimprove/alfa-test";
 import { Style } from "../../src";
@@ -21,5 +22,124 @@ test("#cascaded() parses `background-size: cover`", (t) => {
       separator: ", ",
     },
     source: h.declaration("background-size", "cover").toJSON(),
+  });
+});
+
+test("#cascaded() parses `background-size: 10px`", (t) => {
+  const element = <div style={{ backgroundSize: `10px` }} />;
+
+  const style = Style.from(element, device);
+
+  t.deepEqual(style.cascaded("background-size").get().toJSON(), {
+    value: {
+      type: "list",
+      values: [
+        [
+          {
+            type: "length",
+            value: 10,
+            unit: "px",
+          },
+          {
+            type: "keyword",
+            value: "auto",
+          },
+        ],
+      ],
+      separator: ", ",
+    },
+    source: h.declaration("background-size", "10px").toJSON(),
+  });
+});
+
+test("#cascaded() parses `background-size: 10%`", (t) => {
+  const element = <div style={{ backgroundSize: `10%` }} />;
+
+  const style = Style.from(element, device);
+
+  t.deepEqual(style.cascaded("background-size").get().toJSON(), {
+    value: {
+      type: "list",
+      values: [
+        [
+          {
+            type: "percentage",
+            value: 0.1,
+          },
+          {
+            type: "keyword",
+            value: "auto",
+          },
+        ],
+      ],
+      separator: ", ",
+    },
+    source: h.declaration("background-size", "10%").toJSON(),
+  });
+});
+
+test("#cascaded() parses `background-size: 10px 20px`", (t) => {
+  const element = <div style={{ backgroundSize: `10px 20px` }} />;
+
+  const style = Style.from(element, device);
+
+  t.deepEqual(style.cascaded("background-size").get().toJSON(), {
+    value: {
+      type: "list",
+      values: [
+        [
+          {
+            type: "length",
+            value: 10,
+            unit: "px",
+          },
+          {
+            type: "length",
+            value: 20,
+            unit: "px",
+          },
+        ],
+      ],
+      separator: ", ",
+    },
+    source: h.declaration("background-size", "10px 20px").toJSON(),
+  });
+});
+
+test("#cascaded() parses `background-size: 10px, 20px`", (t) => {
+  const element = <div style={{ backgroundSize: `10px, 20px` }} />;
+
+  const style = Style.from(element, device);
+
+  t.deepEqual(style.cascaded("background-size").get().toJSON(), {
+    value: {
+      type: "list",
+      values: [
+        [
+          {
+            type: "length",
+            value: 10,
+            unit: "px",
+          },
+          {
+            type: "keyword",
+            value: "auto",
+          },
+        ],
+        [
+          {
+            type: "length",
+            value: 20,
+            unit: "px",
+          },
+          {
+            type: "keyword",
+            value: "auto",
+          },
+        ],
+      ],
+      separator: ", ",
+    },
+    source: h.declaration("background-size", "10px, 20px").toJSON(),
   });
 });
