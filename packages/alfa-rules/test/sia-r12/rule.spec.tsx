224265d5df35b04a85ae9f35e0943553df42e49f
elenamongelli <52746406+elenamongelli@users.noreply.github.com>
2021-05-07 16:26:14 +0200

    Pass tests for R12 implemented, starting with failed

---
+++ packages/alfa-rules/test/sia-r12/rule.spec.tsx
@@ -0,0 +1,56 @@
+import { test } from "@siteimprove/alfa-test";
+
+import { Document } from "@siteimprove/alfa-dom";
+
+import R12, { Outcomes } from "../../src/sia-r12/rule";
+
+import { evaluate } from "../common/evaluate";
+import { passed, failed, inapplicable } from "../common/outcome";
+
+test(`evaluates() passes a <button> with accessible name `, async (t) => {
+  const target = <button>My button</button>;
+
+  const document = Document.of([target]);
+
+  t.deepEqual(await evaluate(R12, { document }), [
+    passed(R12, target, {
+      1: Outcomes.HasName,
+    }),
+  ]);
+});
+
+test(`evaluates() passes an input element with accessible value attribute `, async (t) => {
+  const input = <input type="submit" value="Submit" />;
+
+  const document = Document.of([input]);
+
+  t.deepEqual(await evaluate(R12, { document }), [
+    passed(R12, input, {
+      1: Outcomes.HasName,
+    }),
+  ]);
+});
+
+test(`evaluates() passes a <button> with aria label attribute`, async (t) => {
+  const target = <button aria-label="My button"></button>;
+
+  const document = Document.of([target]);
+
+  t.deepEqual(await evaluate(R12, { document }), [
+    passed(R12, target, {
+      1: Outcomes.HasName,
+    }),
+  ]);
+});
+
+test(`evaluates() fails <button> with no attribute`, async (t) => {
+  const target = <button></button>;
+
+  const document = Document.of([target]);
+
+  t.deepEqual(await evaluate(R12, { document }), [
+    failed(R12, target, {
+      1: Outcomes.HasNoName,
+    }),
+  ]);
+});
