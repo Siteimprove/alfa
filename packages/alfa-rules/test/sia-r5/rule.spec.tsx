20efddae3ae2452f637ca519d24c76c27db0d39e
elenamongelli <52746406+elenamongelli@users.noreply.github.com>
2021-05-07 12:14:07 +0200

    variable const target changed to html

--- packages/alfa-rules/test/sia-r5/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r5/rule.spec.tsx
@@ -8,45 +8,44 @@ import { evaluate } from "../common/evaluate";
 import { passed, failed, inapplicable } from "../common/outcome";
 
 test(`evaluate() passes a lang attribute with valid primary tag`, async (t) => {
-    const html = <html lang="en"></html>;
-  
-    const document = Document.of([target]);
-  
-    t.deepEqual(await evaluate(R5, { document }), [
-      passed(R5, target.attribute("lang").get(), {
-        1: Outcomes.HasValidLanguage,
-      }),
-    ]);
-  });
+  const html = <html lang="en"></html>;
 
-  test(`evaluate() passes a lang attribute with valid primary tag and invalid region subtag`, async (t) => {
-    const target = <html lang="en-US-GB"></html>;
-  
-    const document = Document.of([target]);
-  
-    t.deepEqual(await evaluate(R5, { document }), [
-      passed(R5, target.attribute("lang").get(), {
-        1: Outcomes.HasValidLanguage,
-      }),
-    ]);
-  });
+  const document = Document.of([html]);
 
-  test(`evaluate() fails a lang attribute with invalid primary tag.`, async (t) => {
-    const target = <html lang="invalid"></html>;
-  
-    const document = Document.of([target]);
-  
-    t.deepEqual(await evaluate(R5, { document }), [
-      failed(R5, target.attribute("lang").get(), {
-        1: Outcomes.HasNoValidLanguage,
-      }),
-    ]);
-  });
+  t.deepEqual(await evaluate(R5, { document }), [
+    passed(R5, html.attribute("lang").get(), {
+      1: Outcomes.HasValidLanguage,
+    }),
+  ]);
+});
 
+test(`evaluate() passes a lang attribute with valid primary tag and invalid region subtag`, async (t) => {
+  const html = <html lang="en-US-GB"></html>;
 
-  test(`evaluate() is inapplicable to svg elements.`, async (t) => {
-    const target = <svg xmlns="http://www.w3.org/2000/svg" lang="fr"></svg>;
-    const document = Document.of([target]);
+  const document = Document.of([html]);
 
-    t.deepEqual(await evaluate(R5, { document }), [inapplicable(R5)]);
+  t.deepEqual(await evaluate(R5, { document }), [
+    passed(R5, html.attribute("lang").get(), {
+      1: Outcomes.HasValidLanguage,
+    }),
+  ]);
+});
+
+test(`evaluate() fails a lang attribute with invalid primary tag.`, async (t) => {
+  const html = <html lang="invalid"></html>;
+
+  const document = Document.of([html]);
+
+  t.deepEqual(await evaluate(R5, { document }), [
+    failed(R5, html.attribute("lang").get(), {
+      1: Outcomes.HasNoValidLanguage,
+    }),
+  ]);
+});
+
+test(`evaluate() is inapplicable to svg elements.`, async (t) => {
+  const html = <svg xmlns="http://www.w3.org/2000/svg" lang="fr"></svg>;
+  const document = Document.of([html]);
+
+  t.deepEqual(await evaluate(R5, { document }), [inapplicable(R5)]);
 });
