a561f378b2f7d355ceb1a42c6e203776de46f328
elenamongelli <52746406+elenamongelli@users.noreply.github.com>
2021-05-11 14:43:56 +0200

    Apply suggestions from code review
    Co-authored-by: Jean-Yves Moyen <jym@siteimprove.com>

--- packages/alfa-rules/test/sia-r12/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r12/rule.spec.tsx
@@ -19,7 +19,7 @@ test(`evaluates() passes a <button> with accessible name `, async (t) => {
   ]);
 });
 
-test(`evaluates() passes an input element with accessible value attribute `, async (t) => {
+test(`evaluates() passes a submit button element with an accessible name given by the value attribute `, async (t) => {
   const input = <input type="submit" value="Submit" />;
 
   const document = Document.of([input]);
@@ -31,7 +31,7 @@ test(`evaluates() passes an input element with accessible value attribute `, asy
   ]);
 });
 
-test(`evaluates() passes a <button> with aria label attribute`, async (t) => {
+test(`evaluates() passes a <button> with a name given by the \`aria-label\` attribute`, async (t) => {
   const target = <button aria-label="My button"></button>;
 
   const document = Document.of([target]);
@@ -43,7 +43,7 @@ test(`evaluates() passes a <button> with aria label attribute`, async (t) => {
   ]);
 });
 
-test(`evaluates() fails <button> with no attribute`, async (t) => {
+test(`evaluates() fails a button with no accessible name`, async (t) => {
   const target = <button></button>;
 
   const document = Document.of([target]);
@@ -55,19 +55,8 @@ test(`evaluates() fails <button> with no attribute`, async (t) => {
   ]);
 });
 
-test(`evaluates() fails <button> element with no accessible name`, async (t) => {
-  const target = <button type="button" value="read more"></button>;
 
-  const document = Document.of([target]);
-
-  t.deepEqual(await evaluate(R12, { document }), [
-    failed(R12, target, {
-      1: Outcomes.HasNoName,
-    }),
-  ]);
-});
-
-test(`evaluates() fails element with <button> role because it doesn't have a content with accessible name`, async (t) => {
+test(`evaluates() fails an element with \`button\` role because and without an accessible name`, async (t) => {
   const target = <span role="button"></span>;
 
   const document = Document.of([target]);
@@ -79,19 +68,8 @@ test(`evaluates() fails element with <button> role because it doesn't have a con
   ]);
 });
 
-test(`evaluates() fails element with <button> role because it doesn't have a content with accessible name`, async (t) => {
-  const target = <span role="button"></span>;
 
-  const document = Document.of([target]);
-
-  t.deepEqual(await evaluate(R12, { document }), [
-    failed(R12, target, {
-      1: Outcomes.HasNoName,
-    }),
-  ]);
-});
-
-test(`evaluate() is inapplicabile to input element with image attribute`, async (t) => {
+test(`evaluate() is inapplicable to image buttons`, async (t) => {
   const target = <input type="image" value="download" alt="Download" />;
 
   const document = Document.of([target]);
@@ -99,13 +77,6 @@ test(`evaluate() is inapplicabile to input element with image attribute`, async 
   t.deepEqual(await evaluate(R12, { document }), [inapplicable(R12)]);
 });
 
-test(`evaluate() is inapplicabile to input element with image attribute`, async (t) => {
-  const target = <input type="image" value="download" alt="Download" />;
-
-  const document = Document.of([target]);
-
-  t.deepEqual(await evaluate(R12, { document }), [inapplicable(R12)]);
-});
 
 test(`evaluate() is inapplicabile to element with no button role`, async (t) => {
   const target = <div>Press Here</div>;
