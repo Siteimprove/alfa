9973ebe03007d931b36403baea1cfe4d4fbb2e3a
Jean-Yves Moyen <jym@siteimprove.com>
2021-05-31 14:05:56 +0200

    Add Inventory Rule

--- packages/alfa-act/src/rule.ts
+++ packages/alfa-act/src/rule.ts
@@ -368,6 +368,48 @@ export namespace Rule {
   ): value is Composite<I, T, Q> {
     return value instanceof Composite;
   }
+
+  export class Inventory<I = unknown, T = unknown> extends Rule<I, T, never> {
+    public static of<I, T = unknown>(properties: {
+      uri: string;
+      evaluate: Inventory.Evaluate<I, T>;
+    }): Inventory<I, T> {
+      return new Inventory(properties.uri, properties.evaluate);
+    }
+
+    private constructor(uri: string, evaluate: Inventory.Evaluate<I, T>) {
+      super(uri, [], [], (input) => {
+          const { applicability, expectations } = evaluate(input);
+
+          return Future.traverse(applicability(), target =>
+            Future.now(Outcome.inventory(this, target, expectations(target))))
+            }
+      );
+    }
+
+    public toJSON(): Inventory.JSON {
+      return {
+        type: "inventory",
+        uri: this._uri,
+        requirements: [],
+        tags: [],
+      }
+    }
+
+  }
+
+  export namespace Inventory {
+    export interface JSON extends Rule.JSON {
+      type: "inventory";
+    }
+
+    export interface Evaluate<I, T> {
+      (input: Readonly<I>):  {
+        applicability(): Iterable<T>;
+        expectations(target: T): Diagnostic
+      }
+    }
+  }
 }
 
 function resolve<I, T, Q>(
