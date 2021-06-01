8bd20f6aeb92bdaa9e2234418ce0fd871b9ca60c
Jean-Yves Moyen <jym@siteimprove.com>
2021-06-01 16:15:04 +0200

    Accept several ligature values

--- packages/alfa-style/src/property/font-variant-ligatures.ts
+++ packages/alfa-style/src/property/font-variant-ligatures.ts
@@ -1,6 +1,12 @@
-import { Keyword } from "@siteimprove/alfa-css";
+import { Keyword, Token } from "@siteimprove/alfa-css";
+import { Parser } from "@siteimprove/alfa-parser";
+import { Err, Result } from "@siteimprove/alfa-result";
+import { Slice } from "@siteimprove/alfa-slice";
 
 import { Property } from "../property";
+import { List } from "./value/list";
+
+const { either } = Parser;
 
 declare module "../property" {
   interface Longhands {
@@ -14,15 +20,28 @@ declare module "../property" {
 export type Specified =
   | Keyword<"none">
   | Keyword<"normal">
-  | Keyword<"common-ligatures">
-  | Keyword<"no-common-ligatures">
-  | Keyword<"discretionary-ligatures">
-  | Keyword<"no-discretionary-ligatures">
-  | Keyword<"historical-ligatures">
-  | Keyword<"no-historical-ligatures">
-  | Keyword<"contextual">
-  | Keyword<"no-contextual">;
+  | List<Specified.Item>;
 
+/**
+ * @internal
+ */
+export namespace Specified {
+  export type Common =
+    | Keyword<"common-ligatures">
+    | Keyword<"no-common-ligatures">;
+
+  export type Discretionary =
+    | Keyword<"discretionary-ligatures">
+    | Keyword<"no-discretionary-ligatures">;
+
+  export type Historical =
+    | Keyword<"historical-ligatures">
+    | Keyword<"no-historical-ligatures">;
+
+  export type Contextual = Keyword<"contextual"> | Keyword<"no-contextual">;
+
+  export type Item = Common | Discretionary | Historical | Contextual;
+}
 /**
  * @internal
  */
@@ -31,18 +50,106 @@ export type Computed = Specified;
 /**
  * @internal
  */
-export const parse = Keyword.parse(
-  "none",
-  "normal",
-  "common-ligatures",
-  "no-common-ligatures",
+const parseCommon = Keyword.parse("common-ligatures", "no-common-ligatures");
+
+/**
+ * @internal
+ */
+const parseDiscretionary = Keyword.parse(
   "discretionary-ligatures",
-  "no-discretionary-ligatures",
-  "historical-ligatures",
-  "no-historical-ligatures",
-  "contextual",
-  "no-contextual"
+  "no-discretionary-ligatures"
 );
+
+/**
+ * @internal
+ */
+const parseHistorical = Keyword.parse(
+  "historical-ligatures",
+  "no-historical-ligatures"
+);
+
+/**
+ * @internal
+ */
+const parseContextual = Keyword.parse("contextual", "no-contextual");
+
+/**
+ * @internal
+ */
+const parseLigature: Parser<Slice<Token>, List<Specified.Item>, string> = (
+  input
+) => {
+  let common: Specified.Common | undefined;
+  let discretionary: Specified.Discretionary | undefined;
+  let historical: Specified.Historical | undefined;
+  let contextual: Specified.Contextual | undefined;
+
+  while (true) {
+    for (const [remainder] of Token.parseWhitespace(input)) {
+      input = remainder;
+    }
+
+    if (common === undefined) {
+      const result = parseCommon(input);
+
+      if (result.isOk()) {
+        [input, common] = result.get();
+        continue;
+      }
+    }
+
+    if (discretionary === undefined) {
+      const result = parseDiscretionary(input);
+
+      if (result.isOk()) {
+        [input, discretionary] = result.get();
+        continue;
+      }
+    }
+    if (historical === undefined) {
+      const result = parseHistorical(input);
+
+      if (result.isOk()) {
+        [input, historical] = result.get();
+        continue;
+      }
+    }
+    if (contextual === undefined) {
+      const result = parseContextual(input);
+
+      if (result.isOk()) {
+        [input, contextual] = result.get();
+        continue;
+      }
+    }
+    break;
+  }
+
+  if (
+    common === undefined &&
+    discretionary === undefined &&
+    historical === undefined &&
+    contextual === undefined
+  ) {
+    return Err.of("At least one ligature value must be provided");
+  }
+
+  return Result.of([
+    input,
+    List.of(
+      [common, discretionary, historical, contextual].filter(
+        (value) => value !== undefined
+        // filter doesn't narrow so we need to do it manually
+      ) as Array<Specified.Item>,
+      " "
+    ),
+  ]);
+};
+
+/**
+ * @internal
+ */
+export const parse = either(Keyword.parse("none", "normal"), parseLigature);
 
 /**
  * {@link https://developer.mozilla.org/en-US/docs/Web/CSS/font-variant-ligatures}
