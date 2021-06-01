7b3a60543d039aabaa266ffbee0a412d51fbef2e
Jean-Yves Moyen <jym@siteimprove.com>
2021-06-01 15:19:05 +0200

    Support font-variant-caps

--- packages/alfa-style/src/index.ts
+++ packages/alfa-style/src/index.ts
@@ -88,6 +88,7 @@ import "./property/font-size";
 import "./property/font-stretch";
 import "./property/font-style";
 import "./property/font-weight";
+import "./property/font-variant-caps";
 import "./property/font-variant-ligatures";
 import "./property/font-variant-position";
 import "./property/height";
