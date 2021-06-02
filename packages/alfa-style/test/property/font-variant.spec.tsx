04514fa2b3ea38ad2f69d10a2dcb5b49161ea652
Jean-Yves Moyen <jym@siteimprove.com>
2021-06-02 12:25:26 +0200

    Add some test for font-variant

---
+++ packages/alfa-style/test/property/font-variant.spec.tsx
@@ -0,0 +1,57 @@
+import { test } from "@siteimprove/alfa-test";
+
+import { Device } from "@siteimprove/alfa-device";
+
+import { Style } from "../../src/style";
+
+const device = Device.standard();
+
+test(`#cascaded() parses \`font-variant: oldstyle-nums sub ruby
+     historical-ligatures diagonal-fractions ordinal contextual slashed-zero\``, (t) => {
+  const element = (
+    <div
+      style={{
+        "font-variant":
+          "oldstyle-nums sub ruby historical-ligatures diagonal-fractions ordinal contextual slashed-zero",
+      }}
+    />
+  );
+
+  const style = Style.from(element, device);
+
+  t.deepEqual(style.cascaded("font-variant-caps").get().value.toJSON(), {
+    type: "keyword",
+    value: "initial",
+  });
+
+  t.deepEqual(style.cascaded("font-variant-east-asian").get().value.toJSON(), {
+    type: "list",
+    values: [{ type: "keyword", value: "ruby" }],
+    separator: " ",
+  });
+
+  t.deepEqual(style.cascaded("font-variant-ligatures").get().value.toJSON(), {
+    type: "list",
+    values: [
+      { type: "keyword", value: "historical-ligatures" },
+      { type: "keyword", value: "contextual" },
+    ],
+    separator: " ",
+  });
+
+  t.deepEqual(style.cascaded("font-variant-numeric").get().value.toJSON(), {
+    type: "list",
+    values: [
+      { type: "keyword", value: "oldstyle-nums" },
+      { type: "keyword", value: "diagonal-fractions" },
+      { type: "keyword", value: "ordinal" },
+      { type: "keyword", value: "slashed-zero" },
+    ],
+    separator: " ",
+  });
+
+  t.deepEqual(style.cascaded("font-variant-position").get().value.toJSON(), {
+    type: "keyword",
+    value: "sub",
+  });
+});
