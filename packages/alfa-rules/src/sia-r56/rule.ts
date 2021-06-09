0d5828a50330ac822a76ef6f7749cba8387bba35
Jean-Yves Moyen <jym@siteimprove.com>
2021-06-09 13:23:33 +0200

    Write R56

--- packages/alfa-rules/src/sia-r56/rule.ts
+++ packages/alfa-rules/src/sia-r56/rule.ts
@@ -1,93 +1,80 @@
+/// <reference lib="dom" />
+
 import { Diagnostic, Rule } from "@siteimprove/alfa-act";
 import { Node, Role } from "@siteimprove/alfa-aria";
-import { Branched } from "@siteimprove/alfa-branched";
-import { Device } from "@siteimprove/alfa-device";
 import { Element, Namespace } from "@siteimprove/alfa-dom";
+import { List } from "@siteimprove/alfa-list";
+import { Map } from "@siteimprove/alfa-map";
+import { Option } from "@siteimprove/alfa-option";
 import { Predicate } from "@siteimprove/alfa-predicate";
 import { Err, Ok } from "@siteimprove/alfa-result";
 import { Page } from "@siteimprove/alfa-web";
-import { expectation } from "../common/expectation";
 
-import { hasRole } from "../common/predicate/has-role";
-import { isIgnored } from "../common/predicate/is-ignored";
+import { expectation } from "../common/expectation";
+import { Group } from "../common/group";
+import { normalize } from "../common/normalize";
+
+import { hasRole, isIgnored } from "../common/predicate";
 
 const { and, equals, not } = Predicate;
 const { hasNamespace } = Element;
 
-export default Rule.Atomic.of<Page, Iterable<Element>>({
+export default Rule.Atomic.of<Page, Group<Element>>({
   uri: "https://siteimprove.github.io/sanshikan/rules/sia-r56.html",
   evaluate({ device, document }) {
     return {
       applicability() {
-        const elements = document
+        return document
           .descendants({ flattened: true, nested: true })
+          .filter(Element.isElement)
           .filter(
             and(
-              Element.isElement,
-              and(
-                hasNamespace(equals(Namespace.HTML)),
-                not(isIgnored(device)),
-                hasLandmarkRole(device)
-              )
+              hasNamespace(equals(Namespace.HTML)),
+              not(isIgnored(device)),
+              hasRole(device, (role) => role.is("landmark"))
             )
-          );
+          )
+          .reduce((groups, landmark) => {
+            // since we already have filtered by hasLandmarkRole, we can
+            // safely get the role.
+            const role = Node.from(landmark, device).role.get()!;
 
-        console.dir(elements.map((elt) => elt.id.get()).toJSON());
+            groups = groups.set(
+              role,
+              groups
+                .get(role)
+                .getOrElse(() => List.empty<Element>())
+                .append(landmark)
+            );
 
-        const foo = [...elements].map((elt) => elt.id.get());
-        console.log(foo);
-
-        const groups = elements.groupBy((landmark) => {
-          const role = Role.from(landmark);
-
-          console.log(
-            `landmark ${landmark.id.get()} has role ${role
-              .toArray()
-              .map(([r, _]) => r.map((r) => r.name).getOr("no role"))}`
-          );
-
-          return role;
-        });
-
-        console.log(
-          groups
-            .toArray()
-            .map(
-              ([branchedRole, group]) =>
-                `role: ${branchedRole
-                  .toArray()
-                  .map(([r, _]) =>
-                    r.map((role) => role.name).getOr("no role")
-                  )}, groups: ${group
-                  .map((element) => element.id.get())
-                  .toJSON()}.`
-            )
-        );
-
-        const result = groups.values();
-        console.dir(
-          [...result].map((seq) => seq.map((elt) => elt.id.get()).toJSON())
-        );
-
-        return result;
+            return groups;
+          }, Map.empty<Role, List<Element>>())
+          .filter((elements) => elements.size > 1)
+          .map(Group.of)
+          .values();
       },
 
       expectations(target) {
-        const names = arrayBranchedToBranchedArray(
-          [...target].map((landmark) =>
-            Node.from(landmark, device).map((accNode) =>
-              accNode.name().map((str) => str.toLocaleLowerCase())
-            )
-          )
-        );
+        const byNames = [...target]
+          .reduce((groups, landmark) => {
+            const name = Node.from(landmark, device).name.map((name) =>
+              normalize(name.value)
+            );
+            groups = groups.set(
+              name,
+              groups
+                .get(name)
+                .getOrElse(() => List.empty<Element>())
+                .append(landmark)
+            );
+
+            return groups;
+          }, Map.empty<Option<string>, List<Element>>())
+          .filter((landmarks) => landmarks.size > 1);
 
         return {
-          "1": expectation(
-            names.every((namesArray) =>
-              namesArray.every(
-                (opt, idx) => namesArray.findIndex(equals(opt)) === idx
-              )
-            ),
+          1: expectation(
+            byNames.size === 0,
             () => Outcomes.differentNames,
             () => Outcomes.sameNames
           ),
@@ -104,25 +91,5 @@ export namespace Outcomes {
 
   export const sameNames = Err.of(
     Diagnostic.of("Some identical landmarks have the same name.")
-  );
-}
-
-function hasLandmarkRole(device: Device): Predicate<Element> {
-  return hasRole((role) => role.inheritsFrom(Role.hasName(equals("landmark"))));
-}
-
-function addToBranchedArray<T, B>(
-  belt: Branched<T, B>,
-  barr: Branched<Array<T>, B>
-): Branched<Array<T>, B> {
-  return barr.flatMap((arr) => belt.map((elt) => arr.concat(elt)));
-}
-
-function arrayBranchedToBranchedArray<T, B>(
-  arr: Array<Branched<T, B>>
-): Branched<Array<T>, B> {
-  return arr.reduce<Branched<Array<T>, B>>(
-    (acc, cur) => addToBranchedArray(cur, acc),
-    Branched.of([])
   );
 }
--- packages/alfa-rules/test/sia-r56/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r56/rule.spec.tsx
@@ -1,45 +1,55 @@
-import { Device } from "@siteimprove/alfa-device";
-import { Document, Element } from "@siteimprove/alfa-dom";
-import { jsx } from "@siteimprove/alfa-dom/jsx";
-import { Iterable } from "@siteimprove/alfa-iterable";
-import { Predicate } from "@siteimprove/alfa-predicate";
 import { test } from "@siteimprove/alfa-test";
+
+import { Document } from "@siteimprove/alfa-dom";
 
 import R56, { Outcomes } from "../../src/sia-r56/rule";
 
 import { evaluate } from "../common/evaluate";
 import { passed, failed, inapplicable } from "../common/outcome";
 
-const { and, equals } = Predicate;
-const { hasName } = Element;
+import { Group } from "../../src/common/group";
 
-const device = Device.standard();
+test("evaluate() passes when same landmarks have different names", async (t) => {
+  const author = <aside aria-label="About the author" id="author" />;
+  const book = <aside aria-label="About the book" id="book" />;
 
-test("Passes when same landmarks have different names", async (t) => {
-  const document = Document.of((self) => [
-    Element.fromElement(
-      <html>
-        <aside aria-label="About the author" id="author" />
-        <aside aria-label="About the book" id="book" />
-      </html>
-    ),
+  const document = Document.of([author, book]);
+  const target = Group.of([author, book]);
+
+  t.deepEqual(await evaluate(R56, { document }), [
+    passed(R56, target, { 1: Outcomes.differentNames }),
   ]);
-  const target = document
-    .descendants()
-    .filter(and(Element.isElement, hasName(equals("aside"))))
-    .groupBy(() => 0)
-    .values();
+});
 
-  // const actual = await evaluate(R56, { device, document });
-  // const expected = passed(R56, Iterable.first(target).get(), [
-  //   { 1: Outcomes.differentNames },
-  // ]);
+test("evaluate() fails when same landmarks have same names", async (t) => {
+  const aside1 = <aside aria-label="More information" id="author" />;
+  const aside2 = <aside aria-label="More information" id="book" />;
 
-  // console.dir(actual.map(outcome => ({...outcome, target: [...outcome.target!].map(elt => elt.attribute("id").get().value)})));
-  // console.dir(expected);
+  const document = Document.of([aside1, aside2]);
+  const target = Group.of([aside1, aside2]);
 
-  t.deepEqual(await evaluate(R56, { device, document }), [
-    passed(R56, Iterable.first(target).get(), { 1: Outcomes.differentNames }),
+  t.deepEqual(await evaluate(R56, { document }), [
+    failed(R56, target, { 1: Outcomes.sameNames }),
   ]);
-  t(true);
+});
+
+test("evaluate() fails when same landmarks have no names", async (t) => {
+  const aside1 = <aside id="author" />;
+  const aside2 = <aside id="book" />;
+
+  const document = Document.of([aside1, aside2]);
+  const target = Group.of([aside1, aside2]);
+
+  t.deepEqual(await evaluate(R56, { document }), [
+    failed(R56, target, { 1: Outcomes.sameNames }),
+  ]);
+});
+
+test("evaluate() is inapplicable when only different landmarks exist", async (t) => {
+  const aside = <aside />;
+  const nav = <nav />;
+
+  const document = Document.of([aside, nav]);
+
+  t.deepEqual(await evaluate(R56, { document }), [inapplicable(R56)]);
 });
