c8ed9303392fb3158041809821e0bc8e91dfc005
elenamongelli <52746406+elenamongelli@users.noreply.github.com>
2021-06-14 14:54:59 +0200

    Rule 54 is added, tests need to be completed

---
+++ packages/alfa-rules/src/sia-r54/rule.ts
@@ -0,0 +1,51 @@
+import { Rule, Diagnostic, Outcome } from "@siteimprove/alfa-act";
+import { Element } from "@siteimprove/alfa-dom";
+import { Predicate } from "@siteimprove/alfa-predicate";
+import { Err, Ok } from "@siteimprove/alfa-result";
+import { Page } from "@siteimprove/alfa-web";
+import { isIgnored } from "../common/predicate/is-ignored";
+
+import { expectation } from "../common/expectation";
+import { Node } from "@siteimprove/alfa-aria";
+import { Device } from "@siteimprove/alfa-device";
+
+const { and, not } = Predicate;
+const { isElement } = Element;
+
+export default Rule.Atomic.of<Page, Element>({
+  uri: "https://alfa.siteimprove.com/rules/sia-r54",
+  evaluate({ device, document }) {
+    return {
+      applicability() {
+        return document
+          .descendants({ flattened: true, nested: true })
+          .filter(isElement)
+          .filter(
+            and(not(isIgnored(device)), (element) =>
+              Node.from(element, device)
+                .attribute("aria-live")
+                .some((attribute) => attribute.value === "assertive")
+            )
+          );
+      },
+
+      expectations(target) {
+        return {
+          1: expectation(
+            Node.from(target, device)
+              .attribute("aria-atomic")
+              .some((attribute_atomic) => attribute_atomic.value === "true"),
+            () => Outcomes.IsAssertive,
+            () => Outcomes.IsNotAssertive
+          ),
+        };
+      },
+    };
+  },
+});
+
+export namespace Outcomes {
+  export const IsAssertive = Ok.of(Diagnostic.of("Is assertive"));
+
+  export const IsNotAssertive = Err.of(Diagnostic.of("Is not assertive"));
+}
