59f0efb88f6e2ada40cea8d1d8b482812168fe89
Jean-Yves Moyen <jym@siteimprove.com>
2021-06-30 16:47:52 +0200

    Consider nodes with a clipped ancestor as clipped

--- packages/alfa-rules/src/common/predicate/is-clipped.ts
+++ packages/alfa-rules/src/common/predicate/is-clipped.ts
@@ -1,6 +1,6 @@
 import { Cache } from "@siteimprove/alfa-cache";
 import { Device } from "@siteimprove/alfa-device";
-import { Element, Text, Node } from "@siteimprove/alfa-dom";
+import { Element, Node } from "@siteimprove/alfa-dom";
 import { Predicate } from "@siteimprove/alfa-predicate";
 import { Context } from "@siteimprove/alfa-selector";
 import { Style } from "@siteimprove/alfa-style";
@@ -8,7 +8,6 @@ import { Style } from "@siteimprove/alfa-style";
 const { abs } = Math;
 const { isElement } = Element;
 const { or, test } = Predicate;
-const { isText } = Text;
 
 const cache = Cache.empty<Device, Cache<Context, Cache<Node, boolean>>>();
 
@@ -24,13 +23,25 @@ export function isClipped(
         test(
           or(
             isClippedBySize(device, context),
-            isClippedByMasking(device, context)
+            isClippedByIndent(device, context),
+            isClippedByMasking(device, context),
+            (node) =>
+              node
+                .parent({
+                  flattened: true,
+                  nested: true,
+                })
+                .some(isClipped(device, context))
           ),
           node
         )
       );
 }
 
+/**
+ * Checks if an element's size is reduced to 0 or 1 pixel, and overflow is
+ * somehow hidden.
+ */
 function isClippedBySize(
   device: Device,
   context: Context = Context.empty()
@@ -39,13 +50,19 @@ function isClippedBySize(
     if (isElement(node)) {
       const style = Style.from(node, device, context);
 
-      const { value: x } = style.computed("overflow-x");
-      const { value: y } = style.computed("overflow-y");
+      const {
+        value: { value: x },
+      } = style.computed("overflow-x");
+      const {
+        value: { value: y },
+      } = style.computed("overflow-y");
 
       const { value: height } = style.computed("height");
       const { value: width } = style.computed("width");
 
-      if (x.value === "hidden" || y.value === "hidden") {
+      const hasNoScrollBar = x === "hidden" || y === "hidden";
+
+      if (x !== "visible" || y !== "visible") {
         for (const dimension of [height, width]) {
           switch (dimension.type) {
             case "percentage":
@@ -55,8 +72,12 @@ function isClippedBySize(
                 break;
               }
 
+            // technically, 1×1 elements are (possibly) visible since they
+            // show one pixel of background. We assume this is used to hide
+            // elements and that the background is the same as the surrounding
+            // one.
             case "length":
-              if (dimension.value <= 1) {
+              if (dimension.value <= (hasNoScrollBar ? 1 : 0)) {
                 return true;
               } else {
                 break;
@@ -64,66 +85,53 @@ function isClippedBySize(
           }
         }
       }
-
-      if (
-        x.value === "auto" ||
-        x.value === "scroll" ||
-        y.value === "auto" ||
-        y.value === "scroll"
-      ) {
-        for (const dimension of [height, width]) {
-          switch (dimension.type) {
-            case "percentage":
-              if (dimension.value <= 0) {
-                return true;
-              } else {
-                break;
-              }
-
-            case "length":
-              if (dimension.value <= 0) {
-                return true;
-              } else {
-                break;
-              }
-          }
-        }
-      }
-    }
-
-    for (const parent of node.parent({ flattened: true })) {
-      if (isText(node) && isElement(parent)) {
-        const style = Style.from(parent, device, context);
-
-        const { value: x } = style.computed("overflow-x");
-
-        if (x.value === "hidden") {
-          const { value: indent } = style.computed("text-indent");
-          const { value: whitespace } = style.computed("white-space");
-
-          if (indent.value < 0 || whitespace.value === "nowrap") {
-            switch (indent.type) {
-              case "percentage":
-                if (abs(indent.value) >= 1) {
-                  return true;
-                }
-
-              case "length":
-                if (abs(indent.value) >= 999) {
-                  return true;
-                }
-            }
-          }
-        }
-      }
-
-      return isClipped(parent);
     }
 
     return false;
   };
 }
 
+/**
+ * Checks if an element is fully indented out of screen.
+ */
+function isClippedByIndent(device: Device, context: Context): Predicate<Node> {
+  return function isClipped(node: Node): boolean {
+    if (isElement(node)) {
+      const style = Style.from(node, device, context);
+
+      const { value: x } = style.computed("overflow-x");
+
+      if (x.value === "hidden") {
+        const { value: indent } = style.computed("text-indent");
+        const { value: whitespace } = style.computed("white-space");
+
+        if (indent.value < 0 || whitespace.value === "nowrap") {
+          switch (indent.type) {
+            case "percentage":
+              if (abs(indent.value) >= 1) {
+                return true;
+              } else {
+                break;
+              }
+
+            case "length":
+              if (abs(indent.value) >= 999) {
+                return true;
+              } else {
+                break;
+              }
+          }
+        }
+      }
+    }
+
+    return false;
+  };
+}
+
+/**
+ * Checks if an element is fully masked by a clipping shape.
+ */
 function isClippedByMasking(device: Device, context: Context): Predicate<Node> {
   return function isClipped(node: Node): boolean {
     if (isElement(node)) {
@@ -144,11 +152,6 @@ function isClippedByMasking(device: Device, context: Context): Predicate<Node> {
       }
     }
 
-    return node
-      .parent({
-        flattened: true,
-        nested: true,
-      })
-      .some(isClipped);
+    return false;
   };
 }
