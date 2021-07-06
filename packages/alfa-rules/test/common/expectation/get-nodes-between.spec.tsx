5aea63e0eb8d844b25b7d79f3f07b836f8bcebcf
Jean-Yves Moyen <jym@siteimprove.com>
2021-07-06 14:02:46 +0200

    Add tests for getNodesBetween

---
+++ packages/alfa-rules/test/common/expectation/get-nodes-between.spec.tsx
@@ -0,0 +1,96 @@
+import { h } from "@siteimprove/alfa-dom";
+import { Sequence } from "@siteimprove/alfa-sequence";
+import { test } from "@siteimprove/alfa-test";
+import { getNodesBetween } from "../../../src/common/expectation/get-nodes-between";
+
+const hello = h.text("Hello");
+const spanHello = h.element("span", [], [hello]);
+const divHello = <div>{spanHello}</div>;
+
+const spanEmpty = <span></span>;
+const divEmpty = <div>{spanEmpty}</div>;
+
+const world = h.text("World");
+const spanWorld = h.element("span", [], [world]);
+const divWorld = <div>{spanWorld}</div>;
+
+h.document([divHello, divEmpty, divWorld]);
+
+test("getNodesBetween() returns the nodes between two nodes", (t) => {
+  t.deepEqual(
+    getNodesBetween(divHello, divWorld, {
+      includeFirst: true,
+      includeSecond: true,
+    }).toJSON(),
+    Sequence.from([
+      divHello,
+      spanHello,
+      hello,
+      divEmpty,
+      spanEmpty,
+      divWorld,
+    ]).toJSON()
+  );
+
+  t.deepEqual(
+    getNodesBetween(divHello, divWorld, {
+      includeFirst: true,
+      includeSecond: false,
+    }).toJSON(),
+    Sequence.from([divHello, spanHello, hello, divEmpty, spanEmpty]).toJSON()
+  );
+
+  t.deepEqual(
+    getNodesBetween(divHello, divWorld, {
+      includeFirst: false,
+      includeSecond: true,
+    }).toJSON(),
+    Sequence.from([divEmpty, spanEmpty, divWorld]).toJSON()
+  );
+
+  t.deepEqual(
+    getNodesBetween(divHello, divWorld, {
+      includeFirst: false,
+      includeSecond: false,
+    }).toJSON(),
+    Sequence.from([divEmpty, spanEmpty]).toJSON()
+  );
+});
+
+test(`getNodesBetween() return a single node when both arguments are the same
+            and both ends are included`, (t) => {
+  t.deepEqual(
+    getNodesBetween(divHello, divHello, {
+      includeFirst: true,
+      includeSecond: true,
+    }).toJSON(),
+    [divHello.toJSON()]
+  );
+});
+
+test(`getNodesBetween() return an empty sequence when both arguments are the
+            same and at least on ends is included`, (t) => {
+  t.deepEqual(
+    getNodesBetween(divHello, divHello, {
+      includeFirst: true,
+      includeSecond: false,
+    }).toJSON(),
+    []
+  );
+
+  t.deepEqual(
+    getNodesBetween(divHello, divHello, {
+      includeFirst: false,
+      includeSecond: true,
+    }).toJSON(),
+    []
+  );
+
+  t.deepEqual(
+    getNodesBetween(divHello, divHello, {
+      includeFirst: false,
+      includeSecond: false,
+    }).toJSON(),
+    []
+  );
+});
