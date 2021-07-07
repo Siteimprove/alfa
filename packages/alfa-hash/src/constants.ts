aaac12c8b72988e1bd69490a0257bf3b0aabc1b5
Kasper Isager <kasperisager@gmail.com>
2021-07-07 13:08:46 +0200

    Refactor `Hash` and add additional methods (#853)

--- packages/alfa-hash/src/constants.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @internal
- */
-export const IntegerOverflow = 0b10000000_00000000_00000000_00000000;
-
-/**
- * @internal
- */
-export const BuiltinOffset = 0b10000_10000_10000_10000_10000_10000_00;
