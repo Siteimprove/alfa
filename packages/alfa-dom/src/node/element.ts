49462ed3e63067619f5a7ed2951fe3c06fb7ef3b
Kasper Isager <kasperisager@gmail.com>
2021-08-20 10:04:33 +0200

    Expose `Element#attributes` and `Element#classes` as sequences

--- packages/alfa-dom/src/node/element.ts
+++ packages/alfa-dom/src/node/element.ts
@@ -96,8 +96,8 @@ export class Element extends Node implements Slot, Slotable {
     );
   }
 
-  public get attributes(): Iterable<Attribute> {
-    return this._attributes.values();
+  public get attributes(): Sequence<Attribute> {
+    return Sequence.from(this._attributes.values());
   }
 
   public get style(): Option<Block> {
@@ -122,8 +122,8 @@ export class Element extends Node implements Slot, Slotable {
   /**
    * {@link https://dom.spec.whatwg.org/#concept-class}
    */
-  public get classes(): Iterable<string> {
-    return this._classes;
+  public get classes(): Sequence<string> {
+    return Sequence.from(this._classes);
   }
 
   public parent(options: Node.Traversal = {}): Option<Node> {
