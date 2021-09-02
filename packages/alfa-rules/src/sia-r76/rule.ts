5a75b55520280eeae7d373fe801a6d8e779b07bb
Jean-Yves Moyen <jym@siteimprove.com>
2021-09-02 14:28:41 +0200

    Add tests for R76

--- packages/alfa-rules/src/sia-r76/rule.ts
+++ packages/alfa-rules/src/sia-r76/rule.ts
@@ -16,8 +16,6 @@ export default Rule.Atomic.of<Page, Element>({
   uri: "https://alfa.siteimprove.com/rules/sia-r76",
   requirements: [Criterion.of("1.3.1")],
   evaluate({ device, document }) {
-    const data = new Map<Element, Cell>();
-
     return {
       *applicability() {
         const tables = document
@@ -32,8 +30,6 @@ export default Rule.Atomic.of<Page, Element>({
           );
 
         for (const table of tables) {
-          const model = Table.from(table);
-
           const headers = table
             .descendants()
             .filter(isElement)
@@ -46,33 +42,13 @@ export default Rule.Atomic.of<Page, Element>({
             );
 
           for (const header of headers) {
-            for (const cell of model.cells.find((cell) =>
-              cell.element.equals(header)
-            )) {
-              data.set(header, cell);
-
-              yield header;
-            }
+            yield header;
           }
         }
       },
       expectations(target) {
-        // header is yielded after storing cell in data.
-        const cell = data.get(target)!;
-
         return {
-          1:
-            // We cannot use `expectation` because we need the narrowing
-            // of cell to Cell.Header in the true branch.
-            //
-            // The scope attribute can be "auto" and resolve to an actual scope.
-            // But cell.scope has been filled by Table > formTable > assignScope
-            // and only defaults to "auto" if no actual scope is found.
-            cell.isHeader() && cell.scope !== "auto"
-              ? Outcomes.HasScope(cell.scope)
-              : Outcomes.HasNoScope,
-
-          2: expectation(
+          1: expectation(
             test(hasRole(device, "columnheader", "rowheader"), target),
             () => Outcomes.HasHeaderRole,
             () => Outcomes.HasNoHeaderRole
@@ -84,25 +60,11 @@ export default Rule.Atomic.of<Page, Element>({
 });
 
 export namespace Outcomes {
-  export const HasScope = (scope: Scope) =>
-    Ok.of(Diagnostic.of(`The header cell is a ${scope}`));
-
-  export const HasNoScope = Err.of(
-    Diagnostic.of(
-      `The header cell is neither a \`column\`, \`column group\`,
-      \`row\`, nor \`row group\` header`
-    )
-  );
-
   export const HasHeaderRole = Ok.of(
-    Diagnostic.of(
-      `The header element has either a \`columnheader\` or \`rowheader\` role`
-    )
+    Diagnostic.of(`The header element is a semantic header`)
   );
 
   export const HasNoHeaderRole = Err.of(
-    Diagnostic.of(
-      `The header element has neither a \`columnheader\` nor a \`rowheader\` role`
-    )
+    Diagnostic.of(`The header element is not a semantic header`)
   );
 }
