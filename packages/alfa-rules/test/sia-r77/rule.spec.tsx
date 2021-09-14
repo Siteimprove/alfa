4fa0903d91740dec740e192fd3f953c39416dd3c
elenamongelli <52746406+elenamongelli@users.noreply.github.com>
2021-09-14 16:38:30 +0200

    First test passing

--- packages/alfa-rules/test/sia-r77/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r77/rule.spec.tsx
@@ -6,7 +6,7 @@ import R77, { Outcomes } from "../../src/sia-r76/rule";
 import { evaluate } from "../common/evaluate";
 import { passed, failed, inapplicable } from "../common/outcome";
 
-test(`evaluate() passes implicit row headers`, async (t) => {
+test("evaluate() passes on explicit header", async (t) => {
   const target1 = <th>Mon-Fri</th>;
   const target2 = <th>Sat-Sun</th>;
 
@@ -14,11 +14,11 @@ test(`evaluate() passes implicit row headers`, async (t) => {
     <table>
       <caption>Opening hours</caption>
       <tr>
-        <th>Mon-Fri</th>
+        {target1}
         <td>8-17</td>
       </tr>
       <tr>
-        <th>Sat-Sun</th>
+        {target2}
         <td>10-14</td>
       </tr>
     </table>,
