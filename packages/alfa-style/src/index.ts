8eaf598f32bed3fb0c700c67960dabf8cd0f36ad
Jean-Yves Moyen <jym@siteimprove.com>
2021-09-30 09:34:51 +0200

    SIA-R83: Support `flex-flow` (#934)
    * Support flex-direction
    * Support flex-flow

--- packages/alfa-style/src/index.ts
+++ packages/alfa-style/src/index.ts
@@ -82,6 +82,8 @@ import "./property/clip";
 import "./property/clip-path";
 import "./property/color";
 import "./property/display";
+import "./property/flex-direction";
+import "./property/flex-flow";
 import "./property/flex-wrap";
 import "./property/font";
 import "./property/font-family";
