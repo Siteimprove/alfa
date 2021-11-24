e4ae4dc975a5ef00caab73742f60e3e2f0167a20
Jean-Yves Moyen <jym@siteimprove.com>
2021-11-24 14:18:45 +0100

    Add test for React.toPage

---
+++ packages/alfa-react/test/react.spec.tsx
@@ -0,0 +1,26 @@
+import { Device } from "@siteimprove/alfa-device";
+import { h } from "@siteimprove/alfa-dom";
+import { Request, Response } from "@siteimprove/alfa-http";
+import { test } from "@siteimprove/alfa-test";
+import { Page } from "@siteimprove/alfa-web";
+
+import { React } from "../src";
+
+import { FunctionComponent } from "react";
+
+test(`.toPage() creates an Alfa page`, (t) => {
+  const Button: FunctionComponent = ({ children }) => (
+    <button className="btn">{children}</button>
+  );
+
+  const actual = React.toPage(<Button />);
+
+  const expected = Page.of(
+    Request.empty(),
+    Response.empty(),
+    h.document([h.element("button", { class: "btn" })]),
+    Device.standard()
+  );
+
+  t.deepEqual(actual.toJSON(), expected.toJSON());
+});
