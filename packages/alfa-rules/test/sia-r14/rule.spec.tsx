452ee1d7f80b0cf1656cf5187aef5fe2682a3252
Zsófia Tóth <zto@siteimprove.com>
2021-12-01 14:47:10 +0100

    SIA-R14: Ignore punctuation in label names (#991)
    * SIA-R14: Ignore punctuation in label names
    
    * Use global normalize functionn
    
    * Use local removePunctuation function
    
    * Merge normalize and removePunctuation into one local function
    
    * Cleanup variables

--- packages/alfa-rules/test/sia-r14/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r14/rule.spec.tsx
@@ -32,6 +32,19 @@ test(`evaluate() passes a <button> element whose perceivable text content
   ]);
 });
 
+test(`evaluate() passes a <button> element whose perceivable text content includes punctuation and
+      is included in its accessible name set by aria-label`, async (t) => {
+  const target = <button aria-label="Hello? (world)">¿Hello?</button>;
+
+  const document = h.document([target]);
+
+  t.deepEqual(await evaluate(R14, { document }), [
+    passed(R14, target, {
+      1: Outcomes.VisibleIsInName("hello", "hello world"),
+    }),
+  ]);
+});
+
 test(`evaluate() fails a <button> element whose perceivable text content
       is not included in its accessible name set by aria-label`, async (t) => {
   const target = <button aria-label="Hello">Hello world</button>;
