be863ff42609b4a4afd587cf878ff9c389261b3d
Jean-Yves Moyen <jym@siteimprove.com>
2021-12-21 13:11:41 +0100

    Improve asserter tests

--- packages/alfa-assert/test/asserter.spec.ts
+++ packages/alfa-assert/test/asserter.spec.ts
@@ -1,11 +1,14 @@
+import { Oracle } from "@siteimprove/alfa-act";
+import { Future } from "@siteimprove/alfa-future";
+import { Option } from "@siteimprove/alfa-option";
 import { test } from "@siteimprove/alfa-test";
 
 import { Asserter } from "../src/asserter";
 
-import { Pass, Fail } from "./fixture/rule";
+import { CantTell, Fail, Pass } from "./fixture/rule";
 
 test("#expect() returns an ok when no rules fail", async (t) => {
-  const { expect } = Asserter.of([Pass]);
+  const { expect } = Asserter.of([Pass("pass")]);
 
   const result = await expect("foo").to.be.accessible();
 
@@ -13,9 +16,32 @@ test("#expect() returns an ok when no rules fail", async (t) => {
 });
 
 test("#expect() returns an error when a rule fails", async (t) => {
-  const { expect } = Asserter.of([Pass, Fail]);
+  const { expect } = Asserter.of([Pass("pass"), Fail("fail")]);
 
   const result = await expect("foo").to.be.accessible();
 
   t.equal(result.isErr(), true);
 });
+
+test("#expect() allows filtering of failed outcomes", async (t) => {
+  const { expect } = Asserter.of([Pass("pass"), Fail("good")], [], {
+    filter: (outcome) => outcome.rule.uri !== "good",
+  });
+
+  const result = await expect("foo").to.be.accessible();
+
+  t.equal(result.isOk(), true);
+});
+
+// test("#expect() accepts an oracle", async (t) => {
+//   const oracle: Oracle<string, string, { boolean: boolean }, string> = <A>() =>
+//     Future.now(Option.of(true as unknown as A));
+//
+//   const { expect } = Asserter.of([Pass("pass"), CantTell("maybe")], [], {
+//     oracle,
+//   });
+//
+//   const result = await expect("foo").to.be.accessible();
+//
+//   t.equal(result.isOk(), true);
+// });
--- packages/alfa-assert/test/fixture/rule.ts
+++ packages/alfa-assert/test/fixture/rule.ts
@@ -1,38 +1,70 @@
-import { Diagnostic, Rule } from "@siteimprove/alfa-act";
+import { Diagnostic, Question, Rule } from "@siteimprove/alfa-act";
 import { Result, Err } from "@siteimprove/alfa-result";
 
-export const Pass = Rule.Atomic.of<string, string>({
-  uri: "pass",
+export const Pass = (uri: string) =>
+  Rule.Atomic.of<string, string>({
+    uri,
 
-  evaluate(input) {
-    return {
-      applicability() {
-        return [input];
-      },
+    evaluate(input) {
+      return {
+        applicability() {
+          return [input];
+        },
 
-      expectations() {
-        return {
-          1: Result.of(Diagnostic.of("Success!")),
-        };
-      },
-    };
-  },
-});
+        expectations() {
+          return {
+            1: Result.of(Diagnostic.of("Success!")),
+          };
+        },
+      };
+    },
+  });
 
-export const Fail = Rule.Atomic.of<string, string>({
-  uri: "fail",
+export const Fail = (uri: string) =>
+  Rule.Atomic.of<string, string>({
+    uri,
 
-  evaluate(input) {
-    return {
-      applicability() {
-        return [input];
-      },
+    evaluate(input) {
+      return {
+        applicability() {
+          return [input];
+        },
 
-      expectations() {
-        return {
-          1: Err.of(Diagnostic.of("Failure!")),
-        };
-      },
-    };
-  },
-});
+        expectations() {
+          return {
+            1: Err.of(Diagnostic.of("Failure!")),
+          };
+        },
+      };
+    },
+  });
+
+export const CantTell = (uri: string) =>
+  Rule.Atomic.of<string, string, { boolean: boolean }>({
+    uri,
+
+    evaluate(input) {
+      return {
+        applicability() {
+          return [input];
+        },
+
+        expectations(target) {
+          const isPassed = Question.of<"boolean", string, string, boolean>(
+            "boolean",
+            "is-passed",
+            "Does the rule pass?",
+            target,
+            target
+          );
+          return {
+            1: isPassed.map((passed) =>
+              passed
+                ? Result.of(Diagnostic.of("Success!"))
+                : Err.of(Diagnostic.of("Failure!"))
+            ),
+          };
+        },
+      };
+    },
+  });
