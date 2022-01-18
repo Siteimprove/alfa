b6e03770c2ee08815855c26ee4a46e9368a113a1
Jean-Yves Moyen <jym@siteimprove.com>
2022-01-18 14:31:41 +0100

    Add some missing tests

--- packages/alfa-rules/test/sia-r49/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r49/rule.spec.tsx
@@ -3,11 +3,11 @@ import { test } from "@siteimprove/alfa-test";
 
 import { None, Option } from "@siteimprove/alfa-option";
 
-import R41, { Outcomes } from "../../src/sia-r49/rule";
+import R49, { Outcomes } from "../../src/sia-r49/rule";
 
 import { evaluate } from "../common/evaluate";
 import { oracle } from "../common/oracle";
-import { passed, failed } from "../common/outcome";
+import { cantTell, passed, failed, inapplicable } from "../common/outcome";
 
 test(`evaluate() passes an autoplaying <video> element that lasts more than 3
       seconds, has audio, and uses native audio controls`, async (t) => {
@@ -17,7 +17,7 @@ test(`evaluate() passes an autoplaying <video> element that lasts more than 3
 
   t.deepEqual(
     await evaluate(
-      R41,
+      R49,
       { document },
       oracle({
         "is-above-duration-threshold": true,
@@ -25,7 +25,7 @@ test(`evaluate() passes an autoplaying <video> element that lasts more than 3
       })
     ),
     [
-      passed(R41, target, {
+      passed(R49, target, {
         1: Outcomes.HasPerceivablePauseMechanism("video"),
       }),
     ]
@@ -42,7 +42,7 @@ test(`evaluate() passes an autoplaying <video> element that lasts more than 3
 
   t.deepEqual(
     await evaluate(
-      R41,
+      R49,
       { document },
       oracle({
         "is-above-duration-threshold": true,
@@ -51,7 +51,7 @@ test(`evaluate() passes an autoplaying <video> element that lasts more than 3
       })
     ),
     [
-      passed(R41, target, {
+      passed(R49, target, {
         1: Outcomes.HasPerceivablePauseMechanism("video"),
       }),
     ]
@@ -66,7 +66,7 @@ test(`evaluate() fails an autoplaying <video> element that lasts more than 3
 
   t.deepEqual(
     await evaluate(
-      R41,
+      R49,
       { document },
       oracle({
         "is-above-duration-threshold": true,
@@ -75,7 +75,7 @@ test(`evaluate() fails an autoplaying <video> element that lasts more than 3
       })
     ),
     [
-      failed(R41, target, {
+      failed(R49, target, {
         1: Outcomes.HasNoPauseMechanism("video"),
       }),
     ]
@@ -92,7 +92,7 @@ test(`evaluate() fails an autoplaying <video> element that lasts more than 3
 
   t.deepEqual(
     await evaluate(
-      R41,
+      R49,
       { document },
       oracle({
         "is-above-duration-threshold": true,
@@ -101,7 +101,7 @@ test(`evaluate() fails an autoplaying <video> element that lasts more than 3
       })
     ),
     [
-      failed(R41, target, {
+      failed(R49, target, {
         1: Outcomes.HasNonPerceivablePauseMechanism("video"),
       }),
     ]
@@ -118,7 +118,7 @@ test(`evaluate() fails an autoplaying <video> element that lasts more than 3
 
   t.deepEqual(
     await evaluate(
-      R41,
+      R49,
       { document },
       oracle({
         "is-above-duration-threshold": true,
@@ -127,9 +127,61 @@ test(`evaluate() fails an autoplaying <video> element that lasts more than 3
       })
     ),
     [
-      failed(R41, target, {
+      failed(R49, target, {
         1: Outcomes.HasNonPerceivablePauseMechanism("video"),
       }),
     ]
+  );
+});
+
+test("evaluate() can't tell when questions are left unanswered", async (t) => {
+  const target = <video autoplay src="foo.mp4" />;
+
+  const controls = <button />;
+
+  const document = h.document([target, controls]);
+
+  t.deepEqual(
+    await evaluate(
+      R49,
+      { document },
+      oracle({
+        "is-above-duration-threshold": true,
+        "has-audio": true,
+      })
+    ),
+    [cantTell(R49, target)]
+  );
+});
+
+test("evaluate() is inapplicable when Applicability questions are left unanswered", async (t) => {
+  const document = h.document([<video autoplay src="foo.mp4" />]);
+
+  t.deepEqual(await evaluate(R49, { document }), [inapplicable(R49)]);
+});
+
+test("evaluate() is inapplicable to short videos", async (t) => {
+  const document = h.document([<video autoplay src="foo.mp4" />]);
+
+  t.deepEqual(
+    await evaluate(
+      R49,
+      { document },
+      oracle({ "is-above-duration-threshold": false })
+    ),
+    [inapplicable(R49)]
+  );
+});
+
+test("evaluate() is inapplicable to audio-less videos", async (t) => {
+  const document = h.document([<video autoplay src="foo.mp4" />]);
+
+  t.deepEqual(
+    await evaluate(
+      R49,
+      { document },
+      oracle({ "is-above-duration-threshold": true, "has-audio": false })
+    ),
+    [inapplicable(R49)]
   );
 });
