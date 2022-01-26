728df3d47805a10a96512b9ce96a45f5bb9fc728
Jean-Yves Moyen <jym@siteimprove.com>
2022-01-26 16:35:49 +0100

    Store offset parent to offset children relation

--- packages/alfa-rules/src/common/predicate/has-interposed-descendant.ts
+++ packages/alfa-rules/src/common/predicate/has-interposed-descendant.ts
@@ -1,18 +1,25 @@
 import { Cache } from "@siteimprove/alfa-cache";
 import { Device } from "@siteimprove/alfa-device";
 import { Element, Node } from "@siteimprove/alfa-dom";
+import { Map } from "@siteimprove/alfa-map";
 import { Predicate } from "@siteimprove/alfa-predicate";
 import { Refinement } from "@siteimprove/alfa-refinement";
+import { Sequence } from "@siteimprove/alfa-sequence";
 
 const { and } = Refinement;
 const { isElement } = Element;
 
-import { getOffsetParent } from "../expectation/get-offset-parent";
+import { getOffsetParent } from "../dom/get-offset-parent";
 
 import { isPositioned } from "./is-positioned";
 import { isVisible } from "./is-visible";
 
-const cache = Cache.empty<Device, Cache<Node, Set<Element>>>();
+// For each device and document's root, we store a map from the offset parents
+// to its interposed descendants
+const cache = Cache.empty<
+  Device,
+  Cache<Node, Map<Element, Sequence<Element>>>
+>();
 
 export function hasInterposedDescendant(device: Device): Predicate<Element> {
   return (element) => {
@@ -22,27 +29,21 @@ export function hasInterposedDescendant(device: Device): Predicate<Element> {
 
     return cache
       .get(device, Cache.empty)
-      .get(
-        root,
-        () =>
-          new Set(
-            root
-              .inclusiveDescendants({
-                flattened: true,
-              })
-              // Find all absolutely positioned elements.
-              .filter(
-                and(
-                  isElement,
-                  and(
-                    isPositioned(device, "absolute", "fixed"),
-                    isVisible(device)
-                  )
-                )
-              )
-              // And store their offset parents.
-              .collect((element) => getOffsetParent(element, device))
+      .get(root, () =>
+        root
+          .inclusiveDescendants({
+            flattened: true,
+          })
+          // Find all absolutely positioned elements.
+          .filter(
+            and(
+              isElement,
+              and(isPositioned(device, "absolute", "fixed"), isVisible(device))
+            )
           )
+          // And store their offset parents.
+          .collect((element) => getOffsetParent(element, device))
+          .groupBy((element) => getOffsetParent(element, device).get())
       )
       .has(element);
   };
