1f0f20446eaf3fe287f91501dd0b9cb5d290ddeb
elenamongelli <52746406+elenamongelli@users.noreply.github.com>
2022-02-08 13:49:38 +0100

    Update rule.spec.tsx

--- packages/alfa-rules/test/sia-r7/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r7/rule.spec.tsx
@@ -90,6 +90,23 @@ test(`evaluate() passes a span with a lang attribute within <body>  when the ele
 });
 
 test("evaluate() fails an element with a lang attribute within <body> with an invalid value", async (t) => {
+  const element = (
+    <span lang="invalid">
+      <img src="/test-assets/shared/fireworks.jpg" alt="Fireworks over Paris" />
+    </span>
+  );
+  const target = element.attribute("lang").get()!;
+
+  const document = h.document([<body>{element}</body>]);
+
+  t.deepEqual(await evaluate(R7, { document }), [
+    failed(R7, target, {
+      1: Outcomes.HasNoValidLanguage,
+    }),
+  ]);
+});
+
+test("evaluate() fails a text element with a lang attribute within <body> with an invalid value", async (t) => {
   const element = <span lang="invalid">Hello World</span>;
   const target = element.attribute("lang").get()!;
 
