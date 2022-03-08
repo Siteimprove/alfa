39a83d2163f3c2556d2ad5c85d3b6de2651ae1d0
elenamongelli <52746406+elenamongelli@users.noreply.github.com>
2022-03-08 16:03:56 +0100

    Predicate updated, test added

--- packages/alfa-rules/test/sia-r11/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r11/rule.spec.tsx
@@ -16,22 +16,6 @@ test(`evaluate() passes a link with a name given by content`, async (t) => {
   ]);
 });
 
-test(`evaluate() fails a link with no name`, async (t) => {
-  const target = <a href="#"></a>;
-
-  const document = h.document([target]);
-
-  t.deepEqual(await evaluate(R11, { document }), [
-    failed(R11, target, { 1: Outcomes.HasNoName }),
-  ]);
-});
-
-test(`evaluate() is inapplicable when there is no link`, async (t) => {
-  const document = h.document([<div></div>]);
-
-  t.deepEqual(await evaluate(R11, { document }), [inapplicable(R11)]);
-});
-
 test(`evaluate() passes an image link with name given by the alt text`, async (t) => {
   const target = (
     <a href="#">
@@ -44,4 +28,34 @@ test(`evaluate() passes an image link with name given by the alt text`, async (t
   t.deepEqual(await evaluate(R11, { document }), [
     passed(R11, target, { 1: Outcomes.HasName }),
   ]);
+});
+
+test(`evaluate() fails a link with no name`, async (t) => {
+  const target = <a href="#"></a>;
+
+  const document = h.document([target]);
+
+  t.deepEqual(await evaluate(R11, { document }), [
+    failed(R11, target, { 1: Outcomes.HasNoName }),
+  ]);
+});
+
+test(`evaluate() fails an 'a' element, which inherits semantically a link role from role, with no accessible name`, async (t) => {
+  const target = (
+    <a href="#" role="I am giving a link role">
+      <img src="Foo.jpg" alt="" />
+    </a>
+  );
+
+  const document = h.document([target]);
+
+  t.deepEqual(await evaluate(R11, { document }), [
+    failed(R11, target, { 1: Outcomes.HasNoName }),
+  ]);
+});
+
+test(`evaluate() is inapplicable when there is no link`, async (t) => {
+  const document = h.document([<div></div>]);
+
+  t.deepEqual(await evaluate(R11, { document }), [inapplicable(R11)]);
 });
