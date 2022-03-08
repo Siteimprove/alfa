05a808cd32c4d7709bc6c8994038031ae7b5f3d0
Jean-Yves Moyen <jym@siteimprove.com>
2022-03-08 16:18:54 +0100

    Comment out R82

--- packages/alfa-rules/src/experimental.ts
+++ packages/alfa-rules/src/experimental.ts
@@ -1,4 +1,4 @@
 export { default as ER62 } from "./sia-er62/rule";
 export { default as ER87 } from "./sia-er87/rule";
-export { default as R82 } from "./sia-r82/rule";
+// export { default as R82 } from "./sia-r82/rule";
 export { default as R109 } from "./sia-r109/rule";
