aa5e00b8fc3e87a2963fc47146ccebadd920ce67
Jean-Yves Moyen <jym@siteimprove.com>
2022-04-05 10:40:45 +0200

    Fix import

--- packages/alfa-aria/src/dom/predicate/has-accessible-name.ts
+++ packages/alfa-aria/src/dom/predicate/has-accessible-name.ts
@@ -1,7 +1,9 @@
-import { Name, Node } from "../..";
 import { Device } from "@siteimprove/alfa-device";
 import { Element, Text } from "@siteimprove/alfa-dom";
 import { Predicate } from "@siteimprove/alfa-predicate";
+
+import { Name } from "../../name";
+import { Node } from "../../node";
 
 import { hasName } from "../../node/predicate/has-name";
 
