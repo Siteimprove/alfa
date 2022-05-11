0c4865a7a4a1afa861277a8f4f55b46714fd8cc5
elenamongelli <elena.mongelli123@gmail.com>
2022-05-11 12:26:40 +0200

    Refactor isVisibleShadow

--- packages/alfa-style/src/element/predicate/is-visible-shadow.ts
+++ packages/alfa-style/src/element/predicate/is-visible-shadow.ts
@@ -3,23 +3,19 @@ import { Color, Shadow } from "@siteimprove/alfa-css";
 /**
  * @public
  * */
-
 export function isVisibleShadow(shadow: Shadow): boolean {
-  // If the shadow color is transparent, it is not visible.
-  if (Color.isTransparent(shadow.color)) {
-    return false;
-  }
+  return !isInvisibleShadow(shadow);
+}
 
-  // If the shadow has no horizontal or vertical offset, no blur and no spread, it is not visible
-  // For text-shadow property, spread value is not allowed
-  if (
-    shadow.vertical.value === 0 &&
-    shadow.horizontal.value === 0 &&
-    shadow.blur.value === 0 &&
-    shadow.spread.value === 0
-  ) {
-    return false;
-  }
-
-  return true;
+function isInvisibleShadow(shadow: Shadow): boolean {
+  // The shadow is not visible if:
+  // - color is transparent
+  // - no horizontal or vertical offset, no blur and no spread
+  return Color.isTransparent(shadow.color) ||
+    (shadow.vertical.value === 0 &&
+      shadow.horizontal.value === 0 &&
+      shadow.blur.value === 0 &&
+      shadow.spread.value === 0)
+    ? true
+    : false;
 }
