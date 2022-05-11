2502931a23d62c277b58cd6e6113b054ea62d9d6
elenamongelli <52746406+elenamongelli@users.noreply.github.com>
2022-05-11 15:44:26 +0200

    Update packages/alfa-style/src/element/predicate/is-visible-shadow.ts
    Co-authored-by: Jean-Yves Moyen <jym@siteimprove.com>

--- packages/alfa-style/src/element/predicate/is-visible-shadow.ts
+++ packages/alfa-style/src/element/predicate/is-visible-shadow.ts
@@ -8,14 +8,13 @@ export function isVisibleShadow(shadow: Shadow): boolean {
 }
 
 function isInvisibleShadow(shadow: Shadow): boolean {
-  // The shadow is not visible if:
-  // - color is transparent
-  // - no horizontal or vertical offset, no blur and no spread
-  return Color.isTransparent(shadow.color) ||
+  return (
+    // Transparent shadows are not visible
+    Color.isTransparent(shadow.color) ||
+    // Shadows with no offset, blur and spread are not visible
     (shadow.vertical.value === 0 &&
       shadow.horizontal.value === 0 &&
       shadow.blur.value === 0 &&
       shadow.spread.value === 0)
-    ? true
-    : false;
+      );
 }
