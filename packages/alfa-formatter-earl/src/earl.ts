f329025e7b027e116987d37293ca4111b5b9b20b
Jean-Yves Moyen <jym@siteimprove.com>
2022-06-22 14:18:25 +0200

    Compact earl output

--- packages/alfa-formatter-earl/src/earl.ts
+++ packages/alfa-formatter-earl/src/earl.ts
@@ -1,6 +1,8 @@
 import { Serializable, EARL } from "@siteimprove/alfa-earl";
 import { Formatter } from "@siteimprove/alfa-formatter";
 
+import * as jsonld from "jsonld";
+
 import { version } from "../package.json";
 
 const { stringify } = JSON;
@@ -9,40 +11,12 @@ const { stringify } = JSON;
  * @public
  */
 export default function <I, T, Q, S>(): Formatter<I, T, Q, S> {
-  return function EARL(input, rules, outcomes) {
+  return async function EARL(input, rules, outcomes) {
     const subject = Serializable.toEARL(input);
 
-    const assertor: EARL = {
-      "@context": {
-        earl: "http://www.w3.org/ns/earl#",
-        doap: "http://usefulinc.com/ns/doap#",
-        foaf: "http://xmlns.com/foaf/0.1/",
-      },
-      "@id": "https://alfa.siteimprove.com/",
-      "@type": ["earl:Assertor", "earl:Software", "doap:Project"],
-      "doap:name": "Alfa",
-      "doap:homepage": "https://alfa.siteimprove.com/",
-      "doap:release": {
-        "@type": "doap:Version",
-        "doap:revision": version,
-      },
-      "doap:license": "https://spdx.org/licenses/MIT",
-      "doap:description":
-        "Suite of open and standards-based tools for performing reliable accessibility conformance testing at scale",
-      "doap:repository": {
-        "@type": "doap:GitRepository",
-        "doap:browse": "https://github.com/siteimprove/alfa",
-        "doap:location": "https://github.com/siteimprove/alfa.git",
-      },
-      "doap:vendor": {
-        "@id": "https://siteimprove.com/",
-        "@type": "foaf:Organization",
-        "foaf:name": "Siteimprove A/S",
-      },
-    };
-
-    return stringify(
-      [
+    let earl = {
+      "@context": ACTContext,
+      "@graph": [
         assertor,
         ...subject,
         ...[...rules].map((rule) => rule.toEARL()),
@@ -62,8 +36,107 @@ export default function <I, T, Q, S>(): Formatter<I, T, Q, S> {
           return earl;
         }),
       ],
-      null,
-      2
-    );
+    } as jsonld.JsonLdDocument;
+
+    const compact = await jsonld.compact(earl, ACTContext);
+
+    return stringify(compact, null, 2);
   };
 }
+
+const assertor: EARL = {
+  "@context": {
+    earl: "http://www.w3.org/ns/earl#",
+    doap: "http://usefulinc.com/ns/doap#",
+    foaf: "http://xmlns.com/foaf/0.1/",
+  },
+  "@id": "https://alfa.siteimprove.com/",
+  "@type": ["earl:Assertor", "earl:Software", "doap:Project"],
+  "doap:name": "Alfa",
+  "doap:homepage": "https://alfa.siteimprove.com/",
+  "doap:release": {
+    "@type": "doap:Version",
+    "doap:revision": version,
+  },
+  "doap:license": "https://spdx.org/licenses/MIT",
+  "doap:description":
+    "Suite of open and standards-based tools for performing reliable accessibility conformance testing at scale",
+  "doap:repository": {
+    "@type": "doap:GitRepository",
+    "doap:browse": "https://github.com/siteimprove/alfa",
+    "doap:location": "https://github.com/siteimprove/alfa.git",
+  },
+  "doap:vendor": {
+    "@id": "https://siteimprove.com/",
+    "@type": "foaf:Organization",
+    "foaf:name": "Siteimprove A/S",
+  },
+};
+
+/**
+ * The JSON-LD context used by ACT rules CG
+ * {@link https://act-rules.github.io/earl-context.json}
+ */
+// We want a local copy of the context to avoid network connexion when
+// compacting JSON-LD documents.
+const ACTContext = {
+  "@vocab": "http://www.w3.org/ns/earl#",
+  earl: "http://www.w3.org/ns/earl#",
+  WCAG: "http://www.w3.org/TR/WCAG/#",
+  WCAG10: "http://www.w3.org/TR/WCAG10/#",
+  WCAG2: "http://www.w3.org/TR/WCAG2/#",
+  WCAG20: "http://www.w3.org/TR/WCAG20/#",
+  WCAG21: "http://www.w3.org/TR/WCAG21/#",
+  WCAG22: "http://www.w3.org/TR/WCAG22/#",
+  WCAG30: "http://www.w3.org/TR/wcag-3.0/#",
+  dct: "http://purl.org/dc/terms/",
+  sch: "https://schema.org/",
+  doap: "http://usefulinc.com/ns/doap#",
+  foaf: "http://xmlns.com/foaf/0.1/",
+  ptr: "http://www.w3.org/2009/pointers#",
+  cnt: "http://www.w3.org/2011/content#",
+  "http-vocab": "http://www.w3.org/2011/http#",
+  WebPage: "sch:WebPage",
+  url: "dct:source",
+  source: "dct:source",
+  redirectedTo: "dct:source",
+  title: "dct:title",
+  Project: "doap:Project",
+  Version: "doap:Version",
+  name: "doap:name",
+  description: "doap:description",
+  shortdesc: "doap:shortdesc",
+  created: "doap:created",
+  release: "doap:release",
+  revision: "doap:revision",
+  homepage: {
+    "@id": "doap:homepage",
+    "@type": "@id",
+  },
+  license: {
+    "@id": "doap:license",
+    "@type": "@id",
+  },
+  assertedThat: {
+    "@reverse": "assertedBy",
+  },
+  assertions: {
+    "@reverse": "subject",
+  },
+  assertedBy: {
+    "@type": "@id",
+  },
+  outcome: {
+    "@type": "@id",
+  },
+  mode: {
+    "@type": "@id",
+  },
+  pointer: {
+    "@type": "ptr:CSSSelectorPointer",
+  },
+  isPartOf: {
+    "@id": "dct:isPartOf",
+    "@type": "@id",
+  },
+};
