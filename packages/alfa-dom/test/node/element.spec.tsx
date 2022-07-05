98a86692baa27d22540c8b92b6ea7538ca2ade1f
Jean-Yves Moyen <jym@siteimprove.com>
2022-07-05 10:58:45 +0200

    Add some tests

--- packages/alfa-dom/test/node/element.spec.tsx
+++ packages/alfa-dom/test/node/element.spec.tsx
@@ -1,4 +1,11 @@
+import { None, Option } from "@siteimprove/alfa-option";
 import { test } from "@siteimprove/alfa-test";
+import { Node as treeNode } from "@siteimprove/alfa-tree";
+
+import { Node } from "../../src/node";
+
+import { Element } from "../../src/node/element";
+import { Namespace } from "../../src/namespace";
 
 test("#tabIndex() returns the tab index explicitly assigned to an element", (t) => {
   t.equal((<div tabindex="42" />).tabIndex().get(), 42);
@@ -48,4 +55,24 @@ test(`#tabIndex() returns None for a <summary> element that is not the first
   </details>;
 
   t.equal(summary.tabIndex().isNone(), true);
+});
+
+test("Element.of() accepts a user nodeId", (t) => {
+  const element = Element.of(
+    Option.of(Namespace.HTML),
+    None,
+    "div",
+    [],
+    [],
+    None,
+    treeNode.Id.user("type", "namespace", 42)
+  );
+
+  t.deepEqual(element.nodeId.kind, treeNode.Id.Kind.User);
+
+  t.deepEqual(element.nodeId.toJSON(), {
+    type: "type",
+    namespace: "namespace",
+    id: 42,
+  });
 });
