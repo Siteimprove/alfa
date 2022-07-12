53823cb06caa49c6c297ea54ac8510575ae83ac2
Jean-Yves Moyen <jym@siteimprove.com>
2022-07-12 16:13:38 +0200

    Export pnrevious heading in R53 diagnostic

--- packages/alfa-rules/test/sia-r53/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r53/rule.spec.tsx
@@ -7,12 +7,13 @@ import { evaluate } from "../common/evaluate";
 import { passed, failed, inapplicable } from "../common/outcome";
 
 test("evaluate() passes when the document headings are structured", async (t) => {
+  const first = <h1>Part one</h1>;
   const target1 = <h2>Chapter one</h2>;
   const target2 = <h3>Section one</h3>;
 
   const document = h.document([
     <html>
-      <h1>Part one</h1>
+      {first}
       {target1}
       {target2}
     </html>,
@@ -20,22 +21,23 @@ test("evaluate() passes when the document headings are structured", async (t) =>
 
   t.deepEqual(await evaluate(R53, { document }), [
     passed(R53, target1, {
-      1: Outcomes.IsStructured,
+      1: Outcomes.IsStructured(first),
     }),
     passed(R53, target2, {
-      1: Outcomes.IsStructured,
+      1: Outcomes.IsStructured(target1),
     }),
   ]);
 });
 
 test("evaluate() fails when the document headings are not properly structured", async (t) => {
+  const first = <h1>Part one</h1>;
   const target1 = <h3>Chapter one</h3>;
   const target2 = <h2>Part two</h2>;
   const target3 = <h6>Chapter one</h6>;
 
   const document = h.document([
     <html>
-      <h1>Part one</h1>
+      {first}
       {target1}
       {target2}
       {target3}
@@ -44,13 +46,13 @@ test("evaluate() fails when the document headings are not properly structured", 
 
   t.deepEqual(await evaluate(R53, { document }), [
     failed(R53, target1, {
-      1: Outcomes.IsNotStructured,
+      1: Outcomes.IsNotStructured(first),
     }),
     passed(R53, target2, {
-      1: Outcomes.IsStructured,
+      1: Outcomes.IsStructured(target1),
     }),
     failed(R53, target3, {
-      1: Outcomes.IsNotStructured,
+      1: Outcomes.IsNotStructured(target2),
     }),
   ]);
 });
@@ -66,12 +68,13 @@ test("evaluate() is inapplicable when the document has only one heading", async 
 });
 
 test("evaluate() ignore headings that are not exposed", async (t) => {
+  const first = <h1>Part one</h1>;
   const target1 = <h2>Chapter one</h2>;
   const target2 = <h2>Chapter two</h2>;
 
   const document = h.document([
     <html>
-      <h1>Part one</h1>
+      {first}
       <h3 hidden>I'm not here</h3>
       {target1}
       {target2}
@@ -80,10 +83,10 @@ test("evaluate() ignore headings that are not exposed", async (t) => {
 
   t.deepEqual(await evaluate(R53, { document }), [
     passed(R53, target1, {
-      1: Outcomes.IsStructured,
+      1: Outcomes.IsStructured(first),
     }),
     passed(R53, target2, {
-      1: Outcomes.IsStructured,
+      1: Outcomes.IsStructured(target1),
     }),
   ]);
 });
