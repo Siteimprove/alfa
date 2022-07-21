b2c5afaf4176921ac3ce8bb8637c6aaf0676aed5
Jean-Yves Moyen <jym@siteimprove.com>
2022-07-21 14:14:18 +0200

    Export a super-type for rules (#1197)
    * Export Flattened.Rule

--- packages/alfa-rules/src/index.ts
+++ packages/alfa-rules/src/index.ts
@@ -29,7 +29,16 @@ export const Rules = Record.of(rules);
  * @public
  */
 export namespace Flattened {
-  type Rule = Record.Value<typeof rules>;
+  /**
+   * The flattened type of all rules. Target, questions, … are a union of
+   * all the possible ones; that is this looks like
+   * Rule\<Page, Document | Element | …, …\>
+   *
+   * This is a super-type of the individual type of each rule.
+   *
+   * @public
+   */
+  export type Rule = Record.Value<typeof rules>;
 
   /**
    * The type of the input of rules
@@ -60,9 +69,7 @@ export namespace Flattened {
   export type Subject = act.Rule.Subject<Rule>;
 
   /**
-   * The flattened type of all rules. Target, questions, … are a union of
-   * all the possible ones; that is this looks like
-   * Rule\<Page, Document | Element | …, …\>
+   * The flattened type of the sequence of all rules.
    *
    * @public
    */
@@ -75,9 +82,9 @@ export namespace Flattened {
  *
  * @public
  */
-export const Flattened: Flattened.Flattened = Sequence.from(
+export const Flattened: Flattened.Flattened = Sequence.from<Flattened.Rule>(
   Rules.values()
-) as Flattened.Flattened;
+);
 
 export default Flattened;
 
