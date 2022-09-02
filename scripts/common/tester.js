740638eed96f53e394a7c88e7b966a7ab59f739d
Jean-Yves Moyen <jym@siteimprove.com>
2022-09-02 15:20:31 +0200

    Revert "Allow testing of a single rule"
    This reverts commit edfb7f498fc73fb00bbecbd7150609edb0148675.

--- scripts/common/tester.js
+++ scripts/common/tester.js
@@ -3,34 +3,9 @@ const async = require("async");
 const exec = require("execa");
 
 const { system } = require("./system");
-const fs = require("fs");
-const path = require("path");
 
 exports.tester = {
   test(root = "packages") {
-    // If testing a single rule, we hardcode the file to run.
-    if (root.match(/^r\d$/)) {
-      exec
-        .node(
-          path.join(
-            "packages",
-            "alfa-rules",
-            "test",
-            `sia-${root}`,
-            "rule.spec.js"
-          ),
-          [],
-          {
-            nodeOptions: [...process.execArgv, "--enable-source-maps"],
-            stdio: "inherit",
-          }
-        )
-        .then(
-          () => system.exit(0),
-          () => system.exit(1)
-        );
-    }
-
     async.eachLimit(
       system.readDirectory(root, [".spec.ts", ".spec.tsx"], ["node_modules"]),
       os.cpus().length,
--- scripts/test.js
+++ scripts/test.js
@@ -3,12 +3,7 @@ const { flags } = require("./common/flags");
 const { builder } = require("./common/builder");
 const { tester } = require("./common/tester");
 
-// If testing a single rule, the project is actually alfa-rules
-const project = flags.project.match(/^r\d$/)
-  ? "packages/alfa-rules"
-  : flags.project;
-
-const status = builder.build(project);
+const status = builder.build(flags.project);
 
 if (status !== 0) {
   system.exit(status);
