0e12cf5ba13aced86dd7acef2b280b93f2a7544c
Jean-Yves Moyen <jym@siteimprove.com>
2022-09-02 13:22:36 +0200

    Add breaking example for R41

--- packages/alfa-rules/test/sia-r41/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r41/rule.spec.tsx
@@ -1,3 +1,4 @@
+/// <reference lib="dom" />
 import { h } from "@siteimprove/alfa-dom";
 import { test } from "@siteimprove/alfa-test";
 
@@ -103,4 +104,20 @@ test("evaluate() correctly resolves relative URLs", async (t) => {
       }),
     ]
   );
+});
+
+test(`evaluate() gather links from the full page`, async (t) => {
+  const link1 = <a href="foo.html">Foo</a>;
+  const link2 = <a href="foo.html">Foo</a>;
+  const target = [link1, link2];
+
+  const document = h.document([link1, <iframe>{link2}</iframe>]);
+
+  console.dir(document.toJSON());
+
+  t.deepEqual(await evaluate(R41, { document }), [
+    passed(R41, Group.of(target), {
+      1: Outcomes.ResolveSameResource,
+    }),
+  ]);
 });
