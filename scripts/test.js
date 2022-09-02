edfb7f498fc73fb00bbecbd7150609edb0148675
Jean-Yves Moyen <jym@siteimprove.com>
2022-09-02 14:32:30 +0200

    Allow testing of a single rule

--- scripts/test.js
+++ scripts/test.js
@@ -3,7 +3,12 @@ const { flags } = require("./common/flags");
 const { builder } = require("./common/builder");
 const { tester } = require("./common/tester");
 
-const status = builder.build(flags.project);
+// If testing a single rule, the project is actually alfa-rules
+const project = flags.project.match(/^r\d$/)
+  ? "packages/alfa-rules"
+  : flags.project;
+
+const status = builder.build(project);
 
 if (status !== 0) {
   system.exit(status);
