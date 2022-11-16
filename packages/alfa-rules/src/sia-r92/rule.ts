37442ff036b1723d50cd7f3a5ad6bf3066b88e16
Jean-Yves Moyen <jym@siteimprove.com>
2022-11-16 11:36:26 +0100

    Write R92 version 2

---
+++ packages/alfa-rules/src/sia-r92/rule.ts
@@ -0,0 +1,29 @@
+import { Rule } from "@siteimprove/alfa-act";
+import { Element } from "@siteimprove/alfa-dom";
+import { Criterion } from "@siteimprove/alfa-wcag";
+import { Page } from "@siteimprove/alfa-web";
+
+import { textWithInlinedImportantProperty } from "../common/applicability/text-with-inlined-important-property";
+import { isWideEnough } from "../common/expectation/is-wide-enough";
+
+import { Scope, Version } from "../tags";
+
+const property = "word-spacing";
+const threshold = 0.16;
+
+export default Rule.Atomic.of<Page, Element>({
+  uri: "https://alfa.siteimprove.com/rules/sia-r92",
+  requirements: [Criterion.of("1.4.12")],
+  tags: [Scope.Component, Version.of(2)],
+  evaluate({ device, document }) {
+    return {
+      applicability() {
+        return textWithInlinedImportantProperty(document, device, property);
+      },
+
+      expectations(target) {
+        return isWideEnough(target, device, property, threshold);
+      },
+    };
+  },
+});
