cdbc6d2c4b15a062256fee6784aee3831e0fccc0
Jean-Yves Moyen <jym@siteimprove.com>
2022-11-25 10:36:00 +0100

    Revert "Adapt tests to new Applicability"
    This reverts commit bc879ccd2dfff86600565cae783e86abf17c5d3b.

--- packages/alfa-rules/test/sia-r47/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r47/rule.spec.tsx
@@ -6,6 +6,16 @@ import R47, { Outcomes } from "../../src/sia-r47/rule";
 import { evaluate } from "../common/evaluate";
 import { passed, failed, inapplicable } from "../common/outcome";
 
+test("evaluate() passes meta-viewport elements who don't affect zoom", async (t) => {
+  const target = <meta name="viewport" content="width=device-width" />;
+
+  const document = h.document([target]);
+
+  t.deepEqual(await evaluate(R47, { document }), [
+    passed(R47, target, { 1: Outcomes.MetaDoesNotPreventZoom }),
+  ]);
+});
+
 test("evaluate() passes meta-viewport elements who let users zoom", async (t) => {
   const target = <meta name="viewport" content="user-scalable=yes" />;
 
@@ -56,22 +66,14 @@ test("evaluate() fails meta-viewport elements who prevent zoom", async (t) => {
   ]);
 });
 
-test("evaluate() is inapplicable to other meta elements", async (t) => {
-  const document = h.document([<meta charset="UTF-8" />]);
-
-  t.deepEqual(await evaluate(R47, { document }), [inapplicable(R47)]);
-});
-
 test("evaluate() is inapplicable when meta-viewport has no `content`", async (t) => {
   const document = h.document([<meta name="viewport" />]);
 
   t.deepEqual(await evaluate(R47, { document }), [inapplicable(R47)]);
 });
 
-test("evaluate() is inappilcable to meta-viewport elements who don't affect zoom", async (t) => {
-  const document = h.document([
-    <meta name="viewport" content="width=device-width" />,
-  ]);
+test("evaluate() is inapplicable to other meta elements", async (t) => {
+  const document = h.document([<meta charset="UTF-8" />]);
 
   t.deepEqual(await evaluate(R47, { document }), [inapplicable(R47)]);
 });
