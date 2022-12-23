6918514b5fa7ed848d7ba0fa6e43353ba81bfcf1
Jean-Yves Moyen <jym@siteimprove.com>
2022-12-23 12:35:31 +0100

    Restrict applicability to media-type objects

--- packages/alfa-rules/src/sia-r63/rule.ts
+++ packages/alfa-rules/src/sia-r63/rule.ts
@@ -11,8 +11,8 @@ import { expectation } from "../common/act/expectation";
 import { Scope } from "../tags";
 
 const { hasNonEmptyAccessibleName, isIncludedInTheAccessibilityTree } = DOM;
-const { isElement, hasName, hasNamespace } = Element;
-const { and } = Predicate;
+const { isElement, hasAttribute, hasName, hasNamespace } = Element;
+const { and, or } = Predicate;
 
 export default Rule.Atomic.of<Page, Element>({
   uri: "https://alfa.siteimprove.com/rules/sia-r63",
@@ -28,7 +28,8 @@ export default Rule.Atomic.of<Page, Element>({
             and(
               hasNamespace(Namespace.HTML),
               hasName("object"),
-              isIncludedInTheAccessibilityTree(device)
+              isIncludedInTheAccessibilityTree(device),
+              embedsMedia
             )
           );
       },
@@ -55,3 +56,54 @@ export namespace Outcomes {
     Diagnostic.of(`The \`<object>\` element does not have an accessible name`)
   );
 }
+
+function embedsMedia(element: Element): boolean {
+  return element
+    .attribute("type")
+    .map((type) => MediaMIMEType.includes(type.value.split("/")[0]))
+    .getOrElse(() =>
+      element
+        .attribute("data")
+        .some((data) =>
+          MediaFileExtension.includes(data.value.split(".").splice(-1)[0])
+        )
+    );
+}
+
+/**
+ * {@see https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types}
+ * {@see https://www.iana.org/assignments/media-types/media-types.xhtml}
+ * We restrict ourselves to common types, but will expand the list if needed.
+ */
+const MediaMIMEType = ["audio", "image", "video"];
+const MediaFileExtension = [
+  "aac",
+  "avif",
+  "avi",
+  "bmp",
+  "gif",
+  "ico",
+  "jpeg",
+  "jpg",
+  "mid",
+  "midi",
+  "mp3",
+  "mp4",
+  "mpeg",
+  "oga",
+  "ogv",
+  "opus",
+  "png",
+  "svg",
+  "tif",
+  "tiff",
+  "ts",
+  "wav",
+  "weba",
+  "webm",
+  "webp",
+  "3gp",
+  "3GPP",
+  "3g2",
+  "3GPP2",
+];
