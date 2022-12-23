ec47c84cf12957fa0b76a205b4bf3d1f43acddda
Jean-Yves Moyen <jym@siteimprove.com>
2022-12-23 16:12:10 +0100

    SIA-R63: guess MIME type, and always expose `<object>` (#1297)
    * Always expose object elements
    * Update examples
    * Add breaking examples
    * Restrict applicability to media-type objects
    
    Co-authored-by: elenamongelli <52746406+elenamongelli@users.noreply.github.com>

--- packages/alfa-aria/src/node.ts
+++ packages/alfa-aria/src/node.ts
@@ -23,7 +23,7 @@ import { Container, Element, Inert, Text } from ".";
 
 import * as predicate from "./node/predicate";
 
-const { equals, not, test } = Predicate;
+const { and, equals, not, test } = Predicate;
 const { isRendered } = Style;
 
 /**
@@ -366,7 +366,7 @@ export namespace Node {
 
         const style = Style.from(node, device);
 
-        // Elements that are not rendered at all by means are never exposed in
+        // Elements that are not rendered at all are never exposed in
         // the accessibility tree, nor are their descendants.
         //
         // Since `aria-owns` can create an accessibility tree that is fairly
@@ -481,7 +481,13 @@ export namespace Node {
         // If the element has neither attributes, a role, nor a tabindex, it is
         // not itself interesting for accessibility purposes. It is therefore
         // exposed as a container.
-        if (attributes.isEmpty() && role.isNone() && node.tabIndex().isNone()) {
+        // Some elements (mostly embedded content) are always exposed.
+        if (
+          attributes.isEmpty() &&
+          role.isNone() &&
+          node.tabIndex().isNone() &&
+          !test(alwaysExpose, node)
+        ) {
           return Container.of(node, children(state));
         }
 
@@ -525,3 +531,13 @@ export namespace Node {
 
   export const { hasName } = predicate;
 }
+
+/**
+ * Some elements do not have an ARIA role but are nonetheless always exposed to
+ * ATs. These are mostly embedded content.
+ */
+// <object> with a non-empty data attribute
+const alwaysExpose = and(
+  dom.Element.hasName("object"),
+  dom.Element.hasAttribute("data", (data) => data.trim() !== "")
+);
