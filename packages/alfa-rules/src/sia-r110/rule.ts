3f912ed12ece0909c6840210d1122a0c9ef11ea1
Jean-Yves Moyen <jym@siteimprove.com>
2023-01-18 16:46:11 +0100

    Add R110 cloning R21

---
+++ packages/alfa-rules/src/sia-r110/rule.ts
@@ -0,0 +1,69 @@
+import { Rule, Diagnostic } from "@siteimprove/alfa-act";
+import { DOM, Role } from "@siteimprove/alfa-aria";
+import { Attribute, Element, Namespace, Node } from "@siteimprove/alfa-dom";
+import { Predicate } from "@siteimprove/alfa-predicate";
+import { Err, Ok } from "@siteimprove/alfa-result";
+import { Criterion, Technique } from "@siteimprove/alfa-wcag";
+import { Page } from "@siteimprove/alfa-web";
+
+import { expectation } from "../common/act/expectation";
+
+import { Scope } from "../tags";
+
+const { isProgrammaticallyHidden } = DOM;
+const { hasAttribute, hasNamespace, isElement } = Element;
+const { and, not } = Predicate;
+
+export default Rule.Atomic.of<Page, Attribute>({
+  uri: "https://alfa.siteimprove.com/rules/sia-r21",
+  requirements: [
+    Criterion.of("1.3.1"),
+    Technique.of("ARIA4"),
+    Technique.of("G108"),
+  ],
+  tags: [Scope.Component],
+  evaluate({ device, document }) {
+    return {
+      applicability() {
+        return (
+          document
+            .descendants(Node.fullTree)
+            .filter(isElement)
+            .filter(
+              and(
+                hasNamespace(Namespace.HTML, Namespace.SVG),
+                hasAttribute("role", (value) => value.trim().length > 0),
+                not(isProgrammaticallyHidden(device))
+              )
+            )
+            // The previous filter ensures the existence of role.
+            .map((element) => element.attribute("role").getUnsafe())
+        );
+      },
+
+      expectations(target) {
+        return {
+          1: expectation(
+            target
+              .tokens()
+              .every(
+                (token) => Role.isName(token) && Role.of(token).isConcrete()
+              ),
+            () => Outcomes.HasValidRole,
+            () => Outcomes.HasNoValidRole
+          ),
+        };
+      },
+    };
+  },
+});
+
+export namespace Outcomes {
+  export const HasValidRole = Ok.of(
+    Diagnostic.of(`The element has a valid role`)
+  );
+
+  export const HasNoValidRole = Err.of(
+    Diagnostic.of(`The element does not have a valid role`)
+  );
+}
---
+++ packages/alfa-rules/test/sia-r110/rule.spec.tsx
@@ -0,0 +1,89 @@
+import { h } from "@siteimprove/alfa-dom";
+import { test } from "@siteimprove/alfa-test";
+
+import R21, { Outcomes } from "../../src/sia-r21/rule";
+
+import { evaluate } from "../common/evaluate";
+import { passed, failed, inapplicable } from "../common/outcome";
+
+test("evaluates() passes an element with a single valid role", async (t) => {
+  const target = h.attribute("role", "button");
+
+  const document = h.document([h("button", [target])]);
+
+  t.deepEqual(await evaluate(R21, { document }), [
+    passed(R21, target, {
+      1: Outcomes.HasValidRole,
+    }),
+  ]);
+});
+
+test("evaluates() passes an element with multiple valid roles", async (t) => {
+  const target = h.attribute("role", "button link");
+
+  const document = h.document([h("button", [target])]);
+
+  t.deepEqual(await evaluate(R21, { document }), [
+    passed(R21, target, {
+      1: Outcomes.HasValidRole,
+    }),
+  ]);
+});
+
+test("evaluates() fails an element with an invalid role", async (t) => {
+  const target = h.attribute("role", "btn");
+
+  const document = h.document([h("button", [target])]);
+
+  t.deepEqual(await evaluate(R21, { document }), [
+    failed(R21, target, {
+      1: Outcomes.HasNoValidRole,
+    }),
+  ]);
+});
+
+test("evaluates() fails an element with both a valid and an invalid role", async (t) => {
+  const target = h.attribute("role", "btn link");
+
+  const document = h.document([h("button", [target])]);
+
+  t.deepEqual(await evaluate(R21, { document }), [
+    failed(R21, target, {
+      1: Outcomes.HasNoValidRole,
+    }),
+  ]);
+});
+
+test("evaluates() fails an element with an invalid role and not included in the accessibility tree by default", async (t) => {
+  const span = <span role="invalid">Foo</span>;
+  const target = span.attribute("role").getUnsafe();
+
+  const document = h.document([span]);
+
+  t.deepEqual(await evaluate(R21, { document }), [
+    failed(R21, target, {
+      1: Outcomes.HasNoValidRole,
+    }),
+  ]);
+});
+
+test("evaluates() is inapplicable to a hidden element", async (t) => {
+  const button = (
+    <button hidden role="invalid">
+      Hello
+    </button>
+  );
+  const document = h.document([button]);
+  t.deepEqual(await evaluate(R21, { document }), [inapplicable(R21)]);
+});
+test("evaluate() is inapplicable when there is no role attribute", async (t) => {
+  const document = h.document([<button />]);
+
+  t.deepEqual(await evaluate(R21, { document }), [inapplicable(R21)]);
+});
+
+test("evaluate() is inapplicable when a role attribute is only whitespace", async (t) => {
+  const document = h.document([<button role=" " />]);
+
+  t.deepEqual(await evaluate(R21, { document }), [inapplicable(R21)]);
+});
