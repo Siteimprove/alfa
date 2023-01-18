eacd74880fdb4cf0cce848cf74ed955732cda6b2
Jean-Yves Moyen <jym@siteimprove.com>
2023-01-18 13:27:23 +0100

    Accept iterable of string as input

--- packages/alfa-dom/src/node/element/predicate/has-id.ts
+++ packages/alfa-dom/src/node/element/predicate/has-id.ts
@@ -1,3 +1,4 @@
+import { Iterable } from "@siteimprove/alfa-iterable";
 import { Predicate } from "@siteimprove/alfa-predicate";
 
 import { Element } from "../../element";
@@ -14,16 +15,22 @@ export function hasId(predicate?: Predicate<string>): Predicate<Element>;
  */
 export function hasId(id: string, ...rest: Array<string>): Predicate<Element>;
 
+/**
+ * @public
+ */
+export function hasId(ids: Iterable<string>): Predicate<Element>;
 export function hasId(
-  idOrPredicate: string | Predicate<string> = () => true,
+  idOrPredicate: string | Predicate<string> | Iterable<string> = () => true,
   ...ids: Array<string>
 ): Predicate<Element> {
   let predicate: Predicate<string>;
 
   if (typeof idOrPredicate === "function") {
     predicate = idOrPredicate;
-  } else {
+  } else if (typeof idOrPredicate === "string") {
     predicate = equals(idOrPredicate, ...ids);
+  } else {
+    predicate = (value) => Iterable.includes(ids, value);
   }
 
   return (element) => element.id.some(predicate);
