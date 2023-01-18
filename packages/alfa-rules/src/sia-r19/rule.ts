4ebbb7334783645a0b4729a9a9e6df1cdbe308f3
Jean-Yves Moyen <jym@siteimprove.com>
2023-01-18 15:20:11 +0100

    Force required ID ref (list) to point to discoverable nodes

--- packages/alfa-rules/src/sia-r19/rule.ts
+++ packages/alfa-rules/src/sia-r19/rule.ts
@@ -1,5 +1,6 @@
 import { Rule, Diagnostic } from "@siteimprove/alfa-act";
 import { Cache } from "@siteimprove/alfa-cache";
+import { Device } from "@siteimprove/alfa-device";
 import { Attribute, Element, Namespace, Node } from "@siteimprove/alfa-dom";
 import { Iterable } from "@siteimprove/alfa-iterable";
 import { Predicate } from "@siteimprove/alfa-predicate";
@@ -22,7 +23,7 @@ const { and } = Refinement;
 export default Rule.Atomic.of<Page, Attribute<aria.Attribute.Name>>({
   uri: "https://alfa.siteimprove.com/rules/sia-r19",
   tags: [Scope.Component],
-  evaluate({ document }) {
+  evaluate({ device, document }) {
     return {
       applicability() {
         return document
@@ -38,10 +39,15 @@ export default Rule.Atomic.of<Page, Attribute<aria.Attribute.Name>>({
 
       expectations(target) {
         const { name, value } = target;
+        const attribute = aria.Attribute.of(name, value);
+
+        // The owner is ensured because the applicability search for elements and
+        // then look for their attributes.
+        const owner = target.owner.getUnsafe();
 
         return {
           1: expectation(
-            isValid(aria.Attribute.of(name, value)),
+            isValid(attribute) && isRequiredRefValid(attribute, owner, device),
             () => Outcomes.HasValidValue,
             () => Outcomes.HasNoValidValue
           ),
@@ -100,4 +106,57 @@ function isValid(attribute: aria.Attribute): boolean {
     case "token-list":
       return value.split(/\s+/).every((value) => options.some(equals(value)));
   }
+}
+
+/**
+ * All ids that exist in the tree of a given root.
+ */
+const idsCache = Cache.empty<Node, Sequence<string>>();
+
+function treeHasId(id: string, node: Node): boolean {
+  // We absolutely need no traversal options here, because `id` are scoped to
+  // trees, so we do not want to cross shadow or content boundaries.
+  return idsCache
+    .get(node.root(), () =>
+      node
+        .root()
+        .descendants()
+        .filter(isElement)
+        .collect((elt) => elt.id)
+    )
+    .includes(id);
+}
+
+function isRequiredRefValid(
+  attribute: aria.Attribute,
+  owner: Element,
+  device: Device
+): boolean {
+  for (const role of aria.Node.from(owner, device).role) {
+    const { name, type, value } = attribute;
+
+    if (role.name === "combobox" && name === "aria-controls") {
+      // combobox only require aria-controls when opened, which we can't really detect
+      // aria-controls is no more required in ARIA 1.3 (and authoring practices)
+      // but that hasn't made it to ARIA 1.2 :-(
+      // @see https://github.com/w3c/aria/pull/1335
+      return true;
+    }
+
+    if (
+      role.isAttributeRequired(name) &&
+      (type === "id-reference" || type === "id-reference-list")
+    ) {
+      // This is a required attribute with an ID ref (list) value, one of the tokens
+      // must exist has an ID in the same tree.
+      // Note: as of ARIA 1.2, this is only aria-controls on scrollbar…
+      return value.split(" ").some((token) => treeHasId(token.trim(), owner));
+    }
+    // The attribute either is not required, or does not have an ID ref (list) type,
+    // so this is good
+    return true;
+  }
+
+  // The owner has no role, so this is good
+  return true;
 }
