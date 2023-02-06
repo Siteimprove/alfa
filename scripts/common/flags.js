2879a72abbf52b34ebbb8c378a4ebd8232b4151a
Rolf Christian Jørgensen <rcj@siteimprove.com>
2023-02-06 16:43:23 +0100

    Show informational build output by default
    To opt out use the new `--quiet` flag which replaces the previous `--verbose` flag

--- scripts/common/flags.js
+++ scripts/common/flags.js
@@ -3,12 +3,12 @@ const process = require("process");
 const minimist = require("minimist");
 
 const argv = minimist(process.argv.slice(2), {
-  boolean: ["force", "verbose", "pretty"],
+  boolean: ["force", "quiet", "pretty"],
 });
 
 exports.flags = {
   project: argv._[0] || path.relative(process.cwd(), process.env.INIT_CWD),
   force: argv.force,
-  verbose: argv.verbose,
+  verbose: !argv.quiet,
   pretty: argv.pretty,
 };
