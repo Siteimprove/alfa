db376ba171f3f9b48ab5f47f3ff2316e6dcdd626
Rolf Christian Jørgensen <rcj@siteimprove.com>
2023-02-07 15:25:08 +0100

    Add support for `webauthn` token in sia-r10
    The rule logic was also been cleaned up a bit and a few bugs where discovered and fixed. The bugs where:
    * Modifiers like `home` followed by nothing was passing
    * Valid tokens followed by invalid tokens was passing

--- packages/alfa-rules/src/sia-r10/rule.ts
+++ packages/alfa-rules/src/sia-r10/rule.ts
@@ -1,4 +1,4 @@
-import { Rule, Diagnostic } from "@siteimprove/alfa-act";
+import { Diagnostic, Rule } from "@siteimprove/alfa-act";
 import { DOM, Node } from "@siteimprove/alfa-aria";
 import { Attribute, Element, Namespace } from "@siteimprove/alfa-dom";
 import { Predicate } from "@siteimprove/alfa-predicate";
@@ -12,6 +12,7 @@ import * as dom from "@siteimprove/alfa-dom";
 import { expectation } from "../common/act/expectation";
 import { normalize } from "../common/normalize";
 
+import { Array } from "@siteimprove/alfa-array";
 import { Scope } from "../tags";
 
 const { hasRole, isPerceivableForAll } = DOM;
@@ -76,104 +77,115 @@ function hasTokens(input: string): boolean {
   return input.trim() !== "" && input.split(/\s+/).length > 0;
 }
 
+/**
+ * {@link https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#autofill-detail-tokens}
+ */ 
 const isValidAutocomplete: Predicate<Attribute> = (autocomplete) => {
   const tokens = autocomplete.value.toLowerCase().trim().split(/\s+/);
 
   let i = 0;
-  let next = tokens[i++];
+  let next: string | undefined = tokens[i++];
 
   if (next === undefined) {
     return false;
   }
 
+  // 1. Optional
   if (next.startsWith("section-")) {
     next = tokens[i++];
   }
 
+  // 2. Optional
   if (next === "shipping" || next === "billing") {
     next = tokens[i++];
   }
-
-  let field: string | null = null;
-
-  switch (next) {
-    case "name":
-    case "honorific-prefix":
-    case "given-name":
-    case "additional-name":
-    case "family-name":
-    case "honorific-suffix":
-    case "nickname":
-    case "username":
-    case "new-password":
-    case "current-password":
-    case "organization-title":
-    case "organization":
-    case "street-address":
-    case "address-line1":
-    case "address-line2":
-    case "address-line3":
-    case "address-level4":
-    case "address-level3":
-    case "address-level2":
-    case "address-level1":
-    case "country":
-    case "country-name":
-    case "postal-code":
-    case "cc-name":
-    case "cc-given-name":
-    case "cc-additional-name":
-    case "cc-family-name":
-    case "cc-number":
-    case "cc-exp":
-    case "cc-exp-month":
-    case "cc-exp-year":
-    case "cc-csc":
-    case "cc-type":
-    case "transaction-currency":
-    case "transaction-amount":
-    case "language":
-    case "bday":
-    case "bday-day":
-    case "bday-month":
-    case "bday-year":
-    case "sex":
-    case "url":
-    case "photo":
-      field = next;
-      break;
-
-    default:
-      switch (next) {
-        case "home":
-        case "work":
-        case "mobile":
-        case "fax":
-        case "pager":
-          next = tokens[i++];
-      }
-
-      switch (next) {
-        case "tel":
-        case "tel-country-code":
-        case "tel-national":
-        case "tel-area-code":
-        case "tel-local":
-        case "tel-local-prefix":
-        case "tel-local-suffix":
-        case "tel-extension":
-        case "email":
-        case "impp":
-          field = next;
-      }
+  
+  if (next === undefined) {
+    return true;
   }
 
-  if (field === null) {
-    return false;
+  // 3. Either of the following two options:
+  if (unmodifiableFields.includes(next)) {
+    next = tokens[i++];
+  } else if (modifiers.includes(next)) {
+    next = tokens[i++];
+    // Optional, but must be followed by:
+    if (modifiableFields.includes(next)) {
+      next = tokens[i++];
+    } else {
+      return false;
+    }
   }
 
-  return true;
+  // 4. Optional
+  if (next === "webauthn") {
+    next = tokens[i++];
+  }
+  
+  // Any additional tokens will be invalid
+  return next === undefined;
 };
+
+const unmodifiableFields = Array.from([
+  "name",
+  "honorific-prefix",
+  "given-name",
+  "additional-name",
+  "family-name",
+  "honorific-suffix",
+  "nickname",
+  "username",
+  "new-password",
+  "current-password",
+  "organization-title",
+  "organization",
+  "street-address",
+  "address-line1",
+  "address-line2",
+  "address-line3",
+  "address-level4",
+  "address-level3",
+  "address-level2",
+  "address-level1",
+  "country",
+  "country-name",
+  "postal-code",
+  "cc-name",
+  "cc-given-name",
+  "cc-additional-name",
+  "cc-family-name",
+  "cc-number",
+  "cc-exp",
+  "cc-exp-month",
+  "cc-exp-year",
+  "cc-csc",
+  "cc-type",
+  "transaction-currency",
+  "transaction-amount",
+  "language",
+  "bday",
+  "bday-day",
+  "bday-month",
+  "bday-year",
+  "sex",
+  "url",
+  "photo",
+]);
+
+const modifiers = Array.from(["home", "work", "mobile", "fax", "pager"]);
+
+const modifiableFields = Array.from([
+  "tel",
+  "tel-country-code",
+  "tel-national",
+  "tel-area-code",
+  "tel-local",
+  "tel-local-prefix",
+  "tel-local-suffix",
+  "tel-extension",
+  "email",
+  "impp",
+]);
 
 export namespace Outcomes {
   export const HasValidValue = Ok.of(
