12a4e0bbcde09f9ecc002b89572658a2ea4cb5bc
Rolf Christian Jørgensen <rcj@siteimprove.com>
2023-02-13 14:18:09 +0100

    Add parser for validating autocomplete attribute

--- packages/alfa-rules/src/sia-r10/rule.ts
+++ packages/alfa-rules/src/sia-r10/rule.ts
@@ -1,27 +1,28 @@
 import { Diagnostic, Rule } from "@siteimprove/alfa-act";
 import { DOM, Node } from "@siteimprove/alfa-aria";
+import { Array } from "@siteimprove/alfa-array";
 import { Attribute, Element, Namespace } from "@siteimprove/alfa-dom";
+import { Parser } from "@siteimprove/alfa-parser";
 import { Predicate } from "@siteimprove/alfa-predicate";
 import { Err, Ok, Result } from "@siteimprove/alfa-result";
+import { Slice } from "@siteimprove/alfa-slice";
 import { Style } from "@siteimprove/alfa-style";
 import { Criterion } from "@siteimprove/alfa-wcag";
 import { Page } from "@siteimprove/alfa-web";
-import { None, Option, Some } from "@siteimprove/alfa-option";
 
 import * as dom from "@siteimprove/alfa-dom";
 
 import { expectation } from "../common/act/expectation";
 import { normalize } from "../common/normalize";
 
-import { Array } from "@siteimprove/alfa-array";
 import { Scope } from "../tags";
-import { Sequence } from "@siteimprove/alfa-sequence";
 
 const { hasRole, isPerceivableForAll } = DOM;
 const { hasAttribute, hasInputType, hasName, hasNamespace, isElement } =
   Element;
 const { and, or, not } = Predicate;
 const { isTabbable } = Style;
+const { either, end, option, right } = Parser;
 
 export default Rule.Atomic.of<Page, Attribute>({
   uri: "https://alfa.siteimprove.com/rules/sia-r10",
@@ -75,24 +76,44 @@ export default Rule.Atomic.of<Page, Attribute>({
   },
 });
 
+export namespace Outcomes {
+  export const HasValidValue = Ok.of(
+    Diagnostic.of(`The \`autocomplete\` attribute has a valid value`)
+  );
+  export const HasNoValidValue = Err.of(
+    Diagnostic.of(`The \`autocomplete\` attribute does not have a valid value`)
+  );
+}
+
 function hasTokens(input: string): boolean {
   return input.trim() !== "" && input.split(/\s+/).length > 0;
 }
 
 /**
  * {@link https://html.spec.whatwg.org/multipage/#autofill-detail-tokens}
- */ 
+ */
 const isValidAutocomplete: Predicate<Attribute> = (autocomplete) => {
-  const tokens = autocomplete.value.toLowerCase();
+  const tokens = autocomplete.value.toLowerCase().trim().split(/\s+/);
 
-  const reader = Reader.of(tokens);
-  const lexer = Lexer.of(reader);
-  const parser = Parser.of(lexer);
+  // The line comments refers to the numbered bullets describing the allowed values outlined in the HTML specification linked above
+  const parse = right(
+    option(section), // 1.
+    right(
+      option(addressType), // 2.
+      right(
+        either(unmodifiable /*3.a*/, right(option(modifier) /*3.b.1*/, modifiable /*3.b.2*/)), // 3.
+        right(
+          option(webauthn), // 4.
+          end((token) => `Expected EOF, but got ${token}`)
+        )
+      )
+    )
+  );
 
-  return parser.parse().isOk();
+  return parse(Slice.of(tokens)).isOk();
 };
 
-const unmodifiableFields = Array.from([
+const unmodifiables = Array.from([
   "name",
   "honorific-prefix",
   "given-name",
@@ -138,9 +159,7 @@ const unmodifiableFields = Array.from([
   "photo",
 ]);
 
-const modifiers = Array.from(["home", "work", "mobile", "fax", "pager"]);
-
-const modifiableFields = Array.from([
+const modifiables = Array.from([
   "tel",
   "tel-country-code",
   "tel-national",
@@ -153,162 +172,39 @@ const modifiableFields = Array.from([
   "impp",
 ]);
 
-export namespace Outcomes {
-  export const HasValidValue = Ok.of(
-    Diagnostic.of(`The \`autocomplete\` attribute has a valid value`)
-  );
+const modifiers = Array.from(["home", "work", "mobile", "fax", "pager"]);
 
-  export const HasNoValidValue = Err.of(
-    Diagnostic.of(`The \`autocomplete\` attribute does not have a valid value`)
-  );
+const addressType = parserOf(["shipping", "billing"]);
+const section = sectionParser();
+const unmodifiable = parserOf(unmodifiables);
+const modifiable = parserOf(modifiables);
+const modifier = parserOf(modifiers);
+const webauthn = parserOf(["webauthn"]);
+
+function parserOf(
+  tokens: Array<string>
+): Parser<Slice<string>, string, string> {
+  return (input) => {
+    const token = input.array[input.offset];
+
+    if (token !== undefined && tokens.includes(token)) {
+      return Result.of([input.slice(1), token]);
+    }
+
+    return Err.of(`Expected valid token, but got ${input.toJSON()}`);
+  };
 }
 
-class Reader {
-  private constructor(private input: string) {}
+function sectionParser(): Parser<Slice<string>, string, string> {
+  return (input) => {
+    const token = input.array[input.offset];
 
-  static of(input: string) {
-    return new Reader(input);
-  }
-
-  private cursor = 0;
-
-  peek(k: number = 0): Option<string> {
-    const char = this.input.charAt(this.cursor + k);
-    return char === "" ? None : Some.of(char);
-  }
-
-  consume() {
-    return this.peek().tee(() => {
-      this.cursor += 1;
-    });
-  }
-}
-
-class Lexer {
-  private readonly tokens: string[] = [];
-  private constructor(private reader: Reader) {
-    while (true) {
-      let next = reader.peek();
-      if (!next.isSome()) {
-        return;
-      }
-      if (next.get() === " ") {
-        reader.consume(); // Consume and discard whitespace
-      } else {
-        this.token();
-      }
-    }
-  }
-
-  static of(reader: Reader) {
-    return new Lexer(reader);
-  }
-
-  private cursor = 0;
-
-  peek() {
-    return Option.from(this.tokens[this.cursor]);
-  }
-
-  consume() {
-    return this.peek().tee(() => {
-      this.cursor += 1;
-    });
-  }
-
-  private token() {
-    let chars: string[] = [];
-    while (true) {
-      const next = this.reader.consume();
-      if (!next.isSome()) {
-        break;
-      }
-      if (next.get() === " ") {
-        break;
-      }
-      chars.push(next.get());
-    }
-    this.tokens.push(chars.join(""));
-  }
-}
-
-class Parser {
-  private constructor(private lexer: Lexer) {
-    this.token = lexer.consume();
-  }
-
-  static of(lexer: Lexer) {
-    return new Parser(lexer);
-  }
-
-  private token: Option<string>;
-  private nextToken() {
-    this.token = this.lexer.consume();
-  }
-
-  accept(token: Option<string>) {
-    if (this.token.equals(token)) {
-      this.nextToken();
-      return true;
-    }
-    return false;
-  }
-
-  expect(token: Option<string>): Result<None, string> {
-    if (this.accept(token)) {
-      return Ok.of(None);
+    if (token !== undefined && token.startsWith("section-")) {
+      return Result.of([input.slice(1), token]);
     }
 
-    return Err.of(`unexpected token: \`${token}\``);
-  }
-
-  section() {
-    if (this.token.isSome() && this.token.get().startsWith("section-")) {
-      this.nextToken();
-    }
-    return Ok.of(None);
-  }
-
-  shippingOrBilling() {
-    this.accept(Some.of("shipping")) || this.accept(Some.of("billing"));
-    return Ok.of(None);
-  }
-
-  // TODO: Come up with a better name
-  required(): Result<None, string> {
-    if (!this.token.isSome()) {
-      return Err.of("expected some token");
-    }
-
-    if (
-      unmodifiableFields.includes(this.token.get()) ||
-      modifiableFields.includes(this.token.get())
-    ) {
-      this.nextToken();
-      return Ok.of(None);
-    } else if (modifiers.includes(this.token.get())) {
-      this.nextToken();
-      if (this.token.isSome() && modifiableFields.includes(this.token.get())) {
-        this.nextToken();
-        return Ok.of(None);
-      }
-
-      return Err.of("expected modifiable");
-    }
-
-    return Err.of("missing required token");
-  }
-
-  webauthn() {
-    this.accept(Some.of("webauthn"));
-    return Ok.of(None);
-  }
-
-  parse() {
-    return this.section().and(
-      this.shippingOrBilling().and(
-        this.required().and(this.webauthn().and(this.expect(None)))
-      )
+    return Err.of(
+      `Expected token beginning with \`section-\`, but got ${input}`
     );
-  }
+  };
 }
