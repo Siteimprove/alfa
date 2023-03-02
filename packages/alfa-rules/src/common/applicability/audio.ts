70a03e04f14cc35e4ee27025a9f48335c14b9557
Jean-Yves Moyen <jym@siteimprove.com>
2023-03-02 15:17:46 +0100

    Cache audio applicability

--- packages/alfa-rules/src/common/applicability/audio.ts
+++ packages/alfa-rules/src/common/applicability/audio.ts
@@ -1,9 +1,11 @@
 import { Interview } from "@siteimprove/alfa-act";
 import { DOM } from "@siteimprove/alfa-aria";
+import { Cache } from "@siteimprove/alfa-cache";
 import { Device } from "@siteimprove/alfa-device";
 import { Document, Element, Namespace, Node } from "@siteimprove/alfa-dom";
 import { None, Option } from "@siteimprove/alfa-option";
 import { Predicate } from "@siteimprove/alfa-predicate";
+import { Sequence } from "@siteimprove/alfa-sequence/src/sequence";
 import { Style } from "@siteimprove/alfa-style";
 
 import { Question } from "../act/question";
@@ -13,30 +15,42 @@ const { isElement, hasName, hasNamespace } = Element;
 const { and } = Predicate;
 const { isRendered } = Style;
 
+const cache = Cache.empty<
+  Document,
+  Cache<
+    Device,
+    Sequence<Interview<Question.Metadata, Element, Element, Option<Element>>>
+  >
+>();
+
 export function audio(
   document: Document,
   device: Device
-): Iterable<Interview<Question.Metadata, Element, Element, Option<Element>>> {
-  return document
-    .descendants(Node.fullTree)
-    .filter(isElement)
-    .filter(
-      // Non-rendered <audio> are not playing
-      and(hasNamespace(Namespace.HTML), hasName("audio"), isRendered(device))
-    )
-    .map((element) =>
-      Question.of("is-audio-streaming", element).map((isStreaming) =>
-        isStreaming
-          ? None
-          : Question.of("is-playing", element).map((isPlaying) =>
-              isPlaying
-                ? Option.of(element)
-                : Question.of("play-button", element).map((playButton) =>
-                    playButton.some(and(isElement, isPerceivableForAll(device)))
-                      ? Option.of(element)
-                      : None
-                  )
-            )
+): Sequence<Interview<Question.Metadata, Element, Element, Option<Element>>> {
+  return cache.get(document, Cache.empty).get(device, () =>
+    document
+      .descendants(Node.fullTree)
+      .filter(isElement)
+      .filter(
+        // Non-rendered <audio> are not playing
+        and(hasNamespace(Namespace.HTML), hasName("audio"), isRendered(device))
       )
-    );
+      .map((element) =>
+        Question.of("is-audio-streaming", element).map((isStreaming) =>
+          isStreaming
+            ? None
+            : Question.of("is-playing", element).map((isPlaying) =>
+                isPlaying
+                  ? Option.of(element)
+                  : Question.of("play-button", element).map((playButton) =>
+                      playButton.some(
+                        and(isElement, isPerceivableForAll(device))
+                      )
+                        ? Option.of(element)
+                        : None
+                    )
+              )
+        )
+      )
+  );
 }
