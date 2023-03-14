f3c6c58f904753f5c7e07aeb32f1e5a5cdfa63a7
Jean-Yves Moyen <jym@siteimprove.com>
2023-03-14 13:00:18 +0100

    Correct Target and other types

--- packages/alfa-rules/src/index.ts
+++ packages/alfa-rules/src/index.ts
@@ -30,42 +30,66 @@ export const Rules = Record.of(rules);
  */
 export namespace Flattened {
   /**
-   * A union of all the possible types of rules
+   * We want to export types for the possible inputs, targets, … of rules.
+   * Since there is no variance annotation on the Rule type, we can't apply
+   * the act.Rule.* extractors to the unions of rules.
+   * That is,
+   * Atomic<I, T, Q, S> | Atomic<I', T', Q', S'>
+   * does **not** extend Rule<?, ?, ?, ?> and act.Rule.Input will result
+   * in `never` rather than `I | I'`
+   *
+   * So, instead, we need to apply act.Rule.Input on each individual type of
+   * the union. This is not directly possible, but can be done through mapped
+   * types on an object type containing all the possibilities.
+   *
+   * We first construct the object type ({R1: Atomic<…>, R2: …}) and its keys
+   * ("R1" | "R2" | …)
+   * Next, for each extractor, we iterate it over the values of the object type
+   * and only keep the resulting values. The unions are then automatically
+   * collapsed as we won them.
    */
-  type RulesUnion = Record.Value<typeof rules>;
+  type RulesObject = typeof rules;
+
+  /**
+   * The keys of all rules
+   * This looks like: "R1" | "R2" | …
+   */
+  type Keys = keyof RulesObject;
 
   /**
    * The type of the input of rules
    *
    * @public
    */
-  export type Input = act.Rule.Input<RulesUnion>;
+  export type Input = { [K in Keys]: act.Rule.Input<RulesObject[K]> }[Keys];
 
   /**
    * The type of the targets of rules
    *
    * @public
    */
-  export type Target = act.Rule.Target<RulesUnion>;
+  export type Target = { [K in Keys]: act.Rule.Target<RulesObject[K]> }[Keys];
 
   /**
    * The type of the questions asked by rules
    *
    * @public
    */
-  export type Question = act.Rule.Question<RulesUnion>;
+  export type Question = {
+    [K in Keys]: act.Rule.Question<RulesObject[K]>;
+  }[Keys];
 
   /**
    * The type of the subjects of questions asked by rules
    *
    * @public
    */
-  export type Subject = act.Rule.Subject<RulesUnion>;
+  export type Subject = { [K in Keys]: act.Rule.Subject<RulesObject[K]> }[Keys];
 
   /**
    * The flattened type of all rules. Target, questions, … are a union of
    * all the possible ones; that is this looks like
-   * Rule\<Page, Document | Element | …, …\>
+   * Rule<Page, Document | Element | …, …>
    *
    * This is a super-type of the individual type of each rule.
    *
