28f103fb2982ba0d23d701bf07ce356bb8843bb5
Rolf Christian Jørgensen <rcj@siteimprove.com>
2023-03-14 14:51:03 +0100

    Simplify `isTransparent`

--- packages/alfa-style/src/node/predicate/is-transparent.ts
+++ packages/alfa-style/src/node/predicate/is-transparent.ts
@@ -3,14 +3,40 @@ import { Color } from "@siteimprove/alfa-css";
 import { Device } from "@siteimprove/alfa-device";
 import { Element, Node, Text } from "@siteimprove/alfa-dom";
 import { Predicate } from "@siteimprove/alfa-predicate";
+import { Refinement } from "@siteimprove/alfa-refinement";
 import { Context } from "@siteimprove/alfa-selector";
 
 import { hasComputedStyle } from "../../element/predicate/has-computed-style";
 
 const { isElement } = Element;
 const { isText } = Text;
+const { or, and } = Refinement;
 
 const cache = Cache.empty<Device, Cache<Context, Cache<Node, boolean>>>();
+
+function isNotOpaque(device: Device, context: Context) {
+  return hasComputedStyle(
+    "opacity",
+    (opacity) => opacity.value === 0,
+    device,
+    context
+  );
+}
+
+function hasTransparentParent(device: Device, context: Context) {
+  return (node: Node<string>) =>
+    node
+      .parent(Node.flatTree)
+      .some(
+        or(
+          and(
+            isElement,
+            hasComputedStyle("color", Color.isTransparent, device, context)
+          ),
+          isTransparent(device, context)
+        )
+      );
+}
 
 /**
  * @internal
@@ -23,37 +49,11 @@ export function isTransparent(
     return cache
       .get(device, Cache.empty)
       .get(context, Cache.empty)
-      .get(node, () => {
-        if (
-          isElement(node) &&
-          hasComputedStyle(
-            "opacity",
-            (opacity) => opacity.value === 0,
-            device,
-            context
-          )(node)
-        ) {
-          return true;
-        }
-
-        for (const parent of node.parent(Node.flatTree)) {
-          if (
-            isText(node) &&
-            isElement(parent) &&
-            hasComputedStyle(
-              "color",
-              Color.isTransparent,
-              device,
-              context
-            )(parent)
-          ) {
-            return true;
-          }
-
-          return isTransparent(parent);
-        }
-
-        return false;
-      });
+      .get(node, () =>
+        or(
+          and(isElement, isNotOpaque(device, context)),
+          and(isText, hasTransparentParent(device, context))
+        )(node)
+      );
   };
 }
