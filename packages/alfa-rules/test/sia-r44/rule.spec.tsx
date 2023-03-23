99a7a7a5ee384c250ff22ffff4e6b437b84291e8
Jean-Yves Moyen <jym@siteimprove.com>
2023-03-23 09:25:47 +0100

    Add breaking examples

--- packages/alfa-rules/test/sia-r44/rule.spec.tsx
+++ packages/alfa-rules/test/sia-r44/rule.spec.tsx
@@ -6,7 +6,7 @@ import R44, { Outcomes } from "../../src/sia-r44/rule";
 import { evaluate } from "../common/evaluate";
 import { passed, failed, inapplicable } from "../common/outcome";
 
-test("evaluate() passes a page with conditional rotation that does not restrict orientation", async (t) => {
+test("evaluate() passes a page with conditional rotation (transform) that does not restrict orientation", async (t) => {
   const target = <div>Hello</div>;
 
   const document = h.document(
@@ -15,6 +15,63 @@ test("evaluate() passes a page with conditional rotation that does not restrict 
       h.sheet([
         h.rule.media("(orientation: portrait)", [
           h.rule.style("div", { transform: "rotateZ(1turn)" }),
+        ]),
+      ]),
+    ]
+  );
+
+  t.deepEqual(await evaluate(R44, { document }), [
+    passed(R44, target, { 1: Outcomes.RotationNotLocked }),
+  ]);
+});
+
+test("evaluate() passes a page with conditional rotation (transform) not around the z axis", async (t) => {
+  const target = <div>Hello</div>;
+
+  const document = h.document(
+    [target],
+    [
+      h.sheet([
+        h.rule.media("(orientation: portrait)", [
+          h.rule.style("div", { transform: "rotateX(90deg)" }),
+        ]),
+      ]),
+    ]
+  );
+
+  t.deepEqual(await evaluate(R44, { document }), [
+    passed(R44, target, { 1: Outcomes.RotationNotLocked }),
+  ]);
+});
+
+test("evaluate() passes a page with conditional rotation (rotate) that does not restrict orientation", async (t) => {
+  const target = <div>Hello</div>;
+
+  const document = h.document(
+    [target],
+    [
+      h.sheet([
+        h.rule.media("(orientation: portrait)", [
+          h.rule.style("div", { rotate: "z 1turn" }),
+        ]),
+      ]),
+    ]
+  );
+
+  t.deepEqual(await evaluate(R44, { document }), [
+    passed(R44, target, { 1: Outcomes.RotationNotLocked }),
+  ]);
+});
+
+test("evaluate() passes a page with conditional rotation (rotate) not around the z axis", async (t) => {
+  const target = <div>Hello</div>;
+
+  const document = h.document(
+    [target],
+    [
+      h.sheet([
+        h.rule.media("(orientation: portrait)", [
+          h.rule.style("div", { rotate: "1 1 0 90deg" }),
         ]),
       ]),
     ]
@@ -47,6 +104,25 @@ test("evaluate() fails a page with conditional matrix that restricts orientation
   ]);
 });
 
+test("evaluate() fails a page with conditional rotation that restricts orientation", async (t) => {
+  const target = <div>Hello</div>;
+
+  const document = h.document(
+    [target],
+    [
+      h.sheet([
+        h.rule.media("(orientation: portrait)", [
+          h.rule.style("div", { rotate: "90deg" }),
+        ]),
+      ]),
+    ]
+  );
+
+  t.deepEqual(await evaluate(R44, { document }), [
+    failed(R44, target, { 1: Outcomes.RotationLocked }),
+  ]);
+});
+
 test("evaluate() is inapplicable to invisible elements", async (t) => {
   const document = h.document(
     [<div>Hello</div>],
@@ -63,7 +139,7 @@ test("evaluate() is inapplicable to invisible elements", async (t) => {
   t.deepEqual(await evaluate(R44, { document }), [inapplicable(R44)]);
 });
 
-test("evaluate() is inapplicable when `transform` is not used", async (t) => {
+test("evaluate() is inapplicable when `transform`/`rotate` is not used", async (t) => {
   const document = h.document(
     [<div>Hello</div>],
     [
