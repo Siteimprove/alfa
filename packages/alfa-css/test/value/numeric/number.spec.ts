ab1a14d7b313023cd5afdf85e6dd8ec68d2d285a
Jean-Yves Moyen <jym@siteimprove.com>
2023-06-13 11:39:58 +0200

    Add some Number tests

---
+++ packages/alfa-css/test/value/numeric/number.spec.ts
@@ -0,0 +1,51 @@
+import { test } from "@siteimprove/alfa-test";
+
+import { Lexer } from "../../../src";
+// TODO clean deep import
+import { Number } from "../../../src/value/numeric/number";
+
+function parse(input: string) {
+  return Number.parse(Lexer.lex(input)).map(([, value]) => value);
+}
+
+test("parse() accepts number", (t) => {
+  t.deepEqual(parse("3.14").getUnsafe().toJSON(), {
+    type: "number",
+    value: 3.14,
+  });
+});
+
+test("parse() accepts math expression reducing to numbers", (t) => {
+  t.deepEqual(parse("calc((12 + 9) * 2)").getUnsafe().toJSON(), {
+    type: "number",
+    math: {
+      type: "math expression",
+      expression: {
+        type: "value",
+        value: {
+          type: "number",
+          value: 42,
+        },
+      },
+    },
+  });
+});
+
+test("parse() rejects math expressions with percentages", (t) => {
+  t.deepEqual(parse("calc(10px + 5%)").isErr(), true);
+});
+
+test("parse() rejects math expressions with angles", (t) => {
+  t.deepEqual(parse("calc(10deg + 1rad)").isErr(), true);
+});
+
+test("parse() rejects math expressions with length", (t) => {
+  t.deepEqual(parse("calc(10em + 1px)").isErr(), true);
+});
+
+test("resolve() returns a bare value", (t) => {
+  t.deepEqual(parse("calc((12 + 9) * 2)").getUnsafe().resolve().toJSON(), {
+    type: "number",
+    value: 42,
+  });
+});
