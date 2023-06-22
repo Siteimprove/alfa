d317f5c61c7743fb4bc36bd0a34afdabc3a252be
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2023-06-22 15:50:46 +0200

    Add tests

---
+++ packages/alfa-dom/test/jsx.spec.tsx
@@ -0,0 +1,19 @@
+import { test } from "@siteimprove/alfa-test";
+
+test("Box attribute gets turned into box property", (t) => {
+  const element = <div box={{ x: 12, y: 7, width: 3, height: 9 }}></div>;
+
+  t.deepEqual(element.box.getUnsafe().toJSON(), {
+    type: "rectangle",
+    x: 12,
+    y: 7,
+    width: 3,
+    height: 9,
+  });
+});
+
+test("Element without box attribute has box property equal to `None`", (t) => {
+  const element = <div></div>;
+
+  t(element.box.isNone);
+});
