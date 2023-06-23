a2946611d814a16aa0e0cb91a24fc21fcb885b11
Jean-Yves Moyen <jym@siteimprove.com>
2023-06-23 11:07:38 +0200

    Scaffold dimension-percentage

--- packages/alfa-css/src/value/numeric/dimension.ts
+++ packages/alfa-css/src/value/numeric/dimension.ts
@@ -1,11 +1,8 @@
 import { Comparable, Comparison } from "@siteimprove/alfa-comparable";
 import { Hash } from "@siteimprove/alfa-hash";
 
-import {
-  Dimension as BaseDimension,
-  Numeric as BaseNumeric,
-} from "../../calculation/numeric";
-import { Convertible } from "../../unit";
+import { Numeric as BaseNumeric } from "../../calculation/numeric";
+import { Convertible, Unit } from "../../unit";
 import { Value } from "../value";
 
 import { Numeric } from "./numeric";
@@ -13,8 +10,9 @@ import { Numeric } from "./numeric";
 /**
  * @public
  */
-export type Dimension<T extends BaseNumeric.Dimension = BaseNumeric.Dimension> =
-  Dimension.Calculated<T> | Dimension.Fixed<T>;
+export type Dimension<T extends Type = Type> =
+  | Dimension.Calculated<T>
+  | Dimension.Fixed<T>;
 
 /**
  * @public
@@ -23,9 +21,7 @@ export namespace Dimension {
   /**
    * Dimensions that are the result of a calculation.
    */
-  export abstract class Calculated<
-      T extends BaseNumeric.Dimension = BaseNumeric.Dimension
-    >
+  export abstract class Calculated<T extends Type = Type>
     extends Numeric.Calculated<T>
     implements IDimension<T, true>
   {
@@ -37,9 +33,7 @@ export namespace Dimension {
       return true;
     }
 
-    public abstract resolve(
-      resolver?: unknown
-    ): Fixed<T, BaseDimension.ToCanonical<T>>;
+    public abstract resolve(resolver?: unknown): Fixed<T, ToCanonical<T>>;
 
     public equals(value: unknown): value is this {
       return value instanceof Calculated && super.equals(value);
@@ -47,23 +41,22 @@ export namespace Dimension {
   }
 
   export namespace Calculated {
-    export interface JSON<
-      T extends BaseNumeric.Dimension = BaseNumeric.Dimension
-    > extends Numeric.Calculated.JSON<T> {}
+    export interface JSON<T extends Type = Type>
+      extends Numeric.Calculated.JSON<T> {}
   }
 
   /**
    * Dimensions that are a fixed (not calculated) value.
    */
   export abstract class Fixed<
-      T extends BaseNumeric.Dimension = BaseNumeric.Dimension,
+      T extends Type = Type,
       // The actual unit in which the dimension is expressed, e.g px, em, rad, …
-      U extends BaseDimension.ToDimension<T> = BaseDimension.ToDimension<T>
+      U extends ToDimension<T> = ToDimension<T>
     >
     extends Numeric.Fixed<T>
     implements
       IDimension<T, false>,
-      Convertible<BaseDimension.ToDimension<T>>,
+      Convertible<ToDimension<T>>,
       Comparable<Fixed<T>>
   {
     protected readonly _unit: U;
@@ -84,26 +77,18 @@ export namespace Dimension {
     /**
      * {@link https://drafts.csswg.org/css-values/#canonical-unit}
      */
-    public get canonicalUnit(): BaseDimension.ToCanonical<T> {
+    public get canonicalUnit(): ToCanonical<T> {
       // The this.type test does not correctly narrow T, so we need to force typing.
-      return (
-        this.type === "angle" ? "deg" : "px"
-      ) as BaseDimension.ToCanonical<T>;
+      return (this.type === "angle" ? "deg" : "px") as ToCanonical<T>;
     }
 
-    public hasUnit<V extends BaseDimension.ToDimension<T>>(
-      unit: V
-    ): this is Fixed<T, V> {
-      return (this._unit as BaseDimension.ToDimension<T>) === unit;
+    public hasUnit<V extends ToDimension<T>>(unit: V): this is Fixed<T, V> {
+      return (this._unit as ToDimension<T>) === unit;
     }
 
-    public abstract withUnit<V extends BaseDimension.ToDimension<T>>(
-      unit: V
-    ): Fixed<T, V>;
+    public abstract withUnit<V extends ToDimension<T>>(unit: V): Fixed<T, V>;
 
-    public abstract resolve(
-      resolver?: unknown
-    ): Fixed<T, BaseDimension.ToCanonical<T>>;
+    public abstract resolve(resolver?: unknown): Fixed<T, ToCanonical<T>>;
 
     public equals(value: unknown): value is this {
       return (
@@ -136,19 +121,17 @@ export namespace Dimension {
 
   export namespace Fixed {
     export interface JSON<
-      T extends BaseNumeric.Dimension = BaseNumeric.Dimension,
-      U extends BaseDimension.ToDimension<T> = BaseDimension.ToDimension<T>
+      T extends Type = Type,
+      U extends ToDimension<T> = ToDimension<T>
     > extends Numeric.Fixed.JSON<T> {
       unit: U;
     }
   }
 
-  interface IDimension<
-    T extends BaseNumeric.Dimension = BaseNumeric.Dimension,
-    CALC extends boolean = boolean
-  > extends Value<T, CALC> {
+  interface IDimension<T extends Type = Type, CALC extends boolean = boolean>
+    extends Value<T, CALC> {
     hasCalculation(): this is Calculated<T>;
-    resolve(resolver?: unknown): Fixed<T, BaseDimension.ToCanonical<T>>;
+    resolve(resolver?: unknown): Fixed<T, ToCanonical<T>>;
   }
 
   export function isCalculated(value: unknown): value is Calculated {
@@ -162,4 +145,40 @@ export namespace Dimension {
   export function isDimension(value: unknown): value is Numeric {
     return value instanceof Calculated || value instanceof Fixed;
   }
+
+  // /**
+  //  * Helper type to remove the "-percentage" bit
+  //  */
+  // type ToFixed<T extends Type> = T extends `${infer U}-percentage` ? U : T;
+
+  /**
+   * Helper type to infer Unit sub-type based on its string representation.
+   */
+  type ToDimension<T extends Type> = T extends "angle"
+    ? Unit.Angle
+    : T extends "angle-percentage"
+    ? Unit.Angle
+    : T extends "length"
+    ? Unit.Length
+    : T extends "length-percentage"
+    ? Unit.Length
+    : // We currently do not really support other dimensions
+      never;
+
+  /**
+   * Helper type to infer the canonical unit of a sub-type based on its string
+   * representation.
+   */
+  type ToCanonical<T extends Type> = T extends "angle"
+    ? "deg"
+    : T extends "angle-percentage"
+    ? "deg"
+    : T extends "length"
+    ? "px"
+    : T extends "length-percentage"
+    ? "px"
+    : // We currently do not really support other dimensions
+      never;
 }
+
+type Type = BaseNumeric.Dimension | `${BaseNumeric.Dimension}-percentage`;
