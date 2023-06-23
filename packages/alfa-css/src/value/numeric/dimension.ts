70b1236af160a431301b1064b6dd7090bba3f494
Jean-Yves Moyen <jym@siteimprove.com>
2023-06-23 11:42:05 +0200

    Clean up

--- packages/alfa-css/src/value/numeric/dimension.ts
+++ packages/alfa-css/src/value/numeric/dimension.ts
@@ -12,7 +12,7 @@ import { Numeric } from "./numeric";
  */
 export type Dimension<T extends Type = Type> =
   | Dimension.Calculated<T>
-  | Dimension.Fixed<Foo<T>[0]>;
+  | Dimension.Fixed<Dimensions<T>[0]>;
 
 /**
  * @public
@@ -22,7 +22,7 @@ export namespace Dimension {
    * Dimensions that are the result of a calculation.
    */
   export abstract class Calculated<T extends Type = Type>
-    extends Numeric.Calculated<T, Foo<T>[0]>
+    extends Numeric.Calculated<T, Dimensions<T>[0]>
     implements IDimension<T, true>
   {
     protected constructor(math: Numeric.ToMath<T>, type: T) {
@@ -33,7 +33,9 @@ export namespace Dimension {
       return true;
     }
 
-    public abstract resolve(resolver?: unknown): Fixed<Foo<T>[0], Foo<T>[2]>;
+    public abstract resolve(
+      resolver?: unknown
+    ): Fixed<Dimensions<T>[0], Dimensions<T>[2]>;
 
     public equals(value: unknown): value is this {
       return value instanceof Calculated && super.equals(value);
@@ -51,12 +53,12 @@ export namespace Dimension {
   export abstract class Fixed<
       T extends BaseNumeric.Dimension = BaseNumeric.Dimension,
       // The actual unit in which the dimension is expressed, e.g px, em, rad, …
-      U extends Foo<T>[1] = Foo<T>[1]
+      U extends Dimensions<T>[1] = Dimensions<T>[1]
     >
     extends Numeric.Fixed<T>
     implements
       IDimension<T, false>,
-      Convertible<Foo<T>[1]>,
+      Convertible<Dimensions<T>[1]>,
       Comparable<Fixed<T>>
   {
     protected readonly _unit: U;
@@ -77,18 +79,18 @@ export namespace Dimension {
     /**
      * {@link https://drafts.csswg.org/css-values/#canonical-unit}
      */
-    public get canonicalUnit(): Foo<T>[2] {
+    public get canonicalUnit(): Dimensions<T>[2] {
       // The this.type test does not correctly narrow T, so we need to force typing.
-      return (this.type === "angle" ? "deg" : "px") as Foo<T>[2];
+      return (this.type === "angle" ? "deg" : "px") as Dimensions<T>[2];
     }
 
-    public hasUnit<V extends Foo<T>[1]>(unit: V): this is Fixed<T, V> {
-      return (this._unit as Foo<T>[1]) === unit;
+    public hasUnit<V extends Dimensions<T>[1]>(unit: V): this is Fixed<T, V> {
+      return (this._unit as Dimensions<T>[1]) === unit;
     }
 
-    public abstract withUnit<V extends Foo<T>[1]>(unit: V): Fixed<T, V>;
+    public abstract withUnit<V extends Dimensions<T>[1]>(unit: V): Fixed<T, V>;
 
-    public abstract resolve(resolver?: unknown): Fixed<T, Foo<T>[2]>;
+    public abstract resolve(resolver?: unknown): Fixed<T, Dimensions<T>[2]>;
 
     public equals(value: unknown): value is this {
       return (
@@ -122,16 +124,16 @@ export namespace Dimension {
   export namespace Fixed {
     export interface JSON<
       T extends Type = Type,
-      U extends Foo<T>[1] = Foo<T>[1]
+      U extends Dimensions<T>[1] = Dimensions<T>[1]
     > extends Numeric.Fixed.JSON<T> {
       unit: U;
     }
   }
 
   interface IDimension<T extends Type = Type, CALC extends boolean = boolean>
-    extends Value<T, CALC, Foo<T>[0]> {
+    extends Value<T, CALC, Dimensions<T>[0]> {
     hasCalculation(): this is Calculated<T>;
-    resolve(resolver?: unknown): Fixed<Foo<T>[0], Foo<T>[2]>;
+    resolve(resolver?: unknown): Fixed<Dimensions<T>[0], Dimensions<T>[2]>;
   }
 
   export function isCalculated(value: unknown): value is Calculated {
@@ -145,45 +147,16 @@ export namespace Dimension {
   export function isDimension(value: unknown): value is Numeric {
     return value instanceof Calculated || value instanceof Fixed;
   }
-
-  // /**
-  //  * Helper type to remove the "-percentage" bit
-  //  */
-  // type ToFixed<T extends Type> = T extends `${infer U}-percentage` ? U : T;
-
-  // /**
-  //  * Helper type to infer Unit sub-type based on its string representation.
-  //  */
-  // type ToDimension<T extends Type> = T extends "angle"
-  //   ? Unit.Angle
-  //   : T extends "angle-percentage"
-  //   ? Unit.Angle
-  //   : T extends "length"
-  //   ? Unit.Length
-  //   : T extends "length-percentage"
-  //   ? Unit.Length
-  //   : // We currently do not really support other dimensions
-  //     never;
-  //
-  // /**
-  //  * Helper type to infer the canonical unit of a sub-type based on its string
-  //  * representation.
-  //  */
-  // type ToCanonical<T extends Type> = T extends "angle"
-  //   ? "deg"
-  //   : T extends "angle-percentage"
-  //   ? "deg"
-  //   : T extends "length"
-  //   ? "px"
-  //   : T extends "length-percentage"
-  //   ? "px"
-  //   : // We currently do not really support other dimensions
-  //     never;
 }
 
 type Type = BaseNumeric.Dimension | `${BaseNumeric.Dimension}-percentage`;
 
-type Foo<T extends Type> = T extends "angle"
+/**
+ * Helper type to turn a dimension or dimension-percentage type into its
+ * components:
+ * [base dimension, corresponding unit, canonical unit]
+ */
+type Dimensions<T extends Type> = T extends "angle"
   ? ["angle", Unit.Angle, "deg"]
   : T extends "angle-percentage"
   ? ["angle", Unit.Angle, "deg"]
