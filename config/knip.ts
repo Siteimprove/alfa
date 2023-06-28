d4413759a781d77407cc518cd517ab55f8dd0414
Jean-Yves Moyen <jym@siteimprove.com>
2023-06-28 16:25:16 +0200

    Improve config and remove unused packages

--- config/knip.ts
+++ config/knip.ts
@@ -1,7 +1,17 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  ignoreDependencies: ["prettier"],
+  workspaces: {
+    ".": {
+      entry: ["scripts/src/changelog.ts", "scripts/*.js"],
+      project: ["scripts/**/*.ts", "scripts/**/*.js"],
+      ignoreDependencies: ["prettier"],
+    },
+    "packages/*": {
+      entry: "src/index.ts!",
+      project: ["src/**/*.ts!", "test/**/*.ts", "test/**/*.tsx"],
+    },
+  },
 };
 
 export default config;
