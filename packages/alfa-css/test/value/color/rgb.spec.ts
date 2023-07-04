7ab02673db709574a641bd220987a0bd31a5294c
Jean-Yves Moyen <jym@siteimprove.com>
2023-07-04 16:03:27 +0200

    Improve RGB tests

--- packages/alfa-css/test/value/color/rgb.spec.ts
+++ packages/alfa-css/test/value/color/rgb.spec.ts
@@ -1,31 +1,11 @@
-import { Slice } from "@siteimprove/alfa-slice";
 import { test } from "@siteimprove/alfa-test";
 
-import { RGB, Token } from "../../../src";
+import { Lexer, RGB } from "../../../src";
 
-const { parse } = RGB;
-const { func, number, comma, whitespace, closeParenthesis } = Token;
+const parse = (str: string) => RGB.parse(Lexer.lex(str)).getUnsafe()[1];
 
-test("parse() parses consecutive whitespace", (t) => {
-  const tokens = Slice.of([
-    func("rgba"),
-    number(255, true, false),
-    comma(),
-    whitespace(),
-    number(255, true, false),
-    comma(),
-    whitespace(),
-    number(255, true, false),
-    comma(),
-    whitespace(),
-    whitespace(),
-    number(1, true, false),
-    closeParenthesis(),
-  ]);
-
-  const result = parse(tokens).map(([_, color]) => color.toJSON());
-
-  t.deepEqual(result.getUnsafe(), {
+test("parse() legacy syntax with numbers", (t) => {
+  const expected: RGB.JSON = {
     type: "color",
     format: "rgb",
     red: {
@@ -44,5 +24,122 @@ test("parse() parses consecutive whitespace", (t) => {
       value: 1,
       type: "number",
     },
-  });
+  };
+
+  for (const actual of [
+    parse("rgb(255, 255, 255)"),
+    parse("rgba(255, 255, 255)"),
+    parse("rgb(255, 255, 255, 1)"),
+    parse("rgba(255, 255, 255, 1)"),
+    parse("rgba(  255,   255  , 255,     1)"),
+  ]) {
+    t.deepEqual(actual.toJSON(), expected);
+  }
+});
+
+test("parse() legacy syntax with numbers", (t) => {
+  const expected: RGB.JSON = {
+    type: "color",
+    format: "rgb",
+    red: {
+      value: 1,
+      type: "percentage",
+    },
+    green: {
+      value: 1,
+      type: "percentage",
+    },
+    blue: {
+      value: 1,
+      type: "percentage",
+    },
+    alpha: {
+      value: 1,
+      type: "number",
+    },
+  };
+
+  for (const actual of [
+    parse("rgb(100%, 100%, 100%)"),
+    parse("rgba(100%, 100%, 100%)"),
+    parse("rgb(100%, 100%, 100%, 1)"),
+    parse("rgba(100%, 100%, 100%, 1)"),
+  ]) {
+    t.deepEqual(actual.toJSON(), expected);
+  }
+});
+
+test("parse() modern syntax with numbers", (t) => {
+  const expected: RGB.JSON = {
+    type: "color",
+    format: "rgb",
+    red: {
+      value: 255,
+      type: "number",
+    },
+    green: {
+      value: 255,
+      type: "number",
+    },
+    blue: {
+      value: 255,
+      type: "number",
+    },
+    alpha: {
+      value: 1,
+      type: "number",
+    },
+  };
+
+  for (const actual of [
+    parse("rgb(255 255 255)"),
+    parse("rgba(255 255 255)"),
+    parse("rgb(255 255 255 / 1)"),
+    parse("rgba(255 255 255 / 1)"),
+  ]) {
+    t.deepEqual(actual.toJSON(), expected);
+  }
+});
+
+test("parse() modern syntax with numbers", (t) => {
+  const expected: RGB.JSON = {
+    type: "color",
+    format: "rgb",
+    red: {
+      value: 1,
+      type: "percentage",
+    },
+    green: {
+      value: 1,
+      type: "percentage",
+    },
+    blue: {
+      value: 1,
+      type: "percentage",
+    },
+    alpha: {
+      value: 1,
+      type: "number",
+    },
+  };
+
+  for (const actual of [
+    parse("rgb(100% 100% 100%)"),
+    parse("rgba(100% 100% 100%)"),
+    parse("rgb(100% 100% 100% / 1)"),
+    parse("rgba(100% 100% 100%/  1)"),
+  ]) {
+    t.deepEqual(actual.toJSON(), expected);
+  }
+});
+
+test("parse() refuse mixing numbers and percentages", (t) => {
+  for (const str of [
+    "rgba(100 255 100%)",
+    "rgba(100% 255 100%)",
+    "rgba(100%, 255, 100)",
+    "rgba(100, 255, 100%)",
+  ]) {
+    t.deepEqual(RGB.parse(Lexer.lex(str)).isErr(), true);
+  }
 });
