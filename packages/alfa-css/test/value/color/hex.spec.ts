3e6655fd931912cade6288409334a7911e5121b2
Jean-Yves Moyen <jym@siteimprove.com>
2023-07-07 11:02:22 +0200

    Add some HSL tests

---
+++ packages/alfa-css/test/value/color/hex.spec.ts
@@ -0,0 +1,40 @@
+import { test } from "@siteimprove/alfa-test";
+
+import { Hex, Lexer, type RGB } from "../../../src";
+
+const parse = (str: string) => Hex.parse(Lexer.lex(str)).getUnsafe()[1];
+
+test("parse() accepts hex format with various numbers of digits", (t) => {
+  const expected = (value: number): Hex.JSON => ({
+    type: "color",
+    format: "hex",
+    value: value,
+  });
+
+  for (const [name, value] of [
+    ["#00F", 65535],
+    ["#00f8", 65416],
+    ["#0000ff", 65535],
+    ["#0000ff88", 65416],
+  ] as const) {
+    t.deepEqual(parse(name).toJSON(), expected(value));
+  }
+});
+
+test("#resolve() returns RGB in percentages", (t) => {
+  const expected = (red: number, green: number, blue: number): RGB.JSON => ({
+    type: "color",
+    format: "rgb",
+    red: { type: "percentage", value: red },
+    green: { type: "percentage", value: green },
+    blue: { type: "percentage", value: blue },
+    alpha: { type: "percentage", value: 1 },
+  });
+
+  for (const [name, red, green, blue] of [
+    ["#00ffff", 0, 1, 1],
+    ["#ff00ff", 1, 0, 1],
+  ] as const) {
+    t.deepEqual(parse(name).resolve().toJSON(), expected(red, green, blue));
+  }
+});
---
+++ packages/alfa-css/test/value/color/hsl.spec.ts
@@ -0,0 +1,64 @@
+import { test } from "@siteimprove/alfa-test";
+
+import { HSL, Lexer, type RGB } from "../../../src";
+
+const parse = (str: string) => HSL.parse(Lexer.lex(str)).getUnsafe()[1];
+
+test("parse() accepts legacy syntax with percentages", (t) => {
+  const expected = (type: "angle" | "number"): HSL.JSON => ({
+    type: "color",
+    format: "hsl",
+    hue:
+      type === "angle"
+        ? { type: "angle", value: 0, unit: "deg" }
+        : { type: "number", value: 0 },
+    saturation: { type: "percentage", value: 1 },
+    lightness: { type: "percentage", value: 1 },
+    alpha: { type: "number", value: 1 },
+  });
+
+  for (const [actual, type] of [
+    [parse("hsl(0, 100%, 100%)"), "number"],
+    [parse("hsla(0deg, 100%, 100%)"), "angle"],
+    [parse("hsl(0deg, 100%, 100%, 1)"), "angle"],
+    [parse("hsla(0, 100%, 100%, 1)"), "number"],
+  ] as const) {
+    t.deepEqual(actual.toJSON(), expected(type));
+  }
+});
+
+test("parse() accepts modern syntax with percentage", (t) => {
+  const expected = (type: "angle" | "number"): HSL.JSON => ({
+    type: "color",
+    format: "hsl",
+    hue:
+      type === "angle"
+        ? { type: "angle", value: 0, unit: "deg" }
+        : { type: "number", value: 0 },
+    saturation: { type: "percentage", value: 1 },
+    lightness: { type: "percentage", value: 1 },
+    alpha: { type: "number", value: 1 },
+  });
+
+  for (const [actual, type] of [
+    [parse("hsl(0 100% 100%)"), "number"],
+    [parse("hsla(0deg 100% 100%)"), "angle"],
+    [parse("hsl(0deg 100% 100% / 1)"), "angle"],
+    [parse("hsla(0 100% 100%/  1)"), "number"],
+  ] as const) {
+    t.deepEqual(actual.toJSON(), expected(type));
+  }
+});
+
+test("parse() rejects numbers for saturation and lightness", (t) => {
+  for (const color of [
+    "hsl(0 100% 1)",
+    "hsl(0 100% 0 / 1)",
+    "hsl(0 0 100%)",
+    "hsl(0, 1, 100%)",
+    "hsl(0, 0, 100%)",
+    "hsl(0, 100%, 1, 1)",
+  ]) {
+    t.deepEqual(HSL.parse(Lexer.lex(color)).isErr(), true);
+  }
+});
