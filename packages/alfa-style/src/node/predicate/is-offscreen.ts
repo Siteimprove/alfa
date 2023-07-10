7adc3b7bf4ad7629f486a7d677d79739df5f800c
Jean-Yves Moyen <jym@siteimprove.com>
2023-07-10 14:19:02 +0200

    Use layout for top/left off screen detection

--- packages/alfa-style/src/node/predicate/is-offscreen.ts
+++ packages/alfa-style/src/node/predicate/is-offscreen.ts
@@ -2,7 +2,9 @@ import { Cache } from "@siteimprove/alfa-cache";
 import { Device } from "@siteimprove/alfa-device";
 import { Element, Node } from "@siteimprove/alfa-dom";
 import { Predicate } from "@siteimprove/alfa-predicate";
+import { Rectangle } from "@siteimprove/alfa-rectangle";
 import { Context } from "@siteimprove/alfa-selector";
+import { Trilean } from "@siteimprove/alfa-trilean";
 
 import { Style } from "../../style";
 
@@ -10,6 +12,59 @@ const { abs } = Math;
 const { isElement } = Element;
 
 const cache = Cache.empty<Device, Cache<Context, Cache<Node, boolean>>>();
+
+/**
+ * @internal
+ */
+export function isOffscreen(
+  device: Device,
+  context: Context = Context.empty()
+): Predicate<Node> {
+  return function isOffscreen(node): boolean {
+    return cache
+      .get(device, Cache.empty)
+      .get(context, Cache.empty)
+      .get(node, () => {
+        if (isElement(node)) {
+          if (node.box.isSome() && context === Context.empty()) {
+            return isOffscreenLayout(node);
+          } else {
+            const style = Style.from(node, device, context);
+
+            const fallback = isOffscreenFallback(style);
+            // If we couldn't find anything on the element itself, we need to
+            // look on its ancestors.
+            return fallback ?? node.parent(Node.flatTree).some(isOffscreen);
+          }
+        }
+
+        return node.parent(Node.flatTree).some(isOffscreen);
+      });
+  };
+}
+
+/**
+ * @remarks
+ * We assume that the layout was gathered with an empty context and therefore
+ * only use it for empty context. Non-empty context may bring content on screen,
+ * typically "skip to content" links are only on screen when focused.
+ *
+ * @remarks
+ * Content that is hidden to the left or top of the screen is fairly safe to
+ * be assumed invisible. Content that is hidden to the right or bottom may
+ * be scrolled to. We assume there is always a way to scroll to the bottom, but
+ * we do search for explicit scroll bar on ancestors' overflow for right.
+ *
+ * @internal
+ */
+export function isOffscreenLayout(element: Element): boolean {
+  // By definition, the viewport top-left corner is (0, 0).
+  // At first, we extend the viewport infinitely to the right and bottom to
+  // avoid considering scrolling.
+  const viewport = Rectangle.of(0, 0, Infinity, Infinity);
+
+  return element.box.some((box) => !box.intersects(viewport));
+}
 
 /**
  * @remarks
@@ -30,57 +85,41 @@ const cache = Cache.empty<Device, Cache<Context, Cache<Node, boolean>>>();
  *
  * Content moved to the bottom simply creates scrolling on vertical pages and is
  * not considered here.
- *
- * @internal
  */
-export function isOffscreen(
-  device: Device,
-  context: Context = Context.empty()
-): Predicate<Node> {
-  return function isOffscreen(node): boolean {
-    return cache
-      .get(device, Cache.empty)
-      .get(context, Cache.empty)
-      .get(node, () => {
-        if (isElement(node)) {
-          const style = Style.from(node, device, context);
+function isOffscreenFallback(style: Style): Trilean {
+  const { value: position } = style.computed("position");
+  const { value: left } = style.computed("left");
+  const { value: right } = style.computed("right");
+  const { value: top } = style.computed("top");
+  const { value: marginLeft } = style.computed("margin-left");
 
-          const { value: position } = style.computed("position");
-          const { value: left } = style.computed("left");
-          const { value: right } = style.computed("right");
-          const { value: top } = style.computed("top");
-          const { value: marginLeft } = style.computed("margin-left");
+  // margin-left affects also statically positioned elements.
+  // margin-left could possibly hide further sibling of the element
+  // until a block container is found.
+  // We ignore this for now, until it proves problematic.
+  if (marginLeft.type === "length" && marginLeft.value <= -9999) {
+    return true;
+  }
 
-          // margin-left affects also statically positioned elements.
-          // margin-left could possibly hide further sibling of the element
-          // until a block container is found.
-          // We ignore this for now, until it proves problematic.
-          if (marginLeft.type === "length" && marginLeft.value <= -9999) {
-            return true;
-          }
+  if (position.value !== "static") {
+    for (const inset of [left, right]) {
+      // A large move on the left or right hides the content.
+      if (inset.type === "length" && abs(inset.value) >= 9999) {
+        return true;
+      }
+    }
 
-          if (position.value !== "static") {
-            for (const inset of [left, right]) {
-              // A large move on the left or right hides the content.
-              if (inset.type === "length" && abs(inset.value) >= 9999) {
-                return true;
-              }
-            }
+    // A large move above the top hides the content.
+    if (top.type === "length" && top.value <= -9999) {
+      return true;
+    }
 
-            // A large move above the top hides the content.
-            if (top.type === "length" && top.value <= -9999) {
-              return true;
-            }
+    // If the element is positioned, with normal margin and normal
+    // inset, it is likely on screen, no matter where its parent is.
+    // We might need to instead recurse to the offset parent. For now,
+    // we'll ignore that case considering it unlikely.
+    return false;
+  }
 
-            // If the element is positioned, with normal margin and normal
-            // inset, it is likely on screen, no matter where its parent is.
-            // We might need to instead recurse to the offset parent. For now,
-            // we'll ignore that case considering it unlikely.
-            return false;
-          }
-        }
-
-        return node.parent(Node.flatTree).some(isOffscreen);
-      });
-  };
+  return undefined;
 }
--- packages/alfa-style/test/node/predicate/is-offscreen.spec.tsx
+++ packages/alfa-style/test/node/predicate/is-offscreen.spec.tsx
@@ -5,6 +5,67 @@ import { isOffscreen as AlfaIsOffscreen } from "../../../src/node/predicate/is-o
 
 const isOffscreen = AlfaIsOffscreen(Device.standard());
 
+/*************************************************************************
+ *
+ * Checks **with** boxes. These use the layout heuristics.
+ *
+ *************************************************************************/
+
+test(`isOffscreen() uses boxes when they are provided`, (t) => {
+  // The element is actually on screen, but the box is lying to us.
+  const element = (
+    <div box={{ x: -100, y: -100, width: 10, height: 10 }}>Hello world</div>
+  );
+
+  t.deepEqual(isOffscreen(element), true);
+});
+
+test(`isOffscreen() returns true for elements that are on the left of the viewport`, (t) => {
+  const element = (
+    <div box={{ x: -100, y: 0, width: 10, height: 100 }}>Hello world</div>
+  );
+
+  t.deepEqual(isOffscreen(element), true);
+});
+
+test(`isOffscreen() returns true for elements that are on top of the viewport`, (t) => {
+  const element = (
+    <div box={{ x: 0, y: -100, width: 100, height: 10 }}>Hello world</div>
+  );
+
+  t.deepEqual(isOffscreen(element), true);
+});
+
+test(`isOffscreen() returns false for elements that are fully on screen`, (t) => {
+  const element = (
+    <div box={{ x: 100, y: 0, width: 100, height: 10 }}>Hello world</div>
+  );
+
+  t.deepEqual(isOffscreen(element), false);
+});
+
+test(`isOffscreen() returns false for elements that partially intersect the viewport on the left`, (t) => {
+  const element = (
+    <div box={{ x: -100, y: 0, width: 101, height: 10 }}>Hello world</div>
+  );
+
+  t.deepEqual(isOffscreen(element), false);
+});
+
+test(`isOffscreen() returns false for elements that partially intersect the viewport on the top`, (t) => {
+  const element = (
+    <div box={{ x: 0, y: -100, width: 10, height: 101 }}>Hello world</div>
+  );
+
+  t.deepEqual(isOffscreen(element), false);
+});
+
+/*************************************************************************
+ *
+ * Checks **without** boxes. These use the fallback heuristics.
+ *
+ *************************************************************************/
+
 test(`isOffscreen() returns true for an element that has been pulled offscreen
       by position: absolute and left: 9999px`, (t) => {
   const element = (
