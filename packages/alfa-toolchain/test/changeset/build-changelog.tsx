e46f201d8342f90bf290ac50a5d74f18c23f025a
Jean-Yves Moyen <jym@siteimprove.com>
2023-08-18 15:22:28 +0200

    Add more tests

--- packages/alfa-toolchain/test/changeset/build-changelog.tsx
+++ packages/alfa-toolchain/test/changeset/build-changelog.tsx
@@ -62,3 +62,35 @@ test("buildLine() handles multi-packages change", (t) => {
       " Some awesome summary. ([NOT A LINK])"
   );
 });
+
+test("buildGroup() builds a group of same kinds changes", (t) => {
+  t.deepEqual(
+    Changelog.buildGroup("Added", [
+      [
+        {
+          kind: "Added",
+          summary: "Some awesome summary",
+          packages: [
+            "@siteimprove/my-package",
+            "@siteimprove/my-other-package",
+          ],
+        },
+        "[NOT A LINK]",
+      ],
+      [
+        {
+          kind: "Added",
+          summary: "Some other summary",
+          packages: ["@siteimprove/my-third-package"],
+        },
+        "[STILL NOT A LINK]",
+      ],
+    ]),
+    "### Added\n\n" +
+      "- [@siteimprove/my-package](packages/my-package/CHANGELOG.md#[INSERT NEW VERSION HERE])," +
+      " [@siteimprove/my-other-package](packages/my-other-package/CHANGELOG.md#[INSERT NEW VERSION HERE]):" +
+      " Some awesome summary. ([NOT A LINK])\n\n" +
+      "- [@siteimprove/my-third-package](packages/my-third-package/CHANGELOG.md#[INSERT NEW VERSION HERE])," +
+      " Some other summary. ([STILL NOT A LINK])\n\n"
+  );
+});
