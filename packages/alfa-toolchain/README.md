82d8dd211078daf1afeabaad97a1629c44cc51a9
Jean-Yves Moyen <jym@siteimprove.com>
2023-09-05 15:38:59 +0200

    Improve changelog generation and format (#1462)
    * Typo
    
    * Add ref to scripts
    
    * Make empty alfa-changelog package
    
    * Rename package
    
    * Add changeset
    
    * Add changelog dependency
    
    * Move changelog code to alfa-toolchain
    
    * Empty Changelog
    
    * Configure toolchain at top level
    
    * Add changeset validation function
    
    * Improve regex
    
    * Add some failing examples
    
    * Add some failing examples
    
    * Add some failing examples
    
    * Improve test cases
    
    * Rename files
    
    * Update packages
    
    * Validate changesets in CI
    
    * Wrap stuff in namespace
    
    * Add functionality to build global changelog lines
    
    * Improve tests
    
    * Improve tests
    
    * Add more tests
    
    * Add more body building examples
    
    * Work on top-level generation
    
    * More on gloabl changelog
    
    * Clean up
    
    * Clean up
    
    * Actually run tests (and fix code)
    
    * Improve global changeset generation
    
    * Try to get PR id
    
    * Auto grab PR link in global changelog
    
    * Clean up
    
    * Improve cohesion
    
    * Read global changelog function from config
    
    * Improve version grabbing
    
    * Pre-fetch commits into changesets
    
    * Clean up
    
    * Grab package name and directory from Packages object
    
    * Refactor
    
    * Auto grab new version number
    
    * Include version heading in body
    
    * Write new body at the start of changelog
    
    * Clean up
    
    * Clean up and test
    
    * Knip stuff
    
    * Adapt Changelog to new format
    
    * Update release flow to use new changelog builder
    
    * Add extracted doc
    
    * Update documentation
    
    * Knip stuff
    
    * Typo
    
    Co-authored-by: Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
    
    * Add Changed kind
    
    ---------
    
    Co-authored-by: Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>

---
+++ packages/alfa-toolchain/README.md
@@ -0,0 +1,20 @@
+# Alfa toolchain
+
+This package contains the toolchain for developing Alfa and its [companion projects](../../docs/guides/README.md#alfa-structure). It is tailored-made for Alfa and makes many (poorly documented) assumptions on code structure that likely do not hold on other codebases. Therefore, it is unlikely to be re-usable by other projects.
+
+## Changelog
+
+Alfa is using [changesets](../../docs/guides/changeset.md). We keep one changelog per package, plus one global changelog (because we keep all packages in sync at the same version number). The per package changelogs are handled directly by changeset, using a custom generation function defined here. The global changelog is built with utilities here.
+
+Individual changesets are expected to have the following shape (after the frontmatter):
+```markdown
+**[kind]:** [title]
+
+[details]
+```
+
+where `[title]` is a one line summary for the changes (the full text is called `summary` in changesets lingo, so we call that summary `title` instead), `[details]` is an arbitrary long detailed explanation, and `[kind]` is one of: `Breaking`, `Removed`, `Added`, `Fixed`.
+
+`Breaking` and `Removed` kinds may only be used on major bumps (or minor bumps pre-1.0.0); `Added` kind may only be used on minor or major bumps (and should only be used on minor bumps); `Fixed` kinds can be used on any bump but should only be used on patch bumps.
+
+The individual changelog contains the full changeset. The global changelog contains only the title, with link to the individual changelog.
---
+++ packages/alfa-toolchain/src/changeset/build-changelog.ts
@@ -0,0 +1,253 @@
+import assembleReleasePlan from "@changesets/assemble-release-plan";
+import { getInfo } from "@changesets/get-github-info";
+import type {
+  Config,
+  NewChangesetWithCommit,
+  ReleasePlan,
+} from "@changesets/types";
+import type { Package, Packages } from "@manypkg/get-packages";
+import { Err, Ok, Result } from "@siteimprove/alfa-result";
+import { Changeset } from "./get-changeset-details";
+import {
+  type ChangelogFunctions,
+  Error,
+  getConfigOption,
+  getOrDie,
+} from "./helpers";
+
+const NON_UNIQUE_VERSION = Error.freeFrom + 1;
+const INVALID_CHANGESETS = Error.freeFrom + 2;
+
+/**
+ * @public
+ */
+export namespace Changelog {
+  export async function getBody(
+    changesets: Array<NewChangesetWithCommit>,
+    packages: Packages,
+    config: Config
+  ): Promise<string> {
+    // Build release plan (compute old and new versions for each package)
+    const releasePlan = assembleReleasePlan(
+      changesets,
+      packages,
+      config,
+      undefined,
+      undefined
+    );
+
+    // Check unicity of versions
+    const { oldVersion, newVersion } = getOrDie(
+      getVersions(releasePlan),
+      NON_UNIQUE_VERSION
+    );
+
+    // Check that changeset config.changelog[1].repo exists, so we can fetch
+    // the PRs.
+    const repo = getConfigOption(config, "repo");
+
+    // Build links to PRs, this is an array of same length as the changesets
+    // with a one-to-one correspondence.
+    const prLinks = await getPRlinks(changesets, repo);
+
+    // Parse the changeset and extract the details.
+    const details = changesets
+      .map(Changeset.getDetails)
+      .filter<Ok<Changeset.Details>>(Result.isOk)
+      .map((changeset) => changeset.get());
+
+    if (details.length !== changesets.length) {
+      console.error("Some changesets are invalid");
+      process.exit(INVALID_CHANGESETS);
+    }
+
+    // Build the body of the global changelog.
+    return (
+      `## [${newVersion}](../../compare/v${oldVersion}...v${newVersion}) (${new Date(
+        Date.now()
+      )
+        .toISOString()
+        .slice(0, 10)})` +
+      "\n\n" +
+      Changelog.buildBody(
+        details.map((detail, idx) => [detail, prLinks[idx]]),
+        packages.packages,
+        newVersion
+      )
+    );
+  }
+
+  /**
+   * @internal
+   */
+  export function mergeBodies(oldBody: string, newBody: string): string {
+    // Insert the new body as third line skipping heading and one more newline
+    return oldBody.replace(/((.*\n){2})/, `$1${newBody}\n\n`);
+  }
+
+  interface Versions {
+    oldVersion: string;
+    newVersion: string;
+  }
+
+  type ChangeSetDetailsWithLink = [
+    changeset: Changeset.Details,
+    prLink: string | undefined
+  ];
+
+  /**
+   * Builds the global changelog body, from an array of changesets.
+   *
+   * @remarks
+   * When we pre-process changesets, we do not yet know the new version number.
+   * So we use a placeholder to be replaced at a later stage.
+   */
+  export function buildBody(
+    changesets: ReadonlyArray<ChangeSetDetailsWithLink>,
+    packages: ReadonlyArray<Package>,
+    newVersion: string
+  ): string {
+    const sorted: {
+      [kind in Changeset.Kind]: Array<ChangeSetDetailsWithLink>;
+    } = { Added: [], Breaking: [], Changed: [], Fixed: [], Removed: [] };
+
+    changesets.forEach((item) => sorted[item[0].kind].push(item));
+
+    return `${(["Breaking", "Removed", "Added", "Changed", "Fixed"] as const)
+      .map((kind) =>
+        sorted[kind].length === 0
+          ? ""
+          : buildGroup(kind, sorted[kind], packages, newVersion)
+      )
+      .filter((group) => group !== "")
+      .join("\n\n")}`;
+  }
+
+  /**
+   * @internal
+   */
+  export function buildGroup(
+    kind: Changeset.Kind,
+    changesets: ReadonlyArray<ChangeSetDetailsWithLink>,
+    packages: ReadonlyArray<Package>,
+    newVersion: string
+  ): string {
+    return `### ${kind}\n\n${changesets
+      .map(buildLine(packages, newVersion))
+      .join("\n\n")}`;
+  }
+
+  /**
+   * Builds a global changelog entry from a changeset.
+   *
+   * @internal
+   */
+  export function buildLine(
+    packages: ReadonlyArray<Package>,
+    newVersion: string
+  ): ([changeset, prLink]: ChangeSetDetailsWithLink) => string {
+    return ([changeset, prLink]) =>
+      `- ${changeset.packages
+        .map(linkToPackage(packages, newVersion))
+        .join(", ")}: ${
+        // Remove trailing dot, if any, then add one.
+        changeset.title.trimEnd().replace(/\.$/, "")
+      }.${prLink === undefined ? "" : ` (${prLink})`}`;
+  }
+
+  /**
+   * Turns "package-name" into a Markdown link to its changelog from the
+   * top-level directory.
+   *
+   * @remarks
+   * The version is sent as a full semver X.Y.Z. However, Github links to markdown
+   * headings strip the dots.
+   */
+  function linkToPackage(
+    packages: ReadonlyArray<Package>,
+    newVersion: string
+  ): (fullName: string) => string {
+    return (fullName) => {
+      // The non-null assertion is wrong if a package is deleted but still
+      // mentioned in a changeset.
+      const packageJSON =
+        // The type assertion is needed as we know our package.json have a
+        // repository.directory field, but manypkg.get-packages totally ignores
+        // that it may even exist.
+        packages.find((p) => p.packageJson.name === fullName)!.packageJson as {
+          [key: string]: any;
+        };
+
+      return `[${fullName}](${
+        // Harden the access against non-existent packages or invalid
+        // package.json; this still leaks an undefined in the Changelog.
+        packageJSON?.repository?.directory
+      }/CHANGELOG.md#${newVersion.replace(/\./g, "")})`;
+    };
+  }
+
+  /**
+   * Check that old and new versions are unique in a release plan, and return them.
+   */
+  function getVersions(releasePlan: ReleasePlan): Result<Versions, string> {
+    return releasePlan.releases.reduce(
+      (previous: Result<Versions, string>, current) => {
+        return previous.flatMap(({ oldVersion, newVersion }) => {
+          if (oldVersion !== "" && oldVersion !== current.oldVersion) {
+            return Err.of(
+              "Not all packages have the same current version, aborting"
+            );
+          }
+          if (newVersion !== "" && newVersion !== current.newVersion) {
+            return Err.of(
+              "Not all packages have the same future version, aborting"
+            );
+          }
+
+          return Ok.of(current);
+        });
+      },
+      Ok.of({ oldVersion: "", newVersion: "" })
+    );
+  }
+
+  /**
+   * Get links to PRs that added the given changesets to the repo.
+   *
+   * @privateRemarks
+   * Trying to bake Result into that gets messy because of the interleaving
+   * of Result and (native) Promise.
+   */
+  async function getPRlinks(
+    changesets: Array<NewChangesetWithCommit>,
+    repo: string
+  ): Promise<Array<string | undefined>> {
+    return Promise.all(
+      changesets.map((changeset) => getPRLink(repo)(changeset.commit))
+    );
+  }
+
+  /**
+   * Get a markdown formatted link to the PR that added a given commit.
+   */
+  function getPRLink(
+    repo: string
+  ): (commit: string | undefined) => Promise<string | undefined> {
+    return async (commit) =>
+      commit === undefined
+        ? undefined
+        : getInfo({ commit, repo }).then(
+            (info) => info?.links.pull ?? undefined
+          );
+  }
+}
+
+const changelogFunctions: ChangelogFunctions = {
+  getBody: Changelog.getBody,
+  insertBody: Changelog.mergeBodies,
+};
+
+/**
+ * @public
+ */
+export default changelogFunctions;
