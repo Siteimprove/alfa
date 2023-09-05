b996ac9ddf7bd8f03f9ee4186344d1dbdc3612d6
Jean-Yves Moyen <jym@siteimprove.com>
2023-09-05 16:30:44 +0200

    Make knip happy

--- packages/alfa-rules/src/rules.ts
+++ packages/alfa-rules/src/rules.ts
@@ -1,88 +1,179 @@
-export { default as R1 } from "./sia-r1/rule";
-export { default as R2 } from "./sia-r2/rule";
-export { default as R3 } from "./sia-r3/rule";
-export { default as R4 } from "./sia-r4/rule";
-export { default as R5 } from "./sia-r5/rule";
-export { default as R7 } from "./sia-r7/rule";
-export { default as R8 } from "./sia-r8/rule";
-export { default as R9 } from "./sia-r9/rule";
-export { default as R10 } from "./sia-r10/rule";
-export { default as R11 } from "./sia-r11/rule";
-export { default as R12 } from "./sia-r12/rule";
-export { default as R13 } from "./sia-r13/rule";
-export { default as R14 } from "./sia-r14/rule";
-export { default as R15 } from "./sia-r15/rule";
-export { default as R16 } from "./sia-r16/rule";
-export { default as R17 } from "./sia-r17/rule";
-export { default as R18 } from "./sia-r18/rule";
-export { default as R19 } from "./sia-r19/rule";
-export { default as R20 } from "./sia-r20/rule";
-export { default as R21 } from "./sia-r21/rule";
-export { default as R22 } from "./sia-r22/rule";
-export { default as R23 } from "./sia-r23/rule";
-export { default as R24 } from "./sia-r24/rule";
-export { default as R25 } from "./sia-r25/rule";
-export { default as R26 } from "./sia-r26/rule";
-export { default as R27 } from "./sia-r27/rule";
-export { default as R28 } from "./sia-r28/rule";
-export { default as R29 } from "./sia-r29/rule";
-export { default as R30 } from "./sia-r30/rule";
-export { default as R31 } from "./sia-r31/rule";
-export { default as R32 } from "./sia-r32/rule";
-export { default as R33 } from "./sia-r33/rule";
-export { default as R35 } from "./sia-r35/rule";
-export { default as R37 } from "./sia-r37/rule";
-export { default as R38 } from "./sia-r38/rule";
-export { default as R39 } from "./sia-r39/rule";
-export { default as R40 } from "./sia-r40/rule";
-export { default as R41 } from "./sia-r41/rule";
-export { default as R42 } from "./sia-r42/rule";
-export { default as R43 } from "./sia-r43/rule";
-export { default as R44 } from "./sia-r44/rule";
-export { default as R45 } from "./sia-r45/rule";
-export { default as R46 } from "./sia-r46/rule";
-export { default as R47 } from "./sia-r47/rule";
-export { default as R48 } from "./sia-r48/rule";
-export { default as R49 } from "./sia-r49/rule";
-export { default as R50 } from "./sia-r50/rule";
-export { default as R53 } from "./sia-r53/rule";
-export { default as R54 } from "./sia-r54/rule";
-export { default as R55 } from "./sia-r55/rule";
-export { default as R56 } from "./sia-r56/rule";
-export { default as R57 } from "./sia-r57/rule";
-export { default as R59 } from "./sia-r59/rule";
-export { default as R60 } from "./sia-r60/rule";
-export { default as R61 } from "./sia-r61/rule";
-export { default as R62 } from "./sia-r62/rule";
-export { default as R63 } from "./sia-r63/rule";
-export { default as R64 } from "./sia-r64/rule";
-export { default as R65 } from "./sia-r65/rule";
-export { default as R66 } from "./sia-r66/rule";
-export { default as R67 } from "./sia-r67/rule";
-export { default as R68 } from "./sia-r68/rule";
-export { default as R69 } from "./sia-r69/rule";
-export { default as R70 } from "./sia-r70/rule";
-export { default as R71 } from "./sia-r71/rule";
-export { default as R72 } from "./sia-r72/rule";
-export { default as R73 } from "./sia-r73/rule";
-export { default as R74 } from "./sia-r74/rule";
-export { default as R75 } from "./sia-r75/rule";
-export { default as R76 } from "./sia-r76/rule";
-export { default as R77 } from "./sia-r77/rule";
-export { default as R78 } from "./sia-r78/rule";
-export { default as R79 } from "./sia-r79/rule";
-export { default as R80 } from "./sia-r80/rule";
-export { default as R81 } from "./sia-r81/rule";
-export { default as R83 } from "./sia-r83/rule";
-export { default as R84 } from "./sia-r84/rule";
-export { default as R85 } from "./sia-r85/rule";
-export { default as R86 } from "./sia-r86/rule";
-export { default as R87 } from "./sia-r87/rule";
-export { default as R90 } from "./sia-r90/rule";
-export { default as R91 } from "./sia-r91/rule";
-export { default as R92 } from "./sia-r92/rule";
-export { default as R93 } from "./sia-r93/rule";
-export { default as R94 } from "./sia-r94/rule";
-export { default as R95 } from "./sia-r95/rule";
-export { default as R96 } from "./sia-r96/rule";
-export { default as R110 } from "./sia-r110/rule";
+import R1 from "./sia-r1/rule";
+import R2 from "./sia-r2/rule";
+import R3 from "./sia-r3/rule";
+import R4 from "./sia-r4/rule";
+import R5 from "./sia-r5/rule";
+import R7 from "./sia-r7/rule";
+import R8 from "./sia-r8/rule";
+import R9 from "./sia-r9/rule";
+import R10 from "./sia-r10/rule";
+import R11 from "./sia-r11/rule";
+import R12 from "./sia-r12/rule";
+import R13 from "./sia-r13/rule";
+import R14 from "./sia-r14/rule";
+import R15 from "./sia-r15/rule";
+import R16 from "./sia-r16/rule";
+import R17 from "./sia-r17/rule";
+import R18 from "./sia-r18/rule";
+import R19 from "./sia-r19/rule";
+import R20 from "./sia-r20/rule";
+import R21 from "./sia-r21/rule";
+import R22 from "./sia-r22/rule";
+import R23 from "./sia-r23/rule";
+import R24 from "./sia-r24/rule";
+import R25 from "./sia-r25/rule";
+import R26 from "./sia-r26/rule";
+import R27 from "./sia-r27/rule";
+import R28 from "./sia-r28/rule";
+import R29 from "./sia-r29/rule";
+import R30 from "./sia-r30/rule";
+import R31 from "./sia-r31/rule";
+import R32 from "./sia-r32/rule";
+import R33 from "./sia-r33/rule";
+import R35 from "./sia-r35/rule";
+import R37 from "./sia-r37/rule";
+import R38 from "./sia-r38/rule";
+import R39 from "./sia-r39/rule";
+import R40 from "./sia-r40/rule";
+import R41 from "./sia-r41/rule";
+import R42 from "./sia-r42/rule";
+import R43 from "./sia-r43/rule";
+import R44 from "./sia-r44/rule";
+import R45 from "./sia-r45/rule";
+import R46 from "./sia-r46/rule";
+import R47 from "./sia-r47/rule";
+import R48 from "./sia-r48/rule";
+import R49 from "./sia-r49/rule";
+import R50 from "./sia-r50/rule";
+import R53 from "./sia-r53/rule";
+import R54 from "./sia-r54/rule";
+import R55 from "./sia-r55/rule";
+import R56 from "./sia-r56/rule";
+import R57 from "./sia-r57/rule";
+import R59 from "./sia-r59/rule";
+import R60 from "./sia-r60/rule";
+import R61 from "./sia-r61/rule";
+import R62 from "./sia-r62/rule";
+import R63 from "./sia-r63/rule";
+import R64 from "./sia-r64/rule";
+import R65 from "./sia-r65/rule";
+import R66 from "./sia-r66/rule";
+import R67 from "./sia-r67/rule";
+import R68 from "./sia-r68/rule";
+import R69 from "./sia-r69/rule";
+import R70 from "./sia-r70/rule";
+import R71 from "./sia-r71/rule";
+import R72 from "./sia-r72/rule";
+import R73 from "./sia-r73/rule";
+import R74 from "./sia-r74/rule";
+import R75 from "./sia-r75/rule";
+import R76 from "./sia-r76/rule";
+import R77 from "./sia-r77/rule";
+import R78 from "./sia-r78/rule";
+import R79 from "./sia-r79/rule";
+import R80 from "./sia-r80/rule";
+import R81 from "./sia-r81/rule";
+import R83 from "./sia-r83/rule";
+import R84 from "./sia-r84/rule";
+import R85 from "./sia-r85/rule";
+import R86 from "./sia-r86/rule";
+import R87 from "./sia-r87/rule";
+import R90 from "./sia-r90/rule";
+import R91 from "./sia-r91/rule";
+import R92 from "./sia-r92/rule";
+import R93 from "./sia-r93/rule";
+import R94 from "./sia-r94/rule";
+import R95 from "./sia-r95/rule";
+import R96 from "./sia-r96/rule";
+import R110 from "./sia-r110/rule";
+
+export {
+  /** @public */ R1,
+  /** @public */ R2,
+  /** @public */ R3,
+  /** @public */ R4,
+  /** @public */ R5,
+  /** @public */ R7,
+  /** @public */ R8,
+  /** @public */ R9,
+  /** @public */ R10,
+  /** @public */ R11,
+  /** @public */ R12,
+  /** @public */ R13,
+  /** @public */ R14,
+  /** @public */ R15,
+  /** @public */ R16,
+  /** @public */ R17,
+  /** @public */ R18,
+  /** @public */ R19,
+  /** @public */ R20,
+  /** @public */ R21,
+  /** @public */ R22,
+  /** @public */ R23,
+  /** @public */ R24,
+  /** @public */ R25,
+  /** @public */ R26,
+  /** @public */ R27,
+  /** @public */ R28,
+  /** @public */ R29,
+  /** @public */ R30,
+  /** @public */ R31,
+  /** @public */ R32,
+  /** @public */ R33,
+  /** @public */ R35,
+  /** @public */ R37,
+  /** @public */ R38,
+  /** @public */ R39,
+  /** @public */ R40,
+  /** @public */ R41,
+  /** @public */ R42,
+  /** @public */ R43,
+  /** @public */ R44,
+  /** @public */ R45,
+  /** @public */ R46,
+  /** @public */ R47,
+  /** @public */ R48,
+  /** @public */ R49,
+  /** @public */ R50,
+  /** @public */ R53,
+  /** @public */ R54,
+  /** @public */ R55,
+  /** @public */ R56,
+  /** @public */ R57,
+  /** @public */ R59,
+  /** @public */ R60,
+  /** @public */ R61,
+  /** @public */ R62,
+  /** @public */ R63,
+  /** @public */ R64,
+  /** @public */ R65,
+  /** @public */ R66,
+  /** @public */ R67,
+  /** @public */ R68,
+  /** @public */ R69,
+  /** @public */ R70,
+  /** @public */ R71,
+  /** @public */ R72,
+  /** @public */ R73,
+  /** @public */ R74,
+  /** @public */ R75,
+  /** @public */ R76,
+  /** @public */ R77,
+  /** @public */ R78,
+  /** @public */ R79,
+  /** @public */ R80,
+  /** @public */ R81,
+  /** @public */ R83,
+  /** @public */ R84,
+  /** @public */ R85,
+  /** @public */ R86,
+  /** @public */ R87,
+  /** @public */ R90,
+  /** @public */ R91,
+  /** @public */ R92,
+  /** @public */ R93,
+  /** @public */ R94,
+  /** @public */ R95,
+  /** @public */ R96,
+  /** @public */ R110,
+};
