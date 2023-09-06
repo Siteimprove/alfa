31851cf66afdeb5923be0400c4875d66e01699f2
Jean-Yves Moyen <jym@siteimprove.com>
2023-09-06 16:07:45 +0200

    Clean up

---
+++ packages/alfa-toolchain/src/validation/index.ts
@@ -0,0 +1,5 @@
+export * from "./has-extractor-config";
+export * from "./validate-all";
+export * from "./validate-changesets";
+export * from "./validate-package-json";
+export * from "./validate-workspace-tsconfig";
