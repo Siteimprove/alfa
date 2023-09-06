10da6ba5892897d45dae1a822445748c1cf8e80f
Jean-Yves Moyen <jym@siteimprove.com>
2023-09-06 09:32:15 +0200

    Update some dependencies (#1467)
    * Clean up
    
    * Upgrade TS
    
    * Upgrade yarn
    
    * Update deps
    
    * Make knip happy
    
    * Clean up

--- packages/alfa-rules/src/deprecated.ts
+++ packages/alfa-rules/src/deprecated.ts
@@ -1,6 +1,15 @@
-export { default as DR6 } from "./sia-dr6/rule";
-export { default as DR18 } from "./sia-dr18/rule";
-export { default as DR34 } from "./sia-dr34/rule";
-export { default as DR36 } from "./sia-dr36/rule";
-export { default as DR66 } from "./sia-dr66/rule";
-export { default as DR69 } from "./sia-dr69/rule";
+import DR6 from "./sia-dr6/rule";
+import DR18 from "./sia-dr18/rule";
+import DR34 from "./sia-dr34/rule";
+import DR36 from "./sia-dr36/rule";
+import DR66 from "./sia-dr66/rule";
+import DR69 from "./sia-dr69/rule";
+
+export {
+  /** @public */ DR6,
+  /** @public */ DR18,
+  /** @public */ DR34,
+  /** @public */ DR36,
+  /** @public */ DR66,
+  /** @public */ DR69,
+};
