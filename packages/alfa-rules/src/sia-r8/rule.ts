e059a18e8e0fe35ab90b908bf20bd5239bfe52d1
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2023-11-14 13:57:54 +0100

    Clean up unused import

--- packages/alfa-rules/src/sia-r8/rule.ts
+++ packages/alfa-rules/src/sia-r8/rule.ts
@@ -1,4 +1,4 @@
-import { Diagnostic, Rule } from "@siteimprove/alfa-act";
+import { Rule } from "@siteimprove/alfa-act";
 import { DOM, Role } from "@siteimprove/alfa-aria";
 import { Element, Namespace, Node, Query } from "@siteimprove/alfa-dom";
 import { Predicate } from "@siteimprove/alfa-predicate";
