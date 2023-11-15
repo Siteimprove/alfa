1921b0d0437408b868775651b292d8d6bbb8b146
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2023-11-15 16:44:08 +0100

    Factor heading diagnostics (#1505)
    * Factor previous and next heading diagnostics into other heading
    
    * Update API documentation
    
    * Add changeset
    
    * Update changeset
    
    * Add heading position to diagnostic
    
    * Use `.tee` to store heading levels
    
    * Update .changeset/funny-ducks-approve.md
    
    Co-authored-by: Jean-Yves Moyen <jym@siteimprove.com>
    
    * Address review comments
    
    * Mark exported type as public
    
    ---------
    
    Co-authored-by: Jean-Yves Moyen <jym@siteimprove.com>

--- packages/alfa-rules/src/common/act/diagnostic.ts
+++ packages/alfa-rules/src/common/act/diagnostic.ts
@@ -1,14 +1,12 @@
 import * as languages from "../../sia-r109/rule";
 import * as labelAndName from "../../sia-r14/rule";
 import * as roleAndRequiredAttributes from "../../sia-r16/rule";
-import * as withPreviousHeading from "../../sia-r53/rule";
 import * as withRoleAndName from "../../sia-r55/rule";
 import * as sameNames from "../../sia-r56/rule";
 import * as withFirstHeading from "../../sia-r61/rule";
 import * as distinguishingStyles from "../../sia-r62/diagnostics";
 import * as matchingClasses from "../../sia-r65/diagnostics";
 import * as withDeclaration from "../../sia-r75/rule";
-import * as withNextHeading from "../../sia-r78/rule";
 import * as clippingAncestors from "../../sia-r83/rule";
 
 // R66, R69
@@ -24,7 +22,6 @@ export namespace Diagnostic {
   export import Languages = languages.Languages;
   export import LabelAndName = labelAndName.LabelAndName;
   export import RoleAndRequiredAttributes = roleAndRequiredAttributes.RoleAndRequiredAttributes;
-  export import WithPreviousHeading = withPreviousHeading.WithPreviousHeading;
   export import WithRoleAndName = withRoleAndName.WithRoleAndName;
   export import SameNames = sameNames.SameNames;
   export import WithFirstHeading = withFirstHeading.WithFirstHeading;
@@ -34,11 +31,11 @@ export namespace Diagnostic {
   export import ColorErrors = colorError.ColorErrors;
   export import MatchingClasses = matchingClasses.MatchingClasses;
   export import WithDeclaration = withDeclaration.WithDeclaration;
-  export import WithNextHeading = withNextHeading.WithNextHeading;
   export import ClippingAncestors = clippingAncestors.ClippingAncestors;
   export import Contrast = diagnostic.Contrast;
   export import TextSpacing = diagnostic.TextSpacing;
   export import WithBadElements = diagnostic.WithBadElements;
   export import WithRole = diagnostic.WithRole;
   export import WithAccessibleName = diagnostic.WithAccessibleName;
+  export import WithOtherHeading = diagnostic.WithOtherHeading;
 }
--- packages/alfa-rules/src/sia-r53/rule.ts
+++ packages/alfa-rules/src/sia-r53/rule.ts
@@ -1,17 +1,18 @@
-import { Diagnostic, Rule } from "@siteimprove/alfa-act";
+import { Rule } from "@siteimprove/alfa-act";
 import { DOM } from "@siteimprove/alfa-aria";
 import { Element, Node, Query } from "@siteimprove/alfa-dom";
-import { Hash } from "@siteimprove/alfa-hash";
 import { Predicate } from "@siteimprove/alfa-predicate";
 import { Err, Ok } from "@siteimprove/alfa-result";
 import { Page } from "@siteimprove/alfa-web";
+import { Option } from "@siteimprove/alfa-option";
 
 import { expectation } from "../common/act/expectation";
 
 import { Scope, Stability } from "../tags";
+import { WithOtherHeading } from "../common/diagnostic";
 
 const { hasHeadingLevel, hasRole, isIncludedInTheAccessibilityTree } = DOM;
-const { and, equals } = Predicate;
+const { and, equals, tee } = Predicate;
 const { getElementDescendants } = Query;
 
 export default Rule.Atomic.of<Page, Element>({
@@ -31,18 +32,42 @@ export default Rule.Atomic.of<Page, Element>({
         // * The target is in headings by construction of the applicability.
         // * The first element of heading is not a target due to the .skip(1)
         // * Therefore headings contain at least on element before the target.
-        const previous = headings.takeUntil(equals(target)).last().getUnsafe();
+        const previousHeading = headings
+          .takeUntil(equals(target))
+          .last()
+          .getUnsafe();
+
+        let previousLevel = -1;
+        let currentLevel = -1;
 
         return {
           1: expectation(
-            hasHeadingLevel(device, (currentLevel) =>
-              hasHeadingLevel(
-                device,
-                (previousLevel) => previousLevel >= currentLevel - 1,
-              )(previous),
+            hasHeadingLevel(
+              device,
+              tee(
+                (currentLevel) =>
+                  hasHeadingLevel(
+                    device,
+                    tee(
+                      (previousLevel) => previousLevel >= currentLevel - 1,
+                      (p) => (previousLevel = p),
+                    ),
+                  )(previousHeading),
+                (c) => (currentLevel = c),
+              ),
             )(target),
-            () => Outcomes.IsStructured(previous),
-            () => Outcomes.IsNotStructured(previous),
+            () =>
+              Outcomes.IsStructured(
+                previousHeading,
+                currentLevel,
+                previousLevel,
+              ),
+            () =>
+              Outcomes.IsNotStructured(
+                previousHeading,
+                currentLevel,
+                previousLevel,
+              ),
           ),
         };
       },
@@ -53,75 +78,34 @@ export default Rule.Atomic.of<Page, Element>({
 /**
  * @public
  */
-export class WithPreviousHeading extends Diagnostic {
-  public static of(message: string): Diagnostic;
-
-  public static of(message: string, previous: Element): WithPreviousHeading;
-
-  public static of(message: string, previous?: Element): Diagnostic {
-    return previous === undefined
-      ? Diagnostic.of(message)
-      : new WithPreviousHeading(message, previous);
-  }
-
-  private readonly _previous: Element;
-
-  constructor(message: string, previous: Element) {
-    super(message);
-    this._previous = previous;
-  }
-
-  public get previous(): Element {
-    return this._previous;
-  }
-
-  equals(value: WithPreviousHeading): value is this;
-
-  equals(value: unknown): value is this;
-
-  equals(value: unknown): boolean {
-    return (
-      value instanceof WithPreviousHeading &&
-      value._message === this._message &&
-      value._previous.equals(this._previous)
-    );
-  }
-
-  public hash(hash: Hash) {
-    super.hash(hash);
-    this._previous.hash(hash);
-  }
-
-  toJSON(): WithPreviousHeading.JSON {
-    return { ...super.toJSON(), previous: this._previous.toJSON() };
-  }
-}
-
-/**
- * @public
- */
-export namespace WithPreviousHeading {
-  export interface JSON extends Diagnostic.JSON {
-    previous: Element.JSON;
-  }
-
-  /**@public */
-  export function isWithPreviousHeading(
-    value: unknown,
-  ): value is WithPreviousHeading {
-    return value instanceof WithPreviousHeading;
-  }
-}
-
-/**
- * @public
- */
 export namespace Outcomes {
-  export const IsStructured = (previous: Element) =>
-    Ok.of(WithPreviousHeading.of(`The heading is correctly ordered`, previous));
+  export const IsStructured = (
+    previousHeading: Element,
+    currentLevel: number,
+    previousLevel: number,
+  ) =>
+    Ok.of(
+      WithOtherHeading.of(
+        `The heading is correctly ordered`,
+        Option.of(previousHeading),
+        currentLevel,
+        previousLevel,
+        "previous",
+      ),
+    );
 
-  export const IsNotStructured = (previous: Element) =>
+  export const IsNotStructured = (
+    previousHeading: Element,
+    currentLevel: number,
+    previousLevel: number,
+  ) =>
     Err.of(
-      WithPreviousHeading.of(`The heading skips one or more levels`, previous),
+      WithOtherHeading.of(
+        `The heading skips one or more levels`,
+        Option.of(previousHeading),
+        currentLevel,
+        previousLevel,
+        "previous",
+      ),
     );
 }
