fc471cba7e11c928c1ece2f3a7c02f6b7683d066
Jean-Yves Moyen <jym@siteimprove.com>
2023-12-19 15:47:19 +0100

    Add some documentation and improve custom getting

--- packages/alfa-cascade/src/cascade.ts
+++ packages/alfa-cascade/src/cascade.ts
@@ -8,14 +8,28 @@ import { Context } from "@siteimprove/alfa-selector";
 import * as json from "@siteimprove/alfa-json";
 
 import { AncestorFilter } from "./ancestor-filter";
-import { Block } from "./block";
-import { Precedence } from "./precedence";
 import { RuleTree } from "./rule-tree";
 import { SelectorMap } from "./selector-map";
 import { UserAgent } from "./user-agent";
 
 /**
  * {@link https://drafts.csswg.org/css-cascade-5/}
+ *
+ * @remarks
+ * The cascade associate to each element a node into a rule tree.
+ * A single rule tree is built for each document or shadow root. The cascade
+ * lazily fills it upon need and caches the associated node for each element.
+ *
+ * Upon creating a cascade, the full rule tree is built for the empty context
+ * in order to leverage the ancestor filter during tree traversal. This assumes
+ * that we will often query style of elements in an empty context (the default)
+ * and thus benefit from pre-building it for all elements.
+ *
+ * For specific contexts, we only add the nodes in the rule tree as needed. We assume
+ * that we mostly query only a few elements in a specific context, and that the cost
+ * of rebuilding a full cascade would be too expensive.
+ *
+ * The cascade automatically includes the user agent style sheet.
  *
  * @public
  */
@@ -57,7 +71,11 @@ export class Cascade implements Serializable {
 
     const visit = (node: Node): void => {
       if (Element.isElement(node)) {
-        this.get(node, context, Option.of(filter));
+        // Since we are traversing the full DOM tree and maintaining our own
+        // ancestor filter on the way, use the simple #add.
+
+        // Entering an element: add it to the rule tree, and to the ancestor filter.
+        this.add(node, context, Option.of(filter));
         filter.add(node);
       }
 
@@ -66,6 +84,7 @@ export class Cascade implements Serializable {
       }
 
       if (Element.isElement(node)) {
+        // Exiting an element: remove it from the ancestor filter.
         filter.remove(node);
       }
     };
@@ -73,16 +92,71 @@ export class Cascade implements Serializable {
     visit(root);
   }
 
-  public get(
+  /**
+   * Add an element to the rule tree, returns the associated node.
+   *
+   * @remarks
+   * This is idempotent since the rule tree already checks physical identity
+   * of selectors upon insertion. However, calling it too often is bad for performance.
+   */
+  private add(
     element: Element,
     context: Context = Context.empty(),
     filter: Option<AncestorFilter> = None,
   ): RuleTree.Node {
-    return this._entries
-      .get(element, Cache.empty)
-      .get(context, () =>
-        this._rules.add(this._selectors.get(element, context, filter)),
-      );
+    return this._rules.add(this._selectors.get(element, context, filter));
+  }
+
+  /**
+   * Adds an element to the tree, with a custom ancestor filter.
+   *
+   * @remarks
+   * A new ancestor filter is built and filled with the element's ancestors.
+   * When building the full cascade for a DOM tree, this is pointless and the
+   * faster #add should be used instead. When looking up the style of a single
+   * element, we assume shat the time spend going up the DOM tree to build an
+   * ancestor filter will be saved by matching less selectors.
+   */
+  private addAncestors(element: Element, context: Context): RuleTree.Node {
+    const filter = AncestorFilter.empty();
+    // Because CSS selectors do not cross shadow or document boundaries,
+    // only get ancestors in the same tree.
+    // Adding elements to the ancestor filter is commutative, so we
+    // can add them from bottom to root without reversing the sequence first.
+    element
+      .ancestors()
+      .filter(Element.isElement)
+      .forEach(filter.add.bind(filter));
+
+    return this._rules.add(
+      this._selectors.get(element, context, Option.of(filter)),
+    );
+  }
+
+  /**
+   * Get the rule tree node associated with an element.
+   *
+   * @remarks
+   * This also adds the element to the rule tree if needed. That is, the rule
+   * tree is build lazily upon need. For the empty context, we pre-build the full
+   * tree, so we can benefit from an ancestor filter as we traverse the full DOM tree.
+   *
+   * For other contexts, we assume that we will only need the style of a few elements
+   * (e.g., when a link is focused we normally only need the style of the link itself).
+   * Therefore, pre-building the full tree is not worth the cost nor the saving we'd
+   * get with an ancestor filter.
+   */
+  public get(
+    element: Element,
+    context: Context = Context.empty(),
+  ): RuleTree.Node {
+    return this._entries.get(element, Cache.empty).get(
+      context,
+      // If the entry hasn't been cached already, we assume we are querying
+      // for a single element and pay the price of building its custom ancestor
+      // filter, hopefully saving on the matching cost.
+      () => this.addAncestors(element, context),
+    );
   }
 
   public toJSON(): Cascade.JSON {
