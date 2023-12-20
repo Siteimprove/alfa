f7233f2408d5a865f38ac339b73ce8449729119a
Jean-Yves Moyen <jym@siteimprove.com>
2023-12-20 13:56:31 +0100

    Add release tag

--- packages/alfa-comparable/src/comparer.ts
+++ packages/alfa-comparable/src/comparer.ts
@@ -11,6 +11,8 @@ export type Comparer<T, U = T, A extends Array<unknown> = []> = (
 
 /**
  * Turns a tuple of types into a tuple of comparer over these types.
+ *
+ * @public
  */
 export type TupleComparer<T extends Array<unknown>> = T extends [
   infer Head,
