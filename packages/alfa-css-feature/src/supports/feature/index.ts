42bd0519387a63596e1652faf4e952c219c714da
Jean-Yves Moyen <jym@siteimprove.com>
2024-01-05 16:06:42 +0100

    Fix imports

--- packages/alfa-css-feature/src/supports/feature/index.ts
+++ packages/alfa-css-feature/src/supports/feature/index.ts
@@ -1,1 +1,16 @@
 export * from "./property";
+
+/**
+ * @remarks
+ * Supports function are not currently supported.
+ *
+ * {@link https://developer.mozilla.org/en-US/docs/Web/CSS/@supports#function_syntax}
+ * {@link https://drafts.csswg.org/css-conditional-4/#at-supports-ext}
+ *
+ * @remarks
+ * * `selector(…)` is currently experimental.
+ * * `font-tech(…)` and `font-format(…)` are somehow not documented in CSS (?!)
+ *
+ * We currently do not support the functional notation and will need to expand support
+ * upon need.
+ */
