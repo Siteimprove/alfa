ecce35a8144590bf94d7c99906cc97b579bebc69
Jean-Yves Moyen <jym@siteimprove.com>
2024-01-26 13:20:44 +0100

    Fix ownership of rules

--- packages/alfa-dom/src/style/rule.ts
+++ packages/alfa-dom/src/style/rule.ts
@@ -1,4 +1,5 @@
 import { Equatable } from "@siteimprove/alfa-equatable";
+import { Iterable } from "@siteimprove/alfa-iterable";
 import { Serializable } from "@siteimprove/alfa-json";
 import { None, Option } from "@siteimprove/alfa-option";
 import { Trampoline } from "@siteimprove/alfa-trampoline";
@@ -84,7 +85,10 @@ export abstract class Rule<T extends string = string>
 
     this._owner = Option.of(owner);
 
-    return true;
+    // Recursively attach the owner to all descendants.
+    return Iterable.every(this.descendants(), (rule) =>
+      rule._attachOwner(owner),
+    );
   }
 
   /**
---
+++ packages/alfa-dom/test/style.spec.ts
@@ -0,0 +1,10 @@
+import { test } from "@siteimprove/alfa-test";
+
+import { h } from "../src";
+
+test("Sheet.of assigns owner to descendants of condition rules", (t) => {
+  const rule = h.rule.style("foo", { foo: "bar" });
+  const sheet = h.sheet([h.rule.media("screen", [rule])]);
+
+  t.equal(rule.owner.getUnsafe(), sheet);
+});
