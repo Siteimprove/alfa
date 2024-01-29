9299210a09d5489028fa8dcbfb9efc72f711074c
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2024-01-29 11:52:41 +0100

    Refactor test script

--- scripts/stryker-test.mjs
+++ scripts/stryker-test.mjs
@@ -6,21 +6,41 @@ import os from "os";
 
 import { system } from "./common/system.mjs";
 
-async.eachLimit(
-  system.readDirectory("packages", [".spec.ts", ".spec.tsx"], ["node_modules"]),
-  os.cpus().length,
-  (fileName, done) => {
-    execaNode(fileName.replace(/\.tsx?$/, ".js"), [], {
-      nodeOptions: [...process.execArgv, "--enable-source-maps"],
-      stdio: "inherit",
-    }).then(
-      () => done(),
-      (err) => done(err),
-    );
-  },
-  (err) => {
+/**
+ * @param {string} fileName the test file to run
+ * @returns {Promise<boolean>} true if any test in the file fails
+ */
+async function test(fileName) {
+  const childProcessResult = await execaNode(fileName, [], {
+    nodeOptions: [...process.execArgv],
+    stdio: "inherit",
+  });
+
+  return childProcessResult.exitCode === 1;
+}
+
+/**
+ * Runs the tests in parallel and exits with code 1 as soon as one test fails.
+ * @returns {Promise<void>} a promise that resolves when one test fails or all tests pass
+ */
+async function main() {
+  const testFiles = system.readDirectory(
+    "packages",
+    [".spec.js"],
+    ["node_modules"],
+  );
+
+  const limit = os.cpus().length;
+
+  async.someLimit(testFiles, limit, test, (err, result) => {
     if (err) {
+      throw err;
+    }
+
+    if (result) {
       system.exit(1);
     }
-  },
-);
+  });
+}
+
+main();
