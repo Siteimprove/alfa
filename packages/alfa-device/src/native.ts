ef621df8d95c1320724a582f7e968a57a2fa1e7e
Jean-Yves Moyen <jym@siteimprove.com>
2024-04-16 14:40:29 +0200

    Inline Native.fromWindow

--- packages/alfa-device/src/native.ts
+++ packages/alfa-device/src/native.ts
@@ -1,48 +1,70 @@
 /// <reference lib="dom" />
 
-import { type Device, Preference } from ".";
+/**
+ * The function defined in this file is destined to be injected into browser
+ * page, either through a web extension or browser automation tool.
+ *
+ * As such, it must be serializable, meaning it must not reference any external
+ * file (only import type are allowed), and may not use annex functions (who
+ * must instead be inlined in the main function).
+ *
+ * We could use Webpack or the like to bundle the current file with its
+ * dependencies into a single file. This is however somewhat heavy-handed, and
+ * the rest of Alfa has no need for such complex machinery, so we stick to a
+ * simple solution for now.
+ */
+
+import type { Device, Preference } from ".";
 
 /**
  * @internal
  */
 export namespace Native {
   export function fromWindow(window: globalThis.Window): Device.JSON {
-    const {
-      documentElement: { clientWidth, clientHeight },
-    } = window.document;
+    return toDevice();
 
-    return {
-      type: "screen",
-      viewport: {
-        width: clientWidth,
-        height: clientHeight,
-        orientation: window.matchMedia("(orientation: landscape)").matches
-          ? "landscape"
-          : "portrait",
-      },
-      display: {
-        resolution: window.devicePixelRatio,
-        scan: "progressive",
-      },
-      scripting: {
-        enabled: !window.matchMedia("(scripting: none)").matches,
-      },
-      preferences: [...toPreferences(window)],
-    };
-  }
+    function toDevice(): Device.JSON {
+      const {
+        documentElement: { clientWidth, clientHeight },
+      } = window.document;
 
-  const userPreferences = Object.keys(
-    Preference.preferences,
-  ) as Array<Preference.Name>;
+      return {
+        type: "screen",
+        viewport: {
+          width: clientWidth,
+          height: clientHeight,
+          orientation: window.matchMedia("(orientation: landscape)").matches
+            ? "landscape"
+            : "portrait",
+        },
+        display: { resolution: window.devicePixelRatio, scan: "progressive" },
+        scripting: { enabled: !window.matchMedia("(scripting: none)").matches },
+        preferences: [...toPreferences()],
+      };
+    }
 
-  function* toPreferences(
-    window: globalThis.Window,
-  ): Iterable<Preference.JSON> {
-    // It seems we need to manually query each preference individually.
-    for (const name of userPreferences) {
-      for (const value of Preference.preferences[name]) {
-        if (window.matchMedia(`(${name}: ${value})`).matches) {
-          yield { name, value };
+    function* toPreferences(): Iterable<Preference.JSON> {
+      // Since everything has to be inlined, we need to redeclare it here.
+      // Typing ensure that there isn't too many mistakes (a value may
+      // still be missing).
+      const preferences: {
+        [K in Preference.Name]: Array<Preference.Value<K>>;
+      } = {
+        "forced-colors": ["none", "active"],
+        inverted: ["none", "inverted"],
+        "prefers-color-scheme": ["no-preference", "light", "dark"],
+        "prefers-contrast": ["no-preference", "less", "more", "custom"],
+        "prefers-reduced-motion": ["no-preference", "reduce"],
+        "prefers-reduced-transparency": ["no-preference", "reduce"],
+        "prefers-reduced-data": ["no-preference", "reduce"],
+      };
+
+      // It seems we need to manually query each preference individually.
+      for (const name of Object.keys(preferences) as Array<Preference.Name>) {
+        for (const value of preferences[name]) {
+          if (window.matchMedia(`(${name}: ${value})`).matches) {
+            yield { name, value };
+          }
         }
       }
     }
