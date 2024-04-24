0f7e182cf6f823e8c3955ad7fc965b60d214011b
Jean-Yves Moyen <jym@siteimprove.com>
2024-04-24 15:54:30 +0200

    Add support condition to import rules

--- packages/alfa-dom/src/style/rule/import.ts
+++ packages/alfa-dom/src/style/rule/import.ts
@@ -17,34 +17,57 @@ export class ImportRule extends ConditionRule<"import"> {
     href: string,
     sheet: Sheet,
     mediaCondition: Option<string> = None,
+    supportCondition: Option<string> = None,
   ): ImportRule {
-    return new ImportRule(href, sheet, mediaCondition);
+    return new ImportRule(href, sheet, mediaCondition, supportCondition);
   }
 
   private readonly _href: string;
   private readonly _sheet: Sheet;
   private readonly _mediaQueries: Feature.Media.List;
+  private readonly _supportConditon: Option<string>;
+  // There may be no support condition, or an unparsable (i.e. non-supported) one.
+  // The former is None, the later is Some(None).
+  private readonly _supportQuery: Option<Option<Feature.Supports.Query>>;
 
   private constructor(
     href: string,
     sheet: Sheet,
     mediaCondition: Option<string>,
+    supportCondition: Option<string>,
   ) {
     super("import", mediaCondition.getOr("all"), []);
 
     this._href = href;
     this._sheet = sheet;
+    this._supportConditon = supportCondition;
+
     this._mediaQueries = mediaCondition
       .flatMap((condition) =>
         Feature.parseMediaQuery(Lexer.lex(condition)).ok(),
       )
       .map(([, queries]) => queries)
       .getOr(Feature.Media.List.of([]));
+
+    this._supportQuery = supportCondition.map((condition) =>
+      Feature.parseSupportsQuery(Lexer.lex(condition))
+        .ok()
+        .map(([, query]) => query),
+    );
+  }
+
+  public get supportCondition(): Option<string> {
+    return this._supportConditon;
   }
 
   public get mediaQueries(): Feature.Media.List {
     return this._mediaQueries;
   }
+
+  public get supportQuery(): Option<Option<Feature.Supports.Query>> {
+    return this._supportQuery;
+  }
+
   public get rules(): Iterable<Rule> {
     return this._sheet.rules;
   }
@@ -81,8 +104,21 @@ export namespace ImportRule {
     return value instanceof ImportRule;
   }
 
-  export function matches(device: Device): Predicate<ImportRule> {
+  function matchesMedia(device: Device): Predicate<ImportRule> {
     return (rule) => rule.mediaQueries.matches(device);
+  }
+
+  function matchesSupport(device: Device): Predicate<ImportRule> {
+    // If there is no support condition (None), it is matched.
+    // If there is an unparsable one (Some(None)), it is not matched.
+    return (rule) =>
+      rule.supportQuery.every((option) =>
+        option.some((query) => query.matches(device)),
+      );
+  }
+
+  export function matches(device: Device): Predicate<ImportRule> {
+    return Predicate.and(matchesMedia(device), matchesSupport(device));
   }
 
   /**
