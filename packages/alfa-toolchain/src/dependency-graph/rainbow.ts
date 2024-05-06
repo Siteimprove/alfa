2de28feeccaccf7c8168a2b7cfd8f0fbd7f667fa
Jean-Yves Moyen <jym@siteimprove.com>
2024-05-06 13:25:14 +0200

    Add rainbow generator

---
+++ packages/alfa-toolchain/src/dependency-graph/rainbow.ts
@@ -0,0 +1,40 @@
+import * as gv from "ts-graphviz";
+
+/**
+ * @internal
+ */
+export namespace Rainbow {
+  /**
+   * Return a color x% along the rainbow spectrum.
+   * {@link http://blog.adamcole.ca/2011/11/simple-javascript-rainbow-color.html}
+   */
+  export function rainbowStop(percentage: number): gv.Color.RGB_RGBA {
+    const f: (n: number, k?: number) => number = (
+      n,
+      k = (n + percentage * 12) % 12,
+    ) => 0.5 - 0.5 * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+
+    return rgb2hex(f(0), f(8), f(4));
+  }
+
+  function rainbow(n: number): Array<gv.Color.RGB_RGBA> {
+    let result: Array<gv.Color.RGB_RGBA> = [];
+
+    for (let i = 0; i < n; i++) {
+      result.push(rainbowStop(i / n));
+    }
+
+    return result;
+  }
+
+  function rgb2hex(r: number, g: number, b: number): gv.Color.RGB_RGBA {
+    return ("#" +
+      [r, g, b]
+        .map((x) =>
+          Math.round(x * 255)
+            .toString(16)
+            .padStart(2, "0"),
+        )
+        .join("")) as gv.Color.RGB_RGBA;
+  }
+}
