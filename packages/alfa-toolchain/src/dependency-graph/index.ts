392555a705e460737be967964d1f8811825133a1
Jean-Yves Moyen <jym@siteimprove.com>
2024-05-07 15:54:12 +0200

    Add individual graphs

--- packages/alfa-toolchain/src/dependency-graph/index.ts
+++
@@ -1,9 +0,0 @@
-import { DependencyGraph } from "./dependency-graph";
-
-main();
-
-async function main() {
-  const graph = await DependencyGraph.from("packages", "alfa", "css");
-
-  graph.save();
-}
