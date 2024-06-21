840bfd3ed2c316151c9c2a73352d9ac9a5ce0fa0
Jean-Yves Moyen <jym@siteimprove.com>
2024-06-21 11:04:48 +0200

    Use node: resolver

--- packages/alfa-toolchain/src/validation/validate-workspace-tsconfig.ts
+++ packages/alfa-toolchain/src/validation/validate-workspace-tsconfig.ts
@@ -1,6 +1,6 @@
 import { PackageJSON } from "@changesets/types";
-import * as fs from "fs";
-import * as path from "path";
+import * as fs from "node:fs";
+import * as path from "node:path";
 
 /**
  * @public
