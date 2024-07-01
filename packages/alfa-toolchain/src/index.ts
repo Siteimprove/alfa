6f13ba4d5cb8695ef8a5429f346e73aae230a920
Jean-Yves Moyen <jym@siteimprove.com>
2024-07-01 15:49:21 +0200

    Move changeset generator to CJS (#1643)
    * Move individual changelog generation to CJS
    
    * Move changeset wrapper to CJS
    
    * Extract API
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/alfa-toolchain/src/index.ts
+++ packages/alfa-toolchain/src/index.ts
@@ -1,7 +1,7 @@
 import { generateGraphs } from "./dependency-graph/generate-graphs.js";
 import changelogFunctions from "./changeset/build-changelog.js";
 import * as globalChangelog from "./changeset/changelog-global.js";
-import * as individualChangelog from "./changeset/changelog-individual.js";
+import * as individualChangelog from "./changeset/changelog-individual.cjs";
 import * as Validation from "./validation/index.js";
 
 export {
