504cb773c3c9f67541120c2b1e2828a349912339
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2024-07-08 13:58:35 +0200

    Test size condition on inclusive descendants

--- packages/alfa-rules/src/common/predicate/has-sufficient-size.ts
+++ packages/alfa-rules/src/common/predicate/has-sufficient-size.ts
@@ -1,19 +1,28 @@
 import type { Device } from "@siteimprove/alfa-device";
 import type { Predicate } from "@siteimprove/alfa-predicate";
-import type { Element } from "@siteimprove/alfa-dom";
+import { Element } from "@siteimprove/alfa-dom";
+import { Refinement } from "@siteimprove/alfa-refinement";
+
+const { and } = Refinement;
+const { hasInclusiveDescendant, isElement } = Element;
 
 /**
  * @remarks
- * This predicate tests that the bounding box of an element has width and height larger than a given value.
+ * This predicate tests that the bounding box of an element or one of it's element descendants
+ * has width and height larger than a given value.
+ *
  * Defaults to true if called on an element without bounding box to avoid false positives.
  */
 export function hasSufficientSize(
   size: number,
   device: Device,
 ): Predicate<Element> {
-  return (element) =>
-    element
-      .getBoundingBox(device)
-      .map((box) => box.width >= size && box.height >= size)
-      .getOr(true);
+  return hasInclusiveDescendant((desc) =>
+    and(isElement, (element) =>
+      element
+        .getBoundingBox(device)
+        .map((box) => box.width >= size && box.height >= size)
+        .getOr(true),
+    )(desc),
+  );
 }
