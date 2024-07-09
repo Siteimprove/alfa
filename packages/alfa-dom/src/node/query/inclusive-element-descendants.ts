113aecb1454ed452fb8107eec983104aee9a573a
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2024-07-09 15:27:03 +0200

    Fix build error

--- packages/alfa-dom/src/node/query/inclusive-element-descendants.ts
+++ packages/alfa-dom/src/node/query/inclusive-element-descendants.ts
@@ -1,9 +1,8 @@
-import { Cache } from "@siteimprove/alfa-cache";
 import type { Sequence } from "@siteimprove/alfa-sequence";
 import { Node } from "../../node.js";
 import { Element } from "../element.js";
 
-const cache = Cache.empty<Element, Array<Sequence<Element>>>();
+import { getElementDescendants } from "./element-descendants.js";
 
 /**
  * @public
@@ -12,5 +11,5 @@ export function getInclusiveElementDescendants(
   node: Element,
   options: Node.Traversal = Node.Traversal.empty,
 ): Sequence<Element> {
-return getElementDescendants(node, options).prepend(node)
+  return getElementDescendants(node, options).prepend(node);
 }
