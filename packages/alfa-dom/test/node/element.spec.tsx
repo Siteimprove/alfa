6973892951c0b3822b4499e23978ef48dd02c7a1
Jean-Yves Moyen <jym@siteimprove.com>
2024-09-17 15:11:40 +0200

    Add tests

--- packages/alfa-dom/test/node/element.spec.tsx
+++ packages/alfa-dom/test/node/element.spec.tsx
@@ -1,5 +1,7 @@
 import { test } from "@siteimprove/alfa-test";
 
+import { type Element, h } from "../../dist/index.js";
+
 test("#tabIndex() returns the tab index explicitly assigned to an element", (t) => {
   t.equal((<div tabindex="42" />).tabIndex().getUnsafe(), 42);
 });
@@ -48,4 +50,54 @@ test(`#tabIndex() returns None for a <summary> element that is not the first
   </details>;
 
   t.equal(summary.tabIndex().isNone(), true);
+});
+
+test("#inputType() returns the explicit type of an <input> element", (t) => {
+  for (const type of [
+    "hidden",
+    "search",
+    "tel",
+    "url",
+    "email",
+    "password",
+    "date",
+    "month",
+    "week",
+    "time",
+    "datetime-local",
+    "number",
+    "range",
+    "color",
+    "checkbox",
+    "radio",
+    "file",
+    "submit",
+    "image",
+    "reset",
+    "button",
+    "text",
+  ] as const) {
+    t.equal(((<input type={type} />) as Element<"input">).inputType(), type);
+  }
+});
+
+test("#inputType() returns None for an <input> element without a type attribute", (t) => {
+  t.equal(((<input />) as Element<"input">).inputType(), "text");
+});
+
+test("#inputType() returns None for an <input> element with an invalid type attribute", (t) => {
+  // We cannot use JSX to create an <input> element with an invalid type attribute
+  t.equal(h.element("input", [h.attribute("type", "foo")]).inputType(), "text");
+});
+
+test("#displaySize() returns 1 for non-multiple <select> without a size attribute", (t) => {
+  t.equal(((<select />) as Element<"select">).displaySize(), 1);
+});
+
+test("#displaySize() returns the size attribute for a non-multiple <select>", (t) => {
+  t.equal(((<select size="42" />) as Element<"select">).displaySize(), 42);
+});
+
+test("#displaySize() returns 4 for a multiple <select> without a size attribute", (t) => {
+  t.equal(((<select multiple />) as Element<"select">).displaySize(), 4);
 });
