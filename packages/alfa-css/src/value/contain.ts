6eddbe8c3c0d41bafb779ab2385637ab2b24a3d5
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2024-09-24 15:01:20 +0200

    Add support for CSS property

---
+++ packages/alfa-css/src/value/contain.ts
@@ -0,0 +1,237 @@
+import type { Hash } from "@siteimprove/alfa-hash";
+import { Err, Result } from "@siteimprove/alfa-result";
+import { Parser } from "@siteimprove/alfa-parser";
+
+import { Keyword } from "./keyword.js";
+import type { Resolvable } from "./resolvable.js";
+import { Value } from "./value.js";
+
+import { Token, type Parser as CSSParser } from "../syntax/index.js";
+import { Slice } from "@siteimprove/alfa-slice";
+
+const { either } = Parser;
+
+/**
+ * {@link https://developer.mozilla.org/en-US/docs/Web/CSS/contain#formal_syntax}
+ *
+ * @public
+ */
+export type Contain =
+  | Keyword<"none">
+  | Keyword<"strict">
+  | Keyword<"content">
+  | ContainFlags;
+
+/**
+ * @public
+ */
+export class ContainFlags
+  extends Value<"contain-flags", false>
+  implements Resolvable<ContainFlags, never>
+{
+  public static of(
+    size: boolean,
+    inlineSize: boolean,
+    layout: boolean,
+    style: boolean,
+    paint: boolean,
+  ) {
+    return new ContainFlags(size, inlineSize, layout, style, paint);
+  }
+
+  private readonly _size: boolean;
+  private readonly _inlineSize: boolean;
+  private readonly _layout: boolean;
+  private readonly _style: boolean;
+  private readonly _paint: boolean;
+
+  private constructor(
+    size: boolean,
+    inlineSize: boolean,
+    layout: boolean,
+    style: boolean,
+    paint: boolean,
+  ) {
+    super("contain-flags", false);
+    this._size = size;
+    this._inlineSize = inlineSize;
+    this._layout = layout;
+    this._style = style;
+    this._paint = paint;
+  }
+
+  public get size(): boolean {
+    return this._size;
+  }
+
+  public get inlineSize(): boolean {
+    return this._inlineSize;
+  }
+
+  public get layout(): boolean {
+    return this._layout;
+  }
+
+  public get style(): boolean {
+    return this._style;
+  }
+
+  public get paint(): boolean {
+    return this._paint;
+  }
+
+  public resolve(): ContainFlags {
+    return this;
+  }
+
+  public equals(value: unknown): value is this {
+    return (
+      value instanceof ContainFlags &&
+      value._size === this._size &&
+      value._inlineSize === this._inlineSize &&
+      value._layout === this._layout &&
+      value._style === this._style &&
+      value._paint === this._paint
+    );
+  }
+
+  public hash(hash: Hash): void {
+    hash
+      .writeBoolean(this._size)
+      .writeBoolean(this._inlineSize)
+      .writeBoolean(this._layout)
+      .writeBoolean(this._style)
+      .writeBoolean(this._paint);
+  }
+
+  public toJSON(): ContainFlags.JSON {
+    return {
+      ...super.toJSON(),
+      size: this._size,
+      inlineSize: this._inlineSize,
+      layout: this._layout,
+      style: this._style,
+      paint: this._paint,
+    };
+  }
+
+  public toString(): string {
+    let keywords: Array<string> = [];
+    if (this._size) {
+      keywords.push("size");
+    }
+    if (this._inlineSize) {
+      keywords.push("inline-size");
+    }
+    if (this._layout) {
+      keywords.push("layout");
+    }
+    if (this._style) {
+      keywords.push("style");
+    }
+    if (this._paint) {
+      keywords.push("paint");
+    }
+
+    return keywords.join(" ");
+  }
+}
+
+/**
+ * @public
+ */
+export namespace ContainFlags {
+  export interface JSON extends Value.JSON<"contain-flags"> {
+    size: boolean;
+    inlineSize: boolean;
+    layout: boolean;
+    style: boolean;
+    paint: boolean;
+  }
+
+  export function isContainFlags(value: unknown): value is ContainFlags {
+    return value instanceof ContainFlags;
+  }
+
+  export const parse: CSSParser<ContainFlags> = (input) => {
+    let size: Keyword<"size"> | Keyword<"inline-size"> | undefined;
+    let layout: Keyword<"layout"> | undefined;
+    let style: Keyword<"style"> | undefined;
+    let paint: Keyword<"paint"> | undefined;
+
+    while (true) {
+      for (const [remainder] of Token.parseWhitespace(input)) {
+        input = remainder;
+      }
+
+      if (size === undefined) {
+        const result = Keyword.parse("size", "inline-size")(input);
+
+        if (result.isOk()) {
+          [input, size] = result.get();
+          continue;
+        }
+      }
+
+      if (layout === undefined) {
+        const result = Keyword.parse("layout")(input);
+
+        if (result.isOk()) {
+          [input, layout] = result.get();
+          continue;
+        }
+      }
+
+      if (style === undefined) {
+        const result = Keyword.parse("style")(input);
+
+        if (result.isOk()) {
+          [input, style] = result.get();
+          continue;
+        }
+      }
+
+      if (paint === undefined) {
+        const result = Keyword.parse("paint")(input);
+
+        if (result.isOk()) {
+          [input, paint] = result.get();
+          continue;
+        }
+      }
+
+      break;
+    }
+
+    if (
+      size === undefined &&
+      layout === undefined &&
+      style === undefined &&
+      paint === undefined
+    ) {
+      return Err.of("Expected at least one keyword");
+    }
+
+    return Result.of([
+      input,
+      ContainFlags.of(
+        size !== undefined && size.value === "size",
+        size !== undefined && size.value === "inline-size",
+        layout !== undefined,
+        style !== undefined,
+        paint !== undefined,
+      ),
+    ]);
+  };
+}
+
+/**
+ * @public
+ */
+export namespace Contain {
+  export const parse: CSSParser<Contain> = either<
+    Slice<Token>,
+    Contain,
+    string
+  >(Keyword.parse("none", "strict", "content"), ContainFlags.parse);
+}
---
+++ packages/alfa-style/test/property/contain.spec.tsx
@@ -0,0 +1,265 @@
+import { h } from "@siteimprove/alfa-dom";
+import { test } from "@siteimprove/alfa-test";
+
+import { Device } from "@siteimprove/alfa-device";
+
+import { Style } from "../../dist/style.js";
+import type { Keyword } from "@siteimprove/alfa-css";
+import { Array } from "@siteimprove/alfa-array";
+import { Comparison } from "@siteimprove/alfa-comparable";
+
+const device = Device.standard();
+
+function permutations<T>(items: Array<T>, k: number): Array<Array<T>> {
+  if (k === 0) {
+    return [[]];
+  }
+
+  const result = [];
+  for (let i = 0; i < items.length; ++i) {
+    // Recursively get permutations of length k-1 of the collection with the i-th element removed
+    const lists = permutations(
+      items.filter((_, j) => j !== i),
+      k - 1,
+    );
+
+    // Prepend the i-th element to each sub-collection in the result of the recursion and add to result
+    for (let list of lists) {
+      result.push([items[i], ...list]);
+    }
+  }
+
+  return result;
+}
+
+/**
+ * When parsing, the kewords are returned in an arbitrary but deterministic order depending on the implementation.
+ * This function sorts the expected keywords to match the implementation.
+ * If the implementation changes, update this function accordingly to fix broken tests.
+ */
+function sortKeywords(keywords: Array<Keyword.JSON>) {
+  const order = ["size", "inline-size", "layout", "style", "paint"];
+  return Array.sortWith(keywords, (kw1, kw2) => {
+    const a = order.indexOf(kw1.value);
+    const b = order.indexOf(kw2.value);
+    return a < b
+      ? Comparison.Less
+      : a === b
+        ? Comparison.Equal
+        : Comparison.Greater;
+  });
+}
+
+test("initial value is `none`", (t) => {
+  const element = <div>Hello</div>;
+
+  const style = Style.from(element, device);
+
+  t.deepEqual(style.computed("contain").toJSON(), {
+    value: {
+      type: "keyword",
+      value: "none",
+    },
+    source: null,
+  });
+});
+
+test("property is not inherited", (t) => {
+  const element = <div>Hello</div>;
+
+  h.document(
+    [<div class="container">{element}</div>],
+    [
+      h.sheet([
+        h.rule.style(".container", [h.declaration("contain", "strict")]),
+      ]),
+    ],
+  );
+  const style = Style.from(element, device);
+
+  t.deepEqual(style.computed("contain").toJSON(), {
+    value: { type: "keyword", value: "none" },
+    source: null,
+  });
+});
+
+test("#computed() parses `none` and shorthand keywords", (t) => {
+  for (const kw of ["none", "strict", "content"] as const) {
+    const element = <div class="container">Hello</div>;
+    const decl = h.declaration("contain", kw);
+    h.document([element], [h.sheet([h.rule.style(".container", [decl])])]);
+    const style = Style.from(element, device);
+
+    t.deepEqual(style.computed("contain").toJSON(), {
+      value: {
+        type: "keyword",
+        value: kw,
+      },
+      source: decl.toJSON(),
+    });
+  }
+});
+
+test("#computed() parses a single flag", (t) => {
+  for (const flag of [
+    "size",
+    "inline-size",
+    "layout",
+    "style",
+    "paint",
+  ] as const) {
+    const element = <div class="container">Hello</div>;
+    const decl = h.declaration("contain", flag);
+    h.document([element], [h.sheet([h.rule.style(".container", [decl])])]);
+    const style = Style.from(element, device);
+
+    t.deepEqual(style.computed("contain").toJSON(), {
+      value: {
+        type: "contain-flags",
+        size: flag === "size",
+        inlineSize: flag === "inline-size",
+        layout: flag === "layout",
+        style: flag === "style",
+        paint: flag === "paint",
+      },
+      source: decl.toJSON(),
+    });
+  }
+});
+
+test("#computed() parses two flags", (t) => {
+  for (const flags of [
+    ...permutations(["size", "layout", "style", "paint"], 2),
+    ...permutations(["inline-size", "layout", "style", "paint"], 2),
+  ]) {
+    const element = <div class="container">Hello</div>;
+
+    const decl = h.declaration("contain", `${flags[0]} ${flags[1]}`);
+    h.document([element], [h.sheet([h.rule.style(".container", [decl])])]);
+    const style = Style.from(element, device);
+
+    t.deepEqual(style.computed("contain").toJSON(), {
+      value: {
+        type: "contain-flags",
+        size: flags.includes("size"),
+        inlineSize: flags.includes("inline-size"),
+        layout: flags.includes("layout"),
+        style: flags.includes("style"),
+        paint: flags.includes("paint"),
+      },
+      source: decl.toJSON(),
+    });
+  }
+});
+
+test("#computed() parses three keywords", (t) => {
+  for (const flags of [
+    ...permutations(["size", "layout", "style", "paint"], 3),
+    ...permutations(["inline-size", "layout", "style", "paint"], 3),
+  ]) {
+    const element = <div class="container">Hello</div>;
+
+    const decl = h.declaration(
+      "contain",
+      `${flags[0]} ${flags[1]} ${flags[2]}`,
+    );
+    h.document([element], [h.sheet([h.rule.style(".container", [decl])])]);
+    const style = Style.from(element, device);
+
+    t.deepEqual(style.computed("contain").toJSON(), {
+      value: {
+        type: "contain-flags",
+        size: flags.includes("size"),
+        inlineSize: flags.includes("inline-size"),
+        layout: flags.includes("layout"),
+        style: flags.includes("style"),
+        paint: flags.includes("paint"),
+      },
+      source: decl.toJSON(),
+    });
+  }
+});
+
+test("#computed() parses four keywords", (t) => {
+  for (const flags of [
+    ...permutations(["size", "layout", "style", "paint"], 4),
+    ...permutations(["inline-size", "layout", "style", "paint"], 4),
+  ]) {
+    const element = <div class="container">Hello</div>;
+
+    const decl = h.declaration(
+      "contain",
+      `${flags[0]} ${flags[1]} ${flags[2]} ${flags[3]}`,
+    );
+    h.document([element], [h.sheet([h.rule.style(".container", [decl])])]);
+    const style = Style.from(element, device);
+
+    t.deepEqual(style.computed("contain").toJSON(), {
+      value: {
+        type: "contain-flags",
+        size: flags.includes("size"),
+        inlineSize: flags.includes("inline-size"),
+        layout: flags.includes("layout"),
+        style: flags.includes("style"),
+        paint: flags.includes("paint"),
+      },
+      source: decl.toJSON(),
+    });
+  }
+});
+
+test("#computed() does not parse invalid keyword combinations", (t) => {
+  const invalids = [
+    ["strict", "content"],
+    ["size", "inline-size"],
+    ["strict", "layout"],
+    ["none", "paint"],
+    ["layout", "layout"],
+  ] as const;
+
+  for (const [fst, snd] of invalids) {
+    const element = <div class="container">Hello</div>;
+
+    h.document(
+      [element],
+      [
+        h.sheet([
+          h.rule.style(".container", [
+            h.declaration("contain", `${fst} ${snd}`),
+          ]),
+        ]),
+      ],
+    );
+    const style = Style.from(element, device);
+
+    t.deepEqual(style.computed("contain").toJSON(), {
+      value: {
+        type: "keyword",
+        value: "none",
+      },
+      source: null,
+    });
+  }
+});
+
+test("#computed() does not parse valid keyword followed by invalid keyword", (t) => {
+  const element = <div class="container">Hello</div>;
+
+  h.document(
+    [element],
+    [
+      h.sheet([
+        h.rule.style(".container", [h.declaration("contain", `layout styel`)]),
+      ]),
+    ],
+  );
+  const style = Style.from(element, device);
+
+  t.deepEqual(style.computed("contain").toJSON(), {
+    value: {
+      type: "keyword",
+      value: "none",
+    },
+    source: null,
+  });
+});
