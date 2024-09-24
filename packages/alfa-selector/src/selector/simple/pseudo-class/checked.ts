11871cadb1d92a9821b0ac788c861a72ccd3efea
Jean-Yves Moyen <jym@siteimprove.com>
2024-09-24 14:14:01 +0200

    Clean up

--- packages/alfa-selector/src/selector/simple/pseudo-class/checked.ts
+++ packages/alfa-selector/src/selector/simple/pseudo-class/checked.ts
@@ -1,10 +1,10 @@
 import { Element } from "@siteimprove/alfa-dom";
-import { Refinement } from "@siteimprove/alfa-refinement";
+import { Predicate } from "@siteimprove/alfa-predicate";
 
 import { PseudoClassSelector } from "./pseudo-class.js";
 
 const { hasAttribute, hasInputType, hasName } = Element;
-const { and, or } = Refinement;
+const { and, or } = Predicate;
 
 /**
  * {@link https://drafts.csswg.org/selectors/#checked}
@@ -22,13 +22,14 @@ export class Checked extends PseudoClassSelector<"checked"> {
     yield this;
   }
 
+  /**
+   * @privateRemarks
+   * Checkedness and selectedness can change during the lifecycle of an element,
+   * but we do not have access to that. We rely on the content attributes being
+   * correctly set in the snapshot wo test.
+   */
   public matches = or(
-    and(
-      hasName("input"),
-      hasInputType("checkbox", "radio"),
-      hasAttribute("checked"),
-    ),
-
+    and(hasInputType("checkbox", "radio"), hasAttribute("checked")),
     and(hasName("option"), hasAttribute("selected")),
   );
 
