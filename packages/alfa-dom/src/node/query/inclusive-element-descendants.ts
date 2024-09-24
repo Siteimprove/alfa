a6a2a08f273a319c7eb6293c56e56db91decb48f
Jean-Yves Moyen <jym@siteimprove.com>
2024-09-24 15:10:54 +0200

    Clean up (#1686)

--- packages/alfa-dom/src/node/query/inclusive-element-descendants.ts
+++ packages/alfa-dom/src/node/query/inclusive-element-descendants.ts
@@ -1,6 +1,6 @@
 import type { Sequence } from "@siteimprove/alfa-sequence";
 import { Node } from "../../node.js";
-import { Element } from "../element.js";
+import type { Element } from "../element.js";
 
 import { getElementDescendants } from "./element-descendants.js";
 
