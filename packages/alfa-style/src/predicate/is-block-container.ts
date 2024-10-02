c686f40b4a8442d2c6bdd480b6f53138914d14b9
Jean-Yves Moyen <jym@siteimprove.com>
2024-10-02 16:04:11 +0200

    Add inline-block as block containers

--- packages/alfa-style/src/predicate/is-block-container.ts
+++ packages/alfa-style/src/predicate/is-block-container.ts
@@ -1,13 +1,27 @@
 import type { Style } from "../style.js";
 
 /**
+ * {@link https://drafts.csswg.org/css-display/#the-display-properties}
  * {@link https://drafts.csswg.org/css2/#block-boxes}
+ *
+ * @remarks
+ * It is not fully clear what "block container" really are and, notably,
+ * whether "block-level" elements should be treated as such. It seems to
+ * work that way…
+ * {@link https://drafts.csswg.org/css-display/#block-level}
+ * {@link https://drafts.csswg.org/css-display/#block-container}
+ * This affects `flex`, `grid`, `block ruby`, `table`, and `list-item` who
+ * have an outer display of `block` but are not listed as "block containers".
  *
  * @internal
  */
 export function isBlockContainer(style: Style): boolean {
-  const [outside] = style.computed("display").value.values;
+  const [outside, inside] = style.computed("display").value.values;
 
-  // `table` and `list-item` both set the outside value as `block`.
-  return outside.value === "block";
+  return (
+    // block
+    outside.value === "block" ||
+    // inline-block
+    (outside.value === "inline" && inside?.value === "flow-root")
+  );
 }
