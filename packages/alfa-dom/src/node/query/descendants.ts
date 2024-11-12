904aa7286570e0bc8748e9e6b039b74a81d28cfc
Jean-Yves Moyen <jym@siteimprove.com>
2024-11-12 14:48:32 +0100

    Add getDescendants query

--- packages/alfa-dom/src/node/query/descendants.ts
+++ packages/alfa-dom/src/node/query/descendants.ts
@@ -1,26 +1,46 @@
 import { Cache } from "@siteimprove/alfa-cache";
+import type { Predicate } from "@siteimprove/alfa-predicate";
 import type { Sequence } from "@siteimprove/alfa-sequence";
+
 import { Node } from "../../node.js";
 import { Element } from "../element.js";
 
-const _elementDescendantsCache = Cache.empty<Node, Array<Sequence<Element>>>();
+const _descendantsCache = Cache.empty<
+  Predicate<Node>,
+  Cache<Node, Array<Sequence<Element>>>
+>();
+
+/**
+ * Get all descendants of a node that satisfy a given predicate.
+ *
+ * @remarks
+ * In order to properly cache results for improved performance, care must be taken
+ * to use the exact same predicate (JS object) and not merely a clone of it.
+ *
+ * @public
+ */
+export function getDescendants(
+  predicate: Predicate<Node>,
+): (node: Node, options?: Node.Traversal) => Sequence<Element> {
+  return (node, options = Node.Traversal.empty) => {
+    const optionsMap = _descendantsCache
+      .get(predicate, Cache.empty)
+      .get(node, () => []);
+
+    if (optionsMap[options.value] === undefined) {
+      optionsMap[options.value] = node
+        .descendants(options)
+        .filter(Element.isElement);
+    }
+
+    return optionsMap[options.value];
+  };
+}
 
 /**
  * @public
  */
-export function getElementDescendants(
-  node: Node,
-  options: Node.Traversal = Node.Traversal.empty,
-): Sequence<Element> {
-  const optionsMap = _elementDescendantsCache.get(node, () => []);
-  if (optionsMap[options.value] === undefined) {
-    optionsMap[options.value] = node
-      .descendants(options)
-      .filter(Element.isElement);
-  }
-
-  return optionsMap[options.value];
-}
+export const getElementDescendants = getDescendants(Element.isElement);
 
 /**
  * @public
--- packages/alfa-dom/src/node/query/index.ts
+++ packages/alfa-dom/src/node/query/index.ts
@@ -5,6 +5,7 @@ import * as elementIdMap from "./element-id-map.js";
  * @public
  */
 export namespace Query {
+  export const getDescendants = descendants.getDescendants;
   export const getElementDescendants = descendants.getElementDescendants;
   export const getElementIdMap = elementIdMap.getElementIdMap;
   export const getInclusiveElementDescendants =
