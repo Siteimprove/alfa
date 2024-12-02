83a4a838c2b5fa1cecc551e279784dd0519fca33
Jean-Yves Moyen <jym@siteimprove.com>
2024-12-02 10:37:31 +0100

    Setup vitest coverage reports

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     include: ["test/**/*.spec.ts?(x)"],
+    coverage: {
+      provider: "v8",
+      reporter: ["json", "html"],
+      exclude: [
+        "config/**",
+        "docs/**",
+        "scratches/**",
+        "scripts/**",
+        "**/dist/**",
+        "**/scripts/**",
+        ...coverageConfigDefaults.exclude,
+      ],
+    },
   },
 });
