bed463a0cb01367898c671181d952ce72620084b
Jean-Yves Moyen <jym@siteimprove.com>
2024-12-18 16:12:00 +0100

    Re-add file

---
+++ packages/alfa-applicative/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * This package provides types for modelling
+ * {@link https://en.wikipedia.org/wiki/Applicative_functor | applicative functors}.
+ *
+ * @packageDocumentation
+ */
+export type * from "./applicative.js";
