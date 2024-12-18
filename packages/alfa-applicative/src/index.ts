91fe85d723d080c215bba54e550bd80c8eba73f9
Jean-Yves Moyen <jym@siteimprove.com>
2024-12-18 15:59:13 +0100

    Make alfa-applicative a type-only package

--- packages/alfa-applicative/src/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * This package provides types for modelling
- * {@link https://en.wikipedia.org/wiki/Applicative_functor | applicative functors}.
- *
- * @packageDocumentation
- */
-export * from "./applicative.js";
