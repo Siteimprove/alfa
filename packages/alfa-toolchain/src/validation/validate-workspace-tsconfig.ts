1450b026a873190881a921a2c0a35f56119d4dbb
Jean-Yves Moyen <jym@siteimprove.com>
2025-01-27 12:44:29 +0100

    Improve reporting of broken internal dependencies

--- packages/alfa-toolchain/src/validation/validate-workspace-tsconfig.ts
+++ packages/alfa-toolchain/src/validation/validate-workspace-tsconfig.ts
@@ -1,4 +1,6 @@
 import type { PackageJSON } from "@changesets/types";
+
+import { Array } from "@siteimprove/alfa-array";
 import * as fs from "node:fs";
 import * as path from "node:path";
 
@@ -13,56 +15,101 @@ export function validateWorkspaceTsconfig({
   packageJson: PackageJSON;
 }): Array<string> {
   const name = packageJson.name;
-  const errors: Array<string> = [];
+  let errors: Array<string> = [];
 
-  const internalDependencies: Array<string> = [];
-  for (const depType of [
-    "dependencies",
-    "devDependencies",
-    "peerDependencies",
-    "optionalDependencies",
-  ] as const) {
+  const internalDependencies = Array.from(
+    getInternalDependencies(
+      packageJson,
+      "dependencies",
+      "peerDependencies",
+      "optionalDependencies",
+    ),
+  );
+  const srcReferences = getTSConfigReferences(dir, "src");
+
+  const internalDevDependencies = Array.from(
+    getInternalDependencies(packageJson, "devDependencies"),
+  );
+  const testReferences = getTSConfigReferences(dir, "test");
+
+  errors = Array.concat(
+    errors,
+    Array.subtract(internalDependencies, srcReferences).map(
+      missingDependency(name, "dependency", "src"),
+    ),
+    Array.subtract(srcReferences, internalDependencies).map(
+      missingReference(name, "dependency", "src"),
+    ),
+    Array.subtract(internalDevDependencies, testReferences).map(
+      missingDependency(name, "devDependency", "test"),
+    ),
+    Array.subtract(testReferences, internalDevDependencies).map(
+      missingReference(name, "devDependency", "test"),
+    ),
+  );
+
+  return errors;
+}
+
+type Kind =
+  | "dependencies"
+  | "devDependencies"
+  | "peerDependencies"
+  | "optionalDependencies";
+
+/**
+ * Get all dependencies to a `workspace` protocol, from the given kinds of
+ * dependencies
+ */
+function* getInternalDependencies(
+  packageJson: PackageJSON,
+  ...kinds: Array<Kind>
+): Iterable<string> {
+  for (const depType of kinds) {
     for (const dependency in packageJson[depType]) {
       if (packageJson[depType]?.[dependency].startsWith("workspace")) {
-        internalDependencies.push(
-          // Keep everything between last / and end of line.
-          dependency.replace(/.*\/(?<name>[a-z\-]*)$/, "$1"),
-        );
+        // Keep everything between last / and end of line.
+        yield dependency.replace(/.*\/(?<name>[a-z\-]*)$/, "$1");
       }
     }
   }
+}
 
-  const tsconfigSrc = JSON.parse(
-    fs.readFileSync(path.join(dir, "src", "tsconfig.json"), "utf-8"),
-  );
-  const tsconfigTest = fs.existsSync(path.join(dir, "test", "tsconfig.json"))
-    ? JSON.parse(
-        fs.readFileSync(path.join(dir, "test", "tsconfig.json"), "utf-8"),
-      )
+function getTSConfigReferences(
+  dir: string,
+  which: "src" | "test",
+): Array<string> {
+  const tsconfigPath = path.join(dir, which, "tsconfig.json");
+  const tsconfigFile = fs.existsSync(tsconfigPath)
+    ? JSON.parse(fs.readFileSync(tsconfigPath, "utf-8"))
     : undefined;
-  const references = (tsconfigSrc?.references ?? [])
-    .concat(tsconfigTest?.references ?? [])
-    // Keep everything between last / and end of line.
-    .map((ref: any) => ref?.path.replace(/.*\/(?<name>[a-z\-]*)$/, "$1") ?? "")
-    // "src" is referenced from the test/tsconfig.json but does not need to be
-    // a listed dependency.
-    .filter((ref: any) => ref !== "" && ref !== "src") as Array<string>;
 
-  for (const dependency of internalDependencies) {
-    if (!references.includes(dependency)) {
-      errors.push(
-        `${name}: dependency ${dependency} is not referenced from tsconfig.json`,
-      );
-    }
-  }
+  return (
+    (tsconfigFile?.references ?? [])
+      // Keep everything between last / and end of line.
+      .map(
+        (ref: any) => ref?.path.replace(/.*\/(?<name>[a-z\-]*)$/, "$1") ?? "",
+      )
+      // "src" is referenced from the test/tsconfig.json but does not need to be
+      // a listed dependency.
+      .filter((ref: any) => ref !== "" && ref !== "src") as Array<string>
+  );
+}
 
-  for (const reference of references) {
-    if (!internalDependencies.includes(reference)) {
-      errors.push(
-        `${name}: reference ${reference} from tsconfig.json is not a dependency.`,
-      );
-    }
-  }
+function missingDependency(
+  pkg: string,
+  kind: string,
+  which: string,
+): (dependency: string) => string {
+  return (dependency) =>
+    `${pkg}: ${kind} ${dependency} is not listed as a reference in ${which} TS config.`;
+}
 
-  return errors;
+function missingReference(
+  pkg: string,
+  kind: string,
+  which: string,
+): (reference: string) => string {
+  return (reference) =>
+    `${pkg}: reference ${reference} from ${which} tsconfig.json is not a ${kind}.`;
 }
