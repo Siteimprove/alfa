c849e3ff0b6e0b6df7ebc053882c3ec0d947c92c
Jean-Yves Moyen <jym@siteimprove.com>
2025-02-24 13:18:34 +0100

    Allow to set timeout for individual tests

--- packages/alfa-test/src/test.ts
+++ packages/alfa-test/src/test.ts
@@ -11,22 +11,21 @@ export async function test<T = number>(
   assertion: (assert: Assertions, rng: RNG<T>) => void | Promise<void>,
   controller?: Partial<Controller<T>>,
 ): Promise<void> {
-  // If the controlled is not overwritten, then T should be number.
+  // If the controlled is not overwritten, then T should be `number`.
   const fullController = {
     ...defaultController(),
     ...controller,
   } as Controller<T>;
 
-  return it(name, async () => {
-    for (let i = 0; i < fullController.iterations; i++) {
-      await assertion(
-        assert,
-        // eta-expansion ensures that the wrapper is evaluated on each call of
-        // the rng, not just once per iteration.
-        fullController.rng,
-      );
-    }
-  });
+  return it(
+    name,
+    async () => {
+      for (let i = 0; i < fullController.iterations; i++) {
+        await assertion(assert, fullController.rng);
+      }
+    },
+    fullController?.timeout,
+  );
 }
 
 /**
@@ -35,6 +34,13 @@ export async function test<T = number>(
 export interface Controller<T = number> {
   rng: RNG<T>;
   iterations: number;
+  // This interface should instead extend Vitest's TestOptions.
+  // However, doing so causes Vitest to be transitively included everywhere,
+  // thus triggering https://github.com/vitejs/vite/issues/9813
+  // Having one vitest.d.ts file in this package is OK, requiring one in every
+  // dependent is not.
+  // So, we just hardcode the property we need, until Vitest bug is fixed.
+  timeout?: number;
 }
 
 function defaultController(): Controller<number> {
