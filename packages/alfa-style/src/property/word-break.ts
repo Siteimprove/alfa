996ffc41bd65a19a11f9b482f30afa8e01f3e1a1
Jean-Yves Moyen <jym@siteimprove.com>
2025-02-24 14:04:25 +0100

    Use level 4 keywords

--- packages/alfa-style/src/property/word-break.ts
+++ packages/alfa-style/src/property/word-break.ts
@@ -2,12 +2,15 @@ import { Longhand } from "../longhand.js";
 
 /**
  * {@link https://developer.mozilla.org/en-US/docs/Web/CSS/word-break}
+ * {@link https://drafts.csswg.org/css-text-4/#word-break-property}
  * @internal
  */
 export default Longhand.fromKeywords(
   { inherits: true },
   "normal",
-  "keep-all",
   "break-all",
+  "keep-all",
+  "manual",
+  "auto-phrase",
   "break-word",
 );
