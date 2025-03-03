6129dd3d3549d8060e191b17ff4dcff36d9edf06
Jean-Yves Moyen <jym@siteimprove.com>
2025-03-03 14:20:06 +0100

    Add support for white-space-trim

---
+++ .changeset/beige-lobsters-exercise.md
@@ -0,0 +1,5 @@
+---
+"@siteimprove/alfa-style": minor
+---
+
+**Added:** The property `white-space-trim` is now supported.
--- packages/alfa-style/src/property/white-space-trim.ts
+++ packages/alfa-style/src/property/white-space-trim.ts
@@ -1,11 +1,37 @@
-import { Longhand } from "../longhand.js";
+import { Keyword, TrimFlags } from "@siteimprove/alfa-css";
+import { Option } from "@siteimprove/alfa-option";
+import { Parser } from "@siteimprove/alfa-parser";
+import { Predicate } from "@siteimprove/alfa-predicate";
 
-export default Longhand.fromKeywords(
-  { inherits: true },
-  "collapse",
-  "discard",
-  "preserve",
-  "preserve-breaks",
-  "preserve-spaces",
-  "break-spaces",
+import { Longhand } from "../longhand.js";
+import { isBlockContainer, isInlineBox } from "../predicate/index.js";
+
+const { either } = Parser;
+const { or } = Predicate;
+
+/**
+ * {@link
+ * https://developer.mozilla.org/en-US/docs/Web/CSS/contain#formal_syntax}
+ *
+ * @public
+ */
+type Specified = Keyword<"none"> | TrimFlags;
+
+type Computed = Specified;
+
+/**
+ * {@link https://developer.mozilla.org/en-US/docs/Web/CSS/contain}
+ * @internal
+ */
+export default Longhand.of<Specified, Computed>(
+  Keyword.of("none"),
+  either(Keyword.parse("none"), TrimFlags.parse),
+  (value) => value,
+  {
+    inherits: false,
+    use: (value, style) =>
+      Option.conditional(value, (_) =>
+        or(isBlockContainer, isInlineBox)(style),
+      ),
+  },
 );
