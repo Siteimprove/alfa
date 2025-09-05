240487af9ba4c162aa8ca6e594c53e1f4ade69df
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2025-09-05 13:43:25 +0200

    Add unit tests for alfa-mapper

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -4,5 +4,10 @@ export default defineConfig({
   test: {
     include: ["packages/alfa-*/test/**/*.spec.ts?(x)"],
     exclude: ["packages/alfa-test-deprecated"],
+    typecheck: {
+      enabled: true,
+      checker: "tsc",
+      include: ["packages/alfa-*/test/**/*.spec-d.ts?(x)"],
+    },
   },
 });
