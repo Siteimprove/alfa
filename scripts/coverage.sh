9c30b2d40b90b9b404f1dab8ec25dedc9a5ae23c
Jean-Yves Moyen <jym@siteimprove.com>
2025-10-06 16:37:12 +0200

    Disable fail on error for individual reports

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,6 +6,13 @@ set -euxo pipefail
 yarn coverage
 
 # Generate package-specific coverage reports
+
+# Since the command fails on packages that don't even have a `test` folder,
+# we disable failing on error temporarily.
+set +e
+
 for package in packages/alfa-*; do
   yarn coverage:package ${package}
 done
+
+set -e
