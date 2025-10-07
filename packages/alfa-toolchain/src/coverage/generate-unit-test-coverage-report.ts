c0f0366fb1a994367d1803002ee86e616588a945
Jean-Yves Moyen <jym@siteimprove.com>
2025-10-07 14:05:03 +0200

    Reorder files

---
+++ packages/alfa-toolchain/src/coverage/generate-unit-test-coverage-report.ts
@@ -0,0 +1,64 @@
+import { Array } from "@siteimprove/alfa-array";
+
+import { getPackages, type Package } from "@manypkg/get-packages";
+import * as fs from "fs";
+
+import {
+  coveragePath,
+  type CoverageSummary,
+  destinationPath,
+  toCoverageData,
+  toHtml,
+} from "./transform-unit-test-coverage-data.js";
+
+const targetPath = process.argv[2] ?? ".";
+
+await generateUnitTestCoverageReport(targetPath);
+
+/**
+ * Find the coverage reports of all packages under `cwd` and generate an
+ * HTML summary of line coverage by unit tests in each package.
+ *
+ * @public
+ */
+export async function generateUnitTestCoverageReport(cwd: string) {
+  const packages = (await getPackages(cwd)).packages;
+
+  const coverages = Array.collect(
+    await Promise.all(packages.map(loadPackageCoverage)),
+    toCoverageData,
+  );
+
+  const coverageReport = toHtml(coverages);
+
+  if (!fs.existsSync(destinationPath)) {
+    fs.mkdirSync(destinationPath, { recursive: true });
+  }
+
+  fs.writeFileSync(
+    `${destinationPath}/unit-test-coverage.html`,
+    coverageReport,
+  );
+
+  console.log(
+    `Wrote unit test coverage report for ${coverages.length} packages to ${destinationPath}/unit-test-coverage.html`,
+  );
+}
+
+/**
+ * Read the coverage summary of an individual package.
+ *
+ * @private
+ */
+export async function loadPackageCoverage(
+  pkg: Package,
+): Promise<[Package, CoverageSummary]> {
+  const summary = (await import(
+    `${pkg.dir}/${coveragePath}/coverage-summary.json`,
+    {
+      with: { type: "json" },
+    }
+  )) as { default: CoverageSummary };
+
+  return [pkg, summary.default] as const;
+}
