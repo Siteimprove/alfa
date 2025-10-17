ff281cd020ab0be0d1e707133bcceed53a00dcbf
Jean-Yves Moyen <jym@siteimprove.com>
2025-10-17 10:13:58 +0200

    Clean up

--- packages/alfa-toolchain/src/coverage/upload-coverage-report.ts
+++ packages/alfa-toolchain/src/coverage/upload-coverage-report.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { simpleGit } from "simple-git";
 
-const url = process.env.API_URL;
-const apiKey = process.env.API_KEY;
+const url = process.env.COVERAGE_API_URL;
+const apiKey = process.env.COVERAGE_API_KEY;
 
 const configFile = "config/upload-coverage-report.json";
 const summaryFile = "docs/coverage/coverage-summary.json";
@@ -13,21 +13,28 @@ const git = simpleGit(targetPath);
 
 await uploadCoverageReport(targetPath);
 
-export async function uploadCoverageReport(cwd: string) {
-  const config = await import(`${cwd}/${configFile}`, {
-    with: { type: "json" },
-  });
+export async function uploadCoverageReport(rootDir: string) {
+  const config = (
+    await import(`${rootDir}/${configFile}`, {
+      with: { type: "json" },
+    })
+  ).default;
 
-  const summary = await import(`${cwd}/${summaryFile}`, {
-    with: { type: "json" },
-  });
+  const summary = (
+    await import(`${rootDir}/${summaryFile}`, {
+      with: { type: "json" },
+    })
+  ).default;
+
+  // console.dir(config);
+  // console.dir(summary);
 
   const git_sha = (await git.revparse(["HEAD"])).trim();
 
   // Add lines_rate, lines_covered, lines_valid, timestamp (in seconds), and git_sha to payload
   const payload = {
     ...config,
-    lines_rate: summary.total.lines.pct / 100,
+    line_rate: summary.total.lines.pct / 100,
     lines_covered: summary.total.lines.covered,
     lines_valid: summary.total.lines.total,
     timestamp_s: Math.floor(Date.now() / 1000),
@@ -41,7 +48,7 @@ export async function uploadCoverageReport(cwd: string) {
   const response = await axios.post(url, payload, {
     headers: {
       "Content-Type": "application/json",
-      "X-API-key": apiKey,
+      Authorization: `Bearer ${apiKey}`,
     },
   });
 
