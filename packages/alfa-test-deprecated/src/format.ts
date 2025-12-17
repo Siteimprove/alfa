e856392059c9c1b249e0f54125c5d301f742fd6e
Jean-Yves Moyen <jym@siteimprove.com>
2025-12-17 11:36:42 +0100

    Revert "Delete alfa-test-deprecated"
    This reverts commit 70e0441152af0279e326ab4b447b08e01a00b2a3.

---
+++ packages/alfa-test-deprecated/src/format.ts
@@ -0,0 +1,56 @@
+/// <reference types="node" />
+
+import * as assert from "assert";
+import chalk from "chalk";
+import * as path from "path";
+
+import { stack } from "./stack.js";
+
+/**
+ * @internal
+ */
+export function format(name: string, error: Error): string {
+  const [callsite] = stack(error);
+
+  const [file, line, column] =
+    callsite.type === "native"
+      ? ["native", -1, -1]
+      : [
+          path.relative(process.cwd(), callsite.file),
+          callsite.line,
+          callsite.column,
+        ];
+
+  let message: string;
+
+  if (error instanceof assert.AssertionError) {
+    message = error.generatedMessage ? "" : `\n${error.message}\n`;
+
+    error = new assert.AssertionError({
+      actual: error.actual,
+      expected: error.expected,
+      operator: error.operator,
+    });
+
+    error.message =
+      message +
+      "\n" +
+      error.message.replace(
+        "Input A expected to strictly deep-equal input B:\n",
+        "",
+      );
+
+    message += error.message;
+  } else {
+    message = error.stack ?? error.message;
+  }
+
+  const output = `
+${chalk.underline(`${file}(${line},${column}):`)} ${chalk.bold(
+    name.trim().replace(/\s+/g, " "),
+  )}
+${message}
+  `;
+
+  return `${output.trim()}\n`;
+}
