a25ab5ed6ca552704f3da70276c438147a78c5ef
Jean-Yves Moyen <jym@siteimprove.com>
2026-02-12 13:01:41 +0100

    Add support for min()

--- packages/alfa-css/src/calculation/math-expression/function/index.ts
+++ packages/alfa-css/src/calculation/math-expression/function/index.ts
@@ -1,16 +1,17 @@
 import { Parser } from "@siteimprove/alfa-parser";
 
-import { type Parser as CSSParser } from "../../../syntax/index.js";
+import { type Parser as CSSParser, Token } from "../../../syntax/index.js";
 
 import { Expression } from "../expression.js";
 
 import { Calculation } from "./calc.js";
 import { Max } from "./max.js";
+import { Min } from "./min.js";
 
-const { either } = Parser;
+const { exclusive } = Parser;
 
 /** @public */
-export type Function = Calculation | Max;
+export type Function = Calculation | Max | Min;
 
 /** @public */
 export namespace Function {
@@ -18,7 +19,35 @@ export namespace Function {
 
   export const { isMax } = Max;
 
+  export const { isMin } = Min;
+
+  const names = ["calc", "max", "min"] as const;
+  type Name = (typeof names)[number];
+
+  const parsers: Record<
+    Name,
+    (parseSum: CSSParser<Expression>) => CSSParser<Function>
+  > = {
+    calc: Calculation.parse,
+    max: Max.parse,
+    min: Min.parse,
+  };
+
+  /**
+   * Parses a math function in a calculation.
+   *
+   * @param parseSum - A parser for the sum production in a calculation.
+   *
+   * @privateRemarks
+   * The `either` will repeatedly try to parse the first token. This could be
+   * optimized a bit by pre-looking at it and selecting the right parser based
+   * on it. This is likely not a hot-path because calculations aren't that
+   * common (especially functions other than `calc`), and there are relatively
+   * few functions now. So keeping it that way for the sake of simplicity for now.
+   */
   export function parse(parseSum: CSSParser<Expression>): CSSParser<Function> {
-    return either(Calculation.parse(parseSum), Max.parse(parseSum));
+    return exclusive(Token.parseFunction(names), (fn) =>
+      parsers[fn.value](parseSum),
+    );
   }
 }
---
+++ packages/alfa-css/test/calculation/function/min.ts
@@ -0,0 +1,132 @@
+import { test } from "@siteimprove/alfa-test";
+
+import { Math } from "../../../dist/index.js";
+import { parser, parserUnsafe, serializer } from "../../common/parse.js";
+
+import { resolver } from "../common.js";
+
+const parseErr = parser(Math.parse);
+const parse = parserUnsafe(Math.parse);
+const serialize = serializer(Math.parse);
+
+test(".parse() parses a min of one or more numbers or calculation", (t) => {
+  for (const [list, value] of [
+    ["1", 1],
+    ["1,2", 1],
+    ["3, 7,8  , 10  ,5, 1", 1],
+    ["1 + 2, 5, 2 * 3", 3],
+    ["2, min(1, 4)", 1],
+  ]) {
+    const calculation = parse(`min(${list})`);
+
+    t(calculation.isNumber());
+
+    t.deepEqual(calculation.toJSON(), {
+      type: "math expression",
+      expression: {
+        type: "value",
+        value: { type: "number", value: value },
+      },
+    });
+  }
+
+  const error = parseErr("min()");
+
+  t.deepEqual(error.isErr(), true);
+});
+
+test(".parse() parses a min of absolute dimensions", (t) => {
+  for (const [list, value, type] of [
+    ["1px", 1, "length"],
+    ["2px, 1cm", 2, "length"],
+    ["2rad, 90deg", 90, "angle"],
+  ] as const) {
+    const actual = serialize(`min(${list})`);
+
+    t.deepEqual(actual, {
+      type: "math expression",
+      expression: {
+        type: "value",
+        value: {
+          type: type,
+          value: value,
+          unit: type === "length" ? "px" : "deg",
+        },
+      },
+    });
+  }
+});
+
+test(".parse() does not reduce relative dimensions", (t) => {
+  const calculation = parse("min(1em, 20px, 2*4px, 1vh + 20%)");
+
+  t.deepEqual(calculation.toJSON(), {
+    type: "math expression",
+    expression: {
+      type: "min",
+      arguments: [
+        {
+          type: "value",
+          value: { value: 1, type: "length", unit: "em" },
+        },
+        {
+          type: "value",
+          value: { value: 20, type: "length", unit: "px" },
+        },
+        {
+          type: "value",
+          value: { value: 8, type: "length", unit: "px" },
+        },
+        {
+          type: "sum",
+          operands: [
+            {
+              type: "value",
+              value: { value: 1, type: "length", unit: "vh" },
+            },
+            { type: "value", value: { value: 0.2, type: "percentage" } },
+          ],
+        },
+      ],
+    },
+  });
+
+  const reduced = calculation.reduce(resolver);
+
+  t.deepEqual(reduced.toJSON(), {
+    type: "math expression",
+    expression: {
+      type: "value",
+      value: { value: 8, type: "length", unit: "px" },
+    },
+  });
+});
+
+test(".parse() does not resolve percentages", (t) => {
+  const actual = serialize("min(5%, 10%)");
+
+  t.deepEqual(actual, {
+    type: "math expression",
+    expression: {
+      type: "min",
+      arguments: [
+        { type: "value", value: { value: 0.05, type: "percentage" } },
+        { type: "value", value: { value: 0.1, type: "percentage" } },
+      ],
+    },
+  });
+});
+
+test("parse() accept mixed min if they can combine", (t) => {
+  for (const list of ["2px, 10%", "10%, 2px"]) {
+    const calculation = parse(`min(${list})`).reduce(resolver);
+
+    t.deepEqual(calculation.toJSON(), {
+      type: "math expression",
+      expression: {
+        type: "value",
+        value: { value: 1.6, type: "length", unit: "px" },
+      },
+    });
+  }
+});
