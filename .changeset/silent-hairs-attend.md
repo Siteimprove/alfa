b694d48b52ffd18d71f10e00a5069dd3f65d55cd
Jean-Yves Moyen <jym@siteimprove.com>
2026-02-20 13:49:28 +0100

    Add Thunk.freeze

---
+++ .changeset/silent-hairs-attend.md
@@ -0,0 +1,5 @@
+---
+"@siteimprove/alfa-thunk": minor
+---
+
+**Added:** A `Thunk.freeze` helper is now available to freeze thunk, making it independant of changes in the global state after its first evaluation.
--- packages/alfa-thunk/src/thunk.ts
+++ packages/alfa-thunk/src/thunk.ts
@@ -32,4 +32,34 @@ export namespace Thunk {
   ): U {
     return reducer(accumulator, thunk());
   }
+
+  /**
+   * Freezes a Thunk into one that will always produce the same value,
+   * even if the original one depends on mutable state.
+   *
+   * @remarks
+   * While this may be desirable in contexts where immutability is required,
+   * it can also break badly Thunks that rely on side effect to maintain an
+   * internal state.
+   *
+   * Frozen thanks store their result internally, thus consuming memory. At the
+   * same time, they ensure that repeated calls are fast. Because the result are
+   * stored internally, they will be destroyed at the same time the thunk is.
+   */
+  export function freeze<T>(thunk: Thunk<T>): Thunk<T> {
+    let state:
+      | { value: undefined; frozen: false }
+      | { value: T; frozen: true } = {
+      value: undefined,
+      frozen: false,
+    };
+
+    return () => {
+      if (!state.frozen) {
+        state = { value: thunk(), frozen: true };
+      }
+
+      return state.value;
+    };
+  }
 }
