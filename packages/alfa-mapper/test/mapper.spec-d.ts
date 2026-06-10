ffbb690a54d7072b4e2a625930f4aaa6989bf2a0
Jean-Yves Moyen <jym@siteimprove.com>
2026-06-10 15:20:42 +0200

    Simplify type tests (#2106)

--- packages/alfa-mapper/test/mapper.spec-d.ts
+++ packages/alfa-mapper/test/mapper.spec-d.ts
@@ -4,22 +4,17 @@ import type { Mapper } from "../src/mapper.ts";
 
 describe("Mapper", () => {
   it("defaults to identity function with no extra args", () => {
-    type MyMapper = Mapper<number>;
-    const fn: MyMapper = (x) => x;
-    expectTypeOf(fn).toEqualTypeOf<(value: number) => number>();
+    expectTypeOf<Mapper<number>>().toEqualTypeOf<(value: number) => number>();
   });
 
   it("can change return type with second generic", () => {
-    type MyMapper = Mapper<number, string>;
-    const fn: MyMapper = (x) => x.toString();
-    expectTypeOf(fn).toEqualTypeOf<(value: number) => string>();
+    expectTypeOf<Mapper<number, string>>().toEqualTypeOf<
+      (value: number) => string
+    >();
   });
 
   it("can accept extra args with third generic", () => {
-    type MyMapper = Mapper<number, string, [boolean, Date]>;
-    const fn: MyMapper = (x, flag, date) =>
-      flag ? date.toISOString() : x.toString();
-    expectTypeOf(fn).toEqualTypeOf<
+    expectTypeOf<Mapper<number, string, [boolean, Date]>>().toEqualTypeOf<
       (value: number, flag: boolean, date: Date) => string
     >();
   });
