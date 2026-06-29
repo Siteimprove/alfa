f0829b2679700d23cf1be30625eea35a7a06ad35
Jean-Yves Moyen <jym@siteimprove.com>
2026-06-29 11:22:25 +0200

    Convert configs to MJS

---
+++ config/typedoc.mjs
@@ -0,0 +1,24 @@
+/** @type {import('typedoc').TypeDocOptions} */
+export default {
+  name: "Alfa API documentation",
+  entryPoints: ["../packages/alfa-act", "../packages/alfa-dom"],
+  entryPointStrategy: "packages",
+  includeVersion: false,
+  excludeExternals: true,
+  plugin: ["typedoc-plugin-markdown"],
+  outputs: [
+    { name: "html", path: "../docs/typedoc/html" },
+    { name: "markdown", path: "../docs/typedoc/markdown" },
+  ],
+  router: "structure",
+  favicon: "../media/icon.svg",
+  sourceLinkExternal: true,
+  navigationLinks: {
+    "Alfa technical documentation": "https://alfa.siteimprove.com",
+    Siteimprove: "https://siteimprove.com",
+  },
+  useFirstParagraphOfCommentAsSummary: true,
+  navigation: {
+    includeCategories: true,
+  },
+};
