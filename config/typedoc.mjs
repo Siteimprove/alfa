e4816f6932ebb541f17ac4568c0cfa800f0c1ef7
Jean-Yves Moyen <jym@siteimprove.com>
2026-07-03 11:11:06 +0200

    Add review and documetation modes

--- config/typedoc.mjs
+++ config/typedoc.mjs
@@ -1,3 +1,45 @@
+let gitRevision;
+let sourceLinkTemplate;
+let markdown = false;
+let html = false;
+let json = false;
+
+const target = process.env.ALFA_DOC_TARGET;
+
+if (target !== "review" && target !== "documentation") {
+  console.error(
+    `Need one ALFA_DOC_TARGET of either "review" or "documentation"`,
+  );
+  process.exit(1);
+}
+
+if (target === "review") {
+  markdown = true;
+  gitRevision = "main";
+  sourceLinkTemplate =
+    "https://github.com/Siteimprove/alfa/blob/{gitRevision}/{path}";
+}
+
+if (target === "documentation") {
+  // Adapted from https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+  // by adding the starting "v" and ending the line after the 3 numbers.
+  // This matches what we use for tags when making a release.
+  const semVerRegex =
+    // v
+    // Three identical capture groups, separated by dots.
+    // Each group is either 0 or any number of digits, starting with a non-0.
+    /^v(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)$/;
+  if ((process.env.ALFA_DOC_VERSION ?? "").match(semVerRegex) === null) {
+    console.error(
+      `An ALFA_DOC_TARGET of "documentation" requires an ALFA_DOC_VERSION of "vx.y.z" with three numbers x, y, and z`,
+    );
+    process.exit(2);
+  }
+  html = true;
+  json = true;
+  gitRevision = process.env.ALFA_DOC_VERSION;
+}
+
 /** @type {import('typedoc').TypeDocOptions & import('typedoc-plugin-markdown').PluginOptions} */
 export default {
   name: "Alfa API documentation",
@@ -8,9 +50,8 @@ export default {
   excludeExternals: true,
   packageOptions: {
     entryPoints: ["src/index.ts"],
-    gitRevision: "main",
-    sourceLinkTemplate:
-      "https://github.com/Siteimprove/alfa/blob/{gitRevision}/{path}",
+    gitRevision,
+    sourceLinkTemplate,
     readme: "none",
     includeVersion: true,
     excludeExternals: true,
