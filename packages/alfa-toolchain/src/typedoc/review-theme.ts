e4e5deea91fe2b03fb9b4038f5b7ea607954cf0f
Jean-Yves Moyen <jym@siteimprove.com>
2026-07-15 11:02:20 +0200

    Improve typedoc markdown review theme

--- packages/alfa-toolchain/src/typedoc/review-theme.ts
+++ packages/alfa-toolchain/src/typedoc/review-theme.ts
@@ -25,54 +25,136 @@ class ReviewContext extends CategorizeMarkdownThemeContext {
   ) {
     super(theme, page, themeOptions);
 
-    const { options, partials } = this;
+    const { helpers, options, partials } = this;
 
-    // This is mostly copied from the default theme:
-    // https://github.com/typedoc2md/typedoc-plugin-markdown/blob/main/packages/typedoc-plugin-markdown/src/theme/context/partials/member.sources.ts
-    // With a change to the link name (first parameter of the `link` call).
-    this.partials.sources = function (model, options) {
-      const md = [];
-      if (!options?.hideLabel) {
-        md.push(`${i18n.theme_defined_in()}:`);
-      }
-      model.sources?.forEach((source, index) => {
-        if (index === 0) {
-          if (source.url) {
-            md.push(link(`${escapeChars(source.fileName)}`, source.url));
-          } else {
-            md.push(`${escapeChars(source.fileName)}`);
-          }
-        }
-      });
-      return md.join(" ");
-    };
-
+    // Remove comments
     this.partials.comment = () => "";
 
-    // this.partials.signature = function signature(
-    //   model,
-    //   { accessor, hideTitle, nested, multipleSignatures, headingLevel },
-    // ) {
-    //   const md = [];
-    //   if (!hideTitle) {
-    //     md.push(partials.signatureTitle(model, { accessor }));
-    //   }
-    //   if (!nested && model.sources && !options.getValue("disableSources")) {
-    //     md.push(partials.sources(model));
-    //   }
-    //   if (!multipleSignatures && model.parent?.documents) {
-    //     md.push(partials.documents(model?.parent, { headingLevel }));
-    //   }
-    //   md.push(partials.inheritance(model, { headingLevel }));
-    //   return md.join("\n\n");
-    // };
+    // Completely removes parameters (including the heading) and return type
+    // from detailed signatures (keep only the title)
+    // Taken from https://github.com/typedoc2md/typedoc-plugin-markdown/blob/main/packages/typedoc-plugin-markdown/src/theme/context/partials/member.signature.ts
+    // Might be cleaned up if https://github.com/typedoc2md/typedoc-plugin-markdown/issues/876
+    // is implemented.
+    this.partials.signature = function signature(
+      model,
+      { accessor, hideTitle, nested, multipleSignatures, headingLevel },
+    ) {
+      const md = [];
+      if (!hideTitle) {
+        md.push(partials.signatureTitle(model, { accessor }));
+      }
+      if (!nested && model.sources && !options.getValue("disableSources")) {
+        md.push(partials.sources(model));
+      }
+      if (!multipleSignatures && model.parent?.documents) {
+        md.push(partials.documents(model?.parent, { headingLevel }));
+      }
+      md.push(partials.inheritance(model, { headingLevel }));
+      return md.join("\n\n");
+    };
+
+    // Expand the types parameters in signatures, adding extends and default.
+    // Taken from https://github.com/typedoc2md/typedoc-plugin-markdown/blob/main/packages/typedoc-plugin-markdown/src/theme/context/partials/member.typeParametersList.ts
+    // injected in https://github.com/typedoc2md/typedoc-plugin-markdown/blob/main/packages/typedoc-plugin-markdown/src/theme/context/partials/member.signatureTitle.ts
+    // Might be cleaned up if https://github.com/typedoc2md/typedoc-plugin-markdown/issues/875
+    // is implemented.
+    this.partials.signatureTitle = function signatureTitle(model, opts) {
+      const md = [];
+      const useCodeBlocks = options.getValue("useCodeBlocks");
+      const keyword = helpers.getKeyword(model.parent.kind);
+      if (useCodeBlocks && helpers.isGroupKind(model.parent) && keyword) {
+        md.push(keyword + " ");
+      }
+      if (opts?.accessor) {
+        md.push(bold(opts?.accessor) + " ");
+      }
+      if (model.parent) {
+        const flagsString = helpers.getReflectionFlags(model.parent?.flags);
+        if (flagsString.length) {
+          md.push(helpers.getReflectionFlags(model.parent.flags) + " ");
+        }
+      }
+      if (!["__call", "__type"].includes(model.name)) {
+        const name = [];
+        if (model.kind === ReflectionKind.ConstructorSignature) {
+          name.push("new");
+        }
+        name.push(escapeChars(model.name));
+        md.push(bold(name.join(" ")));
+      }
+      if (model.typeParameters) {
+        const typesMD: string[] = [];
+        model.typeParameters?.forEach((typeParameter) => {
+          const nameDescription: string[] = [backTicks(typeParameter.name)];
+
+          if (typeParameter.type) {
+            nameDescription.push(
+              `${italic('extends')} ${partials.someType(typeParameter.type)}`,
+            );
+          }
+
+          if (typeParameter.default) {
+            nameDescription.push(
+              `= ${partials.someType(typeParameter.default, { forceCollapse: true })}`,
+            );
+          }
+
+          typesMD.push(nameDescription.join(' '));
+        });
+
+        md.push("<" + typesMD.join(", ") + ">");
+      }
+      md.push(partials.signatureParameters(model.parameters || []));
+      if (model.type) {
+        md.push(`: ${partials.someType(model.type)}`);
+      }
+      if (useCodeBlocks) {
+        md.push(";");
+      }
+      const result = md.join("");
+      return useCodeBlocks ? codeBlock(result) : `> ${result}`;
+    };
   }
 }
 
 // We also need to copy the utilities since they are not exposed by
 // typedoc-plugin-markdown.
 // See https://github.com/typedoc2md/typedoc-plugin-markdown/issues/873
-// and upadte whin/if we can hook on the ones from the plugin.
+// and upadte when/if we can hook on the ones from the plugin.
+function backTicks(text: string) {
+  // If the input string itself contains a pipe, or backslash (which can result in unwanted side effects) the string is escaped instead.
+  if (/(\||\\)/g.test(text)) {
+    return escapeChars(text);
+  }
+  // If the input string itself contains a backtick, the string is wrapped in double backticks.
+  if (/`/g.test(text)) {
+    return `\`\` ${text} \`\``;
+  }
+  // Otherwise, the string is wrapped in single backticks.
+  return `\`${text}\``;
+}
+function bold(text: string) {
+  return `**${text}**`;
+}
+function italic(text: string) {
+  return `*${text}*`;
+}
+function codeBlock(content: string) {
+  const trimLastLine = (content: string) => {
+    const lines = content.split("\n");
+    return lines
+      .map((line, index) => (index === lines.length - 1 ? line.trim() : line))
+      .join("\n");
+  };
+  const trimmedContent =
+    content.endsWith("}") ||
+    content.endsWith("};") ||
+    content.endsWith(">") ||
+    content.endsWith(">;")
+      ? trimLastLine(content)
+      : content;
+  return "```typescript\n" + unEscapeChars(trimmedContent) + "\n```";
+}
 function escapeChars(str: string): string {
   return str
     .replace(/>/g, "\\>")
@@ -90,4 +172,31 @@ function link(label: string, url: string): string {
   const parsedUrl = url?.trim() || "";
   const safeLabel = label.trim();
   return parsedUrl.length ? `[${safeLabel}](${parsedUrl})` : safeLabel;
+}
+function unEscapeChars(str: string) {
+  return str
+    .replace(
+      /(`[^`]*?)\\*([^`]*?`)/g,
+      (match, p1, p2) => `${p1}${p2.replace(/\*/g, "\\*")}`,
+    )
+    .replace(/\\\\/g, "\\")
+    .replace(/(?<!\\)\*/g, "")
+    .replace(/\\</g, "<")
+    .replace(/\\>/g, ">")
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/\\_/g, "_")
+    .replace(/\\{/g, "{")
+    .replace(/\\}/g, "}")
+    .replace(/``.*?``|(?<!\\)`/g, (match) =>
+      match.startsWith("``") ? match : "",
+    )
+    .replace(/`` /g, "")
+    .replace(/ ``/g, "")
+    .replace(/\\`/g, "`")
+    .replace(/\\\*/g, "*")
+    .replace(/\\\|/g, "|")
+    .replace(/\\\]/g, "]")
+    .replace(/\\\[/g, "[")
+    .replace(/\[([^[\]]*)\]\((.*?)\)/gm, "$1");
 }
