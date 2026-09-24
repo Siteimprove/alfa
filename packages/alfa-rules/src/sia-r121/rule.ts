d7be32a97b130a4a119a44408c8037d8c644d487
pooya-si <pahm@siteimprove.com>
2026-09-24 17:12:48 -0400

    [11Y-12317] [11Y-12318] update md file.

--- packages/alfa-rules/src/sia-r121/rule.ts
+++ packages/alfa-rules/src/sia-r121/rule.ts
@@ -47,7 +47,12 @@ const { getElementDescendants } = Query;
  */
 export default Rule.Atomic.of<Page, Element>({
   uri: "https://alfa.siteimprove.com/rules/sia-r121",
-  requirements: [Criterion.of("1.3.1"), EAA.of("9.1.3.1"), Technique.of("H40")],
+  requirements: [
+    Criterion.of("1.3.1"),
+    EAA.of("9.1.3.1"),
+    Technique.of("H40"),
+    Technique.of("H48"),
+  ],
   tags: [Scope.Component, Stability.Experimental],
   evaluate({ device, document }) {
     return {
